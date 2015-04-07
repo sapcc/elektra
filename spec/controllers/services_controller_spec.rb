26a33255d245bf9a51e4eb122494265ae046c5fb
Michael Schmidt <michael02.schmidt@sap.com>
2015-04-07 14:22:08 +0200

    fix specs

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe MonsoonServicesController do
+describe ServicesController do
 
   describe "GET 'index'" do
     it "returns http success" do
--- spec/helpers/services_helper_spec.rb
+++ spec/helpers/services_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe ServicesHelper do
+describe MonsoonServicesHelper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
