ef062d5f7ff486eab0ae49ea9693229668af3ce8
Esther Schmitz <esther.schmitz@sap.com>
2015-05-21 16:17:10 +0200

    Fix specs

--- spec/controllers/authenticated_user/users_controller_spec.rb
+++ spec/controllers/authenticated_user/users_controller_spec.rb
@@ -1,5 +1,25 @@
-require 'rails_helper'
+require 'spec_helper'
+# require 'controllers/authenticated_user/shared_examples'
 
-RSpec.describe AuthenticatedUser::UsersController, type: :controller do
+describe AuthenticatedUser::UsersController do
+  include AuthenticationStub
+
+  # commented out for now because the user controller doesn't have an index action and all the shared examples use the index action
+  # it_behaves_like "an authenticated_user controller"
+  default_params = {domain_id: AuthenticationStub.domain_id}
+
+  before(:each) do
+    stub_authentication
+
+    driver = object_spy('driver')
+    allow_any_instance_of(Openstack::IdentityService).to receive(:driver).and_return(driver)
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get :new, default_params
+      expect(response).to be_success
+    end
+  end
 
 end
