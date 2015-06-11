9dc33d1d99af104368aa7ab01546c065385d97c9
Andreas Pfau <andreas.pfau@sap.com>
2015-06-11 10:11:35 +0200

    do not send property "links" to api on create

--- app/models/forms/project.rb
+++ app/models/forms/project.rb
@@ -3,6 +3,6 @@ require 'fog/openstack/models/identity_v3/project'
 class Forms::Project < Forms::Base
   wrapper_for ::Fog::IdentityV3::OpenStack::Project
   
-  ignore_attributes :parent_id
+  ignore_attributes :parent_id, :links
   default_values enabled: true
 end
