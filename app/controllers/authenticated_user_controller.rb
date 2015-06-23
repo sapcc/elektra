618a30e5c8dd8230aa59f87c614dd1ecb54f8c9d
Andreas Pfau <andreas.pfau@sap.com>
2015-06-23 15:36:02 +0200

    make cucumaber tests working locally

--- app/controllers/authenticated_user_controller.rb
+++ app/controllers/authenticated_user_controller.rb
@@ -1,12 +1,17 @@
 class AuthenticatedUserController < ApplicationController
   # load region, domain and project if given
   prepend_before_filter do
-    # redirect to the same url with default domain unless domain_id is given
-    redirect_to url_for(params.merge(domain_id: MonsoonOpenstackAuth.default_domain.id)) unless params[:domain_id]
-    
-    @region     ||= MonsoonOpenstackAuth.configuration.default_region
-    @domain_id  ||= params[:domain_id]
-    @project_id ||= params[:project_id]    
+    if params[:domain_id]
+      # initialize session unless loaded yet
+      session[:init] = true unless session.loaded? 
+      
+      @region     ||= MonsoonOpenstackAuth.configuration.default_region
+      @domain_id  ||= params[:domain_id]
+      @project_id ||= params[:project_id]  
+    else
+      # redirect to the same url with default domain unless domain_id is given
+      redirect_to url_for(params.merge(domain_id: MonsoonOpenstackAuth.default_domain.id)) unless params[:domain_id]
+    end
   end
    
   authentication_required domain: -> c { c.instance_variable_get("@domain_id") }, project: -> c { c.instance_variable_get('@project_id') }
