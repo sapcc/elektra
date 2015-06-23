4f1ccf78ac2cd938b24999490719a58d3b14e62f
Andreas Pfau <andreas.pfau@sap.com>
2015-06-23 16:24:09 +0200

    print host of cucumber test

--- features/step_definitions/integration/authentication_steps.rb
+++ features/step_definitions/integration/authentication_steps.rb
@@ -7,6 +7,8 @@ When(/^I visit "(.*?)"$/) do |path|
 end
 
 Then(/^I am redirected to login page$/) do
+  puts ":::::::::::::::::::::::::::"
+  puts  page.current_url
   puts ">>>>>>>>>>>>>>>>>>>>>>>HTML"
   puts page.html
   expect(current_path).to eq(monsoon_openstack_auth.new_session_path)
