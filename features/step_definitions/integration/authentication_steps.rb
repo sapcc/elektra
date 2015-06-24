02d94fd580076f18ef94bc897f2ef5b5b0f79cd7
Andreas Pfau <andreas.pfau@sap.com>
2015-06-24 10:39:31 +0200

    test is working, remove debug outputs

--- features/step_definitions/integration/authentication_steps.rb
+++ features/step_definitions/integration/authentication_steps.rb
@@ -3,27 +3,14 @@ Given(/I am on the root page$/) do
 end
 
 Given(/^I am not logged in$/) do
-  puts "::::::::::::::::::::::::::: NOT LOGGED IN(BEVOR)"
-  puts "env: #{ENV['CAPYBARA_APP_HOST']}"
-  puts "page.current_url: #{page.current_url}"
   visit monsoon_openstack_auth.logout_path
-  puts "::::::::::::::::::::::::::: NOT LOGGED IN(AFTER)"
-  puts  "page.current_url: #{page.current_url}"
 end
 
 When(/^I visit "(.*?)"$/) do |path|
-  puts "::::::::::::::::::::::::::: VISIT INSTANCES (BEVOR)"
-  puts  page.current_url
   visit path
-  puts "::::::::::::::::::::::::::: VISIT INSTANCES (AFTER)"
-  puts  page.current_url
 end
 
 Then(/^I am redirected to login page$/) do
-  puts "::::::::::::::::::::::::::: SHOULD BE REDIRECTED"
-  puts  page.current_url
-  puts ">>>>>>>>>>>>>>>>>>>>>>>HTML"
-  puts page.html
   expect(current_path).to eq(monsoon_openstack_auth.new_session_path)
 end
 
