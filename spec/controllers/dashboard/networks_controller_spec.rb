51d7fe1301a3fb5712951e4c0080bc2a374a37a3
Andreas Pfau <andreas.pfau@sap.com>
2015-10-09 09:53:05 +0200

    add networks#show method

--- spec/controllers/dashboard/networks_controller_spec.rb
+++ spec/controllers/dashboard/networks_controller_spec.rb
@@ -1,26 +1,37 @@
 require 'spec_helper'
 require 'controllers/dashboard/shared_examples'
 require 'controllers/dashboard/stub_identity_service'
+require 'controllers/dashboard/stub_neutron_service'
 
 describe Dashboard::NetworksController do
   include AuthenticationStub
   include StubIdentityService
+  include StubNeutronService
   
-  # it_behaves_like "an dashboard controller"
-  #
-  # default_params = {domain_id: AuthenticationStub.domain_id, project_id: AuthenticationStub.project_id}
-  #
-  # before(:all) do
-  #   DatabaseCleaner.clean
-  #   @domain = create(:domain, key: default_params[:domain_id])
-  #   @project = create(:project, key: default_params[:project_id], domain: @domain)
-  # end
-  #
-  # describe "GET #index" do
-  #   it "returns http success" do
-  #     get :index, default_params
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  it_behaves_like "an dashboard controller"
+  
+  default_params = {domain_id: AuthenticationStub.domain_id, project_id: AuthenticationStub.project_id}
+
+  before(:all) do
+    DatabaseCleaner.clean
+    @domain = create(:domain, key: default_params[:domain_id])
+    @project = create(:project, key: default_params[:project_id], domain: @domain)
+  end
+  
+  before(:each) do
+    stub_authentication  
+
+    driver = object_spy('driver')
+    allow_any_instance_of(Openstack::AdminIdentityService).to receive(:new_user?).and_return(false)
+    allow_any_instance_of(Openstack::IdentityService).to receive(:driver).and_return(driver)    
+    allow_any_instance_of(Openstack::ComputeService).to receive(:driver).and_return(driver)
+  end
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get :index, default_params
+      expect(response).to be_success
+    end
+  end
 
 end
--- spec/controllers/dashboard/stub_neutron_service.rb
+++ spec/controllers/dashboard/stub_neutron_service.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-module StubVolumeService
+module StubNeutronService
   def self.included(klass)
     klass.before :each do
       allow_any_instance_of(Openstack::NeutronService).to receive(:networks).and_return(double("networks"))
+      allow_any_instance_of(Openstack::NeutronService).to receive(:driver).and_return(double("driver"))
     end
   end
 end
