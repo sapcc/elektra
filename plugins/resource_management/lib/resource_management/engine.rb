8538f59ba06705c6c014ba2458fa63df67316a50
Stefan Majewsky <stefan.majewsky@sap.com>
2015-11-11 11:44:39 +0100

    add db/migrate for resource_management plugin to Rails config

--- plugins/resource_management/lib/resource_management/engine.rb
+++ plugins/resource_management/lib/resource_management/engine.rb
@@ -1,5 +1,15 @@
 module ResourceManagement
   class Engine < ::Rails::Engine
     isolate_namespace ResourceManagement
+
+    # to run migration on rake db:migrate
+    initializer 'resource_management.append_migrations' do |app|
+      unless app.root.to_s == root.to_s
+        config.paths["db/migrate"].expanded.each do |path|
+          app.config.paths["db/migrate"].push(path)
+        end
+      end
+    end
+
   end
 end
