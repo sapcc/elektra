557b9dd2389bba84a7254a97d042ef84ef14464f
Stefan Majewsky <stefan.majewsky@sap.com>
2015-11-20 11:25:12 +0100

    add KNOWN_RESOURCES list to Resource model

--- plugins/resource_management/app/models/resource_management/resource.rb
+++ plugins/resource_management/app/models/resource_management/resource.rb
@@ -1,7 +1,13 @@
 module ResourceManagement
   class Resource < ActiveRecord::Base
     #validates [:service, :name], presence: true
-    class << self
-    end
+
+    KNOWN_RESOURCES = [
+      # TODO: incomplete
+      { service: :block_storage,  name: :capacity  },
+      { service: :block_storage,  name: :snapshots },
+      { service: :block_storage,  name: :volumes   },
+      { service: :object_storage, name: :capacity  },
+    ]
   end
 end
