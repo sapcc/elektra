48b0496600a7c3eacc2a12bb0ac4da9c1ac8128d
Stefan Majewsky <stefan.majewsky@sap.com>
2015-11-20 13:43:48 +0100

    sort resource list and add "display_unit" field for capacity in GiB
    The "display_unit" info needs to be respected by the view.

--- plugins/resource_management/app/models/resource_management/resource.rb
+++ plugins/resource_management/app/models/resource_management/resource.rb
@@ -3,19 +3,19 @@ module ResourceManagement
     #validates [:service, :name], presence: true
 
     KNOWN_RESOURCES = [
-      { service: :block_storage,  name: :capacity  },
-      { service: :block_storage,  name: :snapshots },
-      { service: :block_storage,  name: :volumes   },
-      { service: :object_storage, name: :capacity  },
-      { service: :compute, name: :cores  },
-      { service: :compute, name: :instances  },
-      { service: :compute, name: :ram  },
-      { service: :network, name: :networks  },
-      { service: :network, name: :routers  },
-      { service: :network, name: :ports  },
-      { service: :network, name: :floating_ips  },
-      { service: :network, name: :security_groups  },
-      { service: :network, name: :subnets  },
+      { service: :compute,        name: :cores           },
+      { service: :compute,        name: :instances       },
+      { service: :compute,        name: :ram             },
+      { service: :network,        name: :floating_ips    },
+      { service: :network,        name: :networks        },
+      { service: :network,        name: :ports           },
+      { service: :network,        name: :routers         },
+      { service: :network,        name: :security_groups },
+      { service: :network,        name: :subnets         },
+      { service: :block_storage,  name: :capacity,       display_unit: 1 << 30 }, # stored in bytes, displayed in GiB
+      { service: :block_storage,  name: :snapshots       },
+      { service: :block_storage,  name: :volumes         },
+      { service: :object_storage, name: :capacity,       display_unit: 1 << 30 }, # stored in bytes, displayed in GiB
     ]
   end
 end
