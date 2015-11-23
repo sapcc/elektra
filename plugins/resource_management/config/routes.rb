fee1ae08cc2228973e059d1629f5c3948f8ea9d9
Stefan Majewsky <stefan.majewsky@sap.com>
2015-11-23 13:51:42 +0100

    cleanup routes.rb

--- plugins/resource_management/config/routes.rb
+++ plugins/resource_management/config/routes.rb
@@ -1,22 +1,25 @@
 ResourceManagement::Engine.routes.draw do
-  get '/' => 'application#index'
+  scope '/', as: 'resources' do
+    get '/' => 'application#index'
+    get ':area',  to: 'application#show_area', constraints: { area: /(?:compute|network|storage)/ }, as: 'area'
+    get 'details'  => 'application#details'
+    get 'request'  => 'application#resource_request'
+  end
 
-  # maybe we can remove resources here?
-  get 'resources'          => 'application#index'
-  get 'resources/:area',  to: 'application#show_area', constraints: { area: /(?:compute|network|storage)/ }, as: 'resources_area'
-  get 'resources/details'  => 'application#details'
-  get 'resources/request'  => 'application#resource_request'
-
-  get 'admin'          => 'domain_admin#index'
-  get 'admin/:area',  to: 'domain_admin#show_area', constraints: { area: /(?:compute|network|storage)/ }, as: 'admin_area'
-  get 'admin/details'  => 'domain_admin#details'
-  get 'admin/request'  => 'domain_admin#resource_request'
+  scope 'admin', as: 'admin' do
+    get '/'        => 'domain_admin#index'
+    get ':area',  to: 'domain_admin#show_area', constraints: { area: /(?:compute|network|storage)/ }, as: 'area'
+    get 'details'  => 'domain_admin#details'
+    get 'request'  => 'domain_admin#resource_request'
+  end
 
   # this is only for demo, I have no idea where I can put the cloudadmin views
-  get 'cloud_admin'          => 'cloud_admin#index'
-  get 'cloud_admin/:area',  to: 'cloud_admin#show_area', constraints: { area: /(?:compute|network|storage)/ }, as: 'cloud_admin_area'
-  get 'cloud_admin/details'  => 'cloud_admin#details'
-  get 'cloud_admin/request'  => 'cloud_admin#resource_request'
+  scope 'cloud_admin', as: 'cloud_admin' do
+    get '/'        => 'cloud_admin#index'
+    get ':area',  to: 'cloud_admin#show_area', constraints: { area: /(?:compute|network|storage)/ }, as: 'cloud_admin_area'
+    get 'details'  => 'cloud_admin#details'
+    get 'request'  => 'cloud_admin#resource_request'
+  end
  
   get 'manual_sync' => 'application#manual_sync'
 
