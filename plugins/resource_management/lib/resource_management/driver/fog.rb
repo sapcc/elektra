99e72ab98703ded104e3db6404ecf32f9073bb70
Stefan Majewsky <stefan.majewsky@sap.com>
2015-11-25 18:08:09 +0100

    sync Swift data with minimal role assignments
    Swift allows the reseller admin to operate on all projects while the
    reseller admin role is assigned to him only on one project. So now we
    assign the reseller admin role only on the service user project, and use
    the connection to that project to retrieve quota/usage data for all
    projects.
    
    Closes #89.

--- plugins/resource_management/lib/resource_management/driver/fog.rb
+++ plugins/resource_management/lib/resource_management/driver/fog.rb
@@ -116,10 +116,8 @@ module ResourceManagement
       # The query for quota and usage in Swift use the same request, so this
       # method caches it.
       def get_swift_account_metadata(domain_id, project_id)
-        # the "service" role usually means "readonly access to everything",
-        # but not for Swift; here only the reseller-admin role works
         @swift_account_metadata_cache ||= {}
-        @swift_account_metadata_cache[project_id] ||= with_service_user_connection(::Fog::Storage::OpenStack, domain_id, project_id, role_name: 'ResellerAdmin') do |connection|
+        @swift_account_metadata_cache[project_id] ||= with_service_user_connection_for_swift(domain_id, project_id) do |connection|
           # the head_account request is not yet implemented in Fog (TODO: add it),
           # so let's use request() directly
           connection.request(
@@ -131,7 +129,12 @@ module ResourceManagement
         end
       end
 
-      def with_service_user_connection(fog_class, domain_id, project_id, options={}, &block)
+      # NOTE: Use like this:
+      #
+      # with_service_user_connection(::Fog::Compute::OpenStack, domain_id, project_id) do |connection|
+      #    ...
+      # end
+      def with_service_user_connection(fog_class, domain_id, project_id, &block)
         # establish service user connection to selected domain/project (this is
         # a bit ugly since MonsoonOpenstackAuth does not want to give us the
         # password back, so we have to resort to ENV there)
@@ -149,14 +152,72 @@ module ResourceManagement
       rescue Excon::Errors::Unauthorized, Excon::Errors::Forbidden
         # dashboard user may not have access to this project yet -> grant
         # service user role in this project
-        service_role_name = options[:role_name] || 'service'
-        roles = @srv_conn.list_roles(name: service_role_name).body['roles']
+        roles = @srv_conn.list_roles(name: 'service').body['roles']
         raise "missing role \"service\" in Keystone" if roles.empty?
         @srv_conn.grant_project_user_role(project_id, @srv_conn.current_user_id, roles.first['id'])
 
         # try again after granting the service role
         return yield(fog_class.new(auth_params))
       end
+
+      def with_service_user_connection_for_swift(domain_id, project_id, options={}, &block)
+        # the "service" role usually means "readonly access to everything",
+        # but not for Swift; here only the reseller-admin role works; but stuff
+        # gets easier again since we only need the reseller-admin role on the
+        # service project of the service user
+
+        # establish service user token for service project (same basic
+        # methodology as above, but since it's always the same token scope, we
+        # can store and reuse the connection object)
+        @service_project_id ||= @srv_conn.auth_projects.body['projects'].first['id']
+        @swift_conn         ||= ::Fog::Storage::OpenStack.new(
+          openstack_auth_url:          @auth_url,
+          openstack_region:            @region,
+          openstack_username:          ENV['MONSOON_OPENSTACK_AUTH_API_USERID'],
+          openstack_user_domain:       ENV['MONSOON_OPENSTACK_AUTH_API_DOMAIN'],
+          openstack_api_key:           ENV['MONSOON_OPENSTACK_AUTH_API_PASSWORD'],
+          openstack_project_domain_id: domain_id,
+          openstack_project_id:        @service_project_id,
+        )
+
+        # extract original storage URL from connection object, and store it
+        # since we're going to modify it now
+        @swift_conn_path ||= @swift_conn.instance_variable_get(:@path)
+
+        # adjust the storage URL to point to the desired project (this looks
+        # insane, but the admin tasks in the swiftclient also allow to supply a
+        # user-defined storage URL, see for example
+        # <http://docs.openstack.org/liberty/config-reference/content/object-storage-account-quotas.html>)
+        # (TODO: use that reasoning to get a storage_path reader/writer into
+        # Fog::Storage::OpenStack)
+        storage_path = @swift_conn_path.gsub(@service_project_id, project_id)
+        @swift_conn.instance_variable_set(:@path, storage_path)
+
+        if options[:retrying]
+          yield(@swift_conn)
+        else
+          begin
+            yield(@swift_conn)
+          rescue Excon::Errors::Unauthorized, Excon::Errors::Forbidden
+            # same pattern as in with_service_user_connection(): if service user
+            # does not have the ResellerAdmin role yet, grant it
+            role_name = ENV.fetch['SWIFT_RESELLERADMIN_ROLE', 'ResellerAdmin']
+            roles = @srv_conn.list_roles(name: role_name).body['roles']
+            raise "missing role \"#{role_name}\" in Keystone" if roles.empty?
+            @srv_conn.grant_project_user_role(@service_project_id, @srv_conn.current_user_id, roles.first['id'])
+
+            # clear all relevant instance variables since we need to recreate
+            # the service user connection with the new set of roles
+            @swift_conn = nil
+            @swift_conn_path = nil
+
+            # retry, but set options[:retrying] to make sure that we don't try
+            # to grant the role again
+            with_service_user_connection_for_swift(domain_id, project_id, options.merge(retrying: true), &block)
+          end
+        end
+      end
+
 
     end
   end
