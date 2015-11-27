04c848b42704e15336f51f849f2fc4979b50ea40
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2015-11-27 09:29:15 +0000

    first version overview domain

--- plugins/resource_management/app/controllers/resource_management/domain_admin_controller.rb
+++ plugins/resource_management/app/controllers/resource_management/domain_admin_controller.rb
@@ -5,47 +5,23 @@ module ResourceManagement
     before_filter :set_usage_stage, :only => [:index,:show_area]
 
     def index
-      stats = ResourceManagement::Resource.
-                 where(:domain_id => @scoped_domain_id).
-                 where.not(project_id: nil).
-                 group("service,name").
-                 pluck("service,name,SUM(current_quota),SUM(approved_quota),SUM(usage)")
-      
-      stats.each do |stat|
-        service = stat[0]
-        name = stat[1]
-        projects_current_quota = stat[2]
-        puts service
-        puts name
-        domain_quota = ResourceManagement::Resource.where(:domain_id => @scoped_domain_id, :project_id => nil, :service => service, :name => name)
-        next if domain_quota.blank?
-        puts "#####"
-        puts domain_quota[0].approved_quota * @usage_stage[:danger]
-        puts projects_current_quota
-        if  projects_current_quota > domain_quota[0].approved_quota * @usage_stage[:danger] 
-
-        end
-#        puts stat[1]
-#        domain_current_quota = stat[2]
-#        domain_quota_value = 
-#        domain_usage = 
-#        puts domain_usage
+      @area_services = []
+      ResourceManagement::Resource::KNOWN_SERVICES.each do |service_config|
+         @area_services << service_config[:service]
       end
+      @domain_quotas = ResourceManagement::Resource.where(:domain_id => @scoped_domain_id, :project_id => nil, :service => @area_services)
+      get_resource_status(true)
     end
 
     def show_area
       @area = params.require(:area).to_sym
       # which services belong to this area?
-      @area_services = ResourceManagement::Resource::KNOWN_RESOURCES.select { |res| res[:area] == @area }.map { |res| res[:service] }.uniq
+      @area_services = ResourceManagement::Resource::KNOWN_SERVICES.select { |srv| srv[:area] == @area }.map { |srv| srv[:service] }
+      pp @area_services
       # load domain quota for these services
       @domain_quotas = ResourceManagement::Resource.where(:domain_id => @scoped_domain_id, :project_id => nil, :service => @area_services)
-     # raise
       # load quota and usage for all projects within these domain
       get_resource_status()
-      pp @resource_status
-
-
-     # raise
     end
 
 
@@ -65,7 +41,7 @@ module ResourceManagement
       @usage_stage = { :danger => 1.0, :warning => 0.8 }
     end
 
-    def get_resource_status()
+    def get_resource_status(critical = false)
 
       # get data for currently existing quota 
       stats = ResourceManagement::Resource.
@@ -79,7 +55,7 @@ module ResourceManagement
           where(:domain_id => @scoped_domain_id, :service => @area_services).
           where.not(project_id: nil).
           where(:current_quota => -1) 
-pp unlimited
+
       @resource_status = {}
       stats.each do |stat|
         service, name, current_project_quota_sum = *stat
@@ -95,6 +71,11 @@ pp unlimited
         domain_service_quota ||= ResourceManagement::Resource.new(
           service: service, name: name, approved_quota: -1,
         )
+        if critical 
+            next unless active_project_quota
+            next if current_project_quota_sum < domain_service_quota.approved_quota
+        end
+ 
         @resource_status[service.to_sym] ||= []
         @resource_status[service.to_sym] << { 
           :name => name,
