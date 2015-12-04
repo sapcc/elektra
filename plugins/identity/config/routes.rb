be95eb6f3844481418a290050611f1d64d352b9f
Andreas Pfau <andreas.pfau@sap.com>
2015-12-04 10:04:16 +0100

    prepare for create project wizard

--- plugins/identity/config/routes.rb
+++ plugins/identity/config/routes.rb
@@ -1,8 +1,14 @@
 Identity::Engine.routes.draw do
-  resources :projects, only:[:index] do
-    get 'wizard' => 'projects#wizard', on: :collection
-    post 'wizard' => 'projects#wizard_create', on: :collection
-  end
+  resources :projects, only:[:index]
+  
+  namespace :projects  do
+    scope :wizard do
+      get 'request' => 'request_wizard#new'
+      post 'request' => 'request_wizard#create'
+      get 'create' => 'create_wizard#new'
+      post 'create' => 'create_wizard#create'
+    end
+  end 
   
   get 'project/home' => 'projects#show', as: :project
   get 'home' => 'domains#show', as: :domain
