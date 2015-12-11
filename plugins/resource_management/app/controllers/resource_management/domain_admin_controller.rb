e39e4d66302e6959797e60fd5391a46db0c20ad7
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2015-12-11 14:07:50 +0000

    removed area param

--- plugins/resource_management/app/controllers/resource_management/domain_admin_controller.rb
+++ plugins/resource_management/app/controllers/resource_management/domain_admin_controller.rb
@@ -77,16 +77,22 @@ module ResourceManagement
       @page     = params[:page] || 1
       @resource = params.require(:resource)
       @service  = params.require(:service)
-      @area     = params.require(:area)
-
       @area_services = [@service.to_sym]
       @show_all_button = true if params[:overview].eql?("true")
 
       @domain_quotas = ResourceManagement::Resource.where(
-          :domain_id => @scoped_domain_id, 
+          :domain_id  => @scoped_domain_id, 
           :project_id => nil, 
-          :service => @service.to_sym, 
-          :name => @resource.to_sym)
+          :service    => @service.to_sym, 
+          :name       => @resource.to_sym)
+      
+      if @domain_quotas.length > 0
+        @area = @domain_quotas[0].attributes[:area]
+      else
+        # fallback if no quota was found (that should not happen)
+        service_cfg = ResourceManagement::Resource::KNOWN_SERVICES. find{ |s| s[:service] == @service.to_sym }
+        @area = service_cfg[:area] 
+      end
       
       get_resource_status(false, @resource, true)
 
