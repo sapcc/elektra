58c1a4f4faa0cf9e4bc0c9b0d35e72f91ffbce29
Andreas Pfau <andreas.pfau@sap.com>
2015-12-16 17:35:22 +0100

    merge

--- plugins/identity/app/helpers/identity/projects_helper.rb
+++ plugins/identity/app/helpers/identity/projects_helper.rb
@@ -3,11 +3,18 @@ module Identity
     
     # This method loads remote projects via ajax into a new created div.
     def remote_projects(options={})
-    
-      content_tag(:div, '', data: {
-        update_path: plugin('identity').projects_path(per_page: (options[:per_page] || 3), filter: (options[:filter] || {}), partial: true ),
-        update_immediately: true
-      })
+      container_id = SecureRandom.hex
+      
+      content_tag(:div, id: container_id) do
+        content_tag(:div, '', data: {
+          update_path: plugin('identity').projects_path({
+            per_page: (options[:per_page] || 3), 
+            filter: (options[:filter] || {}), 
+            partial: true 
+          }),
+          update_immediately: true
+        })
+      end
       
     end
   end
