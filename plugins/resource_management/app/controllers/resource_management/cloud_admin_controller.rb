4a2dddff0dfa0b0659670ab3c72dee89d8d2dc1e
Stefan Majewsky <stefan.majewsky@sap.com>
2016-01-06 15:33:28 +0100

    use Capacity model items in cloud admin view

--- plugins/resource_management/app/controllers/resource_management/cloud_admin_controller.rb
+++ plugins/resource_management/app/controllers/resource_management/cloud_admin_controller.rb
@@ -34,39 +34,38 @@ module ResourceManagement
     private
 
     def prepare_data_for_resource_list(services, options={})
-      # load resources for cloud (capacity) and projects (usage)
-      resources = ResourceManagement::Resource.where(cluster_id: nil, service: services)
+      # load capacities
+      capacities = ResourceManagement::Capacity.where(service: services).to_a
 
-      cloud_resources = resources.where(domain_id: nil, project_id: nil).to_a
-      project_resources = resources.where.not(project_id: nil)
+      # examine project usage
+      project_resources = ResourceManagement::Resource.
+        where(cluster_id: nil, service: services).
+        where.not(project_id: nil)
+      stats = project_resources.group("service, name").pluck("service, name, SUM(usage)")
 
       # check data age (see _data_age partial view)
       @min_updated_at, @max_updated_at = project_resources.pluck("MIN(updated_at), MAX(updated_at)").first
 
-      # examine project usage
-      stats = project_resources.group("service, name").pluck("service, name, SUM(usage)")
-
       # prepare data for each resource for display
       @resource_status = Hash.new { |h,k| h[k] = [] }
       stats.each do |stat|
-        service, name, usage_sum = *stat
+        service, resource, usage_sum = *stat
 
         # use existing domain resource, or create an empty mock object as a placeholder
-        cloud_resource = cloud_resources.find { |q| q.service == service && q.name == name }
-        cloud_resource ||= ResourceManagement::Resource.new(
-          service: service, name: name, approved_quota: -1,
+        capacity = capacities.find { |q| q.service == service && q.resource == resource }
+        capacity ||= ResourceManagement::Capacity.new(
+          service: service, resource: resource, value: -1,
         )
-        capacity = cloud_resource.approved_quota
 
         # on overview, show only critical usage
         if options[:overview]
-          next unless usage_sum > 0.8 * capacity
+          next unless usage_sum > 0.8 * capacity.value
         end
 
         @resource_status[service.to_sym] << {
-          name:           name,
-          usage_sum:      usage_sum,
-          cloud_resource: cloud_resource,
+          name:      resource,
+          usage_sum: usage_sum,
+          capacity:  capacity,
         }
       end
 
