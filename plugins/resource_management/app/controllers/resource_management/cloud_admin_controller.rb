d213b585a22c4e6579e663e455586543b5151c56
Stefan Majewsky <stefan.majewsky@sap.com>
2016-01-08 16:25:04 +0100

    make the details table functional

--- plugins/resource_management/app/controllers/resource_management/cloud_admin_controller.rb
+++ plugins/resource_management/app/controllers/resource_management/cloud_admin_controller.rb
@@ -20,6 +20,45 @@ module ResourceManagement
       prepare_data_for_resource_list(@area_services)
     end
 
+    def details 
+      @show_all_button = true if params[:overview] == 'true'
+
+      @service  = params.require(:service).to_sym
+      @resource = params.require(:resource).to_sym
+      @area     = ResourceManagement::Resource::KNOWN_SERVICES.find { |s| s[:service] == @service }[:area]
+
+      # load resources
+      resources = ResourceManagement::Resource.where(service: @service, name: @resource)
+      domain_resources  = resources.where.not(domain_id: nil).where(project_id: nil)
+      project_resources = resources.where.not(domain_id: nil).where.not(project_id: nil)
+
+      # statistics for the whole cloud
+      @cloud_status = {
+        capacity:         ResourceManagement::Capacity.where(service: @service, resource: @resource),
+        usage_sum:        project_resources.pluck("SUM(usage)").first,
+        domain_quota_sum: domain_resources.pluck("SUM(approved_quota)").first,
+      }
+
+      # statistics per domain
+      domain_status = []
+      domain_resources.each do |domain_resource|
+        project_quota_sum, usage_sum = resources.
+          where(domain_id: domain_resource.domain_id).where.not(project_id: nil).
+          pluck("SUM(approved_quota), SUM(usage)").first
+
+        domain_status << {
+          name:              domain_resource.domain_id, # TODO: retrieve domain names
+          domain_resource:   domain_resource,
+          project_quota_sum: project_quota_sum || 0,
+          usage_sum:         usage_sum || 0,
+        }
+      end
+
+      # prepare the domains table
+      domains = domain_status.sort_by { |entry| entry[:name] }
+      @domains = Kaminari.paginate_array(domains).page(params[:page]).per(6)
+    end
+
     def sync_now
       service = services.resource_management
       service.sync_all_domains(with_projects: true)
@@ -27,25 +66,6 @@ module ResourceManagement
         redirect_to :back
       rescue ActionController::RedirectBackError
         render text: "Synced!"
-      end
-    end
-
-    def details 
-      @show_all_button = true if params[:overview] == 'true'
-
-      @service  = params.require(:service).to_sym
-      @resource = params.require(:resource).to_sym
-      @area     = ResourceManagement::Resource::KNOWN_SERVICES.find { |s| s[:service] == @service }[:area]
-      
-      @domain_resources = ResourceManagement::Resource.
-        where(cluster_id: nil, name: @resource).
-        where(project_id: nil)
-
-      @domain_resources.each do |domain| 
-        usage = ResourceManagement::Resource.
-          where(domain_id: domain.domain_id, name: @resource).
-          where.not(project_id: nil).pluck("SUM(usage)")
-        domain.usage = usage[0]
       end
     end
 
@@ -86,7 +106,6 @@ module ResourceManagement
           capacity:  capacity,
         }
       end
-
     end
 
   end
