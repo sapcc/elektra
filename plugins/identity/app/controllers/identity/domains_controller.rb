82289cf14c84a52fb42a13f2eabffe854525ba66
Esther Schmitz <esther.schmitz@sap.com>
2016-01-15 17:36:58 +0100

    Navigation workflow rework 1: Show sub-projects per context. Remove old project nav.

--- plugins/identity/app/controllers/identity/domains_controller.rb
+++ plugins/identity/app/controllers/identity/domains_controller.rb
@@ -2,18 +2,18 @@ module Identity
   class DomainsController < ::DashboardController
     rescue_from "MonsoonOpenstackAuth::Authentication::NotAuthorized", with: :not_member_error
     authorization_required
-    
+
     def show
-      
+      @root_projects = @user_domain_projects.reject{ |project| !project.parent.blank? }
     end
-    
+
     def index
       @domains = services.identity.domains
     end
-    
+
     def not_member_error
       # just catch the error and continue rendering
       render action: :show
-    end 
+    end
   end
 end
