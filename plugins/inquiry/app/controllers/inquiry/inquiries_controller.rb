619ea28b304be2a82198a7e3a7c7ff5f375145c4
Torsten Lesmann <torsten.lesmann@sap.com>
2016-01-25 15:28:04 +0100

    Fix wrong protocol and UI glitch

--- plugins/inquiry/app/controllers/inquiry/inquiries_controller.rb
+++ plugins/inquiry/app/controllers/inquiry/inquiries_controller.rb
@@ -84,6 +84,7 @@ module Inquiry
 
     def set_mailer_host
       ActionMailer::Base.default_url_options[:host] = request.host_with_port
+      ActionMailer::Base.default_url_options[:protocol] = request.protocol
     end
 
 
