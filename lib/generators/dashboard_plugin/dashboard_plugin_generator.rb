15964c94027823812ae57630bc608ff8458ae4e7
Esther Schmitz <esther.schmitz@sap.com>
2016-01-27 15:56:08 +0100

    Turn engine stylesheets into partials so that imports from the core app are accessible in the engines.

--- lib/generators/dashboard_plugin/dashboard_plugin_generator.rb
+++ lib/generators/dashboard_plugin/dashboard_plugin_generator.rb
@@ -1,10 +1,10 @@
 class DashboardPluginGenerator < Rails::Generators::NamedBase
   PLUGINS_PATH = 'plugins'
 
-  source_root File.expand_path('../templates', __FILE__)  
+  source_root File.expand_path('../templates', __FILE__)
   class_option :mountable, type: :boolean, default: false, description: "Generate mountable isolated application"
   class_option :service_layer, type: :boolean, default: false, description: "Generate service layer (app/services/service_layer/)"
-  
+
   def generate_plugin_skeleton
     plugin_options = "--skip-gemfile --skip-bundle --skip-git --skip-test-unit"
     plugin_options += " --mountable" if options.mountable?
@@ -12,57 +12,57 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
 
     generate(:plugin, "#{PLUGINS_PATH}/#{name}", plugin_options)
   end
-  
+
   def adapt_plugin
     replace_test_with_spec
     update_dependencies_to_gemspec
-    
+
     update_assets
-    
+
     if options.mountable?
       modify_application_controller
       add_routes
       add_controller_spec
     end
-      
-    if options.service_layer? 
+
+    if options.service_layer?
       create_service_layer_service
       create_service_layer_driver
     end
   end
 
   private
-  
+
   def update_dependencies_to_gemspec
     remove_file "#{PLUGINS_PATH}/#{name}/lib/#{name}/version.rb"
     gsub_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", 'NewPlugin::VERSION', '"0.0.1"'
     gsub_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", "# Maintain your gem's version:\n", ''
     gsub_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", "require \"#{name}/version\"", ''
     gsub_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", /TODO:?/, ''
-    
+
     gsub_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", /s.add_dependency "rails"[^\n]*\n/, ''
     gsub_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", /s.add_development_dependency "sqlite3"/, ''
-    
-    #inject_into_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", after: "s.test_files = Dir[\"test/**/*\"]\n" do 
+
+    #inject_into_file "#{PLUGINS_PATH}/#{name}/#{name}.gemspec", after: "s.test_files = Dir[\"test/**/*\"]\n" do
     #  "s.add_dependency \"domain_model_service_layer\""
     #end
   end
-  
+
   def create_service_layer_service
     copy_file "app/services/service_layer/service.rb", "#{PLUGINS_PATH}/#{name}/app/services/service_layer/#{name}_service.rb"
     gsub_file "#{PLUGINS_PATH}/#{name}/app/services/service_layer/#{name}_service.rb", '%{PLUGIN_NAME}', name.classify
   end
-  
+
   def update_assets
     remove_file "#{PLUGINS_PATH}/#{name}/app/assets/stylesheets/#{name}/application.css"
     remove_file "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/application.js"
-    
-    copy_file "app/assets/application.css.scss", "#{PLUGINS_PATH}/#{name}/app/assets/stylesheets/#{name}/application.css.scss"
+
+    copy_file "app/assets/application.css.scss", "#{PLUGINS_PATH}/#{name}/app/assets/stylesheets/#{name}/_application.css.scss"
     copy_file "app/assets/application.js", "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/application.js"
-    
+
     gsub_file "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/application.js", '%{PLUGIN_NAME}', name
   end
-  
+
   def create_service_layer_driver
     copy_file "lib/driver.rb", "#{PLUGINS_PATH}/#{name}/lib/#{name}/driver.rb"
     copy_file "lib/driver/interface.rb", "#{PLUGINS_PATH}/#{name}/lib/#{name}/driver/interface.rb"
@@ -70,33 +70,33 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
 
     gsub_file "#{PLUGINS_PATH}/#{name}/lib/#{name}/driver/interface.rb", '%{PLUGIN_NAME}', name.classify
     gsub_file "#{PLUGINS_PATH}/#{name}/lib/#{name}/driver/my_driver.rb", '%{PLUGIN_NAME}', name.classify
-    
-    inject_into_file "#{PLUGINS_PATH}/#{name}/lib/#{name}.rb", after: "require \"#{name}/engine\"\n" do 
+
+    inject_into_file "#{PLUGINS_PATH}/#{name}/lib/#{name}.rb", after: "require \"#{name}/engine\"\n" do
       "require \"domain_model_service_layer\"\n" +
       "require_relative \"#{name}/driver\"\n"
     end
   end
-  
+
   def modify_application_controller
     remove_file "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb"
     copy_file "app/controllers/application_controller.rb", "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb"
     copy_file "app/views/application/index.html.haml", "#{PLUGINS_PATH}/#{name}/app/views/#{name}/application/index.html.haml"
-    
+
     gsub_file "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb", '%{PLUGIN_NAME}', name.classify
     gsub_file "#{PLUGINS_PATH}/#{name}/app/views/#{name}/application/index.html.haml", '%{PLUGIN_NAME}', name.classify
   end
-  
+
   def replace_test_with_spec
     create_file "#{PLUGINS_PATH}/#{name}/spec/.keep"
   end
-  
+
   def add_controller_spec
     copy_file "spec/controllers/application_controller_spec.rb", "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb"
     gsub_file "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb", '%{PLUGIN_NAME}', name.classify
-       
-    gsub_file "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb", '%{PLUGIN_NAME}', name.classify   
+
+    gsub_file "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb", '%{PLUGIN_NAME}', name.classify
   end
-  
+
   def add_routes
     remove_file "#{PLUGINS_PATH}/#{name}/config/routes.rb"
     copy_file "config/routes.rb", "#{PLUGINS_PATH}/#{name}/config/routes.rb"
