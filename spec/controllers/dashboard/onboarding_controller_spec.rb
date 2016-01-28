430710249d642d8b6ff6e3a23b99de0dc6f7da86
Torsten Lesmann <torsten.lesmann@sap.com>
2016-01-28 13:19:48 +0100

    Onboarding controller tests

--- spec/controllers/dashboard/onboarding_controller_spec.rb
+++ spec/controllers/dashboard/onboarding_controller_spec.rb
@@ -2,37 +2,57 @@ require 'spec_helper'
 
 RSpec.describe Dashboard::OnboardingController, type: :controller do
 
-=begin
   include AuthenticationStub
 
   before(:all) do
-    @domain_default_friendly_id = FriendlyIdEntry.find_or_create_entry('Domain',nil,'o-sap_default','sap_default')
-    @domain_abc_friendly_id = FriendlyIdEntry.find_or_create_entry('Domain',nil,'o-abc','abc')
+    @domain_default_friendly_id = FriendlyIdEntry.find_or_create_entry('Domain', nil, 'o-sap_default', 'sap_default')
+    @domain_abc_friendly_id = FriendlyIdEntry.find_or_create_entry('Domain', nil, 'o-abc', 'abc')
+
   end
 
   before :each do
     stub_admin_services
+    allow(Admin::IdentityService).to receive(:list_scope_admins).and_return([Hashie::Mash.new({id: '***REMOVED***', email: 'torsten.lesmann@sap.com', full_name: "Torsten Lesmann"})])
   end
 
-  context "whatever" do
+  context "onboarding flow" do
 
-    describe "GET 'new_user'" do
+    describe "GET 'onboarding'" do
 
-
-      it "returns http success" do
-
+      it "onboards without inquiry for sap_default domain" do
         stub_authentication do |token|
           # no role user
           token["roles"] = {}
-          token["domain"] = {"id"=>"#{@domain_default_friendly_id.key}", "name"=>"#{@domain_default_friendly_id.name}"}
+          token["domain"] = {"id" => "#{@domain_default_friendly_id.key}", "name" => "#{@domain_default_friendly_id.name}"}
         end
 
-        get :new_user, domain_id: @domain_default_friendly_id.slug
+        get :onboarding, domain_id: @domain_default_friendly_id.slug
         expect(response).to be_success
+        expect(response).to render_template(:onboarding_without_inquiry)
+
+        post :register_without_inquiry, {domain_id: @domain_default_friendly_id.slug, terms_of_use: true}
+        expect(response).to redirect_to "/#{@domain_default_friendly_id.name}/identity/home"
+
+      end
+
+      it "onboards with inquiry for NON sap_default domain" do
+        stub_authentication do |token|
+          # no role user
+          token["roles"] = {}
+          token["domain"] = {"id" => "#{@domain_abc_friendly_id.key}", "name" => "#{@domain_abc_friendly_id.name}"}
+        end
+
+        get :onboarding, domain_id: @domain_abc_friendly_id.slug
+        expect(response).to be_success
+        expect(response).to render_template(:onboarding_with_inquiry)
+
+
+        post :register_with_inquiry, {domain_id: @domain_default_friendly_id.slug, terms_of_use: true}
+        expect(response).to render_template(:onboarding_open_message)
+
       end
     end
-  end
-=end
 
+  end
 
 end
