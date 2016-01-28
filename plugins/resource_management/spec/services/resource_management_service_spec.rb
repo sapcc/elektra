5bb4905290bee8da70566be55a86c66884807797
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-01-28 11:15:14 +0000

    test DB for default entry

--- plugins/resource_management/spec/services/resource_management_service_spec.rb
+++ plugins/resource_management/spec/services/resource_management_service_spec.rb
@@ -182,17 +182,28 @@ RSpec.describe ServiceLayer::ResourceManagementService do
       expect(synced_resources).to eq(expected_resources)
     end
 
-    it 'create new resource with default value' do
+    it 'create new resource with default value', :focus => true  do
        service.sync_project(domain_id, project_id)
        set_project_quota_call = service.driver.set_call_history
+
        set_project_quota_call.each do |call|
          expect(call[:domain_id]).to eq(domain_id)
          expect(call[:project_id]).to eq(project_id)
-         if call.key?(:value)
-           if call[:value].key?(:capacity)
+         
+         if call.key?(:values)
+           if call[:values].key?(:capacity)
              # check that capacity was created with 1GB quota
-             expect(call[:value][:capacity]).to eq (1<<30)
+             expect(call[:values][:capacity]).to eq(1<<30)
            end
+           # check DB entry
+           name = call[:values].keys.first.to_s
+           resource = ResourceManagement::Resource.where(
+             domain_id:      domain_id,
+             project_id:     project_id,
+             name:           name,
+           ).first
+
+           expect(resource.current_quota).to eq(call[:values][name.to_sym])
          end
        end
     end
