003e1522eee06eb89a2c4d4ff1600dfa2a3b7470
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-02-24 08:59:28 +0000

    adjust route

--- plugins/object_storage/config/routes.rb
+++ plugins/object_storage/config/routes.rb
@@ -1,6 +1,10 @@
 ObjectStorage::Engine.routes.draw do
 
-  resources 'containers', param: :container
+  resources 'containers', param: :container do
+    member do
+      get :confirm_deletion
+    end
+  end
 
   scope 'containers/:container', format: false do
     # a simple `resources :objects` won't work since the object path shall be
@@ -9,8 +13,6 @@ ObjectStorage::Engine.routes.draw do
     get  'list(/*path)'        => 'objects#index',       as: 'list_objects'
     get  'raw/*path'           => 'objects#download',    as: 'download_object'
     get  'show/*path'          => 'objects#show',        as: 'object'
-
-    get 'confirm_deletion' => 'containers#confirm_deletion'
 
     get  'upload(/*path)'        => 'folders#new_object',    as: 'new_object'
     post 'upload(/*path)'        => 'folders#create_object', as: 'create_object'
