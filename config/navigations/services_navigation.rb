38ab726988dce20e04f0df22c16d78678ddb2e86
Esther Schmitz <esther.schmitz@sap.com>
2016-02-25 18:19:58 +0100

    Fix image plugin stub to make it reachable.

--- config/navigations/services_navigation.rb
+++ config/navigations/services_navigation.rb
@@ -57,9 +57,10 @@ SimpleNavigation::Configuration.run do |navigation|
     #                            against the current URI.  You may also use a proc, or the symbol <tt>:subpath</tt>.
     #
     primary.item :compute, 'Compute', nil, html: {class: "fancy-nav-header", 'data-icon': "icon moo-cloud"},
-    if: -> {services.available?(:compute,:instances) or services.available?(:identity,:web_console) or plugin_available?(:block_storage)} do |compute_nav|
+    if: -> {services.available?(:compute,:instances) or services.available?(:image,:os_images) or plugin_available?(:block_storage)} do |compute_nav|
       compute_nav.item :instances, 'Instances', -> {plugin('compute').instances_path}, if: -> { services.available?(:compute,:instances) }
       compute_nav.item :block_storage, 'Block Storage', -> {plugin('block_storage').entry_path}, if: -> { plugin_available?(:block_storage) }
+      compute_nav.item :images, 'Images', -> {plugin('image').os_images_path}, if: -> { services.available?(:image,:os_images) }
 
       # compute_nav.dom_attributes = {class: 'content-list'}
     end
--- plugins/image/app/services/service_layer/image_service.rb
+++ plugins/image/app/services/service_layer/image_service.rb
@@ -1,20 +1,21 @@
 module ServiceLayer
   class ImageService < Core::ServiceLayer::Service
-  
+
     def driver
       @driver ||= Image::Driver::Fog.new({
         auth_url:   self.auth_url,
         region:     self.region,
         token:      self.token,
         domain_id:  self.domain_id,
-        project_id: self.project_id  
+        project_id: self.project_id
       })
     end
-    
+
     def available?(action_name_sym=nil)
-      not current_user.service_url('image',region: region).nil?  
+      # not current_user.service_url('image',region: region).nil?
+      true # pretend it's available for now
     end
-  
+
     def images
       driver.map_to(Image::Image).images
     end
