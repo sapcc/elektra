19f5cfe55ca0bc32b0f312e3c4fea277a330767c
Torsten Lesmann <torsten.lesmann@sap.com>
2016-03-03 12:02:30 +0100

    Reactivate request tests and delete force error hack

--- plugins/inquiry/spec/models/inquiry_spec.rb
+++ plugins/inquiry/spec/models/inquiry_spec.rb
@@ -6,28 +6,32 @@ describe 'Inquiry' do
     @payload = {:key1 => "value1", :key2 => "value2"}.to_json
     token = AuthenticationStub.test_token
     @user = ApplicationController::CurrentUserWrapper.new(MonsoonOpenstackAuth::Authentication::AuthUser.new(token),{})
+  end
+
+  before(:each) do
     @processors = Inquiry::Processor.from_users([@user])
   end
 
   describe 'Inquiry' do
-    skip do
+#    skip do
       context 'Create' do
 
         it 'creates a new Inquiry with initial status open' do
-          inq = Inquiry::Inquiry.new()
+          inq = Inquiry::Inquiry.new(description: "Test", requester: @processors[0])
           inq.payload = @payload
           inq.processors = @processors
-          expect(inq.aasm_state).to eq("open")
+          expect(inq.aasm_state).to eq("new")
           expect {
             inq.save
           }.to change { Inquiry::Inquiry.count }.by(1)
-
+          expect(inq.aasm_state).to eq("open")
           puts inq
         end
 
         it 'changes state from open to approved and creates a step record for this' do
-          inq = Inquiry::Inquiry.new(processors: @processors)
+          inq = Inquiry::Inquiry.new(description: "Test", requester: @processors[0])
           inq.payload = @payload
+          inq.processors = @processors
           inq.save
           expect(inq.aasm_state).to eq("open")
           expect {
@@ -36,16 +40,19 @@ describe 'Inquiry' do
           }.to change { inq.process_steps.count }.by(1)
 
           check = Inquiry::Inquiry.find(inq.id)
-          expect(check.process_steps.count).to eq(1)
-          #expect(check.process_steps.first.processor.uid).to eq(@user.id)
-          expect(check.process_steps.first.from_state).to eq("open")
-          expect(check.process_steps.first.to_state).to eq("approved")
+          expect(check.process_steps.count).to eq(2)
+          expect(check.process_steps.first.from_state).to eq("new")
+          expect(check.process_steps.first.to_state).to eq("open")
+          expect(check.process_steps.second.from_state).to eq("open")
+          expect(check.process_steps.second.to_state).to eq("approved")
         end
 
         it 'changes state from open to rejected and creates a step record for this' do
-          inq = Inquiry::Inquiry.new(processors: @processors)
+          inq = Inquiry::Inquiry.new(description: "Test", requester: @processors[0])
           inq.payload = @payload
+          inq.processors = @processors
           inq.save
+          inq.reload
           expect(inq.aasm_state).to eq("open")
           expect {
             inq.reject!({user: @user, description: "For testing reasons"})
@@ -53,13 +60,14 @@ describe 'Inquiry' do
           }.to change { inq.process_steps.count }.by(1)
 
           check = Inquiry::Inquiry.find(inq.id)
-          expect(check.process_steps.count).to eq(1)
-          #expect(check.process_steps.first.processor.uid).to eq(@user.id)
-          expect(check.process_steps.first.from_state).to eq("open")
-          expect(check.process_steps.first.to_state).to eq("rejected")
+          expect(check.process_steps.count).to eq(2)
+          expect(check.process_steps.first.from_state).to eq("new")
+          expect(check.process_steps.first.to_state).to eq("open")
+          expect(check.process_steps.second.from_state).to eq("open")
+          expect(check.process_steps.second.to_state).to eq("rejected")
         end
 
       end
     end
-  end
+#  end
 end
