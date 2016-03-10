4cefc465c0d53b93e7e412609b74c7359dcf615f
Stefan Majewsky <stefan.majewsky@sap.com>
2016-03-10 14:25:17 +0100

    add a simple handling for backend errors
    The whole error reporting situation in the Core::ServiceLayer is kind of
    a mess, so this mostly works around it for now, and reads plain Excon
    errors.
    
    That's enough for now since Swift actually returns useful status codes.
    The response body seems not to add much to the status code, so we just
    ignore it for now.

--- plugins/object_storage/app/controllers/object_storage/application_controller.rb
+++ plugins/object_storage/app/controllers/object_storage/application_controller.rb
@@ -3,6 +3,24 @@ module ObjectStorage
     # This is the base class of all controllers in this plugin. Only put code in here that is shared across controllers.
     authorization_context 'object_storage'
 
+    rescue_from Excon::Errors::Error do |exception|
+      # get exception message
+      @exception_msg = exception.to_s
+
+      # serialize request if it is avilable
+      if req = exception.request
+        str = "#{req[:method].to_s.upcase} #{req[:path]}\n"
+        hdr = req[:headers] || {}
+        hdr.keys.sort.each do |key|
+          value = key.end_with?('-Token') ? '*****' : hdr[key]
+          str += "#{key}: #{value}\n"
+        end
+        @request_dump = str
+      end
+
+      render template: '/object_storage/application/backend_error'
+    end
+
     protected
 
     def metadata_params
