8116f3cd9da9839bd880daa1a592c727f1c494f9
Maurice Schreiber <maurice.schreiber@sap.com>
2016-03-18 10:43:57 +0100

    move simple modal form layout to core, make submit action optional

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,23 +1,26 @@
 module ModalHelper
 
   # Like simple_form_for(), but includes all the boilerplate for a modal dialog as well.
-  # Buttons "Submit" and "Cancel" are added automatically. The following options need to be provided for this:
+  # Button "Cancel" is added automatically. The following options can be provided for this:
   #
-  #     cancel_url    - where the "Cancel" button should go when the dialog is opened in a non-modal window
-  #     submit_action - caption for the "Submit" button -> give this as second position argument
+  #     cancel_url    - mandatory: where the "Cancel" button should go when the dialog is opened in a non-modal window
+  #     submit_action - optional: adds a "Submit" button, change its caption by passing a string value
   #
-  def simple_modal_form_for(model_object, submit_action, options={}, &block)
+  def simple_modal_form_for(model_object, options = {}, &block)
     # check mandatory options
-    raise ArgumentError, 'submit_action missing' unless submit_action.is_a?(String)
-    raise ArgumentError, 'cancel_url missing'    unless options.has_key?(:cancel_url)
+    raise ArgumentError, 'cancel_url missing' unless options.key?(:cancel_url)
     cancel_url = options.delete(:cancel_url)
 
-    # set some options that are required for the simple_form_for() to work inside a modal dialog
-    options[:remote] = request.xhr? unless options.has_key?(:remote)
+    submit_action = options.delete(:submit_action)
+    # default submit caption
+    submit_action = 'Submit' if submit_action && !submit_action.is_a?(String)
 
-    html_hash =   options[:html] ||= {}
+    # set some options that are required for the simple_form_for() to work inside a modal dialog
+    options[:remote] = request.xhr? unless options.key?(:remote)
+
+    html_hash = options[:html] ||= {}
     data_hash = html_hash[:data] ||= {}
-    data_hash[:modal] = true unless data_hash.has_key?(:modal)
+    data_hash[:modal] = true unless data_hash.key?(:modal)
 
     # the actual work is done in a layout; the helper is just there to resemble simple_form_for()
     render(layout: 'simple_modal_form', locals: {
