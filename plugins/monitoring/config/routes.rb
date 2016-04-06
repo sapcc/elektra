d30431502aa85b4a56fb7ce3630c08c087eb1393
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-04-06 13:01:38 +0000

    basic table with options for alarm definitions

--- plugins/monitoring/config/routes.rb
+++ plugins/monitoring/config/routes.rb
@@ -1,7 +1,7 @@
 Monitoring::Engine.routes.draw do
   get '/' => 'application#index', as: :entry
 
-  resources 'alarms', except: :edit do
+  resources 'alarms' do
   end
   
   resources 'alarm_definitions' do
