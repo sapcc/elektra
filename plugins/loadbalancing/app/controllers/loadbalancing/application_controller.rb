cb5a9434a5922a2a7aa9f99e266038ab6670b15b
Torsten Lesmann <torsten.lesmann@sap.com>
2016-04-07 11:46:46 +0200

    LoadBalancing Service opens webCli on idex page. Documentation for lbaas created with large Example which has to be verified some time when v2 is ready!!!!

--- plugins/loadbalancing/app/controllers/loadbalancing/application_controller.rb
+++ plugins/loadbalancing/app/controllers/loadbalancing/application_controller.rb
@@ -1,6 +1,9 @@
 module Loadbalancing
   class ApplicationController < DashboardController
     def index
+      @token = current_user.token
+      @webcli_endpoint = current_user.service_url("webcli")
+      @identity_url = current_user.service_url("identity")
     end
   end
 end
