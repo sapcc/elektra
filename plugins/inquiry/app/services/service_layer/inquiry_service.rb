b583446c341525a9f842fe330e6bac4dd5935dd6
Torsten Lesmann <torsten.lesmann@sap.com>
2016-04-08 12:27:41 +0200

    Fix onboarding process.

--- plugins/inquiry/app/services/service_layer/inquiry_service.rb
+++ plugins/inquiry/app/services/service_layer/inquiry_service.rb
@@ -45,12 +45,7 @@ module ServiceLayer
     end
 
     def find_by_kind_user_states(kind, requester_id, states=[])
-      i = inquiries({kind: kind, state: states, requester_id: requester_id})
-      if i.count > 0
-        return Delegates::Inquiry.new(i.first)
-      else
-        return nil
-      end
+      get_inquiries({kind: kind, state: states, requester_id: requester_id})
     end
 
   end
