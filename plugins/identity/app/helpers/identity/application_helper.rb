e066e0ec171e3615299ce61997795506e6182a8b
Esther Schmitz <esther.schmitz@sap.com>
2016-04-08 15:24:39 +0200

    Replace bootstrap-treeview with haml helper + js behaviour and cc styling.

--- plugins/identity/app/helpers/identity/application_helper.rb
+++ plugins/identity/app/helpers/identity/application_helper.rb
@@ -1,7 +1,7 @@
 module Identity
   module ApplicationHelper
 
-    # creates a tree for projectview based on project_tree delivered by controller 
+    # creates a tree for projectview based on project_tree delivered by controller
     def project_tree(nodes, result=[])
       nodes = [nodes] unless nodes.is_a?(Array)
       nodes.collect do |node|
@@ -12,5 +12,27 @@ module Identity
         attrs
       end
     end
+
+    def render_project_tree(nodes, options={})
+      nodes = [nodes] unless nodes.is_a?(Array)
+      haml_tag :ul, class: ("tree tree-expandable" unless options[:child_tree]) do
+        nodes.each do |node|
+          has_children = node.children.length > 0
+          haml_tag :li, class: ("has-children" if has_children) do
+            haml_tag :i, class: "node-icon"
+            unless node.root?
+              haml_tag :a, node.name, href: plugin('identity').project_path(project_id: node.friendly_id)
+            end
+
+            if has_children
+              render_project_tree(node.children, child_tree: true)
+            end
+          end
+        end
+      end
+
+    end
+
+
   end
 end
