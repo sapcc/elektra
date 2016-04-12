d7c5b8d94556cf6aefbfe016c850b673257c5358
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-04-12 14:04:21 +0000

    add error handling to load alarm definition

--- plugins/monitoring/app/controllers/monitoring/alarm_definitions_controller.rb
+++ plugins/monitoring/app/controllers/monitoring/alarm_definitions_controller.rb
@@ -1,17 +1,16 @@
 module Monitoring
   class AlarmDefinitionsController < DashboardController
     authorization_context 'monitoring'
+    before_filter :load_alarm_definition, except: [ :index, :new, :create ] 
 
     def index
        @alarm_definitions = services.monitoring.alarm_definitions
     end
 
     def show
-       @alarm_definition = services.monitoring.get_alarm_definition(params.require(:id))
     end
 
     def destroy 
-       @alarm_definition = services.monitoring.get_alarm_definition(params.require(:id))
        @alarm_definition.destroy
        back_to_definition_list
     end
@@ -26,6 +25,11 @@ module Monitoring
         end
         format.html { redirect_to plugin('monitoring').alarm_definitions_path }
       end
+    end
+
+    def load_alarm_definition
+      @alarm_definition = services.monitoring.get_alarm_definition(params.require(:id))
+      raise ActiveRecord::RecordNotFound, "alarm definition with id #{params[:id]} not found" unless @alarm_definition
     end
 
   end
