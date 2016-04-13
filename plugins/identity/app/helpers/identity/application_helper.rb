dc0653363da067663c9bf335b3f8f1308301dcca
Andreas Pfau <andreas.pfau@sap.com>
2016-04-13 15:45:33 +0200

    implement search functionality in js

--- plugins/identity/app/helpers/identity/application_helper.rb
+++ plugins/identity/app/helpers/identity/application_helper.rb
@@ -13,21 +13,29 @@ module Identity
       end
     end
 
+    # This helper renders a project tree as unordered list.
+    # Important: to use the jQuery plugin 'searchable' the li elements have to have data attribute search-name -> data-search-name.
     def render_project_tree(nodes, options={})
       nodes = [nodes] unless nodes.is_a?(Array)
-      haml_tag :ul, class: ("tree tree-expandable" unless options[:child_tree]) do
+      html_options = options[:html] || {}
+      html_options[:class] ||= ''
+      html_options[:class] += " tree tree-expandable" unless options[:child_tree]
+      item_html = options[:item_html] || {}
+
+      content_tag :ul, html_options do
         nodes.each do |node|
           has_children = node.children.length > 0
-          haml_tag :li, class: ("has-children" if has_children) do
-            haml_tag :i, class: "node-icon"
+          concat(content_tag(:li, {class: ("has-children" if has_children), data: {search_name: node.name}}) do
+            concat(content_tag(:i, '', class: "node-icon"))
+
             unless node.root?
-              haml_tag :a, node.name, href: plugin('identity').project_path(project_id: node.friendly_id)
+              concat link_to(node.name, plugin('identity').project_path(project_id: node.friendly_id))
             end
 
             if has_children
-              render_project_tree(node.children, child_tree: true)
+              concat render_project_tree(node.children, child_tree: true)
             end
-          end
+          end)
         end
       end
 
