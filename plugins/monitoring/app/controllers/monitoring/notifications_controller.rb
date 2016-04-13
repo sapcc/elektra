826e92728a0d9f7fc745f814045abded5235d70a
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-04-13 09:00:02 +0000

    add pagination to notifications list

--- plugins/monitoring/app/controllers/monitoring/notifications_controller.rb
+++ plugins/monitoring/app/controllers/monitoring/notifications_controller.rb
@@ -3,7 +3,14 @@ module Monitoring
     authorization_context 'monitoring'
 
     def index
-      @notifications = services.monitoring.notifications
+      notifications = services.monitoring.notifications
+      sorted_notifications = []
+      # sort by name
+      notifications.sort_by(&:name).each do |notification|
+        sorted_notifications << notification
+      end
+
+      @notifications = Kaminari.paginate_array(sorted_notifications).page(params[:page]).per(10)
     end
 
   end
