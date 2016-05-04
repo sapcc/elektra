1a0f948855e97f5fbf75229bd835be2b2b8560e0
Stefan Majewsky <stefan.majewsky@sap.com>
2016-05-04 13:33:23 +0200

    simplify routes

--- plugins/monitoring/config/routes.rb
+++ plugins/monitoring/config/routes.rb
@@ -1,14 +1,21 @@
 Monitoring::Engine.routes.draw do
   get '/' => 'overview#index', as: :entry
 
-  get 'alarm_definitions/search/' => 'alarm_definitions#search'
-  get 'notification_methods/search/' => 'notification_methods#search'
-  
   resources 'alarms', except: [:new, :create]
+
   resources 'alarm_definitions' do
+    collection do
+      get 'search'
+    end
     member do
       get 'toggle_alarm_actions'
     end
   end
-  resources 'notification_methods'
+
+  resources 'notification_methods' do
+    collection do
+      get 'search'
+    end
+  end
+
 end
