65192c44baa5cf84aac3de9223a6c561431e95d6
Stefan Majewsky <stefan.majewsky@sap.com>
2016-05-10 14:51:00 +0200

    use Bootstrap breadcrumbs for object listings

--- plugins/object_storage/app/helpers/object_storage/application_helper.rb
+++ plugins/object_storage/app/helpers/object_storage/application_helper.rb
@@ -7,21 +7,19 @@ module ObjectStorage
 
     def make_breadcrumb(container_name, path='')
       # empty path?
-      return container_name if path.gsub('/', '') == ''
+      return [[], container_name] if path.gsub('/', '') == ''
       # first breadcrumb element is container name, linking to its root directory
-      result = link_to(container_name, plugin('object_storage').list_objects_path(container_name))
+      crumbs = [ link_to(container_name, plugin('object_storage').list_objects_path(container_name)) ]
 
       # make one crumb per path element
-      crumbs = []
       elements = path.split('/').delete_if { |e| e.blank? }
       last_crumb = elements.pop
       elements.each_with_index do |name,idx|
         link = plugin('object_storage').list_objects_path(container_name, path: elements[0..idx].join('/'))
         crumbs << link_to(name, link)
       end
-      crumbs << last_crumb
 
-      return result + " > " + crumbs.join('/').html_safe
+      return [crumbs, last_crumb]
     end
 
     def format_bytes(value_in_bytes)
