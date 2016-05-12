81c32cb517ad048262cf6dddd79c6f78c9961c95
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-05-12 08:19:31 +0000

    convert alarm timestamps

--- plugins/monitoring/app/models/monitoring/alarm.rb
+++ plugins/monitoring/app/models/monitoring/alarm.rb
@@ -23,5 +23,36 @@ module Monitoring
       id
     end
 
+    def state_updated_timestamp
+      state_updated_timestamp = read(:state_updated_timestamp)
+      if state_updated_timestamp
+        convert_timestamp(state_updated_timestamp)
+      end
+    end
+
+    def updated_timestamp
+      updated_timestamp = read(:updated_timestamp)
+      if updated_timestamp
+        convert_timestamp(updated_timestamp)
+      end
+    end
+
+    def created_timestamp
+      created_timestamp = read(:created_timestamp)
+      if created_timestamp
+        convert_timestamp(created_timestamp)
+      end
+    end
+
+
+
+    private
+
+    def convert_timestamp(timestamp)
+      date,time_string = timestamp.split("T")
+      time = time_string.split('.')[0]
+      "#{time} #{date}"
+    end
+
   end
 end
