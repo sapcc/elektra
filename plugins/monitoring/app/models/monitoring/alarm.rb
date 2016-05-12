cb3c36595c6493ad1ff9cf96a0ac331167e3e773
Stefan Majewsky <stefan.majewsky@sap.com>
2016-05-12 11:19:37 +0200

    less redundancy

--- plugins/monitoring/app/models/monitoring/alarm.rb
+++ plugins/monitoring/app/models/monitoring/alarm.rb
@@ -10,45 +10,29 @@ module Monitoring
     # created_timestamp
 
     def severity
-      alarm_definition = read(:alarm_definition)
-      severity = alarm_definition["severity"]
-      return nil unless severity
-      severity
+      read(:alarm_definition)["severity"] || nil
     end
 
     def alarm_definition_id
-      alarm_definition = read(:alarm_definition)
-      id = alarm_definition["id"]
-      return nil unless id
-      id
+      read(:alarm_definition)["id"] || nil
     end
 
     def state_updated_timestamp
-      state_updated_timestamp = read(:state_updated_timestamp)
-      if state_updated_timestamp
-        convert_timestamp(state_updated_timestamp)
-      end
+      convert_timestamp(read(:state_updated_timestamp))
     end
 
     def updated_timestamp
-      updated_timestamp = read(:updated_timestamp)
-      if updated_timestamp
-        convert_timestamp(updated_timestamp)
-      end
+      convert_timestamp(read(:updated_timestamp))
     end
 
     def created_timestamp
-      created_timestamp = read(:created_timestamp)
-      if created_timestamp
-        convert_timestamp(created_timestamp)
-      end
+      convert_timestamp(read(:created_timestamp))
     end
-
-
 
     private
 
     def convert_timestamp(timestamp)
+      return nil unless timestamp
       date,time_string = timestamp.split("T")
       time = time_string.split('.')[0]
       "#{time} #{date}"
