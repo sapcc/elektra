23ffcf7677d69b9f0c484e196951ac879c964a56
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-05-18 09:43:23 +0000

    bugfix pagination alarms view

--- plugins/monitoring/app/controllers/monitoring/alarms_controller.rb
+++ plugins/monitoring/app/controllers/monitoring/alarms_controller.rb
@@ -6,12 +6,6 @@ module Monitoring
 
     def index
       @index = 1
-#      all_alarms = services.monitoring.alarms
-#      @alarms_count = all_alarms.length
-#      alarm_definitions = services.monitoring.alarm_definitions
-#      # map alarm definitions for later use in view to have show more information  in the list
-#      @alarm_definitions = Hash[alarm_definitions.map{ |a| [a.id, a] }]
-#      @alarms = Kaminari.paginate_array(all_alarms).page(params[:page]).per(10)
     end
 
     def filter
