93d589dc87df0e92912ee7e258bdfaa5724daabe
Maurice Schreiber <maurice.schreiber@sap.com>
2016-05-18 15:26:47 +0200

    stub count in image spec

--- plugins/image/spec/controllers/os_images/public_controller_spec.rb
+++ plugins/image/spec/controllers/os_images/public_controller_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
-describe Image::OsImagesController, type: :controller do
+describe Image::OsImages::PublicController, type: :controller do
   routes { Image::Engine.routes }
-  
+
   include AuthenticationStub
-  
+
   default_params = {domain_id: AuthenticationStub.domain_id, project_id: AuthenticationStub.project_id}
-  
+
   before(:all) do
     #DatabaseCleaner.clean
     FriendlyIdEntry.find_or_create_entry('Domain',nil,default_params[:domain_id],'default')
     FriendlyIdEntry.find_or_create_entry('Project',default_params[:domain_id],default_params[:project_id],default_params[:project_id])
   end
-  
+
   before :each do
     stub_authentication
     stub_admin_services
-      
+
     identity_driver = double('identity_service_driver').as_null_object
     os_image_driver = double('image_service_driver').as_null_object
+
+    allow(os_image_driver).to receive(:count).and_return(1)
 
     allow_any_instance_of(ServiceLayer::IdentityService).to receive(:driver).and_return(identity_driver)
     allow_any_instance_of(ServiceLayer::ImageService).to receive(:driver).and_return(os_image_driver)
