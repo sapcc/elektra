ed710813fa740d2d6f4be645d408526408bd2fdf
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-05-26 11:10:26 +0000

    overview page more colorfull

--- plugins/monitoring/app/controllers/monitoring/overview_controller.rb
+++ plugins/monitoring/app/controllers/monitoring/overview_controller.rb
@@ -5,9 +5,9 @@ module Monitoring
     def index
       all_alarms = services.monitoring.alarms()
       @severity_cnt = Hash.new(0)
-      @status_cnt = Hash.new(0)
+      @state_cnt = Hash.new(0)
       all_alarms.map{|alarm| alarm.severity }.map{|severity| @severity_cnt[severity] += 1}
-      all_alarms.map{|alarm| alarm.state }.map{|state| @status_cnt[state] += 1}
+      all_alarms.map{|alarm| alarm.state }.map{|state| @state_cnt[state] += 1}
     end
 
   end
--- plugins/monitoring/app/helpers/monitoring/application_helper.rb
+++ plugins/monitoring/app/helpers/monitoring/application_helper.rb
@@ -20,5 +20,23 @@ module Monitoring
       return content_tag(:span, state.capitalize, class: styles.fetch(state, ''))
     end
 
+    def badge_state(state,value)
+      styles = {
+        'OK'           => 'alert-success',
+        'UNDETERMINED' => 'alert-warning',
+        'ALARM'        => 'alert-danger',
+      }
+        content_tag(:span, value, class: "badge #{styles.fetch(state, '')}")
+    end
+
+    def badge_severity(severity,value)
+      styles = {
+        'LOW'      => 'alert-info',
+        'MEDIUM'   => 'alert-warning',
+        'HIGH'     => 'alert-danger',
+        'CRITICAL' => 'alert-danger',
+      }
+      return content_tag(:span, value, class: "badge #{styles.fetch(severity, '')}" )
+    end
   end
 end
