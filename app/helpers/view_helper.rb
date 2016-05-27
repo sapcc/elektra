f1c7aba03f6bbef1b11448a1f644bad5233bb279
Maurice Schreiber <maurice.schreiber@sap.com>
2016-05-27 14:55:51 +0200

    comment on 'N/A' project name [ci skip]

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -2,6 +2,7 @@ module ViewHelper
   def project_id_and_name(project)
     if project
       remote_project = @service_user.find_project_by_name_or_id(project)
+      # projects where the service user does not have permissions or deleted projects get 'N/A'
       project_name = remote_project ? remote_project.name : 'N/A'
       "#{project} (#{project_name})"
     else
