0724f2024066954baa80f67b7f5204f3a3220e69
Stefan Majewsky <stefan.majewsky@sap.com>
2016-06-02 10:35:45 +0200

    submit project/domain name to billing if missing

--- plugins/cost_control/app/controllers/cost_control/cost_object_controller.rb
+++ plugins/cost_control/app/controllers/cost_control/cost_object_controller.rb
@@ -20,6 +20,19 @@ module CostControl
 
     def load_project_metadata
       @metadata = services.cost_control.find_project_metadata(@scoped_project_id)
+      # ensure that the billing service has the correct project and domain name
+      # (this is sadly a bit unwieldy because Core::ServiceLayer::Model does
+      # not have a changed? method)
+      changed = false
+      if @metadata.project_name != @scoped_project_name
+        @metadata.project_name = @scoped_project_name
+        changed = true
+      end
+      if @metadata.domain_name != @scoped_domain_name
+        @metadata.domain_name = @scoped_domain_name
+        changed = true
+      end
+      @metadata.save if changed
     end
 
   end
