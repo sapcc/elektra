cb17c55f51e33789442e7987e36373b34c3ea06d
Esther Schmitz <esther.schmitz@sap.com>
2016-07-01 15:15:16 +0200

    Reintroduce footer links, now without needing scope controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ActionController::Base
   include HighVoltage::StaticPage
 
   layout 'noscope'
+  helper_method :current_region
+
+  private
+
+  def current_region
+    ENV['MONSOON_DASHBOARD_REGION'] || 'eu-de-1'
+  end
 
 end
