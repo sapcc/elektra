f16ccc99db2ae0115119417143bb9b679613fc11
Stefan Majewsky <stefan.majewsky@sap.com>
2016-07-07 14:58:26 +0200

    add link to cloud admin resource mgmt

--- config/navigations/services_navigation.rb
+++ config/navigations/services_navigation.rb
@@ -59,6 +59,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
   primary.item :ccadmin, 'Cloud Administration', nil, html: {class: "fancy-nav-header", 'data-icon': "compute-icon"}, if: -> {current_user and current_user.is_allowed?('cloud_admin')} do |ccadmin_nav|
     ccadmin_nav.item :requests, 'Manage Requests', plugin('inquiry').admin_inquiries_path
+    ccadmin_nav.item :resource_management, 'Resource Management', -> {plugin('resource_management').cloud_admin_path}, if: -> { services.available?(:resource_management,:resources) }
   end
 
   primary.item :compute, 'Compute', nil, html: {class: "fancy-nav-header", 'data-icon': "compute-icon"},
