38aaf8c4c2224e52e2f92fd371fa033284a0f346
Fabian Ruff <fabian.ruff@sap.com>
2016-07-20 14:00:14 +0200

    Tweak sentry to include plugins in app only stacktraces

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,4 +2,5 @@ Raven.configure do |config|
   # httpclient is the only faraday adpater which handles no_proxy 
   config.http_adapter = :httpclient
   config.send_modules = false
+  config.app_dirs_pattern = /(app|bin|config|lib|plugins|spec)/
 end
