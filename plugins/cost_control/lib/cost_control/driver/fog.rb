8da7705997099906fdcc3cd726e3c0effa4a41c3
Arno Uhlig <arno.uhlig@sap.com>
2016-07-29 18:04:36 +0200

    if project cost object is inherited ignore type and id

--- plugins/cost_control/lib/cost_control/driver/fog.rb
+++ plugins/cost_control/lib/cost_control/driver/fog.rb
@@ -10,26 +10,34 @@ module CostControl
           begin
             masterdata = fog.get_project_masterdata(project_id).body['masterdata']
           rescue ::Fog::Billing::OpenStack::NotFound
-            return { 'id' => project_id }
+            return {'id' => project_id}
           end
           cost_object = masterdata.fetch('cost_object', {})
           {
-            'id'               => project_id,
-            'cost_object_type' => cost_object.fetch('type', ''),
-            'cost_object_id'   => cost_object.fetch('name', ''),
-            'cost_object_inherited' => cost_object.fetch('inherited', '')
+              'id'                    => project_id,
+              'cost_object_type'      => cost_object.fetch('type', ''),
+              'cost_object_id'        => cost_object.fetch('name', ''),
+              'cost_object_inherited' => cost_object.fetch('inherited', '')
           }
         end
       end
 
       def update_project_masterdata(project_id, params={})
+        if params.fetch('cost_object_inherited', '') == true
+          cost_object = {
+              'inherited' => params.fetch('cost_object_inherited', '')
+          }
+        else
+          cost_object = {
+              'type'      => params.fetch('cost_object_type', ''),
+              'name'      => params.fetch('cost_object_id', ''),
+              'inherited' => params.fetch('cost_object_inherited', '')
+          }
+
+        end
         handle_response do
           masterdata = {
-            'cost_object' => {
-              'type' => params['cost_object_type'],
-              'name' => params['cost_object_id'],
-              'inherited' => params['cost_object_inherited']
-            },
+              'cost_object' => cost_object
           }
           fog.put_project_masterdata(project_id, masterdata)
           params
@@ -41,13 +49,13 @@ module CostControl
           begin
             masterdata = fog.get_domain_masterdata(domain_id).body['masterdata']
           rescue ::Fog::Billing::OpenStack::NotFound
-            return { 'id' => domain_id }
+            return {'id' => domain_id}
           end
           cost_object = masterdata.fetch('cost_object', {})
           {
-              'id'               => domain_id,
-              'cost_object_type' => cost_object.fetch('type', ''),
-              'cost_object_id'   => cost_object.fetch('name', ''),
+              'id'                                => domain_id,
+              'cost_object_type'                  => cost_object.fetch('type', ''),
+              'cost_object_id'                    => cost_object.fetch('name', ''),
               'cost_object_responsibleController' => cost_object.fetch('responsibleController', '')
           }
         end
@@ -57,9 +65,9 @@ module CostControl
         handle_response do
           masterdata = {
               'cost_object' => {
-                  'type' => params['cost_object_type'],
-                  'name' => params['cost_object_id'],
-                  'inherited' => params['cost_object_responsibleController']
+                  'type'      => params.fetch('cost_object_type', ''),
+                  'name'      => params.fetch('cost_object_id', ''),
+                  'inherited' => params.fetch('cost_object_responsibleController', '')
               },
           }
           fog.put_domain_masterdata(domain_id, masterdata)
@@ -71,8 +79,8 @@ module CostControl
 
       def fog
         @fog ||= ::Fog::Billing::OpenStack.new(
-          # this service has only one (public) endpoint
-          auth_params.merge(openstack_endpoint_type: 'publicURL'),
+            # this service has only one (public) endpoint
+            auth_params.merge(openstack_endpoint_type: 'publicURL'),
         )
       end
 
