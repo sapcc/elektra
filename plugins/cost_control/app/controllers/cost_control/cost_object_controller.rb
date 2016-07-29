36d2ae61d286cef3e5d98a9667e406f5491bc08a
Arno Uhlig <arno.uhlig@sap.com>
2016-07-29 15:27:47 +0200

    try once again after failure

--- plugins/cost_control/app/controllers/cost_control/cost_object_controller.rb
+++ plugins/cost_control/app/controllers/cost_control/cost_object_controller.rb
@@ -4,6 +4,8 @@ module CostControl
 
     before_filter :load_masterdata
 
+    @@retries=0
+
     def show
       if @scoped_project_id
         render action: 'project_show'
@@ -32,7 +34,18 @@ module CostControl
 
       if @masterdata.update_attributes(attrs)
         redirect_to plugin('cost_control').cost_object_path
+        return
       else
+        ## temp. workaround try again if it failed before
+        if @@retries < 2
+          @@retries+=1
+          @masterdata.update_attributes(attrs)
+          redirect_to plugin('cost_control').cost_object_path
+          return
+        else
+          @@retries=0
+        end
+
         if @scoped_project_id
           render action: 'project_edit'
         else
@@ -46,8 +59,16 @@ module CostControl
     def load_masterdata
       if @scoped_project_id
         @masterdata = services.cost_control.find_project_masterdata(@scoped_project_id)
+        ## "temp. workaround": try once again if it failed before
+        if @masterdata.attributes.empty?
+          @masterdata = services.cost_control.find_project_masterdata(@scoped_project_id)
+        end
       else
         @masterdata = services.cost_control.find_domain_masterdata(@scoped_domain_id)
+        ## "temp. workaround": try once again if it failed before
+        if @masterdata.attributes.empty?
+          @masterdata = services.cost_control.find_project_masterdata(@scoped_project_id)
+        end
       end
     end
 
