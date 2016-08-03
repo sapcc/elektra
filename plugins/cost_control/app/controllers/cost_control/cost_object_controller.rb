c5c01875908b7cd985f080fca1fa2841b0203586
Stefan Majewsky <stefan.majewsky@sap.com>
2016-08-03 15:02:31 +0200

    remove temporary workarounds that now break stuff

--- plugins/cost_control/app/controllers/cost_control/cost_object_controller.rb
+++ plugins/cost_control/app/controllers/cost_control/cost_object_controller.rb
@@ -4,8 +4,6 @@ module CostControl
 
     before_filter :load_masterdata
 
-    @@retries=0
-
     def show
       if @scoped_project_id
         render action: 'project_show'
@@ -36,16 +34,6 @@ module CostControl
         redirect_to plugin('cost_control').cost_object_path
         return
       else
-        ## temp. workaround try again if it failed before
-        if @@retries < 2
-          @@retries+=1
-          @masterdata.update_attributes(attrs)
-          redirect_to plugin('cost_control').cost_object_path
-          return
-        else
-          @@retries=0
-        end
-
         if @scoped_project_id
           render action: 'project_edit'
         else
@@ -59,16 +47,8 @@ module CostControl
     def load_masterdata
       if @scoped_project_id
         @masterdata = services.cost_control.find_project_masterdata(@scoped_project_id)
-        ## "temp. workaround": try once again if it failed before
-        if @masterdata.attributes.empty?
-          @masterdata = services.cost_control.find_project_masterdata(@scoped_project_id)
-        end
       else
         @masterdata = services.cost_control.find_domain_masterdata(@scoped_domain_id)
-        ## "temp. workaround": try once again if it failed before
-        if @masterdata.attributes.empty?
-          @masterdata = services.cost_control.find_project_masterdata(@scoped_project_id)
-        end
       end
     end
 
