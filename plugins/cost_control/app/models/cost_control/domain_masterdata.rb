8e93e16e88a0e332aee8c35d3a2d33c1c0861d82
Arno Uhlig <arno.uhlig@sap.com>
2016-08-04 17:40:59 +0200

    responsible controller can only be D or I user

--- plugins/cost_control/app/models/cost_control/domain_masterdata.rb
+++ plugins/cost_control/app/models/cost_control/domain_masterdata.rb
@@ -15,8 +15,8 @@ module CostControl
     }
 
     validates_presence_of :cost_object_type, :cost_object_id, :cost_object_responsibleController
-    validates :cost_object_responsibleController, format: { with: /\A[DdIi]\d{6}|[Cc]\d{7}\z/ }
-    # TODO: validate values
+    validates :cost_object_responsibleController, format: { with: /\A[DdIi]\d{6}\z/ }
+    # TODO: validate cost_object_id
 
     def readable_cost_object_type
       type = cost_object_type
