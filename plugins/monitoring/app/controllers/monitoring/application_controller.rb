c336ac28f4adfa9c9dcac67b181bf4027effd24e
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2016-08-29 14:31:36 +0000

    adjust error handling for monasca api changes

--- plugins/monitoring/app/controllers/monitoring/application_controller.rb
+++ plugins/monitoring/app/controllers/monitoring/application_controller.rb
@@ -7,8 +7,17 @@ module Monitoring
     rescue_from Excon::Errors::HTTPStatusError do |exception|
       # get exception message
       response = JSON.parse(exception.response.body)
-      reason = response.keys[0] || ""
-      @exception_msg = "#{reason.capitalize} - #{response[reason]['message']}"
+
+      # monasca api error handling
+      if response['title'] && response['description']
+        title = response['title'] || ""
+        description = response['description'] || ""
+        @exception_msg = "#{title} - Reason: #{description}"
+      else
+        # other
+        reason = response.keys[0] || ""
+        @exception_msg = "#{reason.capitalize} - #{response[reason]['message']}"
+      end
       
       render template: '/monitoring/application/backend_error'
     end
