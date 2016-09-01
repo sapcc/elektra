0c743259ecd4cd33ca70969fda15d817e320bba5
Fabian Ruff <fabian.ruff@sap.com>
2016-09-01 13:34:46 +0200

    Add sentry breadcrumbs for excon http requests

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,4 +3,41 @@ Raven.configure do |config|
   config.http_adapter = :httpclient
   config.send_modules = false
   config.app_dirs_pattern = /(app|bin|config|lib|plugins|spec)/
+  config.timeout = 3
 end
+
+module Excon
+  module Sentry 
+    class Middleware < Excon::Middleware::Base
+
+      def response_call(datum)
+        add_crumb(datum)
+        @stack.response_call(datum)
+      end
+
+      def error_call(datum)
+        add_crumb(datum)
+        @stack.error_call(datum)
+      end
+
+      private
+
+      def add_crumb(datum)
+        Raven.breadcrumbs.record do |crumb|
+          crumb.type = 'http'
+          crumb.category = 'excon'
+          data = {
+            url: Excon::Utils.request_uri(datum),
+            method: datum[:method].to_s.upcase
+          }
+          data[:status_code] = datum[:response][:status] if datum[:response]
+          crumb.data = data
+        end
+      end
+
+    end
+  end
+end
+
+Excon.defaults[:middlewares].push Excon::Sentry::Middleware
+
