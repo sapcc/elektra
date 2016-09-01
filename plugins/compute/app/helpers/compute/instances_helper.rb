7a803ea896e5427c9c606960876ae603bf5c12a1
Andreas Pfau <andreas.pfau@sap.com>
2016-09-01 17:09:13 +0200

    add subgroups for images in instance create dialog

--- plugins/compute/app/helpers/compute/instances_helper.rb
+++ plugins/compute/app/helpers/compute/instances_helper.rb
@@ -1,21 +1,43 @@
 module Compute
   module InstancesHelper
     
+    # def grouped_images(images)
+    #   public_images = []
+    #   private_images = []
+    #   images.each do |image|
+    #     if image.visibility=='public'
+    #       public_images << image
+    #     elsif image.visibility=='private'
+    #       private_images << image
+    #     end
+    #   end
+    #   [ ['Public', public_images], ['Private', private_images] ]
+    # end
+    
     def grouped_images(images)
-      public_images = []
-      private_images = []
+      public_images = {}
+      private_images = {}
       images.each do |image|
         if image.visibility=='public'
-          public_images << image
+          type = (image.hypervisor_type || 'unknown')
+          public_images[type] ||= []
+          public_images[type] << image
         elsif image.visibility=='private'
-          private_images << image
+          type = (image.hypervisor_type || 'unknown')
+          private_images[type] ||= []
+          private_images[type] << image
         end
       end
-      [ ['Public', public_images], ['Private', private_images] ]
+      result = []
+      result << ['Public',public_images.delete('unknown') ||[]]
+      public_images.each{|hypervisor,images| result << ["--#{hypervisor}",images]}
+      result << ['Private',private_images.delete('unknown') || []]
+      private_images.each{|hypervisor,images| result << ["--#{hypervisor}",images]}
+      result
     end
     
     def image_label_for_select(image)
-      label = "#{image.name}  (Size: #{byte_to_human(image.size)}, Format: #{image.disk_format})"
+      label = "#{image.name} (Size: #{byte_to_human(image.size)}, Format: #{image.disk_format})"
       label += ". Project: #{project_name(image.owner)}" if image.visibility=='private'
       label
     end
