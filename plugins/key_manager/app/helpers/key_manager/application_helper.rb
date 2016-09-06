6251dc26750148dc1cb54284d4e56c0e0139d7e6
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2016-09-06 09:57:37 +0200

    [keymanager-plugin]: fog from master

--- plugins/key_manager/app/helpers/key_manager/application_helper.rb
+++ plugins/key_manager/app/helpers/key_manager/application_helper.rb
@@ -1,6 +1,12 @@
 module KeyManager
   module ApplicationHelper
 
+    def date_humanize(date)
+      unless date.nil?
+        " #{date}".to_time(:local).strftime('%Y-%m-%d %H:%M:%S %Z')
+      end
+    end
+
     def secret_status(status)
       state_class = 'state_success'
       if status != ::KeyManager::Secret::Status::ACTIVE
