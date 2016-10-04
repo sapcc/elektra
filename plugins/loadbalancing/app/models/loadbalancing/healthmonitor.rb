cc1ee59cf73fa1ff5cc0cf8190d6a63a43489d01
Torsten Lesmann <torsten.lesmann@sap.com>
2016-10-04 15:52:01 +0200

    Healthmonitor timeout validations

--- plugins/loadbalancing/app/models/loadbalancing/healthmonitor.rb
+++ plugins/loadbalancing/app/models/loadbalancing/healthmonitor.rb
@@ -5,7 +5,17 @@ module Loadbalancing
     METHODS= ['GET', 'HEAD']
 
     validates :type, presence: true
-    validates :delay, presence: true
-    validates :max_retries, presence: true
+    validates :delay, presence: true, numericality: { greater_than: 0}
+    validates :max_retries, presence: true, inclusion: { in: '1'..'10', message: 'Choose a timeout between 1 and 10'  }
+    validate :timeoutvalue
+
+    def timeoutvalue
+      if self.timeout.to_i <= 0
+        errors.add(:timeout, 'Please enter a timeout greater 0')
+      elsif self.timeout.to_i > delay.to_i
+        errors.add(:timeout, 'Please enter a timeout less than delay value')
+      end
+    end
+
   end
 end
