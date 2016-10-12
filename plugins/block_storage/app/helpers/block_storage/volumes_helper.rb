64f60a5eb015fc5918406915705c6158880e9c90
Maurice Schreiber <maurice.schreiber@sap.com>
2016-10-12 16:56:24 +0200

    fixes https://sentry.eu-de-1.cloud.sap/monsoon/elektra/issues/749

--- plugins/block_storage/app/helpers/block_storage/volumes_helper.rb
+++ plugins/block_storage/app/helpers/block_storage/volumes_helper.rb
@@ -1,16 +1,13 @@
 module BlockStorage
   module VolumesHelper
-
-    def get_attachments volume, servers
+    def get_attachments(volume, servers)
       attachments = []
-      if volume.status == "in-use"
+      if volume.status == 'in-use'
         volume.attachments.each do |att|
-          unless servers
-            servers = services.compute.servers()
-          end
-          serverId = att['server_id'] || att['serverId']
-          server = servers.select { |s| s.id == serverId }
-          attachments << {device: att['device'], server_id: serverId, server_name: server.first.name}
+          servers = services.compute.servers() unless servers
+          server_id = att['server_id'] || att['serverId']
+          server = servers.find { |s| s.id == server_id }
+          attachments << { device: att['device'], server_id: server_id, server_name: server ? server.name : nil }
         end
       end
       return attachments
