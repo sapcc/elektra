cb8e9f2886f117fcee6fcb05b88832223e931d1c
Esther Schmitz <esther.schmitz@sap.com>
2016-11-03 11:13:53 +0100

    Add explanation for release_state

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -134,6 +134,13 @@ class DashboardController < ::ScopeController
 
   helper_method :release_state
 
+  # Overwrite this method in your controller if you want to set the release state of your plugin to a different value. A tag will be displayed in the main toolbar next to the page header
+  # DON'T OVERWRITE THE VALUE HERE IN THE DASHBOARD CONTROLLER
+  # Possible values:
+  # "public_release" (default)
+  # "experimental" (for plugins that barely work or don't work at all)
+  # "tech_preview"
+  # "beta" (if it's almost ready for public release)
   def release_state
     "public_release"
   end
