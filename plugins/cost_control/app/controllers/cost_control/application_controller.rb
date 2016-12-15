8427c8945d7ab50429d0ef25c19ef67a9511171a
Arno Uhlig <arno.uhlig@sap.com>
2016-12-15 15:42:19 +0100

    final touches

--- plugins/cost_control/app/controllers/cost_control/application_controller.rb
+++ plugins/cost_control/app/controllers/cost_control/application_controller.rb
@@ -1,19 +1,19 @@
+require 'fog/billing/openstack/models/error'
+
 module CostControl
   class ApplicationController < DashboardController
     # This is the base class of all controllers in this plugin. Only put code in here that is shared across controllers.
     authorization_context 'cost_control'
 
     rescue_and_render_exception_page [
-                                     {
-                                         "::Fog::Billing::ApiError" => {
-                                             header_title: "Monsoon3 Cost Control",
-                                             title:        "No Billing data available.",
-                                             description:  :description,
-                                             details:      -> e, c { e.details.html_safe }
+                                         {
+                                             "Fog::Billing::ApiError" => {
+                                                 header_title: "Monsoon3 Cost Control",
+                                                 title: "No billing data available.",
+                                                 detail:      :detail
+                                             }
                                          }
-                                     }
-                                 ]
-
+                                     ]
   end
 
   protected
