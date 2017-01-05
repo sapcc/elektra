0e153dd545d2e9b22fc0a23e4fde84ebb0026f7e
Maurice Schreiber <maurice.schreiber@sap.com>
2017-01-05 15:44:26 +0100

    technical user policy: make sure that domain_id is in token for admin role

--- plugins/identity/app/controllers/identity/domains/technical_users_controller.rb
+++ plugins/identity/app/controllers/identity/domains/technical_users_controller.rb
@@ -5,16 +5,16 @@ module Identity
     class TechnicalUsersController < ::DashboardController
 
       authorization_required only: [:index, :new, :create]
-      
+
       def index
         services.identity.users(domain_id:@scoped_domain_id, "name__startswith" => 'T' ).length
         @technical_users = services.identity.users(domain_id:@scoped_domain_id, "name__startswith" => 'T' ).select{|user| user.name.start_with?("T")}
       end
-      
+
       def new
         @technical_user = services.identity.new_user(description: "Created by #{current_user.name}. Purpose: ")
       end
-      
+
       def create
         attributes = {
           name: "T#{SecureRandom.hex(8).upcase}",
@@ -32,16 +32,16 @@ module Identity
           render action: :new
         end
       end
-      
+
       def edit
         @technical_user = services.identity.find_user(params[:id])
         enforce_permissions("identity:technical_user_update", user: @technical_user)
       end
-      
+
       def update
         @technical_user = services.identity.find_user(params[:id])
         enforce_permissions("identity:technical_user_update", user: @technical_user)
-        
+
         @technical_user.description = params[:user][:description]
         if @technical_user.save
           audit_logger.info(current_user, "has updated technical user #{@technical_user.name} (#{@technical_user.id})")
@@ -50,12 +50,12 @@ module Identity
           render action: :edit
         end
       end
-      
+
       def reset_password
         @technical_user = services.identity.find_user(params[:id])
         enforce_permissions("identity:technical_user_reset_password", user: @technical_user)
       end
-      
+
       def change_password
         @technical_user = services.identity.find_user(params[:id])
         enforce_permissions("identity:technical_user_reset_password", user: @technical_user)
@@ -69,17 +69,17 @@ module Identity
           render action: :reset_password
         end
       end
-      
+
       def destroy
         @technical_user = services.identity.find_user(params[:id])
         enforce_permissions("identity:technical_user_delete", user: @technical_user)
-        
+
         if @technical_user.destroy
           audit_logger.info(current_user, "has deleted technical user #{@technical_user.name} (#{@technical_user.id})")
           @deleted = true
         else
           @deleted = false
-        end  
+        end
       end
 
       private
