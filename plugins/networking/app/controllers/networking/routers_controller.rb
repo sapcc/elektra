93586aa468e0a4adcc312016fd45baf27f64be74
Andreas Pfau <andreas.pfau@sap.com>
2017-01-06 13:20:16 +0100

    show Availaility zone on all objects dependant on AZ

--- plugins/networking/app/controllers/networking/routers_controller.rb
+++ plugins/networking/app/controllers/networking/routers_controller.rb
@@ -1,9 +1,9 @@
 module Networking
   class RoutersController < DashboardController
-    
+
     def index
       @routers = services.networking.routers(tenant_id:@scoped_project_id)
-      
+
       usage = @routers.length
       @quota_data = services.resource_management.quota_data([
         {service_name: :networking, resource_name: :routers, usage: usage}
@@ -19,9 +19,9 @@ module Networking
         name: @router.name,
         type: 'router',
         id: @router.id,
-        children: [{name: @external_network.name, type: 'gateway', id: @external_network.id}] + @router_interface_ports.collect do |port| 
+        children: [{name: @external_network.name, type: 'gateway', id: @external_network.id}] + @router_interface_ports.collect do |port|
           node = {name: port.network_object.name, type: 'network', id: port.network_object.id}
-          services.networking.ports(network_id: port.network_id).each do |port| 
+          services.networking.ports(network_id: port.network_id).each do |port|
             if port.device_owner.start_with?('compute:')
               node[:children] ||= []
               node[:children] << {name: '', type: 'server', id: port.device_id}
@@ -31,7 +31,7 @@ module Networking
         end
       }
     end
-    
+
     def node_details
       case params[:type]
       when 'router'
@@ -46,9 +46,9 @@ module Networking
         render partial: 'networking/routers/node_details/server', locals: {server: server, port: port}, status: server.nil? ? 404 : 200
       else
         render text: 'No details available'
-      end          
+      end
     end
-    
+
     def show
       @router = services.networking.find_router(params[:id])
       @external_network = services.networking.network(@router.external_gateway_info["network_id"])
@@ -59,11 +59,11 @@ module Networking
       @quota_data = services.resource_management.quota_data([
         {service_name: :networking, resource_name: :routers}
       ])
-      
+
       # build new router object (no api call done yet!)
-      @router = services.networking.new_router("admin_state_up" => true)      
-      
-      networks = available_networks    
+      @router = services.networking.new_router("admin_state_up" => true)
+
+      networks = available_networks
       @external_networks = networks[:external_networks]
       @internal_subnets = networks[:internal_subnets]
     end
@@ -78,12 +78,12 @@ module Networking
         # router is created -> add subnets as interfaces
         services.networking.add_router_interfaces(@router.id,@selected_internal_subnets)
         audit_logger.info(current_user, "has created", @router)
-        
+
         flash.now[:notice] = "Router successfully created."
         redirect_to plugin('networking').routers_path
       else
         # didn't save -> render new
-        networks = available_networks    
+        networks = available_networks
         @external_networks = networks[:external_networks]
         @internal_subnets = networks[:internal_subnets]
         render action: :new
@@ -93,14 +93,14 @@ module Networking
     def edit
       @router = services.networking.find_router(params[:id])
       @external_network = services.networking.network(@router.external_gateway_info["network_id"])
-      
+
       # load all project networks
       project_networks = services.networking.project_networks(@scoped_project_id)
-      
+
       @external_networks = []
       @internal_subnets = []
-      
-      project_networks.each do |network| 
+
+      project_networks.each do |network|
         if network.attributes["router:external"]==true
           @external_networks << network
         else
@@ -110,7 +110,7 @@ module Networking
           end
         end
       end
-      
+
       attached_ports = services.networking.ports(device_id: @router.id, device_owner:'network:router_interface')
       @selected_internal_subnet_ids = attached_ports.inject([]){|array,port| port.fixed_ips.each{|ip| array << ip["subnet_id"]}; array}
     end
@@ -127,26 +127,26 @@ module Networking
       if @router.save
         attached_ports = services.networking.ports(device_id: @router.id, device_owner:'network:router_interface')
         @old_selected_internal_subnet_ids = attached_ports.inject([]){|array,port| port.fixed_ips.each{|ip| array << ip["subnet_id"]}; array}
-        
+
         to_be_detached = (@old_selected_internal_subnet_ids-@selected_internal_subnet_ids)
         to_be_attached = (@selected_internal_subnet_ids-@old_selected_internal_subnet_ids)
 
         services.networking.remove_router_interfaces(@router.id,to_be_detached)
         services.networking.add_router_interfaces(@router.id,to_be_attached)
         audit_logger.info(current_user, "has updated", @router)
-        
+
         flash.now[:notice] = "Router successfully created."
         redirect_to plugin('networking').routers_path
       else
         @external_network = services.networking.network(@router.external_gateway_info["network_id"])
-      
+
         # load all project networks
         project_networks = services.networking.project_networks(@scoped_project_id)
-      
+
         @external_networks = []
         @internal_subnets = []
-      
-        project_networks.each do |network| 
+
+        project_networks.each do |network|
           if network.attributes["router:external"]==true
             @external_networks << network
           else
@@ -163,11 +163,11 @@ module Networking
     def destroy
       @router = services.networking.find_router(params[:id]) rescue nil
       ports = services.networking.ports(device_owner:'network:router_interface',device_id: @router.id)
-      
+
       @success = false
       if @router
         attached_subnet_ids = (ports || []).inject([]){|array,port| port.fixed_ips.each{|ip| array << ip["subnet_id"]}; array}
-        
+
         services.networking.remove_router_interfaces(@router.id,attached_subnet_ids)
         if @router.destroy
           @success = true
@@ -183,7 +183,7 @@ module Networking
         format.html {redirect_to routers_path}
       end
     end
-    
+
     protected
     def attached_subnet_ids
       # get all router attached ports
@@ -191,15 +191,15 @@ module Networking
       # get subnet ids
       @attached_subnet_ids ||= (ports || []).inject([]){|array,port| port.fixed_ips.each{|ip| array << ip["subnet_id"]}; array}
     end
-    
+
     def available_networks
       # load all project networks
       project_networks = services.networking.project_networks(@scoped_project_id)
       # get external and internal networks
       return @available_networks if @available_networks
-      
+
       @available_networks = { external_networks: [], internal_subnets: [] }
-      project_networks.each do |network| 
+      project_networks.each do |network|
         if network.attributes["router:external"]==true
           @available_networks[:external_networks] << network
         else
@@ -211,6 +211,6 @@ module Networking
       end
       @available_networks
     end
-    
+
   end
 end
