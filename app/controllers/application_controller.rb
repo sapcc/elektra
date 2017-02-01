56172f412ab1986676db47ef926b4b97f9e4bb8d
Maurice Schreiber <maurice.schreiber@sap.com>
2017-02-01 14:03:21 +0100

    [core] add flag to show full exception in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # This module adds two methods for exception handling.
-# With rescue_and_render_exception_page it is possible to catch 
-# exceptions and display a well designed exception page. 
-# The render_exception_page method implements the displaying of an exception. 
+# With rescue_and_render_exception_page it is possible to catch
+# exceptions and display a well designed exception page.
+# The render_exception_page method implements the displaying of an exception.
 module ErrorRenderer
   # add instance and class methods after this module was included or extended.
   def self.included(base)
@@ -12,24 +12,25 @@ module ErrorRenderer
     base.send("extend",ClassMethods)
     base.send("include", InstanceMethods)
   end
-  
+
   # instance methods
   module InstanceMethods
     def render_exception_page(exception,map={})
+      raise exception if Rails.env.development? && ENV.key?('NO_EXCEPTION_PAGE')
       value = lambda do |param|
         v = map[param.to_sym] || map[param.to_s]
-        return nil if v.nil?    
+        return nil if v.nil?
         return exception.send(v).to_s if v.kind_of?(Symbol)
         return v.call(exception,self).to_s if v.kind_of?(Proc)
         return v.to_s
       end
-    
+
       begin
         @title = value.call(:title) || exception.class.name.split('::').last.humanize
         @description = value.call(:description) || (exception.message rescue exception.to_s)
         @details = value.call(:details) || exception.class.name+"\n"+(exception.backtrace rescue '').join("\n")
         @exception_id = value.call(:exception_id) || request.uuid
-        @warning = value.call(:warning) || false 
+        @warning = value.call(:warning) || false
         @status = value.call(:status) || (exception.respond_to?(:status)? exception.status: 503)
       rescue => e
         @title = e.class.name.split('::').last.humanize
@@ -38,13 +39,13 @@ module ErrorRenderer
         @exception_id = request.uuid
         @warning = false
         @status = 503
-      end  
-    
+      end
+
       status = @status
 
       unless @warning
         logger.error(@exception_id+": "+@title+". "+@description)
-      
+
         unless map[:sentry]==false
           Raven::Rack.capture_exception(exception, env)
           @exception_id = Raven.last_event_id if Raven.last_event_id
@@ -52,14 +53,14 @@ module ErrorRenderer
         @sentry_event_id   = Raven.last_event_id
         @sentry_public_dsn = URI.parse(ENV['SENTRY_DSN']).tap {|u| u.password=nil}.to_s if ENV['SENTRY_DSN']
         @sentry_user       = { name: current_user.full_name || current_user.name, email: current_user.email } if current_user
-        
+
         if request.xhr? && params[:polling_service]
           render "/application/exceptions/error_polling.js", format: "JS"
         else
           respond_to do |format|
             format.html { render '/application/exceptions/error.html' }
             format.js { render "/application/exceptions/error.js" }
-            format.json {render json: {error: @description}, status: @status} 
+            format.json {render json: {error: @description}, status: @status}
           end
         end
       else
@@ -69,26 +70,26 @@ module ErrorRenderer
           respond_to do |format|
             format.html { render '/application/exceptions/warning.html' }
             format.js { render "/application/exceptions/warning.js" }
-            format.json { render json: {error: @description}, status: @status } 
+            format.json { render json: {error: @description}, status: @status }
           end
         end
       end
     end
   end
-  
+
   # class methods
   module ClassMethods
     # This method is used in controllers to catch errors and render error page.
-    # It differentiates between html, modal, js and polling errors. 
+    # It differentiates between html, modal, js and polling errors.
     # Request vom Polling service which end up in error will display Flash-Errors.
     # GET-Requests for modal content will show errors inside modal window.
-    # JS-POST requests (e.g.: remote: true) will display error dialog. 
+    # JS-POST requests (e.g.: remote: true) will display error dialog.
     def rescue_and_render_exception_page(*exception_classes)
       exception_classes = exception_classes.first if exception_classes.first.is_a?(Array)
       exception_mapping = {}
       klasses = []
-    
-      exception_classes.each do |exception_class| 
+
+      exception_classes.each do |exception_class|
         if exception_class.is_a?(Hash)
           exception_mapping[exception_class.keys.first.to_s]=exception_class.values.first
           klasses << exception_class.keys.first.to_s
@@ -96,22 +97,22 @@ module ErrorRenderer
           klasses << exception_class
         end
       end
-      
+
       rescue_from *klasses do |exception|
         map = exception_mapping[exception.class.name]
         unless map
           klass = nil
-          exception_mapping.each do |class_name,mapping| 
+          exception_mapping.each do |class_name,mapping|
             found_class = eval(class_name)
             next if klass and found_class > klass
 
             if found_class>exception.class
-              map = mapping 
+              map = mapping
             end
           end
           map ||= {}
         end
-        
+
         render_exception_page(exception,map)
       end
     end
@@ -124,27 +125,27 @@ require 'core/audit_logger'
 class ApplicationController < ActionController::Base
   layout 'application'
   include ApplicationHelper
-  
+
   # includes services method
   # use: services.SERVICE_NAME.METHOD_NAME (e.g. services.identity.auth_projects)
-  include Services  
+  include Services
   include ServiceUser
   include CurrentUserWrapper
   include Core::Paginatable
-  
+
   extend ErrorRenderer
-      
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   helper_method :modal?, :plugin_name
-  
+
   # check if the requested domain is the same as that of the current user.
   before_filter :same_domain_check
-  
+
   # catch all api errors and render exception page
-  rescue_and_render_exception_page [ 
+  rescue_and_render_exception_page [
     { "Excon::Error" => { title: 'Backend Service Error' }},
     { "Fog::OpenStack::Errors::ServiceError" => { title: 'Backend Service Error' }}
   ]
@@ -177,9 +178,9 @@ class ApplicationController < ActionController::Base
     end
     @plugin_name
   end
-  
+
   protected
-  
+
   def same_domain_check
     if current_user and current_user.user_domain_id and service_user and service_user.domain_id
       if current_user.user_domain_id!=service_user.domain_id
