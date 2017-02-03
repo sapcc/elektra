2fbe3f665f116950a1b1cd48ad72a225f15f586a
Maurice Schreiber <maurice.schreiber@sap.com>
2017-02-03 15:44:42 +0100

    [networking] sg rule port display
    remove redundant self
    fix return of unlabeled port

--- plugins/networking/app/models/networking/security_group_rule.rb
+++ plugins/networking/app/models/networking/security_group_rule.rb
@@ -5,47 +5,46 @@ module Networking
     DESCRIPTIONS = RULE_TYPES[:descriptions]
     PREDEFINED_RULE_TYPES = RULE_TYPES[:predefined_types]
     PROTOCOLS = RULE_TYPES[:protocols]
-    
+
     PROTOCOL_LABELS = PROTOCOLS.inject({}){|hash,(protocol_name,label)| hash[label]=protocol_name; hash}
     TYPE_LABELS = PREDEFINED_RULE_TYPES.inject({}){|hash,(rule_name,rule)| hash[rule['label']]=rule_name; hash}
-    
+
     PORT_RANGE_RULE = PREDEFINED_RULE_TYPES.inject({}){|hash,(rule_name,rule)| hash[rule['port_range'].to_s]= rule; hash }
-    
+
     def port_range
       if port_range_min.blank? && port_range_max.blank?
         nil
-      elsif port_range_min.blank? 
+      elsif port_range_min.blank?
         port_range_max
       elsif port_range_max.blank?
         port_range_min
-      else          
+      else
         "#{port_range_min}-#{port_range_max}"
-      end          
+      end
     end
-    
+
     def display_port
-      port = if self.port_range_min.blank? and self.port_range_max.blank? 
-        nil
-      elsif self.port_range_min.blank? and !self.port_range_max.blank?
-        self.port_range_max
-      elsif !self.port_range_min.blank? and self.port_range_max.blank?
-        self.port_range_min
-      elsif self.port_range_min==self.port_range_max
-        self.port_range_min
-      else
-        "#{self.port_range_min}-#{self.port_range_max}"
-      end
-      
-      if port 
-        rule = PORT_RANGE_RULE[port.to_s] 
-        port = port.to_s
-        port += " (#{rule['label']})" if rule 
-      else
-        "Any"
-      end
+      port = if port_range_min.blank? && port_range_max.blank?
+               nil
+             elsif port_range_min.blank? && !port_range_max.blank?
+               port_range_max
+             elsif !port_range_min.blank? && port_range_max.blank?
+               port_range_min
+             elsif port_range_min == port_range_max
+               port_range_min
+             else
+               "#{port_range_min}-#{port_range_max}"
+             end
+
+      return 'Any' unless port
+
+      rule = PORT_RANGE_RULE[port.to_s]
+      port = port.to_s
+      port += " (#{rule['label']})" if rule
+      port
     end
-    
-    def to_s(security_groups=[])      
+
+    def to_s(security_groups=[])
       result = "ALLOW #{self.ethertype} #{self.display_port} #{self.direction=='ingress' ? 'from' : 'to'} "
       result += if self.remote_ip_prefix.blank? and self.remote_group_id.blank?
         if (self.ethertype || '').downcase=='ipv4'
@@ -55,7 +54,7 @@ module Networking
         end
       elsif self.remote_ip_prefix.blank?
         sg = catch :found do
-          security_groups.each do |sg| 
+          security_groups.each do |sg|
             throw :found, sg if sg.id==self.remote_group_id
           end
           nil
