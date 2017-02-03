532ad2b2b844ca2185a137ebfdaeca1ac60e5970
Maurice Schreiber <maurice.schreiber@sap.com>
2017-02-03 16:21:45 +0100

    [networking] fix sg rule icmp empty == any, quota data needs symbols

--- plugins/networking/app/controllers/networking/security_groups/rules_controller.rb
+++ plugins/networking/app/controllers/networking/security_groups/rules_controller.rb
@@ -3,12 +3,12 @@ require 'ipaddr'
 module Networking
   module SecurityGroups
     class RulesController < ::DashboardController
-      
+
       def new
         @security_groups = services.networking.security_groups(tenant_id: @scoped_project_id)
         @security_group = @security_groups.select{|sg| sg.id==params[:security_group_id]}.first
         @rule_type = Networking::SecurityGroupRule::PREDEFINED_RULE_TYPES.keys.first
-        
+
         rule_template = Networking::SecurityGroupRule::PREDEFINED_RULE_TYPES[@rule_type]
         range = (rule_template['port_range'] || '').split('-')
         attributes = {
@@ -18,39 +18,39 @@ module Networking
           port_range_max: range.last,
           remote_ip_prefix: '0.0.0.0/0'
         }
-        @rule = services.networking.new_security_group_rule(attributes)  
-        
+        @rule = services.networking.new_security_group_rule(attributes)
+
         @quota_data = services.resource_management.quota_data([
           {service_name: :networking, resource_name: :security_group_rules}
         ])
       end
-      
+
       def create
         rule_params = params[:security_group_rule]
         @rule_type = rule_params.delete(:type)
-        
+
         unless rule_params.blank?
           attributes = {
             protocol: rule_params[:protocol],
             direction: rule_params[:direction],
             security_group_id: params[:security_group_id]
           }
-          
-          if rule_params[:protocol]=='icmp'
-            attributes[:port_range_min]=rule_params[:icmp_type]
-            attributes[:port_range_max]=rule_params[:icmp_code]
+
+          if rule_params[:protocol] == 'icmp'
+            attributes[:port_range_min] = rule_params[:icmp_type] unless rule_params[:icmp_type].blank?
+            attributes[:port_range_max] = rule_params[:icmp_code] unless rule_params[:icmp_code].blank?
           else
             range = (rule_params[:port_range] || '').split('-')
             attributes[:port_range_min] = range.first
             attributes[:port_range_max] = range.last
           end
-          
-          if rule_params[:remote_source]=='remote_ip_prefix' 
+
+          if rule_params[:remote_source]=='remote_ip_prefix'
             attributes[:remote_ip_prefix]=rule_params[:remote_ip_prefix]
             unless rule_params[:remote_ip_prefix].blank?
               ip = IPAddr.new rule_params[:remote_ip_prefix]
-              if ip.ipv4? 
-                attributes[:ethertype] = 'ipv4' 
+              if ip.ipv4?
+                attributes[:ethertype] = 'ipv4'
               elsif ip.ipv6?
                 attributes[:ethertype] = 'ipv6'
               end
@@ -59,29 +59,29 @@ module Networking
             end
           elsif rule_params[:remote_source]=='remote_group_id'
             attributes[:remote_group_id]=rule_params[:remote_group_id]
-          end 
-          
+          end
+
           @rule = services.networking.new_security_group_rule(attributes)
         end
-        
+
         if @rule && @rule.save
           redirect_to security_group_path(params[:security_group_id])
-        else  
+        else
           @security_groups = services.networking.security_groups(tenant_id: @scoped_project_id)
           @security_group = @security_groups.select{|sg| sg.id==params[:security_group_id]}.first
 
           @quota_data = services.resource_management.quota_data([
-            {service_name: 'networking', resource_name: 'security_group_rules'}
+            {service_name: :networking, resource_name: :security_group_rules}
           ])
           render action: :new
-        end  
-        
+        end
+
       end
-      
+
       def show
-        
+
       end
-      
+
       def destroy
         @rule = services.networking.find_security_group_rule(params[:id]) rescue nil
 
@@ -92,19 +92,19 @@ module Networking
         else
           @error = 'Could not find security group rule.'
         end
-      
+
         respond_to do |format|
-          format.html do 
-            if @error 
+          format.html do
+            if @error
               flash.now[:error] = @error
             else
-              flash.now[:notice] = 'Security Group Rule successfully deleted!'  
+              flash.now[:notice] = 'Security Group Rule successfully deleted!'
             end
-            redirect_to security_group_path(params[:security_group_id]) 
+            redirect_to security_group_path(params[:security_group_id])
           end
           format.js { }
         end
-        
+
       end
     end
   end
