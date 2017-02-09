ca37355996d526e2bc06ad1e8de2932b906fdd41
Maurice Schreiber <maurice.schreiber@sap.com>
2017-02-09 16:38:44 +0100

    [identity] order roles + add manila

--- plugins/identity/config/initializers/constants.rb
+++ plugins/identity/config/initializers/constants.rb
@@ -1,1 +1,14 @@
-ALLOWED_ROLES = %w(admin compute_viewer compute_admin volume_viewer volume_admin member monasca-agent monasca-user monasca-viewer monitoring-delegate swiftoperator).freeze
+ALLOWED_ROLES = %w(
+  admin
+  compute_admin
+  compute_viewer
+  member
+  monasca-agent
+  monasca-user
+  monasca-viewer
+  monitoring-delegate
+  sharedfilesystem_viewer
+  swiftoperator
+  volume_admin
+  volume_viewer
+).freeze
