e1ea64634a5b561aef3651c626c0430134e89771
Andreas Pfau <andreas.pfau@sap.com>
2017-02-13 16:30:30 +0100

    add project profile table

--- plugins/identity/app/helpers/identity/projects_helper.rb
+++ plugins/identity/app/helpers/identity/projects_helper.rb
@@ -22,5 +22,53 @@ module Identity
       t("roles.#{role_name}") + " (#{role_name})"
     end
 
+    def callout_css_class(status)
+      css_class = 'bs-callout-'
+      css_class += if status==ProjectProfile::STATUS_DONE
+        'success'
+      elsif status==ProjectProfile::STATUS_SKIPED
+        'warning'
+      else
+        'info'
+      end
+      css_class
+    end
+
+    def wizard_step(options={},&block)
+      title = options[:title]
+      description = options[:description]
+      mandatory = options[:mandatory] || false
+      status = options[:status]
+      css_class = case status
+      when ProjectProfile::STATUS_DONE then 'success'
+      when ProjectProfile::STATUS_SKIPED then 'warning'
+      else 'info'
+      end
+      action_button = block.call(:action_button)
+      skip_button = block.call(:skip_button)
+
+      byebug
+      content_tag :div, class: "bs-callout bs-callout-emphasize bs-callout-#{css_class}" do
+        content_tag :div, class: 'row' do
+          concat(content_tag(:div, class: 'col-md-8') do
+            concat content_tag(:h4, title)
+            concat content_tag(:p, description)
+          end)
+          concat(content_tag(:div, class: 'col-md-4') do
+            if status==ProjectProfile::STATUS_SKIPED
+              concat(content_tag :span, 'skiped', class: 'pull-right')
+              concat(action_button)
+            elsif status==ProjectProfile::STATUS_DONE
+              content_tag :i, '', class: 'fa fa-check fa-3x pull-right'
+            else
+              content_tag :p, action_button
+              content_tag :p, skip_button
+            end
+          end)
+        end
+      end
+
+    end
+
   end
 end
