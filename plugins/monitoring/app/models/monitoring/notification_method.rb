e592d43939d87968b5ff2e173c9610200e7fa1f5
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-02-14 10:28:51 +0000

    validates url and mail on notification methods

--- plugins/monitoring/app/models/monitoring/notification_method.rb
+++ plugins/monitoring/app/models/monitoring/notification_method.rb
@@ -5,14 +5,14 @@ module Monitoring
     # address
     # name
     # type
-    #
+
     validates_presence_of :name, :address, :type
+    # EMAIL
+    validates :address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, if: Proc.new{|u| u.type == "EMAIL"}
+    # SLACK and WEBHOOCK
+    validates :address, format: { with: URI.regexp }, if: Proc.new{|u| u.type == "SLACK"} ||  Proc.new{|u| u.type == "WEBHOCK"}
     
-    # TODO: do I need address validation here? because the address can be everthing, from email to url
-    # validates :address, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-    # https://github.com/openstack/monasca-api/blob/master/docs/monasca-api-spec.md#request-body-7
-    # validates :address, length: { maximum: 100 }
-    
+    validates :address, length: { maximum: 100 }
     validates :name, length: { maximum: 250 }
     validates :type, length: { maximum: 100 }
 
