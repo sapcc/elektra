b006ab56fcb36899c0fef48793bd9d4d9595d258
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-02-15 12:08:04 +0000

    empty search string if nothing was set

--- plugins/monitoring/app/controllers/monitoring/alarm_definitions_controller.rb
+++ plugins/monitoring/app/controllers/monitoring/alarm_definitions_controller.rb
@@ -18,7 +18,7 @@ module Monitoring
     ]
     
     def index
-      alarm_definition_search = cookies[:alarm_definition_search]
+      alarm_definition_search = cookies[:alarm_definition_search] || ''
       unless alarm_definition_search.empty?
         search
       else
@@ -29,7 +29,7 @@ module Monitoring
     end
 
     def search
-      @search = params[:search] || cookies[:alarm_definition_search]
+      @search = params[:search] || cookies[:alarm_definition_search] || ''
       searched_alarm_definitions = services.monitoring.alarm_definitions(@search)
       @alarm_definitions_count = searched_alarm_definitions.length
       @alarm_definitions = Kaminari.paginate_array(searched_alarm_definitions).page(params[:page]).per(10)
