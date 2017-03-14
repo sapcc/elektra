d266f814151df98240001b13d7e45256d4b36c23
andypf <ap@a-pfau.de>
2017-03-14 10:58:22 +0100

    show ip availability in floating ip allocation dialog (#71)

--- plugins/networking/app/controllers/networking/networks_controller.rb
+++ plugins/networking/app/controllers/networking/networks_controller.rb
@@ -96,7 +96,11 @@ module Networking
     end
 
     def subnets
-      render json: services.networking.subnets(network_id: params[:network_id])
+      #byebug
+      availability = service_user.cloud_admin_service(:networking).network_ip_availability(params[:network_id])
+      # subnets = services.networking.subnets(network_id: params[:network_id])
+      #render json: services.networking.subnets(network_id: params[:network_id])
+      render json: availability.subnet_ip_availability
     end
 
     private
