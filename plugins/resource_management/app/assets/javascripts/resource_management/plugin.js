b6aa153a30619ec09ea71c33a352f3fa6f8ea209
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-03-21 12:48:26 +0000

    show loading progress on sorting

--- plugins/resource_management/app/assets/javascripts/resource_management/plugin.js
+++ plugins/resource_management/app/assets/javascripts/resource_management/plugin.js
@@ -11,3 +11,14 @@
 // about supported directives.
 //
 //= require_tree .
+
+// This is always executed on page load.
+$(document).ready(function(){
+  // show small loading spinner on active tab during ajax calls 
+  $(document).ajaxStart( function() {
+    $('.loading-place').addClass('loading');
+  });
+  $(document).ajaxStop( function() {
+    $('.loading-place').removeClass('loading');
+  });
+}); 
