2b97188ca3442a095af75d51bfb453cc0d630d57
Maurice Schreiber <maurice.schreiber@sap.com>
2017-03-28 09:53:00 +0200

    [compute] allow flavor member modification without project get

--- plugins/compute/app/controllers/compute/flavors/members_controller.rb
+++ plugins/compute/app/controllers/compute/flavors/members_controller.rb
@@ -1,31 +1,35 @@
 module Compute
   module Flavors
     class MembersController < Image::ApplicationController
-    
+
       def index
         @flavor = services.compute.flavor(params[:flavor_id])
         @members = services.compute.flavor_members(params[:flavor_id])
       end
-    
+
       def create
         @member = services.compute.new_flavor_access(params[:member])
         @member.flavor_id = params[:flavor_id]
-        @project = services.identity.find_project(@member.tenant_id.strip) 
 
-        if @project.nil?
-          @member.errors.add(:project,"Could not find project #{@member.tenant_id}")
-        else
-          @member.save
+        # validate if we are allowed to :)
+        if current_user.is_allowed?('identity:project_get')
+          @project = services.identity.find_project(@member.tenant_id.strip)
+          if @project.nil?
+            @member.errors.add(:project, "Could not find project #{@member.tenant_id}")
+            return
+          end
         end
+
+        @member.save
       end
-    
+
       def destroy
         member = services.compute.new_flavor_access
         member.flavor_id = params[:flavor_id]
         member.tenant_id = params[:id]
         @success = member.destroy
       end
-    
+
     end
   end
 end
