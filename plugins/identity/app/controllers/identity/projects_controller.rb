f1fa123110cba1b1d0e9f87be5c058837b706347
Esther Schmitz <esther.schmitz@sap.com>
2017-04-03 14:52:41 +0200

    Disable project delete action

--- plugins/identity/app/controllers/identity/projects_controller.rb
+++ plugins/identity/app/controllers/identity/projects_controller.rb
@@ -60,27 +60,30 @@ module Identity
     end
 
     def destroy
-
-      # first close all open and rejeced requests
-      inquirys = Inquiry::Inquiry.where(:domain_id => @scoped_domain_id, :project_id => @scoped_project_id, :aasm_state => ['open','rejected'])
-      inquirys.each do |inquiry|
-        state_change_result = inquiry.change_state(:closed, 'Closed, because project was deleted!', current_user)
-        unless state_change_result
-          flash[:error] = "Something went wrong when trying to close all open or rejected requests for this project"
-          redirect_to plugin('identity').project_path
-        end
-      end
-
-      # second delete the project itself
-      response = service_user.delete_project(@project_id)
-      if response
-        audit_logger.info(current_user, "has deleted project", @project_id)
-        flash[:notice] = "Project successfully deleted."
-        redirect_to plugin('identity').domain_path(project_id: nil)
-      else
-        flash[:error] = response #"Something when wrong when trying to delete the project"
-        redirect_to plugin('identity').project_path
-      end
+      #### Disable project delete for now
+      #
+      # # first close all open and rejeced requests
+      # inquirys = Inquiry::Inquiry.where(:domain_id => @scoped_domain_id, :project_id => @scoped_project_id, :aasm_state => ['open','rejected'])
+      # inquirys.each do |inquiry|
+      #   state_change_result = inquiry.change_state(:closed, 'Closed, because project was deleted!', current_user)
+      #   unless state_change_result
+      #     flash[:error] = "Something went wrong when trying to close all open or rejected requests for this project"
+      #     redirect_to plugin('identity').project_path
+      #   end
+      # end
+      #
+      # # second delete the project itself
+      # response = service_user.delete_project(@project_id)
+      # if response
+      #   audit_logger.info(current_user, "has deleted project", @project_id)
+      #   flash[:notice] = "Project successfully deleted."
+      #   redirect_to plugin('identity').domain_path(project_id: nil)
+      # else
+      #   flash[:error] = response #"Something when wrong when trying to delete the project"
+      #   redirect_to plugin('identity').project_path
+      # end
+      flash.now[:error] = "Deleting projects is currently not allowed because it creates orphaned dependant objects in backend services."
+      render action: :show
     end
 
     def api_endpoints
