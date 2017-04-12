74ebc4051315080be674f36d720b129144e5ff0f
Andreas Pfau <andreas.pfau@sap.com>
2017-04-12 16:12:31 +0200

    fix error in dns self service

--- plugins/dns_service/app/models/dns_service/zone_request.rb
+++ plugins/dns_service/app/models/dns_service/zone_request.rb
@@ -1,8 +1,12 @@
 module DnsService
   class ZoneRequest < Core::ServiceLayer::Model
     validates :domain_type, presence: {message: 'Please choose a domain type'}
-    validates :domain_pool, presence: {message: 'Please select a domain pool'}
+    validates :domain_pool, presence: {message: 'Please select a domain pool'}, if: :subdomain?
     validates :name, presence: {message: 'Please provide the domain name'}
     validates :email, presence: {message: 'Please provide an email'}
+
+    def subdomain?
+      domain_type=='rootdomain'
+    end
   end
 end
