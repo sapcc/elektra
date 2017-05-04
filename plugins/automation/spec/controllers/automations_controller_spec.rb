5e09dbe39cbfafaef2cbd675e96b4d42adad72cb
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2017-05-04 14:56:33 +0200

    [automation]: fix test

--- plugins/automation/spec/controllers/automations_controller_spec.rb
+++ plugins/automation/spec/controllers/automations_controller_spec.rb
@@ -155,14 +155,14 @@ describe Automation::AutomationsController, type: :controller do
     it "returns http success and renders view" do
       delete :destroy, default_params.merge({id: 'automation_id'})
       expect(response).to be_success
-      expect(response).to render_template(:index)
+      expect(response).to render_template("automation/automations/update_item.js")
     end
 
     it "something wrong happens show a flash error" do
       allow_any_instance_of(ServiceLayer::AutomationService).to receive(:automation).and_raise("boom")
       delete :destroy, default_params.merge({id: 'automation_id'})
       expect(response).to be_success
-      expect(response).to render_template(:index)
+      expect(response).to render_template("automation/automations/update_item.js")
       expect(flash.now[:error]).to_not be_nil
     end
 
