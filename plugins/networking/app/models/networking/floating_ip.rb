55182bb016200993fa7d2bfeb3774b770bd0caa8
Andreas Pfau <andreas.pfau@sap.com>
2017-05-12 16:10:40 +0200

    fix bug in floating ips

--- plugins/networking/app/models/networking/floating_ip.rb
+++ plugins/networking/app/models/networking/floating_ip.rb
@@ -14,7 +14,7 @@ module Networking
       @subnet_object if @subnet_object
 
       subnets = Rails.cache.fetch("network_#{self.floating_network_id}_subnets", expires_in: 1.hours) do
-        services.networking.subnets(network_id: self.floating_network_id)
+        @driver.map_to(Networking::Subnet).subnets(network_id: self.floating_network_id)
       end || []
 
       @subnet_object = subnets.find{|subnet| IPAddr.new(subnet.cidr).include?(self.floating_ip_address) }
