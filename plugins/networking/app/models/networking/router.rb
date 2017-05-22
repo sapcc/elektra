0ef806c60130056a2247a736115c35419e32d99e
Andreas Pfau <andreas.pfau@sap.com>
2017-05-22 14:32:19 +0200

    fix bug in router list UI. Do not show an error on routers without gateway

--- plugins/networking/app/models/networking/router.rb
+++ plugins/networking/app/models/networking/router.rb
@@ -6,21 +6,23 @@ module Networking
     validates :internal_subnets, presence: { message: 'Please select at least one subnet from the private networks' }
 
     def ip_subnet_objects
+      unless @ip_subnet_objects
+        if external_gateway_info
+          if external_gateway_info["external_fixed_ips"]
+            ip_infos = external_gateway_info["external_fixed_ips"]
+            ip_infos = [ip_infos] unless ip_infos.is_a?(Array)
 
-      if external_gateway_info
-        if external_gateway_info["external_fixed_ips"]
-          ip_infos = external_gateway_info["external_fixed_ips"]
-          ip_infos = [ip_infos] unless ip_infos.is_a?(Array)
-
-          ip_infos.inject({}) do |hash,ip_info|
-            subnet = Rails.cache.fetch("subnet_#{id}", expires_in: 2.hours) do
-              @driver.get_subnet(ip_info["subnet_id"])
+            @ip_subnet_objects = ip_infos.inject({}) do |hash,ip_info|
+              subnet = Rails.cache.fetch("subnet_#{id}", expires_in: 2.hours) do
+                @driver.get_subnet(ip_info["subnet_id"])
+              end
+              hash[ip_info["ip_address"]] = Networking::Subnet.new(@driver,subnet)
+              hash
             end
-            hash[ip_info["ip_address"]] = Networking::Subnet.new(@driver,subnet)
-            hash
           end
         end
       end
+      return @ip_subnet_objects
     end
 
     def network_object
