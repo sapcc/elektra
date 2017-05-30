fc9452c8cea4eafdf27e4ac34b62593f21e39448
andypf <ap@a-pfau.de>
2017-05-30 11:12:31 +0200

    Multiple Tokens (#102)
    * switch to multiple tokens auth
    * move to newer version of auth gem
    * fix specs

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -6,8 +6,6 @@ describe DashboardController, type: :controller do
       head :ok
     end
   end
-  
-  
 
   default_params = {domain_id: AuthenticationStub.domain_id, project_id: AuthenticationStub.project_id}
 
@@ -19,7 +17,7 @@ describe DashboardController, type: :controller do
   before :each do
     stub_authentication
     stub_admin_services
-    
+
     identity_driver = double('identity_service_driver').as_null_object
     allow_any_instance_of(ServiceLayer::IdentityService).to receive(:driver).and_return(identity_driver)
   end
