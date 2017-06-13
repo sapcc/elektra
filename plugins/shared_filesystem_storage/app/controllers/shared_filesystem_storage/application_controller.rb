043c430f7e73cf9a9530d7303d55e8a6c5d61c80
Andreas Pfau <andreas.pfau@sap.com>
2017-06-13 13:55:29 +0200

    Beautify Code

--- plugins/shared_filesystem_storage/app/controllers/shared_filesystem_storage/application_controller.rb
+++ plugins/shared_filesystem_storage/app/controllers/shared_filesystem_storage/application_controller.rb
@@ -1,27 +1,31 @@
+# frozen_string_literal: true
+
 module SharedFilesystemStorage
+  # Application controller for SharedFilesystemStorage
   class ApplicationController < ::DashboardController
     # set policy context
     authorization_context 'shared_filesystem_storage'
-    # enforce permission checks. This will automatically investigate the rule name.
+    # enforce permission checks. This will automatically
+    # investigate the rule name.
     authorization_required
 
     def show
       @permissions = {
         shares: {
-          list: current_user.is_allowed?("shared_filesystem_storage:share_list"),
-          create: current_user.is_allowed?("shared_filesystem_storage:share_create")
+          list: current_user.is_allowed?('shared_filesystem_storage:share_list'),
+          create: current_user.is_allowed?('shared_filesystem_storage:share_create')
         },
         snapshots: {
-          list: current_user.is_allowed?("shared_filesystem_storage:snapshot_list"),
-          create: current_user.is_allowed?("shared_filesystem_storage:snapshot_create")
+          list: current_user.is_allowed?('shared_filesystem_storage:snapshot_list'),
+          create: current_user.is_allowed?('shared_filesystem_storage:snapshot_create')
         },
         share_networks: {
-          list: current_user.is_allowed?("shared_filesystem_storage:share_network_list"),
-          create: current_user.is_allowed?("shared_filesystem_storage:share_network_create")
+          list: current_user.is_allowed?('shared_filesystem_storage:share_network_list'),
+          create: current_user.is_allowed?('shared_filesystem_storage:share_network_create')
         },
         security_services: {
-          list: current_user.is_allowed?("shared_filesystem_storage:security_service_list"),
-          create: current_user.is_allowed?("shared_filesystem_storage:security_service_create")
+          list: current_user.is_allowed?('shared_filesystem_storage:security_service_list'),
+          create: current_user.is_allowed?('shared_filesystem_storage:security_service_create')
         }
       }
     end
--- plugins/shared_filesystem_storage/app/controllers/shared_filesystem_storage/shares/rules_controller.rb
+++ plugins/shared_filesystem_storage/app/controllers/shared_filesystem_storage/shares/rules_controller.rb
@@ -1,30 +1,37 @@
+# frozen_string_literal: true
+
 module SharedFilesystemStorage
   module Shares
+    # This class implements the access control
     class RulesController < ApplicationController
       authorization_context 'shared_filesystem_storage'
       authorization_required
 
       def index
-        render json: services.shared_filesystem_storage.share_rules(params[:share_id])
+        render json: services.shared_filesystem_storage.share_rules(
+          params[:share_id]
+        )
       end
 
       def create
-        rule = services.shared_filesystem_storage.new_share_rule(params[:share_id],rule_params)
+        rule = services.shared_filesystem_storage.new_share_rule(
+          params[:share_id],
+          rule_params
+        )
 
         if rule.save
-          rule.permissions = {
-            delete: current_user.is_allowed?("shared_filesystem_storage:rule_delete"),
-            update: current_user.is_allowed?("shared_filesystem_storage:rule_update")
-          }
+          add_permissions(rule)
           render json: rule
         else
-          render json: { errors: rule.errors}
+          render json: { errors: rule.errors }
         end
       end
 
       def destroy
-        rule = services.shared_filesystem_storage.new_share_rule(params[:share_id])
-        rule.id=params[:id]
+        rule = services.shared_filesystem_storage.new_share_rule(
+          params[:share_id]
+        )
+        rule.id = params[:id]
 
         if rule.destroy
           head :no_content
@@ -34,10 +41,21 @@ module SharedFilesystemStorage
       end
 
       protected
+
       def rule_params
-        params.require(:rule).permit(:access_type,:access_level,:access_to)
+        params.require(:rule).permit(:access_type, :access_level, :access_to)
       end
 
+      def add_permissions(rule)
+        rule.permissions = {
+          delete: current_user.is_allowed?(
+            'shared_filesystem_storage:rule_delete'
+          ),
+          update: current_user.is_allowed?(
+            'shared_filesystem_storage:rule_update'
+          )
+        }
+      end
     end
   end
 end
