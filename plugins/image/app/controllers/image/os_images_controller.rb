4fa9ef0557095899085d12d8765b7a4ce63aa9f4
Andreas Pfau <andreas.pfau@sap.com>
2017-06-22 13:48:46 +0200

    add search field to images

--- plugins/image/app/controllers/image/os_images_controller.rb
+++ plugins/image/app/controllers/image/os_images_controller.rb
@@ -6,13 +6,25 @@ module Image
       @images = paginatable(per_page: 15) do |pagination_options|
         services.image.images(filter_params.merge(pagination_options))
       end
+
+      # this is relevant in case an ajax paginate call is made.
+      # in this case we don't render the layout, only the list!
+      if request.xhr?
+        render partial: 'list', locals: { images: @images }
+      else
+        # comon case, render index page with layout
+        render action: :index
+      end
     end
 
     def show
       @image = services.image.find_image(params[:id])
 
       properties = @image.attributes.clone.stringify_keys
-      known_attributes = %w(name id status visibility protected size container_format disk_format created_at updated_at owner)
+      known_attributes = %w[
+        name id status visibility protected size
+        container_format disk_format created_at updated_at owner
+      ]
       known_attributes.each { |known| properties.delete(known) }
       additional_properties = properties.delete('properties')
       properties.merge!(additional_properties) if additional_properties
@@ -20,15 +32,13 @@ module Image
       @properties = properties.sort_by { |k, _v| k }
     end
 
-    def new
-    end
+    def new; end
 
     def create
       render text: 'create'
     end
 
-    def edit
-    end
+    def edit; end
 
     def update
       render text: 'update'
@@ -36,10 +46,11 @@ module Image
 
     def destroy
       @image = services.image.find_image(params[:id])
-      @success =  (@image and @image.destroy)
+      @success = (@image && @image.destroy)
     end
 
     protected
+
     def filter_params
       raise 'has to be implemented in subclass'
     end
