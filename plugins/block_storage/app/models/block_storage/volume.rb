9753dd3d2ddc5390cc8f56fa1cfa38ac971b3879
Andreas Pfau <andreas.pfau@sap.com>
2017-06-23 09:49:27 +0200

    make availabilty_tone attribute in volumes mandatory

--- plugins/block_storage/app/models/block_storage/volume.rb
+++ plugins/block_storage/app/models/block_storage/volume.rb
@@ -1,6 +1,6 @@
 module BlockStorage
   class Volume < Core::ServiceLayer::Model
-    validates :name, :description, :size, presence: true
+    validates :name, :description, :size, :availability_zone, presence: true
     attr_accessor :assigned_server
 
     STATUS = [
