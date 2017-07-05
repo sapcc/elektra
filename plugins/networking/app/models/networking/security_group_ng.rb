eeb673cac951e0d493fe200620e38055c57c82fd
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-07-05 14:06:11 +0000

    do not need model ng in that case

--- plugins/networking/app/models/networking/security_group_ng.rb
+++
@@ -1,9 +0,0 @@
-module Networking
-  class SecurityGroupNg < Core::ServiceLayerNg::Model
-    validates :name, presence: true
-    
-    def rule_objects
-      attribute_to_object("security_group_rules",Networking::SecurityGroupRule)
-    end
-  end
-end
