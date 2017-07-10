c40bfae59afb2addef381432bc2979670bcb72f1
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-07-10 10:38:16 +0000

    resize and flavor

--- plugins/compute/app/services/service_layer_ng/flavor.rb
+++ plugins/compute/app/services/service_layer_ng/flavor.rb
@@ -6,6 +6,23 @@ module ServiceLayerNg
       debug "[compute-service][Flavor] -> flavors -> GET /flavors/detail"
       api.compute.list_flavors_with_details(filter).map_to(Compute::Flavor)
     end
+    
+    def flavor(flavor_id,use_cache = false)
+      debug "[compute-service][Flavor] -> flavor -> GET /flavors/#{flavor_id}"
+
+      flavor_data = nil
+      unless use_cache
+        flavor_data = api.compute.show_flavor_details(flavor_id).data
+        Rails.cache.write("server_flavor_#{flavor_id}",flavor_data, expires_in: 24.hours)
+      else
+        flavor_data = Rails.cache.fetch("server_flavor_#{flavor_id}", expires_in: 24.hours) do
+          api.compute.show_flavor_details(flavor_id).data
+        end
+      end
+
+      return nil if flavor_data.nil?
+      map_to(Compute::Flavor,flavor_data)
+    end
 
   end
 end
