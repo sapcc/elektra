0c87873cc282595321f30c684fceffb2c65e2142
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-07-10 10:20:01 +0000

    use data instead of body

--- plugins/compute/app/services/service_layer_ng/server.rb
+++ plugins/compute/app/services/service_layer_ng/server.rb
@@ -13,11 +13,11 @@ module ServiceLayerNg
 
       server_data = nil
       unless use_cache
-        server_data = api.compute.list_servers_detailed(filter).body['servers']
+        server_data = api.compute.list_servers_detailed(filter).data
         Rails.cache.write("#{@scoped_project_id}_servers",server_data, expires_in: 2.hours)
       else
         server_data = Rails.cache.fetch("#{@scoped_project_id}_servers", expires_in: 2.hours) do
-          api.compute.list_servers_detailed(filter).body['servers']
+          api.compute.list_servers_detailed(filter).data
         end
       end
 
