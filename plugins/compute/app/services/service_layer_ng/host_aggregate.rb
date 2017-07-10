f013badbd68d8161552bfb1da23daa985465ab30
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-07-10 13:17:37 +0000

    migrate host aggreagates

--- plugins/compute/app/services/service_layer_ng/host_aggregate.rb
+++ plugins/compute/app/services/service_layer_ng/host_aggregate.rb
@@ -2,5 +2,10 @@ module ServiceLayerNg
   # This module implements Openstack Domain API
   module HostAggregate
 
+    def host_aggregates(filter = {})
+      debug "[compute-service][HostAggregate] -> host_aggregates -> GET /os-aggregates"
+      api.compute.list_aggregates(filter).map_to(Compute::HostAggregate)
+    end
+
   end
 end
