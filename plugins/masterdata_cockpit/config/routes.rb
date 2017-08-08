cd6a3734f24efcc0ad20dd0c4695aa24eae583cf
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-08-08 14:40:40 +0000

    prepare domain masterdata

--- plugins/masterdata_cockpit/config/routes.rb
+++ plugins/masterdata_cockpit/config/routes.rb
@@ -1,7 +1,10 @@
 MasterdataCockpit::Engine.routes.draw do
   
-  scope '/', as: 'masterdata' do
+  scope '/project', as: 'project_masterdata' do
     get  '/' => 'project_masterdata#index'
   end
 
+  scope '/domain', as: 'domain_masterdata' do
+    get  '/' => 'domain_masterdata#index'
+  end
 end
