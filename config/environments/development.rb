d4f8d5ce3a0debadd82f293c576f00ecccc91af8
Torsten Lesmann <torsten.lesmann@sap.com>
2017-08-10 16:01:33 +0200

    Reduce logging volume in production. No more Rendered partial messages from Action View

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  #config.action_view.logger = nil
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Disable Rendered XXX partial messages to reduce log volume
+  config.action_view.logger = nil
+
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
