ace4180ceb95f15be0d9e267e3611e08fabb0d21
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-08-22 13:35:08 +0000

    simplify domain masterdata controllert

--- plugins/masterdata_cockpit/app/controllers/masterdata_cockpit/domain_masterdata_controller.rb
+++ plugins/masterdata_cockpit/app/controllers/masterdata_cockpit/domain_masterdata_controller.rb
@@ -3,10 +3,37 @@
 module MasterdataCockpit
   class DomainMasterdataController < DashboardController
 
+    before_filter :load_domain_masterdata, only: [:index, :edit]
+    before_filter :prepare_params, only: [:create, :update]
+
     authorization_context 'masterdata_cockpit'
     authorization_required
     
     def index
+    end
+
+    def new
+      @domain_masterdata = services_ng.masterdata_cockpit.new_domain_masterdata
+    end
+
+    def create
+      unless @domain_masterdata.save
+        render action: :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      unless @domain_masterdata.update
+        render action: :edit
+      end
+    end
+    
+    private
+    
+    def load_domain_masterdata
       begin
         @domain_masterdata = services_ng.masterdata_cockpit.get_domain(@scoped_domain_id)
       rescue Exception => e
@@ -17,34 +44,11 @@ module MasterdataCockpit
         end
       end
     end
-
-    def new
-      @domain_masterdata = services_ng.masterdata_cockpit.new_domain_masterdata
-      @domain_masterdata.domain_id = @scoped_domain_id
-      @domain_masterdata.domain_name = @scoped_domain_name
-    end
-
-    def create
+    
+    def prepare_params
       @domain_masterdata = services_ng.masterdata_cockpit.new_domain_masterdata
       # to merge options into .merge(domain_id: @scoped_domain_id)
       @domain_masterdata.attributes =params.fetch(:domain_masterdata,{})
-      
-      unless @domain_masterdata.save
-        render action: :new
-      end
-    end
-
-    def edit
-      @domain_masterdata = services_ng.masterdata_cockpit.get_domain(@scoped_domain_id)
-    end
-
-    def update
-      @domain_masterdata = services_ng.masterdata_cockpit.new_domain_masterdata
-      @domain_masterdata.attributes =params.fetch(:domain_masterdata,{})
-
-      unless @domain_masterdata.update
-        render action: :edit
-      end
     end
 
   end
