d9d1d51ee07b3ab1e9c221ac03e0e8eae244557a
Andreas Pfau <andreas.pfau@sap.com>
2017-08-31 17:11:29 +0200

    first working version of react on webpacker

--- plugins/identity/javascript/packs/application.js
+++ plugins/identity/javascript/packs/application.js
@@ -7,4 +7,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker Identity2')
+import './hello_react';
+
+export default { reducers: 'identityReducer' };
