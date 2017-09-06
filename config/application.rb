ad50fb523742f8eec79fb80a324253743c114f4a
Fabian Ruff <fabian.ruff@sap.com>
2017-09-06 10:27:06 +0200

    remove unused code

--- config/application.rb
+++ config/application.rb
@@ -42,23 +42,9 @@ module MonsoonDashboard
     plugin_mount_points = {}
     Core::PluginsManager.available_plugins.each{|plugin| plugin_mount_points[plugin.mount_point] = plugin.mount_point}
 
-    label_builder = proc do |env, code|
-      controller_name = env.fetch("action_dispatch.request.path_parameters",{}).fetch(:controller, '')
-      {
-        code: code,
-        method: env['REQUEST_METHOD'].downcase,
-        host:   env['HTTP_HOST'].to_s,
-        # just take the first component of the path as a label
-        path:   env['REQUEST_PATH'][0, env['REQUEST_PATH'].index('/',1) || 20 ],
-        controller: controller_name,
-        action: env.fetch("action_dispatch.request.path_parameters",{}).fetch(:action,''),
-        plugin: controller_name[%r{^([^/]+)/},1]
-      }
-    end
-
     config.middleware.insert_after(ActionDispatch::DebugExceptions, Prometheus::Middleware::Collector, {
       counter_label_builder: proc do |env, code|
-      controller_name = env.fetch("action_dispatch.request.path_parameters",{}).fetch(:controller, '')
+        controller_name = env.fetch("action_dispatch.request.path_parameters",{}).fetch(:controller, '')
         {
           code: code,
           method: env['REQUEST_METHOD'].downcase,
