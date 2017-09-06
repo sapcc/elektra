c89ea9a69c672c138b5cbc9001629452fdaba67a
Esther Schmitz <esther.schmitz@sap.com>
2017-09-06 15:07:22 +0200

    adds kubernetes_admin permission check

--- plugins/kubernetes/app/controllers/kubernetes/application_controller.rb
+++ plugins/kubernetes/app/controllers/kubernetes/application_controller.rb
@@ -2,7 +2,11 @@
 
 module Kubernetes
   class ApplicationController < DashboardController
+    authorization_context 'audit'
+    authorization_required
+
     def index
+      enforce_permissions('kubernetes:application_get')
       @kubernikus_endpoint = "#{current_user.service_url('kubernikus')}/api/v1"
     end
   end
