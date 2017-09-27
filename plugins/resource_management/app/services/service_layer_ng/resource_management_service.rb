84e267301fbb45a6b5a40dda434a05d188dc3a3a
Stefan Majewsky <stefan.majewsky@sap.com>
2017-09-27 13:21:58 +0200

    less confusion
    I don't know about you, but I find it extremely frustrating when I have
    to hunt for every single method definition in a different file.

--- plugins/resource_management/app/services/service_layer_ng/resource_management_service.rb
+++ plugins/resource_management/app/services/service_layer_ng/resource_management_service.rb
@@ -1,13 +1,103 @@
 module ServiceLayerNg
-
   class ResourceManagementService < Core::ServiceLayerNg::Service
-
-    include ResourceManagementService::ProjectResource
-    include ResourceManagementService::DomainResource
-    include ResourceManagementService::CloudResource
 
     def available?(_action_name_sym = nil)
       api.catalog_include_service?('resources', region)
+    end
+
+    ############################################################################
+    # cloud-admin level
+
+    def find_current_cluster(query={})
+      api.resources.get_current_cluster(query).map_to(ResourceManagement::Cluster)
+    end
+
+    def put_cluster_data(services)
+      api_client.resources.set_capacity_for_current_cluster(:cluster => {:services => services})
+    end
+
+    ############################################################################
+    # domain-admin level
+
+    def find_domain(domain_id, query={})
+      api.resources.get_domain(domain_id,query).map_to(ResourceManagement::Domain)
+    end
+
+    def list_domains(query={})
+      api.resources.get_domains(query).map_to(ResourceManagement::Domain)
+    end
+
+    def put_domain_data(domain_id, services)
+      api_client.resources.set_quota_for_domain(domain_id, :domain => {:services => services})
+    end
+
+    def discover_projects(domain_id)
+      api_client.resources.discover_projects(domain_id)
+    end
+
+    ############################################################################
+    # project-admin level
+
+    def find_project(domain_id, project_id, query={})
+      # give the domain_id to enrich the Project object with domain_id
+      api.resources.get_project(domain_id, project_id, query).map_to(ResourceManagement::Project, domain_id: domain_id)
+    end
+
+    def has_project_quotas?(domain_id,project_id,project_domain_id=nil)
+      project = find_project(
+        domain_id || project_domain_id,
+        project_id,
+        service:  [ 'compute',                   'network',  'object-store' ],
+        resource: [ 'instances', 'ram', 'cores', 'networks', 'capacity'     ],
+      )
+      # return true if approved_quota of the resource networking:networks is greater than 0
+      # OR
+      # return true if the sum of approved_quota of the resources compute:instances,
+      # compute:ram, compute:cores and object_storage:capacity is greater than 0
+      return project.resources.any? { |r| r.quota > 0 }
+    end
+
+    def list_projects(domain_id, query={})
+      # give the domain_id to enrich the Project object with domain_id
+      api.resources.get_projects(domain_id, query).map_to(ResourceManagement::Project, domain_id: domain_id)
+    end
+
+    def sync_project_asynchronously(domain_id, project_id)
+      api_client.resources.sync_project(domain_id, project_id)
+    end
+
+    def put_project_data(domain_id, project_id, services)
+      api.resources.set_quota_for_project(domain_id,project_id, :project => {:services => services})
+    end
+
+    def quota_data(domain_id,project_id,options=[])
+      return [] if options.empty?
+
+      project = find_project(
+        domain_id,
+        project_id,
+        service: options.collect { |values| values[:service_type] },
+        resource: options.collect { |values| values[:resource_name] },
+      )
+      result = []
+      options.each do |values|
+        service = project.services.find { |srv| srv.type == values[:service_type].to_sym }
+        next if service.nil?
+        resource = service.resources.find { |res| res.name == values[:resource_name].to_sym }
+        next if resource.nil?
+
+        if values[:usage] and values[:usage].is_a?(Fixnum)
+          resource.usage = values[:usage]
+        end
+
+        result << resource
+      end
+
+      result
+
+    rescue => e
+      Rails.logger.error "Error trying to get quota data for project: #{project_id}. Error: #{e}"
+      []
     end
 
   end
