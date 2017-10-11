3aeae14fdb69e041cdda3bccd4189c35b1d9ee0d
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2017-10-11 15:17:11 +0000

    fix test

--- plugins/metrics/spec/controllers/application_controller_spec.rb
+++ plugins/metrics/spec/controllers/application_controller_spec.rb
@@ -21,10 +21,10 @@ describe Metrics::ApplicationController, type: :controller do
     stub_authentication
   end
 
-  #describe 'GET index' do
-  #  it 'returns http success' do
-  #    get :index, default_params
-  #    expect(response).to be_success
-  #  end
-  #end
+  describe 'GET index' do
+    it 'returns http success' do
+      get :index
+      expect(response).to be_success
+    end
+  end
 end
