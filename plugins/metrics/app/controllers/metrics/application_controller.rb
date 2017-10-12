09bdae8bfeb03e1b2e548496435631ebf759d609
Esther Schmitz <esther.schmitz@sap.com>
2017-10-12 15:52:26 +0200

    style changes, typos, hide auth token in link

--- plugins/metrics/app/controllers/metrics/application_controller.rb
+++ plugins/metrics/app/controllers/metrics/application_controller.rb
@@ -4,5 +4,9 @@ module Metrics
   class ApplicationController < DashboardController
     def index
     end
+
+    def maia
+      redirect_to "https://maia.#{current_region}.cloud.sap/#{@scoped_domain_name}?x-auth-token:#{current_user.token}"
+    end
   end
 end
--- plugins/metrics/config/routes.rb
+++ plugins/metrics/config/routes.rb
@@ -1,3 +1,4 @@
 Metrics::Engine.routes.draw do
   get '/' => 'application#index', as: :index
+  get 'maia' => 'application#maia', as: :maia
 end
