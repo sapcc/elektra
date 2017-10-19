9f74dbfbda197a0e03401d908a6ffedeb08e9f4c
Rudolf Vriend <rudolf.vriend@sap.com>
2017-10-19 11:01:35 +0200

    The CAM url should be configurable, since staging has a different one

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -10,4 +10,12 @@ module UrlHelper
   def url_for_avatar
     return eval('"' + ENV['MONSOON_DASHBOARD_AVATAR_URL'] + '"') rescue "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.email ? current_user.email : '')}?d=mm&size=24x24"
   end
+
+  def url_for_cam(domainname)
+    if ENV['MONSOON_DASHBOARD_CAM_URL']
+      ENV['MONSOON_DASHBOARD_CAM_URL'] + "?item=request&profile=CC%20#{domainname.upcase}%20Openstack%20Domain%20Access"
+    else
+      "https://spc40-emea.byd.sap.corp/sap/bc/webdynpro/a1sspc/cam_wd_central?item=request&profile=CC%20#{domainname.upcase}%20Openstack%20Domain%20Access"
+    end
+  end
 end
