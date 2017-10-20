c69481c46b100c524f10e7177dc0884d59dfb24b
Esther Schmitz <esther.schmitz@sap.com>
2017-10-20 15:43:25 +0200

    fix cam link

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -3,19 +3,11 @@ module UrlHelper
   def sap_url_for(servicename)
     # special handling for global services
     region = servicename == 'documentation' ? 'global' : "#{current_region}"
-    
+
     return "https://#{servicename}.#{region}.#{request.domain}/"
   end
 
   def url_for_avatar
     return eval('"' + ENV['MONSOON_DASHBOARD_AVATAR_URL'] + '"') rescue "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.email ? current_user.email : '')}?d=mm&size=24x24"
-  end
-
-  def url_for_cam(domainname)
-    if ENV['MONSOON_DASHBOARD_CAM_URL']
-      ENV['MONSOON_DASHBOARD_CAM_URL'] + "?item=request&profile=CC%20#{domainname.upcase}%20Openstack%20Domain%20Access"
-    else
-      "https://spc40-emea.byd.sap.corp/sap/bc/webdynpro/a1sspc/cam_wd_central?item=request&profile=CC%20#{domainname.upcase}%20Openstack%20Domain%20Access"
-    end
   end
 end
