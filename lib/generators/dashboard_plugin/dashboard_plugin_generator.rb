d1d36275938563422a21e576b9803dbbcf54f292
Andreas Pfau <andreas.pfau@sap.com>
2017-11-07 16:21:08 +0100

    refactor plugin generator

--- lib/generators/dashboard_plugin/dashboard_plugin_generator.rb
+++ lib/generators/dashboard_plugin/dashboard_plugin_generator.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 class DashboardPluginGenerator < Rails::Generators::NamedBase
   PLUGINS_PATH = 'plugins'
 
   source_root File.expand_path('../templates', __FILE__)
-  class_option :mountable, type: :boolean, default: false, description: "Generate mountable isolated application"
-  class_option :service_layer, type: :boolean, default: false, description: "Generate service layer (app/services/service_layer/)"
+  class_option :mountable, type: :boolean, default: false, description: 'Generate mountable isolated application'
+  class_option :service_layer, type: :boolean, default: false, description: 'Generate service layer (app/services/service_layer/)'
+  class_option :react, type: :boolean, default: false, description: 'Generate plugin using react/redux js-lib'
 
   def generate_plugin_skeleton
-    plugin_options = "--skip-gemfile --skip-bundle --skip-git --skip-test-unit"
-    plugin_options += " --mountable" if options.mountable?
-    plugin_options += " --full" if options.service_layer? and !options.mountable?
+    plugin_options = '--skip-gemfile --skip-bundle --skip-git --skip-test-unit'
+    plugin_options += ' --mountable' if options.mountable?
+    plugin_options += ' --full' if options.service_layer? and !options.mountable?
 
     generate(:plugin, "#{PLUGINS_PATH}/#{name}", plugin_options)
   end
@@ -44,8 +47,8 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
   end
 
   def create_service_layer_service
-    copy_file "app/services/service_layer/service.rb", "#{PLUGINS_PATH}/#{name}/app/services/service_layer/#{name}_service.rb"
-    gsub_file "#{PLUGINS_PATH}/#{name}/app/services/service_layer/#{name}_service.rb", '%{PLUGIN_NAME}', name.camelize
+    copy_file "app/services/service_layer_ng/service.rb", "#{PLUGINS_PATH}/#{name}/app/services/service_layer_ng/#{name}_service.rb"
+    gsub_file "#{PLUGINS_PATH}/#{name}/app/services/service_layer_ng/#{name}_service.rb", '%{PLUGIN_NAME}', name.camelize
   end
 
   def update_assets
@@ -53,18 +56,33 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
     remove_file "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/application.js"
 
     copy_file "app/assets/_application.scss", "#{PLUGINS_PATH}/#{name}/app/assets/stylesheets/#{name}/_application.scss"
-    copy_file "app/assets/plugin.js", "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/plugin.js"
 
-    gsub_file "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/plugin.js", '%{PLUGIN_NAME}', name
+    if options.react?
+      directory "#{PLUGINS_PATH}/#{name}/app/javascript", 'react_app/javascript'
+      byebug
+      Dir.glob("**/*").each do |file|
+        p file
+        gsub_file file, '%{PLUGIN_NAME}', name
+      end
+    else
+      copy_file "app/assets/plugin.js", "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/plugin.js"
+      gsub_file "#{PLUGINS_PATH}/#{name}/app/assets/javascripts/#{name}/plugin.js", '%{PLUGIN_NAME}', name
+    end
   end
 
   def modify_application_controller
     remove_file "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb"
-    copy_file "app/controllers/application_controller.rb", "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb"
-    copy_file "app/views/application/index.html.haml", "#{PLUGINS_PATH}/#{name}/app/views/#{name}/application/index.html.haml"
 
+    if options.react?
+      copy_file "app/controllers/application_controller.rb", "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb"
+      remove_dir "#{PLUGINS_PATH}/#{name}/app/views"
+    else
+      copy_file "app/controllers/application_controller.rb", "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb"
+      copy_file "app/views/application/index.html.haml", "#{PLUGINS_PATH}/#{name}/app/views/#{name}/application/index.html.haml"
+      gsub_file "#{PLUGINS_PATH}/#{name}/app/views/#{name}/application/index.html.haml", '%{PLUGIN_NAME}', name.camelize
+    end
+    
     gsub_file "#{PLUGINS_PATH}/#{name}/app/controllers/#{name}/application_controller.rb", '%{PLUGIN_NAME}', name.camelize
-    gsub_file "#{PLUGINS_PATH}/#{name}/app/views/#{name}/application/index.html.haml", '%{PLUGIN_NAME}', name.camelize
   end
 
   def replace_test_with_spec
@@ -72,7 +90,7 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
   end
 
   def add_controller_spec
-    copy_file "spec/controllers/application_controller_spec.rb", "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb"
+    copy_file 'spec/controllers/application_controller_spec.rb', "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb"
     gsub_file "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb", '%{PLUGIN_NAME}', name.camelize
 
     gsub_file "#{PLUGINS_PATH}/#{name}/spec/controllers/application_controller_spec.rb", '%{PLUGIN_NAME}', name.camelize
@@ -80,7 +98,11 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
 
   def add_routes
     remove_file "#{PLUGINS_PATH}/#{name}/config/routes.rb"
-    copy_file "config/routes.rb", "#{PLUGINS_PATH}/#{name}/config/routes.rb"
+    if options.react?
+      copy_file 'react_app/config/routes.rb', "#{PLUGINS_PATH}/#{name}/config/routes.rb"
+    else
+      copy_file 'config/routes.rb', "#{PLUGINS_PATH}/#{name}/config/routes.rb"
+    end
     gsub_file "#{PLUGINS_PATH}/#{name}/config/routes.rb", '%{PLUGIN_NAME}', name.camelize
   end
 
---
+++ lib/generators/dashboard_plugin/templates/react_app/javascript/packs/application.jsx
@@ -0,0 +1,36 @@
+/* eslint no-console:0 */
+import { HashRouter, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux';
+import * as Reducers from './reducers';
+
+import Tabs from './components/tabs';
+
+import Welcome from './components/welcome';
+
+import Entries from './containers/entries/list'
+import EditEntryModal from './containers/entries/edit';
+import ShowEntryModal from './containers/entries/show';
+import NewEntryModal from './containers/entries/new';
+
+const tabsConfig = [
+  { to: '/welcome', label: 'Welcome', component: Welcome },
+  { to: '/entries', label: 'Entries', component: Entries }
+]
+
+// render all components inside a hash router
+const Container = (props) =>
+  <HashRouter /*hashType="noslash"*/ >
+    <div>
+      {/* redirect root to shares tab */}
+      <Route exact path="/" render={ () => <Redirect to="/welcome"/>}/>
+      <Route path="/:activeTab" children={ ({match, location, history}) =>
+        React.createElement(Tabs, Object.assign({}, {match, location, history, tabsConfig}, props))
+      }/>
+
+    <Route exact path="/entries/new" component={NewShareModal}/>
+      <Route exact path="/entries/:id/show" component={ShowShareModal}/>
+      <Route exact path="/entries/:id/edit" component={EditShareModal}/>
+    </div>
+  </HashRouter>
+
+export default { Reducers, Container };
