d9636239ac9f0ee9fd480df137f4d29337e01cd7
Andreas Pfau <andreas.pfau@sap.com>
2017-11-09 15:14:46 +0100

    improve plugin generator

--- lib/generators/dashboard_plugin/react_plugin.rb
+++ lib/generators/dashboard_plugin/react_plugin.rb
@@ -3,7 +3,8 @@
 class ReactPluginGenerator
   extend Forwardable
   def_delegators :@context, :options, :copy_file, :remove_file, :gsub_file,
-                 :create_file, :name, :remove_dir, :directory, :source_paths
+                 :create_file, :name, :remove_dir, :directory, :source_paths,
+                 :append_to_file
   attr_reader :plugin_path
 
   def initialize(context, plugin_path)
@@ -18,6 +19,7 @@ class ReactPluginGenerator
     modify_app
     add_routes
     update_assets
+    update_application_version
   end
 
   private
@@ -41,5 +43,16 @@ class ReactPluginGenerator
   def add_routes
     remove_file "#{plugin_path}/#{name}/config/routes.rb"
     copy_file 'react/config/routes.rb', "#{plugin_path}/#{name}/config/routes.rb"
+  end
+
+  def update_application_version
+    found = false
+    gsub_file "app/javascript/packs/application.jsx.erb", /\/\/ version\s+.+/ do |match|
+      found = true
+      version = /\/\/ version\s+(.+)/.match(match)[1]
+      new_version = version.to_i+1
+      match.gsub!(version, new_version.to_s)
+    end
+    append_to_file "app/javascript/packs/application.jsx.erb", '// version 1' unless found
   end
 end
