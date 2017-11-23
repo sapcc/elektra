e93c794733814fcfddd5f27ee0256f84caeaabea
Torsten Lesmann <torsten.lesmann@sap.com>
2017-11-23 10:54:38 +0100

    LBaaS: Enable Esd's in UI in all Regions

--- plugins/loadbalancing/app/models/loadbalancing/l7policy.rb
+++ plugins/loadbalancing/app/models/loadbalancing/l7policy.rb
@@ -2,15 +2,10 @@ module Loadbalancing
   class L7policy < Core::ServiceLayer::Model
     include ActiveModel::Conversion
 
-    if ENV['MONSOON_DASHBOARD_REGION'] != 'staging'
-      PREDEFINED_POLICIES = [ { protocols: ['TCP'], ids: ['proxy_protocol_2edF_v1_0', 'standard_tcp_a3de_v1_0', ]}
-                            ]
-    else
-      PREDEFINED_POLICIES = [ { protocols: ['TCP'], ids: ['proxy_protocol_2edF_v1_0', 'standard_tcp_a3de_v1_0', ]},
-                              { protocols: ['HTTP', 'HTTPS', 'TERMINATED_HTTPS'], ids: ['x_forward_5b6e_v1_0', 'no_one_connect_3caB_v1_0', 'http_compression_e4a2_v1_0', 'cookie_encryption_b82a_v1_0']},
-                              { protocols: ['TERMINATED_HTTPS'], ids: ['sso_22b0_v1_0']}
-                            ]
-    end
+    PREDEFINED_POLICIES = [ { protocols: ['TCP'], ids: ['proxy_protocol_2edF_v1_0', 'standard_tcp_a3de_v1_0', ]},
+                            { protocols: ['HTTP', 'HTTPS', 'TERMINATED_HTTPS'], ids: ['x_forward_5b6e_v1_0', 'no_one_connect_3caB_v1_0', 'http_compression_e4a2_v1_0', 'cookie_encryption_b82a_v1_0']},
+                            { protocols: ['TERMINATED_HTTPS'], ids: ['sso_22b0_v1_0']}
+                          ]
 
     ACTIONS= ['REDIRECT_TO_URL', 'REDIRECT_TO_POOL', 'REJECT']
 
