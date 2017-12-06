5969d9ca71d7d42a6f29e4e63d5520629f85fac5
Esther Schmitz <esther.schmitz@sap.com>
2017-12-06 11:30:25 +0100

    Fix policy check to show custom unauthorized page

--- plugins/kubernetes/app/controllers/kubernetes/application_controller.rb
+++ plugins/kubernetes/app/controllers/kubernetes/application_controller.rb
@@ -2,15 +2,15 @@
 
 module Kubernetes
   class ApplicationController < DashboardController
-    authorization_context 'kubernetes'
-    authorization_required
+    # authorization_context 'kubernetes'
+    # authorization_required
 
     def release_state
       'beta'
     end
 
     def index
-      # enforce_permissions('kubernetes:application_get')
+      # enforce_permissions('kubernetes:application_list')
       @kubernikus_endpoint = "#{current_user.service_url('kubernikus')}"
       # Settings from Elektra extension
       @beta_contact = {name: Settings.beta_contact_name, email: Settings.beta_contact_email}
