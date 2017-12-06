f5fdd91d7aa50461a9b803abec3bf41379b35dd8
Torsten Lesmann <torsten.lesmann@sap.com>
2017-12-06 08:28:09 +0100

    Revert "fix for service name change in latest misty"
    This reverts commit 3ab2902

--- plugins/dns_service/app/services/service_layer_ng/dns_service_services/pool.rb
+++ plugins/dns_service/app/services/service_layer_ng/dns_service_services/pool.rb
@@ -5,11 +5,11 @@ module ServiceLayerNg
     # This module implements Openstack Designate Pool API
     module Pool
       def pools(filter = {})
-        api.domain_name_server.list_all_pools(filter).map_to(pools: DnsService::PoolNg)
+        api.dns.list_all_pools(filter).map_to(pools: DnsService::PoolNg)
       end
 
       def find_pool!(id)
-        api.domain_name_server.show_a_pool(id).map_to(response_body: DnsService::PoolNg)
+        api.dns.show_a_pool(id).map_to(response_body: DnsService::PoolNg)
       end
 
       def find_pool(id)
