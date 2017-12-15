0e4dcaacd046d2612d425c35fca582706d98538e
Esther Schmitz <esther.schmitz@sap.com>
2017-12-15 16:50:02 +0100

    Fix user search bugs for cloud admin and keystone admin

--- plugins/identity/app/helpers/identity/projects/members_helper.rb
+++ plugins/identity/app/helpers/identity/projects/members_helper.rb
@@ -1,13 +1,4 @@
 module Identity
   module Projects::MembersHelper
-
-    def render_user_name(user_name, user_description)
-      unless user_description.blank?
-        rendered_name = "#{user_description} (#{user_name})"
-      else
-        rendered_name = user_name
-      end
-      rendered_name
-    end
   end
 end
