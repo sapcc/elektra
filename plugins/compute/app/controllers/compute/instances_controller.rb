c1ebb28f248aaf5aec5ed3d759afb26872c1c1ac
Andreas Pfau <andreas.pfau@sap.com>
2017-12-19 10:23:44 +0100

    Change the sort order of the availability zones in server create dialog

--- plugins/compute/app/controllers/compute/instances_controller.rb
+++ plugins/compute/app/controllers/compute/instances_controller.rb
@@ -93,7 +93,7 @@ module Compute
       azs = services_ng.compute.availability_zones
       if azs
         @availability_zones = azs.select { |az| az.zoneState['available'] }
-        @availability_zones.sort_by!{|az| az.zoneName}
+        @availability_zones.sort_by!(&:zoneName).reverse! 
       else
         @instance.errors.add :availability_zone, 'not available'
       end
