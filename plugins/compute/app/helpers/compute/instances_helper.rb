9599aa0ae10817c6079031ceb63dc32fe606d62c
Esther Schmitz <esther.schmitz@sap.com>
2018-01-29 16:25:07 +0100

    Handling for images array nil

--- plugins/compute/app/helpers/compute/instances_helper.rb
+++ plugins/compute/app/helpers/compute/instances_helper.rb
@@ -2,29 +2,33 @@ module Compute
   module InstancesHelper
 
     def grouped_images(images)
-      public_images = {}
-      private_images = {}
-      # before sorting delete images that don't have a name
-      # if for whatever reason in the future we want to show images without names in the list
-      # this needs to be adjusted
-      images.delete_if{|image| image.name.nil?}.sort_by!{|image| [image.name, image.created_at]}
-      images.each do |image|
-        if image.visibility=='public'
-          type = (image.hypervisor_type || 'no hypervisor type')
-          public_images[type] ||= []
-          public_images[type] << image
-        elsif image.visibility=='private'
-          type = (image.hypervisor_type || 'no hypervisor type')
-          private_images[type] ||= []
-          private_images[type] << image
+      if images.blank?
+        [["Couldn't retrieve images. Please try again", []]]
+      else
+        public_images = {}
+        private_images = {}
+        # before sorting delete images that don't have a name
+        # if for whatever reason in the future we want to show images without names in the list
+        # this needs to be adjusted
+        images.delete_if{|image| image.name.nil?}.sort_by!{|image| [image.name, image.created_at]}
+        images.each do |image|
+          if image.visibility=='public'
+            type = (image.hypervisor_type || 'no hypervisor type')
+            public_images[type] ||= []
+            public_images[type] << image
+          elsif image.visibility=='private'
+            type = (image.hypervisor_type || 'no hypervisor type')
+            private_images[type] ||= []
+            private_images[type] << image
+          end
         end
+        result = []
+        result << ['Public', []]
+        public_images.each{|hypervisor,images| result << ["--#{hypervisor}",images.collect{|image| [image_label_for_select(image), image.id, data: {vmware_ostype: image.vmware_ostype}]} ]}
+        result << ['Private', []]
+        private_images.each{|hypervisor,images| result << ["--#{hypervisor}",images.collect{|image| [image_label_for_select(image), image.id, data: {vmware_ostype: image.vmware_ostype}]} ]}
+        result
       end
-      result = []
-      result << ['Public', []]
-      public_images.each{|hypervisor,images| result << ["--#{hypervisor}",images.collect{|image| [image_label_for_select(image), image.id, data: {vmware_ostype: image.vmware_ostype}]} ]}
-      result << ['Private', []]
-      private_images.each{|hypervisor,images| result << ["--#{hypervisor}",images.collect{|image| [image_label_for_select(image), image.id, data: {vmware_ostype: image.vmware_ostype}]} ]}
-      result
     end
 
     def grouped_flavors(flavors)
