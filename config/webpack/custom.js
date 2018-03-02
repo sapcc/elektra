476226f4f00d430ac47fd311941591d34c88e54e
Andreas Pfau <andreas.pfau@sap.com>
2018-03-02 11:12:53 +0100

    remove debug output in webpack config

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -16,8 +16,8 @@ for(let index in widget_paths) {
   const name_tokens = widget_path.match(name_regex);
   const widget_name = `${name_tokens[1]}_${name_tokens[2]}`
 
-  console.log('name_tokens',name_tokens)
-  console.log('widget_name',widget_name)
+  // console.log('name_tokens',name_tokens)
+  // console.log('widget_name',widget_name)
 
   let widget_absolute_path = path.resolve(__dirname, `../../${widget_path}`)
   customConfig.entry[widget_name] = widget_absolute_path
