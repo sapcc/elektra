c7f88870c92e3b023440b24628efd2b23854c73f
Andreas Pfau <andreas.pfau@sap.com>
2018-05-02 14:55:57 +0200

    fixes ##277, show snapshots in image select box

--- plugins/compute/app/helpers/compute/instances_helper.rb
+++ plugins/compute/app/helpers/compute/instances_helper.rb
@@ -5,29 +5,35 @@ module Compute
       if images.blank?
         [["Couldn't retrieve images. Please try again", []]]
       else
-        public_images = {}
-        private_images = {}
-        # before sorting delete images that don't have a name
-        # if for whatever reason in the future we want to show images without names in the list
-        # this needs to be adjusted
-        images.delete_if{|image| image.name.nil?}.sort_by!{|image| [image.name, image.created_at]}
-        images.each do |image|
-          if image.visibility=='public'
-            type = (image.hypervisor_type || 'no hypervisor type')
-            public_images[type] ||= []
-            public_images[type] << image
-          elsif image.visibility=='private'
-            type = (image.hypervisor_type || 'no hypervisor type')
-            private_images[type] ||= []
-            private_images[type] << image
+
+        # prepare images map: { visibility => { hypervisor => [images] } }
+        groupd_images_map = images.each_with_object({}) do |image, map|
+          next if image.name.nil?
+          hypervisor_type = image.hypervisor_type || 'bare metal'
+          visibility = image.visibility
+          visibility = 'snapshot' if image.image_type == 'snapshot'
+          map[visibility] ||= {}
+          map[visibility][hypervisor_type] ||= []
+          map[visibility][hypervisor_type] << image
+        end.sort
+
+        # build an array with labels
+        groupd_images_map.each_with_object([]) do |(visibility, hypervisors), container|
+          container << [visibility, []]
+
+          hypervisors.each do |hypervisor_type, images|
+            images = images.sort_by { |image| [image.name, image.created_at] }
+            items = images.collect do |image|
+              [
+                image_label_for_select(image),
+                image.id,
+                data: { vmware_ostype: image.vmware_ostype }
+              ]
+            end
+
+            container << ["--#{hypervisor_type}", items]
           end
         end
-        result = []
-        result << ['Public', []]
-        public_images.each{|hypervisor,images| result << ["--#{hypervisor}",images.collect{|image| [image_label_for_select(image), image.id, data: {vmware_ostype: image.vmware_ostype}]} ]}
-        result << ['Private', []]
-        private_images.each{|hypervisor,images| result << ["--#{hypervisor}",images.collect{|image| [image_label_for_select(image), image.id, data: {vmware_ostype: image.vmware_ostype}]} ]}
-        result
       end
     end
 
@@ -42,8 +48,8 @@ module Compute
         end
       end
       result = []
-      result << ['Public',public_flavors.sort_by{|a| [a.ram, a.vcpus]}] if public_flavors.length>0
-      result << ['Private',private_flavors.sort_by{|a| [a.ram, a.vcpus]}] if private_flavors.length>0
+      result << ['Public', public_flavors.sort_by { |a| [a.ram, a.vcpus] }] if public_flavors.length>0
+      result << ['Private', private_flavors.sort_by { |a| [a.ram, a.vcpus] }] if private_flavors.length>0
       result
     end
 
