59813daf07c490902089286e46bfa5b573727b36
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-06-05 16:42:32 +0200

    barchart completed

--- plugins/reports/app/javascript/app/components/app.jsx
+++ plugins/reports/app/javascript/app/components/app.jsx
@@ -1,17 +1,5 @@
 import BarChart from './barChart'
-import { geoCentroid } from 'd3-geo'
-import worlddata from './world'
 import { range } from 'd3-array'
-
-const appdata = worlddata.features
-  .filter(d => geoCentroid(d)[0] < -20)
-
-appdata
-  .forEach((d,i) => {
-    const offset = Math.random()
-    d.launchday = i
-    d.data = range(30).map((p,q) => q < i ? 0 : Math.random() * 2 + offset)
-  })
 
 class App extends React.Component {
 
@@ -19,7 +7,6 @@ class App extends React.Component {
     screenWidth: 1000,
     screenHeight: 500,
     hover: "none",
-    brushExtent: [0,40],
     filter: '',
     error: null
   };
@@ -31,11 +18,9 @@ class App extends React.Component {
   }
 
   render() {
-    const filteredAppdata = appdata.filter((d,i) => d.launchday >= this.state.brushExtent[0] && d.launchday <= this.state.brushExtent[1])
     return (
       <React.Fragment>
-        <h1>Helloo</h1>
-        <BarChart data={filteredAppdata} realData={this.props.cost.data} size={[500,500]} />
+        <BarChart data={this.props.cost.data} size={[700,300]} />
       </React.Fragment>
     )
   }
--- plugins/reports/app/javascript/app/components/barChart.jsx
+++ plugins/reports/app/javascript/app/components/barChart.jsx
@@ -1,9 +1,10 @@
-import { max, sum } from 'd3-array'
-import { scaleLinear, scaleThreshold, scaleBand } from 'd3-scale'
+import { max } from 'd3-array'
+import { scaleLinear, scaleBand, scaleOrdinal } from 'd3-scale'
 import { select } from 'd3-selection'
 import { legendColor } from 'd3-svg-legend'
 import { transition } from 'd3-transition'
 import { stack } from 'd3-shape'
+import { axisBottom, axisLeft } from 'd3-axis'
 
 class BarChart extends React.Component {
 
@@ -16,18 +17,18 @@ class BarChart extends React.Component {
   }
 
   createBarChart = () => {
-    if (!this.props.realData) {
+    if (!this.props.data) {
       return
     }
+
     const node = this.node
-    const barWidth = this.props.size[0] / this.getData().length
-    const width = this.props.size[0]
-    const height = this.props.size[1]
-    const colorScaleOld = scaleThreshold().domain([5,10,20,30]).range(["#75739F", "#5EAFC6", "#41A368", "#93C464"])
-    const dataMax = this.getDataMax()
-    const yScale = scaleLinear().domain([0, dataMax]).range([0, this.props.size[1]])
+    const chartWidth = this.props.size[0] - 150
+    const chartHeight = this.props.size[1] - 40
+    const margin = 20
+    const xAxisYpos = this.props.size[1] - margin
     const colorScale = this.setColorScale()
     const services = this.getServices()
+    const data = this.getData()
     const legend = legendColor()
       .scale(colorScale)
       .labels(services)
@@ -41,94 +42,71 @@ class BarChart extends React.Component {
       .call(legend)
 
     // Position legend node
-    //  select(node)
-    //   .select("g.legend")
-    //   .attr("transform", "translate(" + (this.props.size[0] - 100) + ", 20)")
+    select(node)
+      .select("g.legend")
+      .attr("transform", "translate(" + (this.props.size[0] - 100) + ", 20)")
 
-    // select(node)
-    //   .selectAll("rect.bar")
-    //   .data(this.getData())
-    //   .enter()
-    //   .append("rect")
-    //   .attr("class", "bar")
-    //   .on("mouseover", this.props.onHover)
-
-    // select(node)
-    //   .selectAll("rect.bar")
-    //   .data(this.getData())
-    //   .exit()
-    //   .remove()
-
-    console.log(stack().keys(this.getServices())(this.getTestData()))
-    console.log(this.getDates())
-    console.log(this.getServices())
-    console.log(this.getTestData())
-
+    // create chart
     var x = scaleBand()
-        .rangeRound([0, width])
+        .rangeRound([0, chartWidth])
         .paddingInner(0.05)
-        .align(0.1);
+        .align(0.1)
 
     var y = scaleLinear()
-        .rangeRound([height, 0]);
+        .rangeRound([chartHeight, 0])
 
-    x.domain(this.getTestData().map(function(d) { return d.date; }));
-    y.domain([0, d3.max(this.getTestData(), function(d) { return d.total; })]).nice();
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, max(data, function(d) { return d.total; })]).nice();
 
     select(node)
       .selectAll("g.service")
-      .data(stack().keys(this.getServices())(this.getTestData()))
+      .data(stack().keys(services)(data))
       .enter().append("g")
         .attr("class", "service")
-        .attr("fill", (d,i) => this.setColorV2(d,i,colorScale))
+        .attr("fill", (d,i) => this.setColor(d,i,colorScale))
+        .attr("transform", "translate("+margin+","+margin+")")
       .selectAll("rect")
       .data(function(d) { return d; })
       .enter().append("rect")
-        .attr("x", function(d) { console.log(d.data.date);console.log(x(d.data.date)); return x(d.data.date); })
+        .attr("x", function(d) { return x(d.data.date); })
         .attr("y", function(d) { return y(d[1]); })
         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-        .attr("width", x.bandwidth());
+        .attr("width", x.bandwidth())
 
-    // select(node)
-    //   .selectAll("g")
-    //   .data(stack().keys(this.getServices())(this.getTestData()))
-    //   .enter().append("g")
-    //     .attr("fill", (d,i) => this.setColorV2(d,i,colorScale))
-    //   .selectAll("rect")
-    //   .data(function(d) { console.log(d); return d; })
-    //   .enter().append("rect")
-    //     .attr("x", function(d) { console.log(x(d.data.Date)); return x(d.data.Date); })
-    //     .attr("y", function(d) { return y(d[1]); })
-    //     .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-    //     .attr("width", x.bandwidth());
+    // create axis
+    const xAxis = axisBottom().scale(x)
 
-    // select(node)
-    //   .selectAll("rect.bar")
-    //   .data(this.getData())
-    //   .attr("x", (d,i) => this.setX(d,i,barWidth))
-    //   .attr("y", (d) => this.setY(d,yScale))
-    //   .attr("height", (d) => this.setHeight(d,yScale))
-    //   .attr("width", barWidth)
-    //   .style("fill",(d,i) =>  this.setColor(d,i,colorScale))
-    //   .style("stroke", "black")
-    //   .style("stroke-opacity", 0.25)
-  }
+    select(node)
+       .selectAll("g.xaxis")
+       .data([0])
+       .enter()
+       .append("g")
+         .attr("class", "xaxis")
+         .attr("transform", "translate("+margin+","+xAxisYpos+")")
 
-  setX = (d,i,barWidth) => {
-    return i * barWidth
-  }
+    select(node)
+      .select("g.xaxis")
+        .call(xAxis)
 
-  setY = (d, yScale) => {
-    return this.props.size[1] - yScale(d.data)
-  }
+    const yAxis = axisLeft().scale(y).ticks(10)
 
-  setHeight = (d, yScale) => {
-    return yScale(d.data)
-  }
+    select(node)
+       .selectAll("g.yaxis")
+       .data([0])
+       .enter()
+       .append("g")
+         .attr("class", "yaxis")
+         .attr("transform", "translate("+margin+","+margin+")")
 
-  setColorV2 = (d,i,colorScale) => {
-    console.log(d)
-    return colorScale(d.key)
+     select(node)
+      .select("g.yaxis")
+        .call(yAxis)
+      .append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", 6)
+       .attr("dy", ".71em")
+       .style("text-anchor", "end")
+       .text("Value ($)");
   }
 
   setColor = (d,i,colorScale) => {
@@ -138,72 +116,46 @@ class BarChart extends React.Component {
     //   console.log(d.service)
     //   return colorScale(d.service)
     // }
-    return colorScale(d.service)
+    return colorScale(d.key)
   }
 
   setColorScale = () => {
-    let colorScale = d3.scale.category10()
-    if (this.props.realData) {
-      colorScale.domain(this.getServices())
-    }
-    return colorScale
+    return scaleOrdinal()
+      .domain(this.getServices())
+      .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
   }
 
   getServices = () => {
-    if (this.props.realData) {
-      return [].concat(...this.props.realData).map(i => i.service + "::" + i.measure).filter( (item, pos, arr) => arr.indexOf(item)==pos)
-    }
-  }
-
-  getDates = () => {
-    if (this.props.realData) {
-      return [].concat(...this.props.realData).map(i => i.year + "/" + i.month).filter( (item, pos, arr) => arr.indexOf(item)==pos)
+    if (this.props.data) {
+      return [].concat(...this.props.data).map(i => i.service + "::" + i.measure).filter( (item, pos, arr) => arr.indexOf(item)==pos)
     }
   }
 
   getData = () => {
-    if (this.props.realData) {
-      return [].concat(...this.props.realData)
-                .map(i => {
-                  let key = i.year + "/" + i.month
-                  let data = {data: i.price_loc + i.price_sec, service: i.service + "::" + i.measure, date: i.year + "/" + i.month}
-                  data[key] = i.year + "/" + i.month
-                  return data
-                })
-    }
-  }
-
-getTestData = () => {
-  if (this.props.realData) {
-    let resultData = {}
-    let temp = [].concat(...this.props.realData) //flate data
-    temp.map(i => {
-      let key = i.year + "/" + i.month
-      let service = i.service + "::" + i.measure
-      if (resultData[key]) {
-        resultData[key][service] = i.price_loc + i.price_sec
-        resultData[key]["total"] += i.price_loc + i.price_sec
-      } else {
-        resultData[key] = {
-          date: i.year + "/" + i.month,
-          total: 0
+    if (this.props.data) {
+      let resultData = {}
+      let temp = [].concat(...this.props.data) //flate data
+      temp.map(i => {
+        let key = i.year + "/" + i.month
+        let service = i.service + "::" + i.measure
+        if (resultData[key]) {
+          resultData[key][service] = i.price_loc + i.price_sec
+          resultData[key]["total"] += i.price_loc + i.price_sec
+        } else {
+          resultData[key] = {
+            date: i.year + "/" + i.month,
+            total: 0
+          }
+          resultData[key][service] = i.price_loc + i.price_sec
+          resultData[key]["total"] += i.price_loc + i.price_sec
         }
-        resultData[key][service] = i.price_loc + i.price_sec
-        resultData[key]["total"] += i.price_loc + i.price_sec
-      }
-    })
-    return Object.keys(resultData).map(i => resultData[i]) // remove keys to just have array of objects
-  }
-}
-
-  getDataMax = () => {
-    if (this.getData()) {
-      return max(this.getData().map(i => i.data))
+      })
+      return Object.keys(resultData).map(i => resultData[i]) // remove keys to just have array of objects
     }
   }
 
   render() {
-    return <svg ref={node => this.node = node} width={500} height={500}></svg>
+    return <svg ref={node => this.node = node} width={this.props.size[0]} height={this.props.size[1]}></svg>
   }
 }
 export default BarChart;
