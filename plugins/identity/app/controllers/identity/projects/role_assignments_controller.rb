82b70280606f780b162d153a041cf01c34e8b480
Andreas Pfau <andreas.pfau@sap.com>
2018-07-02 13:34:42 +0200

    fixes #313, add domain_id parameter to the  API call list_users by name

--- plugins/identity/app/controllers/identity/projects/role_assignments_controller.rb
+++ plugins/identity/app/controllers/identity/projects/role_assignments_controller.rb
@@ -34,7 +34,7 @@ module Identity
           # render an error if user could not be found
           # Cloud admin is important for the user lookup!
           user = cloud_admin.identity.find_user(user_id) ||
-                 cloud_admin.identity.users(name: user_id).first
+                 cloud_admin.identity.users(name: user_id, domain_id: @scoped_domain_id).first
           unless user
             render json: { errors: "Could not find user with id #{user_id}"}
             return
@@ -48,7 +48,7 @@ module Identity
           # try to load group.
           # render an error if group could not be found
           group = cloud_admin.identity.find_group(group_id) ||
-                  cloud_admin.identity.groups(name: group_id).first
+                  cloud_admin.identity.groups(name: group_id, domain_id: @scoped_domain_id).first
 
           unless group
             render json: { errors: "Could not find group with id #{group_id}"}
