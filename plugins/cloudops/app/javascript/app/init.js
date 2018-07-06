19d9b08a86e02dc5602a98347e2aed98b0f1e65f
Andreas Pfau <andreas.pfau@sap.com>
2018-07-06 16:06:19 +0200

    cloudops: do not reset baseURL on widget init

--- plugins/cloudops/app/javascript/app/init.js
+++ plugins/cloudops/app/javascript/app/init.js
@@ -3,11 +3,7 @@ import * as reducers from './reducers';
 import App from './components/application';
 
 createWidget(__dirname, {html: {class: 'flex-body'}}).then((widget) => {
-  widget.configureAjaxHelper(
-    {
-      baseURL: widget.config.scriptParams.url
-    }
-  )
+  widget.configureAjaxHelper()
   widget.setPolicy()
   widget.createStore(reducers)
   widget.render(App)
