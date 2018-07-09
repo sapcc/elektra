e68eb9ac01d50d83939904bd980f4fb1ca60821c
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-07-09 16:32:50 +0200

    test component

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,11 @@ module.exports = {
   "verbose": true,
   "testRegex": "\\.test\\.js$",
   "transform": {
+    "^.+\\.jsx$": "babel-jest",
     "^.+\\.js$": "babel-jest"
   },
   "moduleNameMapper": {
     "ajax_helper": "<rootDir>/app/javascript/ajax_helper.js"
-  }
+  },
+  "setupTestFrameworkScriptFile": "<rootDir>/plugins/lookup/app/javascript/reverse_app/test/setupEnzyme.js"
 };
--- plugins/lookup/app/javascript/reverse_app/test/components/groups.test.js
+++ plugins/lookup/app/javascript/reverse_app/test/components/groups.test.js
@@ -1,11 +1,18 @@
 import React from 'react'
+
 import Groups from '../../components/groups'
+import { shallow, render } from 'enzyme'
 
 describe('Groups component', () => {
 
-  it('should render the groups', () => {
-    const mockData = {id: "9d4cd354b558aa59a842bc2e74cfd18a0f0ade1ba5277bfdfcacaa50916a8072", name: "CCADMIN_CLOUD_ADMINS"}
-    const wrapper = shallow(<Groups data={mockData}/>);
+  it('should render one groups', () => {
+    const mockGroups = [{id: "9d4cd354b558aa59a842bc2e74cfd18a0f0ade1ba5277bfdfcacaa50916a8072", name: "CCADMIN_CLOUD_ADMINS"}]
+    const mockGroupMembers = {"9d4cd354b558aa59a842bc2e74cfd18a0f0ade1ba5277bfdfcacaa50916a8072": {data: {name: "C5275535", id: "518a33a81eb191d7935065337e22e72ec69d0ed61c55753a08a5264f0ea91303", fullname: "Fabian Peter"}}}
+    const wrapper = shallow(<Groups groups={mockGroups} groupMembers={mockGroupMembers}/>)
+    // console.log(wrapper.debug())
+    expect(wrapper.find("ul.tree").exists()).toBe(true)
+    expect(wrapper.find('li').length).toBe(1)
+    expect(wrapper.find('li').text().includes("CCADMIN_CLOUD_ADMINS")).toBe(true)
   })
 
 })
