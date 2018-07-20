8d98e9977869b4af812e20cb27f81a8c39c485a5
Andreas Pfau <andreas.pfau@sap.com>
2018-07-20 11:30:56 +0200

    react: import babel polyfil on top of import stack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,11 @@
+import "babel-polyfill";
+
 function importAll (r) {
   r.keys().forEach(r);
 }
 
 // import all global js from plugins.
-// Normally, a pack is not loaded until explicitly requested. However, 
+// Normally, a pack is not loaded until explicitly requested. However,
 // there are situations where packs should be globally available. In this case,
 // each such plug-in should define a special file called global.js.
 // Everything within global.js is executed on every page load.
