950d8bb7ed61ef7627b0e1a8510f45b30afbd90e
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-08-17 10:25:47 +0200

    rubocop formatter

--- features/step_definitions/integration/network_subnets_steps.rb
+++ features/step_definitions/integration/network_subnets_steps.rb
@@ -2,9 +2,9 @@ Given(/^the test network for subnets exists$/) do
   unless page.has_content? 'Test Network For Subnets'
     # create test network
     click_on 'Create new'
-    fill_in "network[name]", :with => 'Test Network For Subnets'
-    fill_in "network[subnets][name]", :with => 'Test Network For Subnets Subnet1'
-    fill_in "network[subnets][cidr]", :with => '10.180.0.0/8'
+    fill_in 'network[name]', with: 'Test Network For Subnets'
+    fill_in 'network[subnets][name]', with: 'Test Network For Subnets Subnet1'
+    fill_in 'network[subnets][cidr]', with: '10.180.0.0/8'
     click_on 'Create'
   end
 end
