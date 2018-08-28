7a6dbd06291c97d6370b6e4be2983dd800f189f3
Andreas Pfau <andreas.pfau@sap.com>
2018-08-28 15:19:41 +0200

    fixes #203, networking#routers: show private networks in router list

--- plugins/networking/app/helpers/networking/routers_helper.rb
+++ plugins/networking/app/helpers/networking/routers_helper.rb
@@ -3,5 +3,35 @@ module Networking
     def foreign_router?(router)
       (router.tenant_id || router.project_id) != @scoped_project_id
     end
+
+    def router_internal_subnets(router_id)
+      ports = ObjectCache.where(
+        ["payload ->> 'device_id' = ? AND payload ->> 'device_owner' = ?",
+          router_id, 'network:router_interface'
+        ])
+        .where(cached_object_type: 'port')
+      subnet_ids = ports.each_with_object([]) do |port,array|
+        (port.payload['fixed_ips'] || []).each {|ip| array << ip['subnet_id']}
+      end
+
+      ObjectCache.where(id: subnet_ids)
+    end
+
+    def router_internal_networks(router)
+      ports = ObjectCache.where(
+        ["payload ->> 'device_id' = ? AND payload ->> 'device_owner' = ?",
+          router.id, 'network:router_interface'
+        ]).where(cached_object_type: 'port')
+        
+      ports = cloud_admin.networking.ports(
+        device_id: router.id, device_owner: 'network:router_interface'
+      ) if ports.empty?
+
+      network_ids = ports.collect do |port|
+        port.respond_to?(:payload) ? port.payload['network_id'] : port.network_id
+      end
+
+      ObjectCache.where(id: network_ids)
+    end
   end
 end
