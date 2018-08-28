d62e072924925a361fb06f724275f4cbd6327e87
Andreas Pfau <andreas.pfau@sap.com>
2018-08-28 13:36:24 +0200

    fixes #270, show shares usage data

--- plugins/shared_filesystem_storage/app/controllers/shared_filesystem_storage/application_controller.rb
+++ plugins/shared_filesystem_storage/app/controllers/shared_filesystem_storage/application_controller.rb
@@ -8,5 +8,22 @@ module SharedFilesystemStorage
     # enforce permission checks. This will automatically
     # investigate the rule name.
     authorization_required
+
+    def show
+      @quota_data = []
+      if current_user.is_allowed?("access_to_project")
+        @quota_data = services.resource_management.quota_data(
+          current_user.domain_id || current_user.project_domain_id,
+          current_user.project_id,
+          [
+            { service_type: :sharev2, resource_name: :gigabytes },
+            { service_type: :sharev2, resource_name: :shares },
+            { service_type: :sharev2, resource_name: :snapshots },
+            { service_type: :sharev2, resource_name: :share_networks },
+            { service_type: :sharev2, resource_name: :share_groups }
+          ]
+        )
+      end
+    end
   end
 end
