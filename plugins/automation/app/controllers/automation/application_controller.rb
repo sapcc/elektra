938fcd65a38df827d4f5bc80b6972e7bfd95b390
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-09-14 11:07:57 +0200

    [automation]: catch backend exceptions when using polling
    Fixed #280

--- plugins/automation/app/controllers/automation/application_controller.rb
+++ plugins/automation/app/controllers/automation/application_controller.rb
@@ -1,16 +1,28 @@
 module Automation
-
   class ApplicationController < DashboardController
-    rescue_and_render_exception_page [
-      {
-        "ArcClient::ApiError" => {
-          header_title: "Monsoon Automation", 
-          details: -> e, c { e.json_hash.empty? ? e.inspect : e.json_hash},
-          description: :title,
-          title: :status
-        }
+    rescue_from 'ArcClient::ApiError' do |exception|
+      options = {
+        title: :title,
+        description: :detail,
+        warning: true, sentry: true
       }
-    ]
-  end
 
+      if params[:polling_service]
+        head status: 500
+      else
+        render_exception_page(exception, options)
+      end
+    end
+
+    # rescue_and_render_exception_page [
+    #   {
+    #     "ArcClient::ApiError" => {
+    #       header_title: "Monsoon Automation",
+    #       details: -> e, c { e.json_hash.empty? ? e.inspect : e.json_hash},
+    #       description: :title,
+    #       title: :status
+    #     }
+    #   }
+    # ]
+  end
 end
