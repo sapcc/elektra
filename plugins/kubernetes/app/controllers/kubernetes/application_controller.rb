377da241f5881bc15d6c895fc9f528b04730a50e
Michael Schmidt <michael.j.schmidt@gmail.com>
2018-09-21 13:59:25 +0200

    GA for Kubernikus (#373)

--- plugins/kubernetes/app/controllers/kubernetes/application_controller.rb
+++ plugins/kubernetes/app/controllers/kubernetes/application_controller.rb
@@ -6,15 +6,12 @@ module Kubernetes
     # authorization_required
 
     def release_state
-      'beta'
+      'public_release'
     end
 
     def index
       # enforce_permissions('kubernetes:application_list')
       @kubernikus_endpoint = current_user.has_service?("kubernikus-#{@scoped_project_name}") ? current_user.service_url("kubernikus-#{@scoped_project_name}") : current_user.service_url('kubernikus')
-
-      # Settings from Elektra extension
-      @beta_contact = {name: Settings.beta_contact_name, email: Settings.beta_contact_email}
     end
   end
 end
