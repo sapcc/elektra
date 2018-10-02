ccecaab514276e6eef818bdb728d532e7baed572
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-10-02 18:45:11 +0200

    rubocop

--- app/controllers/concerns/error_renderer.rb
+++ app/controllers/concerns/error_renderer.rb
@@ -18,13 +18,15 @@ module ErrorRenderer
 
   # instance methods
   module InstanceMethods
-    def render_exception_page(exception,map={})
+    def render_exception_page(exception, map = {})
       raise exception if Rails.env.development? && ENV.key?('NO_EXCEPTION_PAGE')
+
       value = lambda do |param|
         v = map[param.to_sym] || map[param.to_s]
         return nil if v.nil?
         return exception.send(v).to_s if v.is_a?(Symbol)
         return v.call(exception, self).to_s if v.is_a?(Proc)
+
         return v.to_s
       end
 
@@ -38,53 +40,64 @@ module ErrorRenderer
 
       begin
         @title = value.call(:title) || exception.class.name.split('::').last.humanize
-        @description = value.call(:description) || (exception.message rescue exception.to_s)
-        @details = value.call(:details) || exception.class.name+"\n"+(exception.backtrace rescue '').join("\n")
+        @description = value.call(:description) || (begin
+                                                      exception.message
+                                                    rescue StandardError
+                                                      exception.to_s
+                                                    end)
+        @details = value.call(:details) || exception.class.name + "\n" + (begin
+                                                                        exception.backtrace
+                                                                      rescue StandardError
+                                                                        ''
+                                                                      end).join("\n")
         @exception_id = value.call(:exception_id) || request.uuid
         @warning = value.call(:warning) || false
         @status = value.call(:status) || status
-      rescue => e
+      rescue StandardError => e
         @title = e.class.name.split('::').last.humanize
         @description = e.message
-        @details = e.class.name+"\n"+(e.backtrace rescue '').join("\n")
+        @details = e.class.name + "\n" + (begin
+                                        e.backtrace
+                                      rescue StandardError
+                                        ''
+                                      end).join("\n")
         @exception_id = request.uuid
         @warning = false
         @status = 503
       end
 
-      unless @warning
+      if @warning
+        if request.xhr? && params[:polling_service]
+          render '/application/exceptions/error_polling.js', format: 'JS'
+        else
+          # byebug
+          respond_to do |format|
+            format.html { render '/application/exceptions/warning.html' }
+            format.js { render '/application/exceptions/warning.js' }
+            format.json { render json: { error: @description }, status: @status }
+          end
+        end
+      else
         logger.error("#{@exception_id}: #{@title}. #{@description}")
 
-        unless map[:sentry]==false
+        unless map[:sentry] == false
           Raven::Rack.capture_exception(exception, request.env)
           @exception_id = Raven.last_event_id if Raven.last_event_id
         end
         @sentry_event_id   = Raven.last_event_id
-        @sentry_public_dsn = URI.parse(ENV['SENTRY_DSN']).tap {|u| u.password=nil}.to_s if ENV['SENTRY_DSN']
+        @sentry_public_dsn = URI.parse(ENV['SENTRY_DSN']).tap { |u| u.password = nil }.to_s if ENV['SENTRY_DSN']
         @sentry_user       = { name: current_user.full_name || current_user.name, email: current_user.email } if current_user
 
         # no render error if polling service
         unless params[:polling_service]
           respond_to do |format|
             format.html { render '/application/exceptions/error.html' }
-            format.js { render "/application/exceptions/error.js" }
-            format.json {render json: {error: @description}, status: @status}
-          end
-        end
-      else
-        if request.xhr? && params[:polling_service]
-          render "/application/exceptions/error_polling.js", format: "JS"
-        else
-          # byebug
-          respond_to do |format|
-            format.html { render '/application/exceptions/warning.html' }
-            format.js { render "/application/exceptions/warning.js" }
-            format.json { render json: {error: @description}, status: @status }
+            format.js { render '/application/exceptions/error.js' }
+            format.json { render json: { error: @description }, status: @status }
           end
         end
       end
     rescue AbstractController::DoubleRenderError => _e
-
     end
   end
 
@@ -102,7 +115,7 @@ module ErrorRenderer
 
       exception_classes.each do |exception_class|
         if exception_class.is_a?(Hash)
-          exception_mapping[exception_class.keys.first.to_s]=exception_class.values.first
+          exception_mapping[exception_class.keys.first.to_s] = exception_class.values.first
           klasses << exception_class.keys.first.to_s
         else
           klasses << exception_class
@@ -113,18 +126,16 @@ module ErrorRenderer
         map = exception_mapping[exception.class.name]
         unless map
           klass = nil
-          exception_mapping.each do |class_name,mapping|
+          exception_mapping.each do |class_name, mapping|
             found_class = eval(class_name)
             next if klass && found_class > klass
 
-            if found_class > exception.class
-              map = mapping
-            end
+            map = mapping if found_class > exception.class
           end
           map ||= {}
         end
 
-        render_exception_page(exception,map)
+        render_exception_page(exception, map)
       end
     end
   end
