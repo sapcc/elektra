b68beeb1e6d9b5fecd2d44bec7d10b741e65fafb
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2018-10-05 13:31:29 +0200

    Added Atention box when exception is raised

--- plugins/automation/app/controllers/automation/application_controller.rb
+++ plugins/automation/app/controllers/automation/application_controller.rb
@@ -24,5 +24,34 @@ module Automation
     #     }
     #   }
     # ]
+
+    rescue_from 'MonsoonOpenstackAuth::Authorization::SecurityViolation' do |exception|
+      if exception.resource[:action] == 'index' && exception.resource[:controller] == 'automation/nodes'
+        @title = 'Unauthorized'
+        @status = 401
+        @description = 'You are not authorized to view this page.'
+        if exception.respond_to?(:involved_roles) && exception.involved_roles && exception.involved_roles.length.positive?
+          @description += " Please check (role assignments) if you have one of the following roles: #{exception.involved_roles.flatten.join(', ')}."
+        end
+        render '/automation/shared/warning.html', status: @status
+      end
+
+      options = {
+        title: 'Unauthorized',
+        sentry: false,
+        warning: true,
+        status: 401,
+        description: lambda do |e, _c|
+          m = 'You are not authorized to view this page.'
+          if e.involved_roles && e.involved_roles.length.positive?
+            m += " Please check (role assignments) if you have one of the \
+          following roles: #{e.involved_roles.flatten.join(', ')}."
+          end
+          m
+        end
+      }
+
+      render_exception_page(exception, options)
+    end
   end
 end
