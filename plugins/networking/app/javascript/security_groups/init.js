cfaa02ab627af496cf8f299924cf58fedd88aee2
Andreas Pfau <andreas.pfau@sap.com>
2018-10-10 11:26:03 +0200

    replace title by react component to allow to switch between components fast

--- plugins/networking/app/javascript/security_groups/init.js
+++ plugins/networking/app/javascript/security_groups/init.js
@@ -1,6 +1,7 @@
 import { createWidget } from 'widget'
 import * as reducers from './reducers';
 import App from './components/application';
+import Title from './components/title'
 
 createWidget().then((widget) => {
   widget.configureAjaxHelper(
@@ -12,3 +13,8 @@ createWidget().then((widget) => {
   widget.createStore(reducers)
   widget.render(App)
 })
+
+let title = document.getElementsByClassName('page-title');
+
+// const ShowTheLocationWithRouter = withRouter(ShowTheLocation);
+if(title && title.length>0) ReactDOM.render(<Title/>,title[0])
