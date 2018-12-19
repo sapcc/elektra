4b817d9aebaa4e61416c9b227cb78f5c23de89f3
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2018-12-19 13:49:37 +0100

    show allocation type in cost report to identify burst

--- plugins/reports/app/javascript/app/components/serviceDetail.jsx
+++ plugins/reports/app/javascript/app/components/serviceDetail.jsx
@@ -8,7 +8,7 @@ const DetailsViewHighlightTransition = ({
   {children}
 </CSSTransition>);
 
-const keysNotPrinted = ["region", "year", "month", "project_id", "price_sec", "allocation_type"]
+const keysNotPrinted = ["region", "year", "month", "project_id", "price_sec"]
 const keysNotFormatted = ["cost_object", "object_id"]
 
 const renderValue = (props, key) => {
