e7e97cbcfa9db6f663e5dbd663808b6524d7069c
Stefan Majewsky <stefan.majewsky@sap.com>
2019-01-08 15:38:34 +0100

    resmgmt project overview: add a navbar

--- plugins/resources/app/javascript/app/components/project/overview.jsx
+++ plugins/resources/app/javascript/app/components/project/overview.jsx
@@ -1,5 +1,9 @@
+import { STRINGS } from '../../constants';
+
 export default class ProjectOverview extends React.Component {
-  state = {}
+  state = {
+    currentArea: null,
+  }
 
   componentWillReceiveProps(nextProps) {
     // load dependencies unless already loaded
@@ -18,15 +22,50 @@ export default class ProjectOverview extends React.Component {
     })
   }
 
+  changeArea = (area) => {
+    this.setState({...this.state, currentArea: area});
+  }
+
+  // TODO make this its own component
+  renderNavbar(currentArea) {
+    return (
+      <nav className='nav-with-buttons'>
+        <ul className='nav nav-tabs'>
+          { Object.keys(this.props.overview.areas).sort().map((area, idx) => (
+            <li key={area} role="presentation" className={currentArea == area ? "active" : ""}>
+              <a href="#" onClick={(e) => { e.preventDefault(); this.changeArea(area); }}>
+                {STRINGS[area]}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+
   render() {
     if (!policy.isAllowed("project:show")) {
-      return <span>You are not allowed to see this page</span>;
+      return <p>You are not allowed to see this page</p>;
     }
 
-    // console.log("Props for ProjectOverview");
-    // console.log(this.props);
+    const props = this.props;
+    if (props.isFetching) {
+      return <p><span className='spinner'/> Loading project...</p>;
+    }
+    console.log("Props for ProjectOverview");
+    console.log(this.props);
+
+    const currentArea = this.state.currentArea || Object.keys(props.overview.areas).sort()[0];
+
+    // TODO: overview page with critical resources?
+    // TODO: Settings dialog for enabling/disabling bursting
+    // TODO: button: Request Quota Package
+
     return (
       <React.Fragment>
+        {this.renderNavbar(currentArea)}
+        <p>This is my state:</p>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
         <p>These are my props:</p>
         <pre>{JSON.stringify(this.props, null, 2)}</pre>
       </React.Fragment>
--- plugins/resources/app/javascript/app/containers/project/overview.js
+++ plugins/resources/app/javascript/app/containers/project/overview.js
@@ -6,6 +6,7 @@ import {
 
 export default connect(
   (state, props) => ({
+    isFetching: state.project.isFetching || state.project.receivedAt == null,
     metadata: state.project.metadata,
     overview: state.project.overview,
   }),
