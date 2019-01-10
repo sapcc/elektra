69ab7dbc7ac9702e7514d493f564edd46234b6da
Stefan Majewsky <stefan.majewsky@sap.com>
2019-01-10 15:41:12 +0100

    rough draft for resource bars

--- plugins/resources/app/javascript/app/components/project/resource.jsx
+++ plugins/resources/app/javascript/app/components/project/resource.jsx
@@ -16,16 +16,83 @@ const ResourceName = ({name, flavorData}) => {
   );
 };
 
+const ResourceError = (props) => (
+  <p className='resource-error text-danger'>
+    <i className='fa fa-lg fa-warning' />{' '}
+    {props.children}
+  </p>
+);
+
 export default class ProjectResource extends React.Component {
   state = {}
+
+  renderBarContents(quota, usage, desiredBackendQuota) {
+    //get some edge cases out of the way first
+    if (quota == 0 && usage == 0) {
+      return <div className='progress-bar progress-bar-disabled' style={{width:'100%'}} />;
+    }
+    if (usage > desiredBackendQuota) {
+      return <div className='progress-bar progress-bar-danger progress-bar-striped' style={{width:'100%'}} />;
+    }
+
+    //common case: quota, usage <= desiredBackendQuota
+    //NOTE: 100% of the bar's width is always equal to desiredBackendQuota.
+    const usageWidthPerc = 100 * (usage / desiredBackendQuota);
+    const quotaWidthPerc = 100 * (quota / desiredBackendQuota);
+    const bars = [
+      <div key='usage'
+        className={usage >= quota ? 'progress-bar progress-bar-warning' : 'progress-bar'}
+        style={{width: usageWidthPerc + '%'}}
+      />
+    ];
+    //bridge the gap between usage and the shaded bursting area
+    if (usageWidthPerc < quotaWidthPerc - 0.01) {
+      bars.push(<div key='empty'
+        className='progress-bar progress-bar-empty'
+        style={{width: (quotaWidthPerc - usageWidthPerc) + '%'}}
+      />);
+    }
+    //shade the area that can only be reached by bursting
+    if (quotaWidthPerc < 99.99) {
+      bars.push(<div key='burst'
+        className='progress-bar progress-bar-burst'
+        style={{width: (100 - quotaWidthPerc) + '%'}}
+      />);
+    }
+
+    return bars;
+  }
 
   render() {
     const displayName = t(this.props.resource.name);
     const flavorData = this.props.flavorData[displayName] || {};
 
+    const { quota, usage, backendQuota } = this.props.resource || {};
+    const { enabled: hasBursting, multiplier: burstMultiplier } =
+      this.props.metadata.bursting || {};
+
+    const desiredBackendQuota =
+      hasBursting ? Math.floor(quota * (1 + burstMultiplier)) : quota;
+    const actualBackendQuota = backendQuota == null ? desiredBackendQuota : backendQuota;
+
+    //TODO: value formatting in ResourceError
+    //TODO: value formatting in columns
     return (
       <div className='row'>
         <ResourceName name={displayName} flavorData={flavorData} />
+        <div className='col-md-6'>
+          <div className='progress'>
+            {this.renderBarContents(quota, usage, desiredBackendQuota)}
+          </div>
+          {usage > desiredBackendQuota && <ResourceError>
+            Usage ({usage}) exceeds expected backend quota ({desiredBackendQuota}).
+          </ResourceError>}
+          {desiredBackendQuota != actualBackendQuota && <ResourceError>
+            Expected backend quota to be {desiredBackendQuota}, but is {actualBackendQuota}.
+          </ResourceError>}
+        </div>
+        <div className='col-md-1 text-right'>{usage}</div>
+        <div className='col-md-1 text-right'>{quota}</div>
       </div>
     );
   }
