d5c9723d3b7ab8caa44c519b5ce6912072e0101a
Stefan Majewsky <stefan.majewsky@sap.com>
2019-01-15 14:35:31 +0100

    feedback from Esther

--- plugins/resources/app/javascript/app/components/project/resource.jsx
+++ plugins/resources/app/javascript/app/components/project/resource.jsx
@@ -5,12 +5,13 @@ import { byUIString, t, formatLargeInteger } from '../../utils';
 
 const ResourceName = ({name, flavorData}) => {
   if (!flavorData.primary || !flavorData.secondary) {
-    return <div className='col-md-2'>{name}</div>;
+    return <div className='col-md-2 text-right'>{name}</div>;
   }
 
   let tooltip = <Tooltip id={`tooltip-${name}`} className='tooltip-no-break'>{flavorData.secondary}</Tooltip>;
+  // TODO: line breaks make the .small.text-muted.flavor-data look ugly -- maybe place below bar like we had for .resource-error?
   return (
-    <div className='col-md-2'>
+    <div className='col-md-2 text-right'>
       <OverlayTrigger overlay={tooltip} placement='right' delayShow={300} delayHide={150}><span>{name}</span></OverlayTrigger>
       <div className='small text-muted flavor-data'>{flavorData.primary}</div>
     </div>
