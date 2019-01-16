fc930e06d86fcb303da63b633df19610d1cc8ede
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-01-16 14:22:33 +0100

    [automation] added comment

--- plugins/automation/config/initializers/automation_config.rb
+++ plugins/automation/config/initializers/automation_config.rb
@@ -2,6 +2,7 @@ require 'restclient'
 
 # RestClient logs using << which isn't supported by the Rails logger,
 # so wrap it up with a little proxy object.
+# downgraded to debug level in production because of sensitive data (tokens)
 RestClient.log =
   Object.new.tap do |proxy|
     def proxy.<<(message)
