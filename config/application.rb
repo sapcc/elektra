d6328905a5c31a2c76738ff791f7123053469a5f
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2019-04-08 13:45:55 +0200

    added config to use web-console in prod

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module MonsoonDashboard
     config.load_defaults 5.2
     config.react.addons = true
 
+    config.web_console.development_only = false
+
     # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
     # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
     config.require_master_key = false
