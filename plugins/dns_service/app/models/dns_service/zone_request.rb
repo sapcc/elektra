04285866db5682c6740adaba60a94c7c378bf871
Hans-Georg Winkler <Hans-Georg.Winkler@sap.com>
2019-04-12 16:13:16 +0200

    value checks for fqdn and email

--- plugins/dns_service/app/models/dns_service/zone_request.rb
+++ plugins/dns_service/app/models/dns_service/zone_request.rb
@@ -4,8 +4,12 @@ module DnsService
   class ZoneRequest < Core::ServiceLayer::Model
     validates :domain_type, presence: { message: 'Please choose a domain type' }
     validates :domain_pool, presence: { message: 'Please select a domain pool' }
-    validates :name, presence: { message: 'Please provide the domain name' }
-    validates :email, presence: { message: 'Please provide an email' }
+    validates :name,
+      presence: { message: 'Please provide the domain name' }
+    validates :email,
+      presence: { message: 'Please provide an email' },
+      format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Please use a valid email address" }
+    validate :simple_name_check
 
     def subdomain?
       domain_type == 'subdomain'
@@ -21,5 +25,34 @@ module DnsService
       return "#{fq_name}." unless fq_name.last == '.'
       fq_name
     end
+
+    def simple_name_check
+      # https://stackoverflow.com/questions/11809631/fully-qualified-domain-name-validation
+      # Hostnames are composed of a series of labels concatenated with dots. Each label is 1 to 63 characters long, and may contain:
+      # the ASCII letters a-z and A-Z, the digits 0-9, and the hyphen ('-'). Additionally: labels cannot start or end with hyphens (RFC 952)
+      # labels can start with numbers (RFC 1123) trailing dot is not allowed max length of ascii hostname including dots is 253 characters
+      if domain_type == "subdomain"
+        if name.include?("_")
+          errors.add('name','Do not use underscore in subdomain names')
+        end
+        if name.include?(".")
+          errors.add('name','Dots are not allowed in subdomain names')
+        end
+        if name.start_with?("-")
+          errors.add('name','Subdomain name cannot start with "-"')
+        end
+        if name.end_with?("-")
+          errors.add('name','Subdomain name cannot end with "-"')
+        end
+        if name.length > 63
+          errors.add('name','Subdomain name cannot longer than 63 characters')
+        end
+      else
+        unless name.match(/(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,63}$)/i)
+          errors.add('name','Please use a fully qualified domain name')
+        end
+      end
+    end
+
   end
 end
