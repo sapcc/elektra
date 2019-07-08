c8d760f9a27b5bfd6b3d950415d28b4c2699e2f7
Stefan Majewsky <stefan.majewsky@sap.com>
2019-07-08 13:10:49 +0200

    render assets with scraping errors into a proper table

--- plugins/shared_filesystem_storage/app/javascript/app/containers/castellum/scraping_errors.js
+++ plugins/shared_filesystem_storage/app/javascript/app/containers/castellum/scraping_errors.js
@@ -1,14 +1,18 @@
 import { connect } from  'react-redux';
 import CastellumScrapingErrors from '../../components/castellum/scraping_errors';
 import { fetchCastellumDataIfNeeded } from '../../actions/castellum';
+import { deleteShare, forceDeleteShare } from '../../actions/shares';
 
 const path = 'assets/nfs-shares';
 export default connect(
   state => ({
     assets: (state.castellum || {})[path],
+    shares: (state.shares || {}).items,
   }),
   dispatch => ({
     loadAssetsOnce: (projectID) =>
       dispatch(fetchCastellumDataIfNeeded(projectID, path)),
+    handleDelete:      (shareID) => dispatch(deleteShare(shareID)),
+    handleForceDelete: (shareID) => dispatch(forceDeleteShare(shareID))
   }),
 )(CastellumScrapingErrors);
