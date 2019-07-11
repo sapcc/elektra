3b8439dde223e5da4faa39d05d67cd4290286320
Stefan Majewsky <stefan.majewsky@sap.com>
2019-07-11 13:33:58 +0200

    fix TypeError when size_constraints is null

--- plugins/shared_filesystem_storage/app/javascript/app/components/castellum/configuration/view.jsx
+++ plugins/shared_filesystem_storage/app/javascript/app/components/castellum/configuration/view.jsx
@@ -38,7 +38,7 @@ export default class CastellumConfigurationView extends React.Component {
         <p>Autoscaling is enabled:</p>
         <ul>
           {config.low_threshold && <li>Shares will be shrunk when usage is below <strong>{percent(config.low_threshold.usage_percent)}</strong> for <strong>{duration(config.low_threshold.delay_seconds)}</strong>.</li>}
-          {config.high_threshold && <li>Shares will be extended when usage exceeds <strong>{percent(config.high_threshold.usage_percent)}</strong> {config.size_constraints.minimum_free && <React.Fragment>(or when free space is below <strong>{config.size_constraints.minimum_free} GiB</strong>)</React.Fragment>} for <strong>{duration(config.high_threshold.delay_seconds)}</strong>.</li>}
+          {config.high_threshold && <li>Shares will be extended when usage exceeds <strong>{percent(config.high_threshold.usage_percent)}</strong> {config.size_constraints && config.size_constraints.minimum_free && <React.Fragment>(or when free space is below <strong>{config.size_constraints.minimum_free} GiB</strong>)</React.Fragment>} for <strong>{duration(config.high_threshold.delay_seconds)}</strong>.</li>}
           {config.critical_threshold && <li>Shares will be extended immediately when usage exceeds <strong>{percent(config.critical_threshold.usage_percent)}</strong>.</li>}
         </ul>
         {config.size_constraints ? (
