af1943df497bc97e8f971f37c14b55171f35e97e
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2020-02-21 23:04:25 +0100

    [lbaas2]: optimized with memo

--- plugins/lbaas2/app/javascript/app/components/LoadbalancerList.js
+++ plugins/lbaas2/app/javascript/app/components/LoadbalancerList.js
@@ -1,79 +1,63 @@
 import React from 'react'
 import { useDispatch, useGlobalState } from './StateProvider'
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 import { ajaxHelper } from 'ajax_helper';
 import LoadbalancerItem from './LoadbalancerItem';
-import Unavailable from './Unavailable';
+import ErrorPage from './ErrorPage';
 
 const LoadbalancerList = () => {
   const dispatch = useDispatch()
   const state = useGlobalState().loadbalancers
   const items = state.items
 
-  const errorMessage = (error) =>
-    error.response && error.response.data && (error.response.data.errors || error.response.data.error) ||
-    error.message
-
   useEffect(() => {
-    console.log('fetch loadbalancers')
+    console.log('fetch initial loadbalancers')
     dispatch({type: 'REQUEST_LOADBALANCERS', requestedAt: Date.now()})
     ajaxHelper.get(`/loadbalancers`).then((response) => {
       dispatch({type: 'RECEIVE_LOADBALANCERS', items: response.data.loadbalancers, hasNext: response.data.has_next})
     })
     .catch( (error) => {
-      console.log("error loading loadbalancers -->" + error.response.data.error)
-      dispatch({type: 'REQUEST_LOADBALANCERS_FAILURE', error: errorMessage(error)})
+      dispatch({type: 'REQUEST_LOADBALANCERS_FAILURE', error: error})
     })
   }, []);
 
-  return (
-    <React.Fragment>
-      { state.isLoading ? 
-        <span className='spinner'/> 
-        :
-        <React.Fragment>
-          { state.error ? 
-            <h1>Snap!
-              {/* <Unavailable errorMessage={state.error} /> */}
-            </h1>            
+  return useMemo(() => {
+    console.log("RENDER loadbalancer list")
+    return (
+      <table className="table loadbalancers">
+        <thead>
+            <tr>
+                <th>Name/ID</th>
+                <th>Description</th>
+                <th>State</th>
+                <th>Prov. Status</th>
+                <th>Tags</th>
+                <th className="snug-nowrap">Subnet/IP Address</th>
+                <th>Listeners</th>
+                <th>Pools</th>
+                <th className='snug'></th>
+            </tr>
+        </thead>
+        <tbody>
+          {items && items.length>0 ?
+            items.map( (loadbalancer, index) =>
+              <LoadbalancerItem 
+                loadbalancer={loadbalancer}
+                key={index}
+              />
+            )
             :
-            <table className="table loadbalancers">
-              <thead>
-                  <tr>
-                      <th>Name/ID</th>
-                      <th>Description</th>
-                      <th>State</th>
-                      <th>Prov. Status</th>
-                      <th>Tags</th>
-                      <th className="snug-nowrap">Subnet/IP Address</th>
-                      <th>Listeners</th>
-                      <th>Pools</th>
-                      <th className='snug'></th>
-                  </tr>
-              </thead>
-              <tbody>
-                {items && items.length>0 ?
-                  items.map( (loadbalancer, index) =>
-                    <LoadbalancerItem 
-                      loadbalancer={loadbalancer}
-                      key={index}
-                    />
-                  )
-                  :
-                  <tr>
-                    <td colSpan="8">
-                      { state.isLoading ? <span className='spinner'/> : 'No loadbalancers found.' }
-                    </td>
-                  </tr>  
-                }
-              </tbody>
-            </table>
+            <tr>
+              <td colSpan="8">
+                { state.isLoading ? <span className='spinner'/> : 'No loadbalancers found.' }
+              </td>
+            </tr>  
           }
-        </React.Fragment>
-      }
-    </React.Fragment>
-  )
-
+        </tbody>
+    </table>
+    )
+  }, [items])
+  
 }
 
 export default LoadbalancerList;
