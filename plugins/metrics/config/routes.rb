61fe418f5d39a7f2951e6083df2a32dbc391c989
d058695 <stefan.hipfel@sap.com>
2020-03-11 18:20:12 +0100

    moves gaas function to application_controller

--- plugins/metrics/config/routes.rb
+++ plugins/metrics/config/routes.rb
@@ -1,5 +1,5 @@
 Metrics::Engine.routes.draw do
   get '/' => 'application#index', as: :index
   get '/maia' => 'application#maia', as: :maia
-  get '/grafana' => 'gaas#gaas', as: :gaas
+  get '/grafana' => 'application#gaas', as: :gaas
 end
