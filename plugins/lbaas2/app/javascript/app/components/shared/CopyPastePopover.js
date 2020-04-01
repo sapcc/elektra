1b1185372c9b2a15f288a79ac5b09857a506249b
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2020-04-01 22:14:34 +0200

    [lbaas]: copy paste plugin

--- plugins/lbaas2/app/javascript/app/components/shared/CopyPastePopover.js
+++ plugins/lbaas2/app/javascript/app/components/shared/CopyPastePopover.js
@@ -1,48 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uniqueId from 'lodash/uniqueId'
-import { OverlayTrigger, Popover } from 'react-bootstrap'
+import { OverlayTrigger, Overlay, Popover, Tooltip, Button } from 'react-bootstrap'
+import Clipboard from 'react-clipboard.js';
 
 const CopyPastePopover = ({text, size}) => {
+  const [showTooltip, setShowTooltip] = useState(false)
+  const [target, setTarget] = useState(null)
+  const [showIcon, setShowIcon] = useState(false)
 
-  const handleCopy = (e) => {
-    e.preventDefault();
-    
+  const onCopySuccess = event => {
+    setShowTooltip(true)
+    setTimeout(() => setShowTooltip(false),500)
   }
 
-  
-  const textId = uniqueId("copy-text-")
-  const clipboard = new ClipboardJS(`#${textId}`);
-  clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    e.clearSelection();
-  });
+  const tooltip = <Overlay
+      show={showTooltip}
+      placement="top"
+      container={this}
+      target={target}
+    >
+      <Tooltip id={uniqueId("copy-paste-tooltip-")}>Copied!</Tooltip>
+    </Overlay>
 
-  clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-  });
-
-  const popOver =  <Popover id={uniqueId("help-popover-")}>
+  const popOver =  <Popover id={uniqueId("copy-paste-popover-")}>
     <div className="lbaas2">
-        <span id={textId}>{text}</span>
+        <span>{text}</span>
         <div className="text-right">
-          <a className='help-link' href='javascript:void(0)'>
+          <Clipboard ref={cb => {setTarget(cb)}} className="btn btn-link" data-clipboard-text={text} onSuccess={onCopySuccess}>
             <i className="fa fa-copy"></i>
-          </a>
+          </Clipboard>
+          {tooltip}
         </div>
     </div>
   </Popover>
 
+  const onMouseEnter = event => {
+    if (showIcon) return 
+    setShowIcon(true)
+  }
+
+  const onMouseLeave = event => {
+    setShowIcon(false)
+  }
+
   return ( 
     <React.Fragment>
       { text.length>size ?
-        <div className="display-flex">
+        <div className="cp">
           <span>{text.slice(0,size)}</span>
-          <div className="dots-help">
-            <OverlayTrigger trigger="click" placement="top" rootClose 
-              overlay={popOver}>
+          <div className="cp-dots-help">
+            <OverlayTrigger trigger="click" placement="top" rootClose overlay={popOver}>
               <a className='help-link' href='javascript:void(0)'>
                 <i className="fa fa-ellipsis-h"></i>
               </a>
@@ -50,13 +57,18 @@ const CopyPastePopover = ({text, size}) => {
           </div>
         </div>
       :
-        <div className="display-flex">
-          <span>{text}</span>
-          <div className="copy-paste-help">
-            <a className='help-link' href='javascript:void(0)' onClick={handleCopy}>
-              <i className="fa fa-copy"></i>
-            </a>
-          </div>
+        <div className="cp" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+          <span>
+            {text}
+          </span>
+            {showIcon &&
+              <div className="copy-paste-icon">
+                <Clipboard ref={cb => {setTarget(cb)}} className="btn btn-link" data-clipboard-text={text} onSuccess={onCopySuccess}>
+                  <i className="fa fa-copy"></i>
+                </Clipboard>
+                {tooltip}
+              </div>
+            }
         </div>
       }
     </React.Fragment>
