4cca85dec1f15cb753eb36238596245a65ee44bb
ArtieReus <reuschenbach@gmail.com>
2020-08-13 14:01:47 +0200

    lbaas2 switcher with info text and back link

--- plugins/lbaas2/app/javascript/app/App.js
+++ plugins/lbaas2/app/javascript/app/App.js
@@ -1,18 +1,33 @@
-import {StateProvider} from './components/StateProvider'
-import reducers from './reducers'
-import Router from './components/Router'
+import { StateProvider } from "./components/StateProvider"
+import reducers from "./reducers"
+import Router from "./components/Router"
 // import { FlashMessages } from 'lib/flashes';
-import FloatingFlashMessages from  './components/shared/FloatingFlashMessages'
+import FloatingFlashMessages from "./components/shared/FloatingFlashMessages"
 
 const App = () => {
   console.log("RENDER App")
   return (
     <React.Fragment>
-      {/* <FlashMessages/>*/}
-      <FloatingFlashMessages/>
-      <Router/>
+      <div className="bs-callout bs-callout-info bs-callout-emphasize">
+        <p>
+          This is the new beta UI based on Octavia driver for Neutron LBaaS.
+          Feel free to try it! We are happy to get your feedback on our slack
+          channel{" "}
+          <a href="https://convergedcloud.slack.com/archives/C018SPLDB9Q">
+            #octavia-users
+          </a>
+          .
+        </p>
+      </div>
+
+      <FloatingFlashMessages />
+      <Router />
     </React.Fragment>
   )
 }
 
-export default () => <StateProvider reducers={reducers}><App/></StateProvider>
+export default () => (
+  <StateProvider reducers={reducers}>
+    <App />
+  </StateProvider>
+)
