51a409738d92f77922cf0909e881c4df29cce6a6
Arturo Reuschenbach <a.reuschenbach.puncernau@sap.com>
2020-12-01 10:41:39 +0100

    [lbaas2]: custom dropdown menus for scrolling container (#798)
    * working scroll
    
    * [lbaas2]: custom dropdowns applied in the scroll containers

--- plugins/lbaas2/app/javascript/app/components/l7Rules/L7RuleListItem.js
+++ plugins/lbaas2/app/javascript/app/components/l7Rules/L7RuleListItem.js
@@ -9,6 +9,7 @@ import SmartLink from "../shared/SmartLink"
 import { policy } from "policy"
 import { scope } from "ajax_helper"
 import Log from "../shared/logger"
+import DropDownMenu from '../shared/DropdownMenu'
 
 const L7RuleListItem = ({
   props,
@@ -135,49 +136,39 @@ const L7RuleListItem = ({
         />
       </td>
       <td>
-        <div className="btn-group">
-          <button
-            className="btn btn-default btn-sm dropdown-toggle"
-            type="button"
-            data-toggle="dropdown"
-            aria-expanded={true}
-          >
-            <span className="fa fa-cog"></span>
-          </button>
-          <ul className="dropdown-menu dropdown-menu-right" role="menu">
-            <li>
-              <SmartLink
-                to={`/loadbalancers/${loadbalancerID}/listeners/${listenerID}/l7policies/${l7PolicyID}/l7rules/${
-                  l7Rule.id
-                }/edit?${searchParamsToString(props)}`}
-                isAllowed={canEdit}
-                notAllowedText="Not allowed to edit. Please check with your administrator."
-              >
-                Edit
-              </SmartLink>
-            </li>
-            <li>
-              <SmartLink
-                onClick={handleDelete}
-                isAllowed={canDelete}
-                notAllowedText="Not allowed to delete. Please check with your administrator."
-              >
-                Delete
-              </SmartLink>
-            </li>
-            <li>
-              <SmartLink
-                to={`/loadbalancers/${loadbalancerID}/listeners/${listenerID}/l7policies/${l7PolicyID}/l7rules/${
-                  l7Rule.id
-                }/json?${searchParamsToString(props)}`}
-                isAllowed={canShowJSON}
-                notAllowedText="Not allowed to get JSOn. Please check with your administrator."
-              >
-                JSON
-              </SmartLink>
-            </li>
-          </ul>
-        </div>
+        <DropDownMenu buttonIcon={<span className="fa fa-cog"/>}>
+          <li>
+            <SmartLink
+              to={`/loadbalancers/${loadbalancerID}/listeners/${listenerID}/l7policies/${l7PolicyID}/l7rules/${
+                l7Rule.id
+              }/edit?${searchParamsToString(props)}`}
+              isAllowed={canEdit}
+              notAllowedText="Not allowed to edit. Please check with your administrator."
+            >
+              Edit
+            </SmartLink>
+          </li>
+          <li>
+            <SmartLink
+              onClick={handleDelete}
+              isAllowed={canDelete}
+              notAllowedText="Not allowed to delete. Please check with your administrator."
+            >
+              Delete
+            </SmartLink>
+          </li>
+          <li>
+            <SmartLink
+              to={`/loadbalancers/${loadbalancerID}/listeners/${listenerID}/l7policies/${l7PolicyID}/l7rules/${
+                l7Rule.id
+              }/json?${searchParamsToString(props)}`}
+              isAllowed={canShowJSON}
+              notAllowedText="Not allowed to get JSOn. Please check with your administrator."
+            >
+              JSON
+            </SmartLink>
+          </li>
+        </DropDownMenu>
       </td>
     </tr>
   )
