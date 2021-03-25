9157c387b79d3203c12e204bf7325fa73f1f4af5
d058266 <hans-georg.winkler@sap.com>
2021-03-25 16:45:59 +0100

    email-service: add simple authorization

--- plugins/email_service/app/controllers/email_service/application_controller.rb
+++ plugins/email_service/app/controllers/email_service/application_controller.rb
@@ -2,7 +2,11 @@
 
 module EmailService
   class ApplicationController < DashboardController
+    authorization_context 'email_service'
+    authorization_required
+
     def index
+      enforce_permissions('email_service:application_list')
     end
   end
 end
