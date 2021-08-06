3e1a53b52ff5ead926f4163d2b5748068e55aca5
Stefan Majewsky <stefan.majewsky@sap.com>
2021-08-06 14:04:55 +0200

    keppel: extract the clipboard copy function from <Digest> into <HoverCopier>

--- plugins/keppel/app/javascript/app/components/digest.jsx
+++ plugins/keppel/app/javascript/app/components/digest.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react';
-import Clipboard from 'react-clipboard.js';
+import HoverCopier from './hovercopier';
 
 const Digest = ({digest, wideDisplay}) => {
-  const [ isCopied, setCopied ] = useState(false);
-  if (isCopied) {
-    setTimeout(() => setCopied(false), 3000);
-  }
-
   const [ algo, hash ] = digest.split(':');
   const shortDigest = wideDisplay ? digest : `${algo}:${hash.slice(0, 12)}… `;
-  return (
-    <span className='shortened-digest'>
-      {shortDigest}
-      <span className='full-digest'>
-        {digest}
-        <span className='digest-actions'>
-          {isCopied ? (
-            <button className='btn btn-link btn-xs' disabled={true}>
-              <i className="fa fa-copy fa-fw"></i> Copied!
-            </button>
-          ) : (
-            <Clipboard className='btn btn-link btn-xs' data-clipboard-text={digest} onSuccess={() => setCopied(true)}>
-              <i className="fa fa-copy fa-fw"></i> Copy
-            </Clipboard>
-          )}
-        </span>
-      </span>
-    </span>
-  );
+
+  const copyActions = [
+    {label: "Copy", value: digest},
+  ];
+
+  return <HoverCopier shortText={shortDigest} longText={digest} actions={copyActions} />;
 };
 
 export default Digest;
---
+++ plugins/keppel/app/javascript/app/components/hovercopier.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Clipboard from 'react-clipboard.js';
+
+//This component can be used to display a simple one-line text. When hovered,
+//an overlay is shown that allows to copy text. Params:
+//
+//  shortText - the text to display while not hovered (either equal to or a shortened form of longText)
+//  longText - the text to display while hovered
+//  actions - an array of entries like { "label": "Copy full URL", "value": "http://example.org" }
+//
+const HoverCopier = ({ shortText, longText, actions }) => {
+  const [ copiedLabel, setCopiedLabel ] = useState("");
+  if (copiedLabel) {
+    setTimeout(() => setCopiedLabel(""), 3000);
+  }
+
+  return (
+    <span className='hover-copier'>
+      {shortText}
+      <span className='hover-copier-expanded'>
+        {longText}
+        <span className='hover-copier-actions'>
+          {actions.map(action => (
+            (copiedLabel == action.label) ? (
+              <button key={action.label} className='btn btn-link btn-xs' disabled={true}>
+                <i className="fa fa-copy fa-fw"></i> Copied!
+              </button>
+            ) : (
+              <Clipboard key={action.label} className='btn btn-link btn-xs' data-clipboard-text={action.value} onSuccess={() => setCopiedLabel(action.label)}>
+                <i className="fa fa-copy fa-fw"></i> {action.label}
+              </Clipboard>
+            )
+          ))}
+        </span>
+      </span>
+    </span>
+  );
+};
+
+export default HoverCopier;
