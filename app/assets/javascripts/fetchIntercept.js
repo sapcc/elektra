70c0087bfe1246e267e655b1079cf096a818b349
d064310 <andreas.pfau@sap.com>
2021-09-14 13:04:48 +0200

    js ugliifier: convert es6 to es5

--- app/assets/javascripts/fetchIntercept.js
+++ app/assets/javascripts/fetchIntercept.js
@@ -1,12 +1,14 @@
+"use strict"
+
 window.activeAjaxCallsCount = window.activeAjaxCallsCount || 0
 
 // window.activeAjaxCallsCount is used in e2e tests
 // intercept fetch and handle the count, increase before start ajax call
 // and decrease after done.
 var origFetch = window.fetch
-window.fetch = async (...args) => {
+window.fetch = async function () {
   window.activeAjaxCallsCount += 1
-  return origFetch(...args).finally(() => {
+  return origFetch.apply(undefined, arguments).finally(function () {
     window.activeAjaxCallsCount -= 1
   })
 }
