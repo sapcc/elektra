4ef78af2f7081188189b5a47d9ce48d518993f2e
d064310 <andreas.pfau@sap.com>
2021-09-17 14:18:01 +0200

    networking: add rbac UI for security groups

--- plugins/networking/app/javascript/security_groups/components/security_groups/rbacs.jsx
+++ plugins/networking/app/javascript/security_groups/components/security_groups/rbacs.jsx
@@ -1,6 +1,7 @@
 import { Modal, Button, Alert } from "react-bootstrap"
 import { useParams, useHistory } from "react-router-dom"
 import { pluginAjaxHelper } from "ajax_helper"
+import { AutocompleteField } from "lib/components/autocomplete_field"
 import React from "react"
 const ajaxHelper = pluginAjaxHelper("networking")
 
@@ -19,12 +20,26 @@ function reducer(state, action) {
       else items.unshift(action.item)
       return { ...state, items }
     }
+    case "request_remove": {
+      const index = state.items.findIndex((i) => i.id === action.id)
+      if (index < 0) return state
+      const items = state.items.slice()
+      items[index] = { ...items[index], isDeleting: true }
+      return { ...state, items }
+    }
     case "remove": {
-      const index = state.items.findIndex((i) => i.id === action.item.id)
+      const index = state.items.findIndex((i) => i.id === action.id)
       if (index < 0) return state
       const items = state.items.slice()
       items.splice(index, 1)
       return { ...state, items }
+    }
+    case "remove_failure": {
+      const index = state.items.findIndex((i) => i.id === action.id)
+      if (index < 0) return state
+      const items = state.items.slice()
+      items[index] = { ...items[index], isDeleting: false }
+      return { ...state, items, error: action.error }
     }
     case "error":
       return { ...state, error: action.error }
@@ -35,13 +50,80 @@ function reducer(state, action) {
   }
 }
 
+const Item = ({ item, onDelete, canDelete, cachedProject }) => {
+  const [confirm, setConfirm] = React.useState(false)
+  let timer
+
+  const getConfirmation = React.useCallback(() => {
+    if (timer) clearTimeout(timer)
+    setConfirm(true)
+    timer = setTimeout(() => setConfirm(false), 5000)
+  }, [timer])
+
+  const remove = React.useCallback(() => {
+    setConfirm(false)
+    onDelete(item.id)
+  }, [timer, item])
+
+  return (
+    <tr>
+      <td>{item.id}</td>
+      <td>
+        {cachedProject ? (
+          <React.Fragment>
+            {cachedProject.name}
+            <br />
+            <span className="info-text">{item.target_tenant}</span>
+          </React.Fragment>
+        ) : (
+          item.target_tenant
+        )}
+      </td>
+      <td>
+        {canDelete && (
+          <button
+            className={`btn btn-${confirm ? "danger" : "default"} btn-sm ${
+              item.isDeleting ? "loading" : ""
+            }`}
+            onClick={() => (confirm ? remove() : getConfirmation())}
+          >
+            {confirm && "Confirm"}
+            <i className="fa fa-trash fa-fw" />
+          </button>
+        )}
+      </td>
+    </tr>
+  )
+}
+
 const RBACs = ({ securityGroup }) => {
   const { securityGroupId } = useParams()
   const [state, dispatch] = React.useReducer(reducer, initialState)
   const [show, setShow] = React.useState(!!securityGroupId)
   const [newItem, setNewItem] = React.useState()
   const [isCreating, setIsCreating] = React.useState(false)
+  const [cachedProjects, setCachedProjects] = React.useState({})
+
   const history = useHistory()
+
+  React.useEffect(() => {
+    if (!state.items || state.items.length === 0) return
+
+    const projectIDs = state.items
+      .map((i) => i.target_tenant)
+      .filter((value, index, self) => self.indexOf(value) === index)
+
+    ajaxHelper
+      .get(`cache/objects-by-ids?ids=${projectIDs.join(",")}`)
+      .then((response) => {
+        const items = response.data
+        const itemsById = items.reduce((map, i) => {
+          map[i.id] = i
+          return map
+        }, {})
+        setCachedProjects(itemsById)
+      })
+  }, [state.items])
 
   React.useEffect(() => {
     if (!securityGroupId) return
@@ -66,7 +148,6 @@ const RBACs = ({ securityGroup }) => {
       })
       .then((response) => {
         dispatch({ type: "add", item: response.data })
-        setNewItem("")
       })
       .catch((error) => {
         const message =
@@ -76,8 +157,28 @@ const RBACs = ({ securityGroup }) => {
           error.message
         dispatch({ type: "error", error: message })
       })
-      .finally(() => setIsCreating(false))
+      .finally(() => {
+        setIsCreating(false)
+        setNewItem("")
+      })
   }, [newItem])
+
+  const remove = React.useCallback((id) => {
+    dispatch({ type: "request_remove", id })
+    ajaxHelper
+      .delete(`security-groups/${securityGroupId}/rbacs/${id}`)
+      .then((response) => {
+        dispatch({ type: "remove", id })
+      })
+      .catch((error) => {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.errors) ||
+          error.message
+        dispatch({ type: "remove_failure", id, error: message })
+      })
+  }, [])
 
   const close = React.useCallback((e) => {
     setShow(false)
@@ -126,46 +227,61 @@ const RBACs = ({ securityGroup }) => {
             <thead>
               <tr>
                 <th>ID</th>
-                <th>Target Project</th>
+                <th width="45%">Target Project</th>
+                <th className="snug"></th>
               </tr>
             </thead>
             <tbody>
               {state.items.map((item, i) => (
-                <tr key={i}>
-                  <td>{item.id}</td>
-                  <td>{item.target_tenant}</td>
-                </tr>
+                <Item
+                  key={i}
+                  item={item}
+                  cachedProject={cachedProjects[item.target_tenant]}
+                  onDelete={remove}
+                  canDelete={policy.isAllowed(
+                    "networking:security_groups_rbac_policy_delete",
+                    {
+                      security_group: securityGroup,
+                    }
+                  )}
+                />
               ))}
             </tbody>
           </table>
         )}
-        <div className="row">
-          <div className="col-sm-6" />
-          <div className="col-sm-6">
-            <div className="pull-right">
-              <div className="input-group">
-                <input
-                  disabled={isCreating}
-                  type="text"
-                  value={newItem || ""}
-                  onChange={(e) => setNewItem(e.target.value)}
-                  className="form-control"
-                  placeholder="Project ID"
-                />
-                <div className="input-group-btn">
+
+        {policy.isAllowed("networking:security_groups_rbac_policy_create", {
+          security_group: securityGroup,
+        }) && (
+          <table className="table">
+            <tbody>
+              <tr>
+                <td></td>
+                <td width="45%">
+                  <AutocompleteField
+                    type="projects"
+                    disabled={isCreating}
+                    onSelected={(list) => {
+                      const id = list[0]?.id
+                      if (id) setNewItem(id)
+                    }}
+                    onInputChange={(id) => setNewItem(id)}
+                  />
+                </td>
+                <td className="snug">
                   <button
                     disabled={isCreating}
                     type="button"
-                    className="btn btn-primary"
+                    className={`btn btn-primary ${isCreating ? "loading" : ""}`}
                     onClick={add}
                   >
-                    {isCreating ? "...Adding" : "Add"}
+                    Add
                   </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={close}>Close</Button>
