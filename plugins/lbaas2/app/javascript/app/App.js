09c1115c57b6a6d599d3f541e71f4372595eeb46
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2021-10-08 10:56:57 +0200

    [lbaas2]: removed info block about new ui and slack channel

--- plugins/lbaas2/app/javascript/app/App.js
+++ plugins/lbaas2/app/javascript/app/App.js
@@ -1,33 +1,21 @@
-import { StateProvider } from "./components/StateProvider"
-import reducers from "./reducers"
-import Router from "./components/Router"
-import FloatingFlashMessages from "./components/shared/FloatingFlashMessages"
-import Log from "./components/shared/logger"
+import { StateProvider } from "./components/StateProvider";
+import reducers from "./reducers";
+import Router from "./components/Router";
+import FloatingFlashMessages from "./components/shared/FloatingFlashMessages";
+import Log from "./components/shared/logger";
 
 const App = () => {
-  Log.debug("RENDER App")
+  Log.debug("RENDER App");
   return (
     <React.Fragment>
-      <div className="bs-callout bs-callout-info bs-callout-emphasize">
-        <p>
-          This is the new UI based on Octavia driver for Neutron LBaaS.
-          We are happy to get your feedback on our slack
-          channel{" "}
-          <a href="https://convergedcloud.slack.com/archives/C018SPLDB9Q">
-            #octavia-users
-          </a>
-          .
-        </p>
-      </div>
-
       <FloatingFlashMessages />
       <Router />
     </React.Fragment>
-  )
-}
+  );
+};
 
 export default () => (
   <StateProvider reducers={reducers}>
     <App />
   </StateProvider>
-)
+);
