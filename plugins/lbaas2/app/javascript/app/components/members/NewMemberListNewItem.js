ebb7676d1bde8ba51840353f63bff6f78e96c404
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2021-10-21 11:58:02 +0200

    avoid unnecessary rerenders

--- plugins/lbaas2/app/javascript/app/components/members/NewMemberListNewItem.js
+++ plugins/lbaas2/app/javascript/app/components/members/NewMemberListNewItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import FormInput from "../shared/FormInput";
 import TagsInput from "../shared/TagsInput";
 import { Button } from "react-bootstrap";
@@ -30,6 +30,13 @@ const CustomLabel = ({ htmlFor, labelText, required }) => {
   );
 };
 
+const shouldAlert = (results) => {
+  if (results) {
+    return results.saved == false;
+  }
+  return false;
+};
+
 const NewMemberListNewItem = ({
   member,
   index,
@@ -42,15 +49,14 @@ const NewMemberListNewItem = ({
   const [address, setAddress] = useState(member.address);
   const [showServers, setShowServers] = useState(false);
 
+  // create a new id just if the index is different.
+  // Avoid rerenders just because it creates a new object with the same id
+  const collapseId = useMemo(() => {
+    return uniqueId("collapseServerSelect-");
+  }, [index]);
+
   const onRemoveClick = (e) => {
     onRemoveMember(member.id);
-  };
-
-  const shouldAlert = () => {
-    if (results) {
-      return results.saved == false;
-    }
-    return false;
   };
 
   const onChangeServers = (values) => {
@@ -59,167 +65,176 @@ const NewMemberListNewItem = ({
     setAddress(values.address);
   };
 
-  const collapseId = uniqueId("collapseServerSelect-");
-
   Log.debug("RENDER NewMemberListExistingItem");
-  return (
-    <>
-      {index > 0 && <hr />}
-      <div className="row display-flex">
-        <div className="col-md-12">
-          <div className="row">
-            <div className="col-md-1"></div>
-            <div className="col-md-11">
-              <div className="display-flex">
-                <div
-                  className="collapse-trigger"
-                  onClick={() => setShowServers(!showServers)}
-                  data-toggle="collapse"
-                  data-target={`#${collapseId}`}
-                  aria-expanded={showServers}
-                  aria-controls={collapseId}
-                >
-                  {showServers ? (
-                    <>
-                      <span>Choose name and IP from an existing server</span>
-                      <i className="fa fa-chevron-circle-up" />
-                    </>
-                  ) : (
-                    <>
-                      <span>Choose name and IP from an existing server</span>
-                      <i className="fa fa-chevron-circle-down" />
-                    </>
-                  )}
+
+  return useMemo(() => {
+    return (
+      <>
+        {index > 0 && <hr />}
+        <div className="row display-flex">
+          <div className="col-md-12">
+            <div className="row">
+              <div className="col-md-1"></div>
+              <div className="col-md-11">
+                <div className="display-flex">
+                  <div
+                    className="collapse-trigger"
+                    onClick={() => setShowServers(!showServers)}
+                    data-toggle="collapse"
+                    data-target={`#${collapseId}`}
+                    aria-expanded={showServers}
+                    aria-controls={collapseId}
+                  >
+                    {showServers ? (
+                      <>
+                        <span>Choose name and IP from an existing server</span>
+                        <i className="fa fa-chevron-circle-up" />
+                      </>
+                    ) : (
+                      <>
+                        <span>Choose name and IP from an existing server</span>
+                        <i className="fa fa-chevron-circle-down" />
+                      </>
+                    )}
+                  </div>
+                </div>
+                <div className="collapse margin-top" id={collapseId}>
+                  <Select
+                    className="basic-single server-select"
+                    classNamePrefix="select"
+                    isDisabled={false}
+                    isLoading={servers.isLoading}
+                    isClearable={true}
+                    isRtl={false}
+                    isSearchable={true}
+                    name="servers"
+                    onChange={onChangeServers}
+                    options={servers.items}
+                    isMulti={false}
+                    closeMenuOnSelect={true}
+                    styles={styles}
+                    value={selectedServers}
+                    placeholder="Select..."
+                  />
                 </div>
               </div>
-              <div className="collapse margin-top" id={collapseId}>
-                <Select
-                  className="basic-single server-select"
-                  classNamePrefix="select"
-                  isDisabled={false}
-                  isLoading={servers.isLoading}
-                  isClearable={true}
-                  isRtl={false}
-                  isSearchable={true}
-                  name="servers"
-                  onChange={onChangeServers}
-                  options={servers.items}
-                  isMulti={false}
-                  closeMenuOnSelect={true}
-                  styles={styles}
-                  value={selectedServers}
-                  placeholder="Select..."
+            </div>
+
+            <div className="row margin-top">
+              <div className="col-md-1">
+                <CustomLabel
+                  htmlFor={`member[${member.id}][name]`}
+                  labelText="Name"
+                  required
+                />
+              </div>
+              <div className="col-md-6">
+                <FormInput name={`member[${member.id}][name]`} value={name} />
+              </div>
+              <div className="col-md-1">
+                <CustomLabel
+                  htmlFor={`member[${member.id}][backup]`}
+                  labelText="Backup"
+                />
+              </div>
+              <div className="col-md-4">
+                <FormInput
+                  type="checkbox"
+                  name={`member[${member.id}][backup]`}
+                  value={member.backup}
                 />
               </div>
             </div>
-          </div>
 
-          <div className="row margin-top">
-            <div className="col-md-1">
-              <CustomLabel
-                htmlFor={`member[${member.id}][name]`}
-                labelText="Name"
-                required
-              />
-            </div>
-            <div className="col-md-6">
-              <FormInput name={`member[${member.id}][name]`} value={name} />
-            </div>
-            <div className="col-md-1">
-              <CustomLabel
-                htmlFor={`member[${member.id}][backup]`}
-                labelText="Backup"
-              />
-            </div>
-            <div className="col-md-4">
-              <FormInput
-                type="checkbox"
-                name={`member[${member.id}][backup]`}
-                value={member.backup}
-              />
-            </div>
-          </div>
-
-          <div className="row margin-top">
-            <div className="col-md-1">
-              <CustomLabel
-                htmlFor={`member[${member.id}][address]`}
-                labelText="IPs"
-                required
-              />
-            </div>
-            <div className="col-md-6">
-              <div className="display-flex member-icon-in-input member-required-icon">
-                <MemberIpIcon />
-                <FormInput
-                  name={`member[${member.id}][address]`}
-                  value={address}
-                  placeholder="IP Address"
-                  extraClassName="icon-in-input"
+            <div className="row margin-top">
+              <div className="col-md-1">
+                <CustomLabel
+                  htmlFor={`member[${member.id}][address]`}
+                  labelText="IPs"
+                  required
                 />
-                <span className="horizontal-padding-min">:</span>
+              </div>
+              <div className="col-md-6">
+                <div className="display-flex member-icon-in-input member-required-icon">
+                  <MemberIpIcon />
+                  <FormInput
+                    name={`member[${member.id}][address]`}
+                    value={address}
+                    placeholder="IP Address"
+                    extraClassName="icon-in-input"
+                  />
+                  <span className="horizontal-padding-min">:</span>
+                  <FormInput
+                    type="number"
+                    name={`member[${member.id}][protocol_port]`}
+                    value={member.protocol_port}
+                    placeholder="Port"
+                    size="lg"
+                  />
+                </div>
+              </div>
+              <div className="col-md-1">
+                <CustomLabel
+                  htmlFor={`member[${member.id}][weight]`}
+                  labelText="Weight"
+                />
+              </div>
+              <div className="col-md-4">
                 <FormInput
                   type="number"
-                  name={`member[${member.id}][protocol_port]`}
-                  value={member.protocol_port}
-                  placeholder="Port"
-                  size="lg"
+                  name={`member[${member.id}][weight]`}
+                  value={member.weight || 1}
                 />
               </div>
             </div>
-            <div className="col-md-1">
-              <CustomLabel
-                htmlFor={`member[${member.id}][weight]`}
-                labelText="Weight"
-              />
-            </div>
-            <div className="col-md-4">
-              <FormInput
-                type="number"
-                name={`member[${member.id}][weight]`}
-                value={member.weight || 1}
-              />
-            </div>
-          </div>
 
-          <div className="row margin-top">
-            <div className="col-md-1"></div>
-            <div className="col-md-6">
-              <div className="display-flex member-icon-in-input">
-                <MemberMonitorIcon />
-                <FormInput
-                  name={`member[${member.id}][monitor_address]`}
-                  value={member.monitor_address}
-                  placeholder="Alternate Monitor IP"
-                  extraClassName="icon-in-input"
-                />
-                <span className="horizontal-padding-min">:</span>
-                <FormInput
-                  type="number"
-                  name={`member[${member.id}][monitor_port]`}
-                  value={member.monitor_port}
-                  placeholder="Port"
-                  size="lg"
+            <div className="row margin-top">
+              <div className="col-md-1"></div>
+              <div className="col-md-6">
+                <div className="display-flex member-icon-in-input">
+                  <MemberMonitorIcon />
+                  <FormInput
+                    name={`member[${member.id}][monitor_address]`}
+                    value={member.monitor_address}
+                    placeholder="Alternate Monitor IP"
+                    extraClassName="icon-in-input"
+                  />
+                  <span className="horizontal-padding-min">:</span>
+                  <FormInput
+                    type="number"
+                    name={`member[${member.id}][monitor_port]`}
+                    value={member.monitor_port}
+                    placeholder="Port"
+                    size="lg"
+                  />
+                </div>
+              </div>
+              <div className="col-md-1">
+                <CustomLabel
+                  htmlFor={`member[${member.id}][tags]`}
+                  labelText="Tags"
                 />
               </div>
-            </div>
-            <div className="col-md-1">
-              <CustomLabel
-                htmlFor={`member[${member.id}][tags]`}
-                labelText="Tags"
-              />
-            </div>
-            <div className="col-md-4">
-              <TagsInput
-                name={`member[${member.id}][tags]`}
-                initValue={member.tags}
-              />
+              <div className="col-md-4">
+                <TagsInput
+                  name={`member[${member.id}][tags]`}
+                  initValue={member.tags}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }, [
+    showServers,
+    collapseId,
+    JSON.stringify(servers),
+    selectedServers,
+    JSON.stringify(member),
+    name,
+    address,
+  ]);
 };
 
 export default NewMemberListNewItem;
