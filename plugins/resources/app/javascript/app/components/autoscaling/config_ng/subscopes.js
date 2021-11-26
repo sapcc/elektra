d675ac26edcddd1632c719560797cd6ef100835d
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2021-11-26 15:38:40 +0100

    formatter

--- plugins/resources/app/javascript/app/components/autoscaling/config_ng/subscopes.js
+++ plugins/resources/app/javascript/app/components/autoscaling/config_ng/subscopes.js
@@ -1,8 +1,8 @@
-import { Table, FormControl, Button, Row, Col } from "react-bootstrap"
-import SubscopeItem from "./subscope_item"
-import { t } from "../../../utils"
-import { parseConfig, generateConfig, isUnset } from "../helper"
-import { Unit } from "../../../unit"
+import { Table, FormControl, Button, Row, Col } from "react-bootstrap";
+import SubscopeItem from "./subscope_item";
+import { t } from "../../../utils";
+import { parseConfig, generateConfig, isUnset } from "../helper";
+import { Unit } from "../../../unit";
 
 const styles = {
   detailsWrapper: {
@@ -22,7 +22,7 @@ const styles = {
     display: "flex",
     justifyContent: "space-between",
   },
-}
+};
 
 /****************************************
  * Details View
@@ -31,23 +31,23 @@ const styles = {
 function subscopesReducer(state, action) {
   switch (action.type) {
     case "init":
-      return { ...state, items: action.items }
+      return { ...state, items: action.items };
     case "editAll":
       return {
         ...state,
         items: state.items.map((i) => ({ ...i, error: null, editMode: true })),
-      }
+      };
     case "updateAll":
       return {
         ...state,
         items: state.items.map((i) => ({ ...i, value: action.value })),
-      }
+      };
 
     case "saveAll":
       return {
         ...state,
         items: state.items.map((i) => ({ ...i, isSaving: true, error: null })),
-      }
+      };
 
     case "savedAll":
       return {
@@ -58,7 +58,7 @@ function subscopesReducer(state, action) {
           editMode: false,
           error: null,
         })),
-      }
+      };
 
     case "cancelAll":
       return {
@@ -70,38 +70,38 @@ function subscopesReducer(state, action) {
           isSaving: false,
           value: i.originValue,
         })),
-      }
+      };
   }
 
-  const index = state.items.findIndex((i) => i.assetType === action.assetType)
-  if (index < 0) return state
-  const items = state.items.slice()
-  const item = items[index]
+  const index = state.items.findIndex((i) => i.assetType === action.assetType);
+  if (index < 0) return state;
+  const items = state.items.slice();
+  const item = items[index];
 
   switch (action.type) {
     case "update":
-      items[index] = { ...item, value: action.value }
-      return { ...state, items }
+      items[index] = { ...item, value: action.value };
+      return { ...state, items };
 
     case "updateMinFree":
-      items[index] = { ...item, minFree: action.value }
-      return { ...state, items }
+      items[index] = { ...item, minFree: action.value };
+      return { ...state, items };
 
     case "save":
-      items[index] = { ...item, isSaving: true, error: null }
-      return { ...state, items }
+      items[index] = { ...item, isSaving: true, error: null };
+      return { ...state, items };
 
     case "saved":
-      items[index] = { ...item, isSaving: false, editMode: false, error: null }
-      return { ...state, items }
+      items[index] = { ...item, isSaving: false, editMode: false, error: null };
+      return { ...state, items };
 
     case "error":
-      items[index] = { ...item, isSaving: false, error }
-      return { ...state, items }
+      items[index] = { ...item, isSaving: false, error };
+      return { ...state, items };
 
     case "edit":
-      items[index] = { ...item, error: null, editMode: true }
-      return { ...state, items }
+      items[index] = { ...item, error: null, editMode: true };
+      return { ...state, items };
 
     case "cancel":
       items[index] = {
@@ -110,11 +110,11 @@ function subscopesReducer(state, action) {
         editMode: false,
         isSaving: false,
         value: item.originValue,
-      }
-      return { ...state, items }
+      };
+      return { ...state, items };
 
     default:
-      throw new Error()
+      throw new Error();
   }
 }
 
@@ -130,35 +130,35 @@ const AutoscalingConfigNgSubscopes = ({
   updateConfig,
   deleteConfig,
 }) => {
-  const [height, setHeight] = React.useState(0)
-  const [searchTerm, setSearchTerm] = React.useState("")
+  const [height, setHeight] = React.useState(0);
+  const [searchTerm, setSearchTerm] = React.useState("");
 
   React.useEffect(() => {
-    const updateHeight = () => setHeight(window.innerHeight)
-    const closeOnEscape = (e) => e.key === "Escape" && onClose()
-    window.addEventListener("keyup", closeOnEscape)
-    window.addEventListener("resize", updateHeight)
-    updateHeight()
+    const updateHeight = () => setHeight(window.innerHeight);
+    const closeOnEscape = (e) => e.key === "Escape" && onClose();
+    window.addEventListener("keyup", closeOnEscape);
+    window.addEventListener("resize", updateHeight);
+    updateHeight();
     return () => {
-      window.removeEventListener("resize", updateHeight)
-      window.removeEventListener("keyup", closeOnEscape)
-    }
-  }, [])
+      window.removeEventListener("resize", updateHeight);
+      window.removeEventListener("keyup", closeOnEscape);
+    };
+  }, []);
 
   const [subscopesState, dispatch] = React.useReducer(subscopesReducer, {
     items: [],
-  })
+  });
 
-  const applyToAllRef = React.useRef()
-  const initialized = React.useRef(false)
+  const applyToAllRef = React.useRef();
+  const initialized = React.useRef(false);
 
   React.useEffect(() => {
     if (project?.subscopes) {
       dispatch({
         type: "init",
         items: project.subscopes.map((s) => {
-          const assetType = `project-quota:${s.service}:${s.resource}`
-          const parsedData = parseConfig(s.data, assetType)
+          const assetType = `project-quota:${s.service}:${s.resource}`;
+          const parsedData = parseConfig(s.data, assetType);
 
           return {
             assetType,
@@ -173,95 +173,95 @@ const AutoscalingConfigNgSubscopes = ({
             custom: parsedData?.custom,
             unit: new Unit(s.unitName),
             editMode: initialized.current !== project.id && editMode,
-          }
+          };
         }),
-      })
-      initialized.current = project.id
+      });
+      initialized.current = project.id;
     }
-  }, [project?.subscopes])
+  }, [project?.subscopes]);
 
   const submit = React.useCallback(
     (assetType, newValue, minFree) => {
-      if (!project?.id) return Promise.reject()
+      if (!project?.id) return Promise.reject();
       if (isUnset(newValue)) {
-        return deleteConfig(project.id, assetType)
+        return deleteConfig(project.id, assetType);
       } else {
-        const newValueInt = parseInt(newValue)
-        minFree = minFree && parseInt(minFree)
-        const cfg = generateConfig(newValueInt, minFree, assetType)
-        return updateConfig(project.id, assetType, cfg)
+        const newValueInt = parseInt(newValue);
+        minFree = minFree && parseInt(minFree);
+        const cfg = generateConfig(newValueInt, minFree, assetType);
+        return updateConfig(project.id, assetType, cfg);
       }
     },
     [project?.id]
-  )
+  );
 
   const save = React.useCallback(
     (assetType) => {
       if (!subscopesState.items)
-        return Promise.reject(new Error("no items in subscope state!"))
+        return Promise.reject(new Error("no items in subscope state!"));
 
       const subscope = subscopesState.items.find(
         (s) => s.assetType === assetType
-      )
+      );
 
       if (!subscope)
-        return Promise.reject(new Error(`subscope ${assetType} not found!`))
+        return Promise.reject(new Error(`subscope ${assetType} not found!`));
 
-      const newValue = isUnset(subscope.value) ? "" : subscope.value
+      const newValue = isUnset(subscope.value) ? "" : subscope.value;
       const originValue = isUnset(subscope.originValue)
         ? ""
-        : subscope.originValue
-      const newMinFree = subscope.minFree
-      const originMinFree = subscope.originMinFree
+        : subscope.originValue;
+      const newMinFree = subscope.minFree;
+      const originMinFree = subscope.originMinFree;
 
-      if (newValue === originValue && newMinFree === originMinFree) return
+      if (newValue === originValue && newMinFree === originMinFree) return;
 
-      dispatch({ type: "save", assetType })
+      dispatch({ type: "save", assetType });
       submit(assetType, newValue, newMinFree)
         .then(() => dispatch({ type: "saved", assetType }))
         .catch((error) =>
           dispatch({ type: "error", assetType, error: error.message })
-        )
+        );
     },
     [subscopesState]
-  )
+  );
 
   const saveAll = React.useCallback(() => {
-    dispatch({ type: "saveAll" })
-    const promises = []
+    dispatch({ type: "saveAll" });
+    const promises = [];
     for (let subscope of subscopesState.items) {
-      const newValue = isUnset(subscope.value) ? "" : subscope.value
+      const newValue = isUnset(subscope.value) ? "" : subscope.value;
       const originValue = isUnset(subscope.originValue)
         ? ""
-        : subscope.originValue
-      const newMinFree = subscope.minFree
-      const originMinFree = subscope.originMinFree
+        : subscope.originValue;
+      const newMinFree = subscope.minFree;
+      const originMinFree = subscope.originMinFree;
 
-      if (newValue === originValue && newMinFree === originMinFree) return
+      if (newValue === originValue && newMinFree === originMinFree) return;
 
       promises.push(
         submit(subscope.assetType, newValue, newMinFree).catch((error) =>
           dispatch({ type: "error", assetType, error: error.message })
         )
-      )
+      );
     }
-    Promise.all(promises).then(() => dispatch({ type: "savedAll" }))
-  }, [subscopesState, dispatch])
+    Promise.all(promises).then(() => dispatch({ type: "savedAll" }));
+  }, [subscopesState, dispatch]);
 
   const editAll = React.useCallback(
     () => dispatch({ type: "editAll" }),
     [dispatch]
-  )
+  );
 
   const cancelAll = React.useCallback(
     () => dispatch({ type: "cancelAll" }),
     [dispatch]
-  )
+  );
 
-  const [top, maxHeight] = React.useMemo(() => [93, height - 93], [height])
+  const [top, maxHeight] = React.useMemo(() => [93, height - 93], [height]);
 
   const filteredSubscopes = React.useMemo(() => {
-    let items = subscopesState.items || []
+    let items = subscopesState.items || [];
     if (searchTerm && searchTerm !== "") {
       items = items.filter(
         (s) =>
@@ -271,16 +271,16 @@ const AutoscalingConfigNgSubscopes = ({
             0 ||
             s.resourceLabel.toLowerCase().indexOf(searchTerm.toLowerCase()) >=
               0)
-      )
+      );
     }
     return items.sort((a, b) =>
       a.service < b.service ? -1 : a.service > b.service ? 1 : 0
-    )
-  }, [subscopesState, searchTerm])
+    );
+  }, [subscopesState, searchTerm]);
 
   const editModeCount = React.useMemo(
     () => filteredSubscopes.filter((s) => s.editMode).length
-  )
+  );
 
   return (
     <div
@@ -463,7 +463,7 @@ const AutoscalingConfigNgSubscopes = ({
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default AutoscalingConfigNgSubscopes
+export default AutoscalingConfigNgSubscopes;
