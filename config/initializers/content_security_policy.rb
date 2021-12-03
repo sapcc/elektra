d3e0168a89e4da9e76bc6007f4f23ecc2af41f34
d058266 <hans-georg.winkler@sap.com>
2021-12-03 12:02:00 +0100

    webpacker compiles without errors

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,9 +1,10 @@
 # You need to allow webpack-dev-server host as allowed origin for connect-src.
 # This can be done in Rails 5.2+ for development environment in the CSP initializer
 # config/initializers/content_security_policy.rb with a snippet like this:
-# policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
-# https://www.stackhawk.com/blog/rails-content-security-policy-guide-what-it-is-and-how-to-enable-it/
-
+# https://github.com/rails/webpacker#development
+# You need to allow webpack-dev-server host as allowed origin for connect-src.
+# This can be done in Rails 5.2+ for development environment in the CSP initializer
+# config/initializers/content_security_policy.rb
 Rails.application.config.content_security_policy do |policy|
-  policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  policy.connect_src :self, :https, "http://0.0.0.0:3035", "ws://0.0.0.0:3035" if Rails.env.development?
 end
