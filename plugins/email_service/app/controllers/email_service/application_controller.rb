e873704522ab1c7ba5551fb20c46d50909211cff
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2021-12-10 12:46:21 +0000

    move pagintable from global to local configset_controller

--- plugins/email_service/app/controllers/email_service/application_controller.rb
+++ plugins/email_service/app/controllers/email_service/application_controller.rb
@@ -10,7 +10,7 @@ module EmailService
 
     authorization_context 'email_service'
     authorization_required
-    
+
     def ui_switcher
       if current_user.has_role?('cloud_support_tools_viewer')
         redirect_to emails_path
@@ -22,38 +22,6 @@ module EmailService
         redirect_to index_path
       end
     end 
-
-    def render_paginatable_for_configset(items, filter={})
-      return if !@pagination_enabled || !items || items.length.zero?
-      content_tag(:div, class: 'pagination') do
-        if @pagination_current_page > 1 || @pagination_has_next
-          concat(content_tag(:span, "#{@pagination_seen_items + 1} - #{@pagination_seen_items + items.length} ", class: 'current-window'))
-          if @pagination_current_page > 1
-            concat(' | ')
-            if filter.key?(:search) and filter.key?(:searchfor)
-              concat(link_to('Previous Page', page: @pagination_current_page - 1, marker: items.first.id, reverse: true, search: filter[:search], searchfor: filter[:searchfor]))
-            else
-              concat(link_to('Previous Page', page: @pagination_current_page - 1, marker: items.first.id, reverse: true))
-            end
-          end
-          if @pagination_has_next
-            concat(' | ')
-            if filter.key?(:search) and filter.key?(:searchfor)
-              concat(link_to('Next Page', page: @pagination_current_page + 1, marker: items.last.id, search: filter[:search], searchfor: filter[:searchfor]))
-            else
-              concat(link_to('Next Page', page: @pagination_current_page + 1, marker: items.last.id))
-            end
-          end
-          concat(' | ')
-          if filter.key?(:search) and filter.key?(:searchfor)
-            concat(link_to('All', per_page: 9999, search: filter[:search], searchfor: filter[:searchfor]))
-          else
-            concat(link_to('All', per_page: 9999))
-          end
-        end
-      end
-    end
-
     
   end
 end
--- plugins/email_service/app/controllers/email_service/configset_controller.rb
+++ plugins/email_service/app/controllers/email_service/configset_controller.rb
@@ -4,7 +4,7 @@ module EmailService
 
     authorization_context 'email_service'
     authorization_required
-    
+
     def index
       creds = get_ec2_creds
       if creds.error.empty?
@@ -73,6 +73,38 @@ module EmailService
       flash[:error] = "Status Code: #{e.code} : Error: #{e.message}"
     rescue Exception => e
       flash[:error] = "Status Code: 500 : Error: #{e.message}"
+    end
+
+
+    def render_paginatable_for_configset(items, filter={})
+      return if !@pagination_enabled || !items || items.length.zero?
+      content_tag(:div, class: 'pagination') do
+        if @pagination_current_page > 1 || @pagination_has_next
+          concat(content_tag(:span, "#{@pagination_seen_items + 1} - #{@pagination_seen_items + items.length} ", class: 'current-window'))
+          if @pagination_current_page > 1
+            concat(' | ')
+            if filter.key?(:search) and filter.key?(:searchfor)
+              concat(link_to('Previous Page', page: @pagination_current_page - 1, marker: items.first.id, reverse: true, search: filter[:search], searchfor: filter[:searchfor]))
+            else
+              concat(link_to('Previous Page', page: @pagination_current_page - 1, marker: items.first.id, reverse: true))
+            end
+          end
+          if @pagination_has_next
+            concat(' | ')
+            if filter.key?(:search) and filter.key?(:searchfor)
+              concat(link_to('Next Page', page: @pagination_current_page + 1, marker: items.last.id, search: filter[:search], searchfor: filter[:searchfor]))
+            else
+              concat(link_to('Next Page', page: @pagination_current_page + 1, marker: items.last.id))
+            end
+          end
+          concat(' | ')
+          if filter.key?(:search) and filter.key?(:searchfor)
+            concat(link_to('All', per_page: 9999, search: filter[:search], searchfor: filter[:searchfor]))
+          else
+            concat(link_to('All', per_page: 9999))
+          end
+        end
+      end
     end
 
   end
