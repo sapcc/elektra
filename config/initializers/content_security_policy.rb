628a86b6e41fff6ae5de45164c5bf58e02321640
d064310 <andreas.pfau@sap.com>
2021-12-22 10:50:51 +0100

    [identity]: bugfixes in role assignemnets

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,6 +5,18 @@
 # You need to allow webpack-dev-server host as allowed origin for connect-src.
 # This can be done in Rails 5.2+ for development environment in the CSP initializer
 # config/initializers/content_security_policy.rb
+domains = ["*.cloud.sap"]
+
+if Rails.env.development?
+domains.concat([
+  "http://0.0.0.0:8081", 
+  "ws://0.0.0.0:8081", 
+  "http://localhost:8081",
+  "ws://localhost:8081",
+  ])
+end
+  
 Rails.application.config.content_security_policy do |policy|
-  policy.connect_src :self, :https, "http://0.0.0.0:3035", "ws://0.0.0.0:3035" if Rails.env.development?
+  policy.connect_src :self, :https, *domains
+  policy.img_src     :self, :https, :data, "*"
 end
