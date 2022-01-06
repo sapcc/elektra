a74a49c21831028d98d1438273d2cd35abf7d91a
d063222 <a.reuschenbach.puncernau@sap.com>
2022-01-06 12:51:27 +0100

    finished test for instances

--- e2e/cypress/integration/instances.js
+++ e2e/cypress/integration/instances.js
@@ -1,19 +1,3 @@
-// @javascript
-// Feature: Instances
-//   Background:
-//     Given I visit domain path "home"
-//     And I log in as test_user
-//     Given Test user has accepted terms of use
-//     Then I am redirected to domain path "home"
-
-//   Scenario: The Instances page is reachable
-//     When I visit project path "compute/instances"
-//     Then the page status code is successful
-//     And I see "Create new" button
-//     When I click on "Create new"
-//     Then the page status code is successful
-//     #And I see "Max count"
-
 describe("Instances", () => {
   before(() => {
     cy.elektraLogin(
@@ -36,12 +20,13 @@ describe("Instances", () => {
     cy.get(".btn").contains("Create New")
   })
 
-  // it("click on 'Create New' button opens a modal window", () => {
-  //   cy.get(".btn").contains("Create New").click()
-  //   cy.url().should("include", "/?r=/volumes/new")
-  //   cy.get(".modal-content").as("modal")
+  // instances?overlay=new
+  it("click on 'Create New' button opens a modal window", () => {
+    cy.get(".btn").contains("Create New").click()
+    cy.url().should("include", "instances?overlay=new")
+    cy.get(".modal-content").as("modal")
 
-  //   cy.get("@modal").contains("New Volume")
-  //   cy.get("@modal").contains("Save")
-  // })
+    cy.get("@modal").contains("New Instance")
+    cy.get("@modal").contains("Create")
+  })
 })
