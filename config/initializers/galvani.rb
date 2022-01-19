5765da8591bbfb2ba8db35c9fdd5da5fa8a1110c
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-01-19 15:24:59 +0100

    changed path to galvani config file

--- config/initializers/galvani.rb
+++ config/initializers/galvani.rb
@@ -1,5 +1,5 @@
 # require 'ostruct'
 require 'yaml'
 
-GalvaniConfig = YAML.load_file("#{Rails.root}/config/galvani.yaml") || {}
+GalvaniConfig = YAML.load_file("#{Rails.root}/config/support/galvani.yaml") || {}
 # GalvaniConfig = OpenStruct.new(galvani_config)
