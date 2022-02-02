38d43efc7ce5aa5a3a2c971fa5023bc2b3adb80a
d058266 <hans-georg.winkler@sap.com>
2022-02-02 15:55:53 +0100

    vmware tools adjust error handling and tests

--- e2e/cypress/integration/instances.js
+++ e2e/cypress/integration/instances.js
@@ -5,18 +5,23 @@ describe("Instances", () => {
       Cypress.env("TEST_USER"),
       Cypress.env("TEST_PASSWORD")
     )
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/member/compute/instances`)
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/test/compute/instances`)
   })
 
-  it("The Instances page is reachable", () => {
+  it("The Instances page is reachable and you can see VM with title 'elektra-test-vm' ", () => {
     cy.get(".table.instances")
     cy.contains("Servers")
-    cy.request(
-      `/${Cypress.env("TEST_DOMAIN")}/member/compute/instances`
-    ).should((response) => {
-      expect(response.status).to.eq(200)
-    })
-    cy.get(".btn").contains("Create New")
+    cy.contains("Create New")
+
+    cy.get("#search")
+      .should('be.visible')
+      .type('elektra{enter}')
+    
+    cy.contains("elektra-test-vm").click()
+    // because wrong SSL we test 400 from Hermes 
+    cy.get('span.guest_tools_problem').should('be.visible')
+    cy.get('span.guest_tools_problem_text').should('contain','Problem to get')
+
   })
 
   it("click on 'Create New' button opens a modal window", () => {
@@ -24,4 +29,5 @@ describe("Instances", () => {
     cy.url().should("include", "instances?overlay=new")
     cy.get("button.btn.btn-primary").contains("Create")
   })
+
 })
