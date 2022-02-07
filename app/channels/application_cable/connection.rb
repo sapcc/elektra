a7b3298ce05f5e711bc6e6de090116233d8f9b3c
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2022-02-07 20:34:52 +0530

    fix create ec2 credentials (#1041)
    * fix create ec2 credentials
    
    * fix generic to specific name reference for ec2 credentials

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,7 +10,7 @@ module ApplicationCable
 
     def find_verified_user
       # return if no domain provided
-      return reject_unauthorized_connectio unless request.params[:domain_id]
+      return reject_unauthorized_connection unless request.params[:domain_id]
 
       # access auth gem directly to get current user_id
       token_store = MonsoonOpenstackAuth::Authentication::TokenStore.new(
