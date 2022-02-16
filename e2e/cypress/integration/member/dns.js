c26cdb7e201db2b78ccebe651be77db3f95e7412
d058266 <hans-georg.winkler@sap.com>
2022-02-16 16:42:01 +0100

    cypress: add tests for dns

---
+++ e2e/cypress/integration/member/dns.js
@@ -0,0 +1,45 @@
+describe("dns", () => {
+  beforeEach(() => {
+    cy.elektraLogin(
+      Cypress.env("TEST_DOMAIN"),
+      Cypress.env("TEST_USER"),
+      Cypress.env("TEST_PASSWORD")
+    )
+  })
+
+  it("open dns page and test Request New Zone dialog", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/member/dns-service/zones`)
+    cy.contains('[data-test=page-title]','DNS')
+    cy.contains('Request New Zone').click()
+    cy.contains('Request New Domain')
+    cy.contains('button','Cancel').click()
+  })
+
+  it("open dns page and test Request New Zone with Internal SAP Hosted Zone", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/member/dns-service/zones`)
+    cy.contains('[data-test=page-title]','DNS')
+    cy.contains('Request New Zone').click()
+    cy.contains('Request New Domain')
+    cy.get('#zone_request_domain_pool').select('Internal SAP Hosted Zone')
+    // click Subdomain
+    cy.get('input#zone_request_domain_type_subdomain').should('be.visible').click()
+    cy.get('input#zone_request_name').should('be.visible')
+    // click Custom Domain
+    cy.get('input#zone_request_domain_type_rootdomain').should('be.visible').click()
+    cy.get('input#zone_request_name').should('be.visible')
+    cy.contains('ns2.qa-de-1.cloud.sap').should('be.visible')
+
+    cy.contains('button','Cancel').click()
+  })
+
+  it("open dns page and test Request New Zone with Internal SAP Hosted Zone on F5", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/member/dns-service/zones`)
+    cy.contains('[data-test=page-title]','DNS')
+    cy.contains('Request New Zone').click()
+    cy.contains('Request New Domain')
+    cy.get('#zone_request_domain_pool').select('Internal SAP Hosted Zone on F5')
+    cy.get('input#zone_request_name').should('be.visible')
+    cy.contains('button','Cancel').click()
+  })
+
+})
