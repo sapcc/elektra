8216c06c12b58e687a131e57268475dec8f189a3
d058266 <hans-georg.winkler@sap.com>
2022-02-17 12:20:50 +0100

    cypress: add test for unsupported domain and bad project

--- e2e/cypress/integration/member/authentication.js
+++ e2e/cypress/integration/member/authentication.js
@@ -15,8 +15,24 @@ describe("authentication", () => {
   })
 
   it("login failed", () => {
-    cy.elektraLogin("cc3test", "BATMAN", "BAD_PASSWORD")
+    cy.elektraLogin("cc3test", "EVIL_MAN", "EVIL_PASSWORD")
     cy.contains("Invalid username/password combination.")
+  })
+
+  it("user is not logged in and tries to visit BAD DOMAIN ans sees Unsupported Domain", () => {
+    cy.visit(`/BAD_DOMAIN/`)
+    cy.contains('a','Enter the cloud').click()
+    cy.contains('Unsupported Domain')
+  })
+
+  it("user is logged in and tries to visit BAD PROJECT ans sees Project Not Found", () => {
+    cy.elektraLogin(
+      Cypress.env("TEST_DOMAIN"),
+      Cypress.env("TEST_USER"),
+      Cypress.env("TEST_PASSWORD")
+    )
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/BAD_PROJECT/identity/project/home`)
+    cy.contains('Project Not Found')
   })
 
 })
