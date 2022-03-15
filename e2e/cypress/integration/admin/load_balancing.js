5b997ac044f7eac9e16051b4f64d7cad939c64b5
d058266 <hans-georg.winkler@sap.com>
2022-03-15 14:12:19 +0100

    cypress: small admin test for load balancers

---
+++ e2e/cypress/integration/admin/load_balancing.js
@@ -0,0 +1,15 @@
+describe("load balancing", () => {
+  beforeEach(() => {
+    cy.elektraLogin(
+      Cypress.env("TEST_DOMAIN"),
+      Cypress.env("TEST_USER"),
+      Cypress.env("TEST_PASSWORD")
+    )
+  })
+
+  it("open load balancer page and test new Load Balancer button", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/lbaas2/?r=/loadbalancers`)
+    cy.contains('[data-test=page-title]','Load Balancers')
+  })
+
+})
