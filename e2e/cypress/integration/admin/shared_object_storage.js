636f1dd77cce17acdb8552c4e8540e294ed75ecb
d058266 <hans-georg.winkler@sap.com>
2022-03-15 14:57:10 +0100

    cypress: more object storage tests

--- e2e/cypress/integration/admin/shared_object_storage.js
+++ e2e/cypress/integration/admin/shared_object_storage.js
@@ -7,15 +7,6 @@ describe("shared object storage", () => {
     )
   })
 
-  /*
-  it("open object storage and see that object storage is not enabled", () => {
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/test/object-storage/`)
-    cy.contains('[data-test=page-title]','Object Storage')
-    cy.contains('Object storage is not enabled for this project, yet. To enable it, request an Object Storage quota in the Resource Management tool.')
-    cy.contains('a','Go to Resource Management')
-  })
-  */
-
   it("open object storage and check create container button", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/object-storage/`)
     cy.contains('[data-test=page-title]','Object Storage')
@@ -30,6 +21,44 @@ describe("shared object storage", () => {
     cy.contains('textarea','.rlistings')
     cy.contains('a','Check ACLs').click()
     cy.contains('valid token required: false')
+  })
+
+  it("open object storage and check elektra-test container", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/object-storage/containers/elektra-test`)
+    cy.contains('Object count')
+    cy.contains('Metadata')
+  })
+
+  it("open object storage and elektra-test container and check actions", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/object-storage/`)
+    cy.contains('[data-test=page-title]','Object Storage')
+    cy.contains('a','elektra-test').click()
+    cy.contains('a','Create folder').click()
+    cy.contains('Folder name')
+    cy.contains('button','Cancel').click()
+    cy.contains('a','Upload file').click()
+    cy.contains('This dialog only accepts files smaller than 1 MiB. To upload larger files, please use a different client.')
+    cy.contains('button','Cancel').click()
+  })
+
+  it("open elektra-test container directly and check item", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/object-storage/containers/elektra-test/list`)
+    cy.contains('[data-test=page-title]','Object Storage')
+    cy.contains('a','amiga.jpg')
+    cy.get('div.btn-group').click()
+    cy.contains('a','Properties').click()
+    cy.contains('File: amiga.jpg')
+    cy.contains('button','Cancel').click()
+
+    cy.get('div.btn-group').click()
+    cy.contains('a','Copy').click()
+    cy.contains('Copy object: amiga.jpg')
+    cy.contains('button','Cancel').click()
+
+    cy.get('div.btn-group').click()
+    cy.contains('a','Move/Rename').click()
+    cy.contains('Move and/or rename object: amiga.jpg')
+    cy.contains('button','Cancel').click()
   })
 
 })
