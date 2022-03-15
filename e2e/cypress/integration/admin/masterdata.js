f2bc43ac5dea5a8024af8ecb3ed8ccb12544beb5
d058266 <hans-georg.winkler@sap.com>
2022-03-15 18:43:53 +0100

    cypress: masterdata tests

---
+++ e2e/cypress/integration/admin/masterdata.js
@@ -0,0 +1,32 @@
+describe("masterdata", () => {
+  beforeEach(() => {
+    cy.elektraLogin(
+      Cypress.env("TEST_DOMAIN"),
+      Cypress.env("TEST_USER"),
+      Cypress.env("TEST_PASSWORD")
+    )
+  })
+
+  it("open masterdata", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/masterdata-cockpit/project`)
+    cy.contains('[data-test=page-title]','Project Masterdata')
+    cy.contains('Masterdata Status')
+  })
+
+  it("open masterdata and check edit project", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/masterdata-cockpit/project`)
+    cy.contains('[data-test=page-title]','Project Masterdata')
+    cy.get('#edit_project_btn').click()
+    cy.contains('Edit Project')
+    cy.contains('button','Cancel').click()
+  })
+
+  it("open masterdata and check edit masterdata", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/masterdata-cockpit/project`)
+    cy.contains('[data-test=page-title]','Project Masterdata')
+    cy.get('#edit_masterdata_btn').click()
+    cy.contains('Edit masterdata for project: admin')
+    cy.contains('button','Cancel').click()
+  })
+
+})
---
+++ e2e/cypress/integration/member/masterdata.js
@@ -0,0 +1,19 @@
+describe("masterdata", () => {
+  beforeEach(() => {
+    cy.elektraLogin(
+      Cypress.env("TEST_DOMAIN"),
+      Cypress.env("TEST_USER"),
+      Cypress.env("TEST_PASSWORD")
+    )
+  })
+
+  it("open masterdata", () => {
+    cy.request({
+      url: `/${Cypress.env("TEST_DOMAIN")}/test/masterdata-cockpit/project`,
+      failOnStatusCode: false
+    }).should((response) => {
+      expect(response.status).to.eq(401)
+    })
+  })
+
+})
