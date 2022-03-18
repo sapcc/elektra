017eeb432d4613afab2a667a1c483971b4ab2148
D064310 <andreas.pfau@sap.com>
2022-03-18 09:54:36 +0100

    [rails6]: remove obsolete code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  config.x.webpacker[:dev_server_host] = "http://127.0.0.1:8080"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
