6fbf48aab32628c843111562a9b58de3e75c8cf3
D064310 <andreas.pfau@sap.com>
2022-03-28 22:10:40 +0200

    [rails6]: fix bugs in custom lib

--- lib/sassc/script/functions.rb
+++ lib/sassc/script/functions.rb
@@ -4,41 +4,45 @@
 require 'sassc'
 require 'cgi'
 
-module SassC::Script::Functions
-  def inline_svg(path, options = nil)
-    svg = read_file(path.value.strip)
-    svg = replace_options(svg, options)
-    SassC::Script::Value::String.new(encode_url(svg))
-  end
+module SassC
+  module Script
+    module Functions
+      def inline_svg(path, options = nil)
+        svg = read_file(path.value.strip)
+        svg = replace_options(svg, options)
+        SassC::Script::Value::String.new(encode_url(svg))
+      end
 
-  private
+      private
 
-  def read_file(path)
-    return find_rails_asset(path) if defined?(Rails) && Rails.application
+      def read_file(path)
+        return find_rails_asset(path) if defined?(Rails) && ::Rails.application
 
-    raise SassC::SyntaxError, "File not found or cannot be read (native): #{path}" unless File.readable?(path)
+        raise ::SassC::SyntaxError, "File not found or cannot be read (native): #{path}" unless File.readable?(path)
 
-    File.open(path, 'rb') { |f| f.read }.strip
-  end
+        File.open(path, 'rb') { |f| f.read }.strip
+      end
 
-  def find_rails_asset(path)
-    source = Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)
-    asset = source.find_asset(path)
-    raise "File not found or cannot be read (Sprockets): #{path}" if asset.nil?
-    asset.to_s
-  end
+      def find_rails_asset(path)
+        source = ::Rails.application.assets || ::Sprockets::Railtie.build_environment(::Rails.application)
+        asset = source.find_asset(path)
+        raise "File not found or cannot be read (Sprockets): #{path}" if asset.nil?
+        asset.to_s
+      end
 
-  def replace_options(svg, options)
-    return svg if options.nil?
+      def replace_options(svg, options)
+        return svg if options.nil?
 
-    options.value.each_pair do |k, v|
-      svg.gsub!(k.value, v.value) if svg.include?(k.value)
+        options.value.each_pair do |k, v|
+          svg.gsub!(k.value, v.value) if svg.include?(k.value)
+        end
+        svg
+      end
+
+      def encode_url(svg)
+        encoded = CGI::escape(svg).gsub("+", "%20")
+        "url('data:image/svg+xml;charset=utf-8," + encoded + "')"
+      end
     end
-    svg
-  end
-
-  def encode_url(svg)
-    encoded = CGI::escape(svg).gsub("+", "%20")
-    "url('data:image/svg+xml;charset=utf-8," + encoded + "')"
-  end
+    end
 end
