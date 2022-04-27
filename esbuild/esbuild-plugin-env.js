dd864d3faf2b3dd61fe58865724fa833b8de966e
D064310 <andreas.pfau@sap.com>
2022-04-27 15:12:59 +0200

    [esbuild]: fix the env plugin

--- esbuild/esbuild-plugin-env.js
+++ esbuild/esbuild-plugin-env.js
@@ -9,7 +9,7 @@ module.exports = {
   name: "env",
   setup(build) {
     function _findEnvFile(dir) {
-      if (!fs.existsSync(dir)) return "" // this is the fix. Instead of "false" we return an empty string here
+      if (!fs.existsSync(dir)) return false
       let filePath = `${dir}/.env`
       if (fs.existsSync(filePath)) {
         return filePath
@@ -18,22 +18,32 @@ module.exports = {
       }
     }
 
-    build.onResolve({ filter: /^env$/ }, async (args) => {
-      // find a .env file in current directory or any parent:
-      return {
-        path: _findEnvFile(args.resolveDir),
-        namespace: "env-ns",
-      }
-    })
+    // Intercept import paths called "env" so esbuild doesn't attempt
+    // to map them to a file system location. Tag them with the "env-ns"
+    // namespace to reserve them for this plugin.
+    build.onResolve({ filter: /^env$/ }, (args) => ({
+      path: args.path,
+      namespace: "env-ns",
+      pluginData: { currentDir: args.resolveDir },
+    }))
 
+    // Load paths tagged with the "env-ns" namespace and behave as if
+    // they point to a JSON file containing the environment variables.
     build.onLoad({ filter: /.*/, namespace: "env-ns" }, async (args) => {
-      // read in .env file contents and combine with regular .env:
-      let data = await fs.promises.readFile(args.path, "utf8")
-      const buf = Buffer.from(data)
-      const config = require("dotenv").parse(buf)
+      // try to find the env file
+      let envPath = _findEnvFile(args.pluginData.currentDir)
+      let contents = JSON.stringify(process.env)
+
+      // if env file exists then add it to contents
+      if (envPath) {
+        let data = await fs.promises.readFile(envPath, "utf8")
+        const buf = Buffer.from(data)
+        const config = require("dotenv").parse(buf)
+        contents = JSON.stringify({ ...process.env, ...config })
+      }
 
       return {
-        contents: JSON.stringify({ ...process.env, ...config }),
+        contents,
         loader: "json",
       }
     })
