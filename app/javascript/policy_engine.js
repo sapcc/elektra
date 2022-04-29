f5938f25230ab5ce3b74486f050ecad497918f5a
D064310 <andreas.pfau@sap.com>
2022-04-29 15:39:03 +0200

    load js policy if policy config is set

--- app/javascript/policy_engine.js
+++ app/javascript/policy_engine.js
@@ -1,25 +1,24 @@
-if (!window.policyConfig)
-  throw "Missing data. Please provide window.policyConfig"
-if (!window.policyConfig.rules)
-  throw "Missing rules. Please provide window.policyConfig.rules"
-if (!window.policyConfig.locals)
-  throw "Missing locals. Please provide window.policyConfig.locals"
-
-// evaluate rules
-const rules = {}
-for (let name in window.policyConfig.rules) {
-  const rule = window.policyConfig.rules[name]
-  try {
-    rules[name] = eval(rule)
-  } catch (e) {
-    console.info("Policy Engine Error: ", e)
+if (window.policyConfig?.rules && window.policyConfig?.locals) {
+  // evaluate rules
+  const rules = {}
+  for (let name in window.policyConfig.rules) {
+    const rule = window.policyConfig.rules[name]
+    try {
+      rules[name] = eval(rule)
+    } catch (e) {
+      console.info("Policy Engine Error: ", e)
+    }
   }
-}
 
-window.policy = {
-  isAllowed: function (name, params = {}) {
-    const rule = rules[name]
-    if (!rule) throw "Policy Engine Error: rule #{name} not found."
-    return rule(rules, window.policyConfig.locals, params)
-  },
+  window.policy = {
+    isAllowed: function (name, params = {}) {
+      const rule = rules[name]
+      if (!rule) throw "Policy Engine Error: rule #{name} not found."
+      return rule(rules, window.policyConfig.locals, params)
+    },
+  }
+} else {
+  console.info(
+    "Policy engine is not loaded because window.policyConfig does not contain required data"
+  )
 }
