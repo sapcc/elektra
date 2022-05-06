e2b0101986650dc3181ce9b0b8e2f8230aab0612
D064310 <andreas.pfau@sap.com>
2022-05-06 10:45:08 +0200

    fix errors in shared_file_system plugin (do not use <this. ... />)

--- plugins/shared_filesystem_storage/app/javascript/widgets/app/components/security_services/form.jsx
+++ plugins/shared_filesystem_storage/app/javascript/widgets/app/components/security_services/form.jsx
@@ -1,10 +1,10 @@
-import { Modal, Button } from 'react-bootstrap';
-import { Form } from 'lib/elektra-form';
+import { Modal, Button } from "react-bootstrap"
+import { Form } from "lib/elektra-form"
 
 export default class SecurityServiceForm extends React.Component {
-  constructor(props){
-  	super(props);
-  	this.state = {show: true};
+  constructor(props) {
+    super(props)
+    this.state = { show: true }
     this.close = this.close.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
     this.renderForm = this.renderForm.bind(this)
@@ -14,92 +14,109 @@ export default class SecurityServiceForm extends React.Component {
     return values.type && values.name && values.ou && true
   }
 
-  close(e){
-    if(e) e.stopPropagation()
-    this.setState({show: false})
-    setTimeout(() => this.props.history.replace('/security-services'),300)
+  close(e) {
+    if (e) e.stopPropagation()
+    this.setState({ show: false })
+    setTimeout(() => this.props.history.replace("/security-services"), 300)
   }
 
-  onSubmit(values){
-    return this.props.handleSubmit(values).then(() => this.close());
+  onSubmit(values) {
+    return this.props.handleSubmit(values).then(() => this.close())
   }
 
-  renderForm({values}) {
-    return(
+  renderForm({ values }) {
+    return (
       <React.Fragment>
         <Modal.Body>
-          <Form.Errors/>
+          <Form.Errors />
 
-          <Form.ElementHorizontal label='Type' name="type" required>
-            <Form.Input elementType='select' type='text'>
+          <Form.ElementHorizontal label="Type" name="type" required>
+            <Form.Input elementType="select" type="text">
               <option></option>
-              <option value='active_directory'>Active Directory</option>
-              <option value='ldap'>LDAP</option>
+              <option value="active_directory">Active Directory</option>
+              <option value="ldap">LDAP</option>
             </Form.Input>
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='OU (Organizational Unit)' name="ou" required>
-            <Form.Input elementType='input'/>
+          <Form.ElementHorizontal
+            label="OU (Organizational Unit)"
+            name="ou"
+            required
+          >
+            <Form.Input elementType="input" />
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='Name' name="name">
-            <Form.Input elementType='input'/>
+          <Form.ElementHorizontal label="Name" name="name">
+            <Form.Input elementType="input" />
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='Description' name="description">
-            <Form.Input elementType='textarea' className="text"/>
+          <Form.ElementHorizontal label="Description" name="description">
+            <Form.Input elementType="textarea" className="text" />
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='DNS IP' name="dns_ip">
-            <Form.Input elementType='input'/>
+          <Form.ElementHorizontal label="DNS IP" name="dns_ip">
+            <Form.Input elementType="input" />
             <p className="help-block">
-              <i className="fa fa-info-circle"/>You can provide an IP (ipv4) of your AD's DNS. It is possible to specify multiple addresses separated by commas.
+              <i className="fa fa-info-circle" />
+              You can provide an IP (ipv4) of your AD's DNS. It is possible to
+              specify multiple addresses separated by commas.
             </p>
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='User'  name="user">
-            <Form.Input elementType='input'/>
+          <Form.ElementHorizontal label="User" name="user">
+            <Form.Input elementType="input" />
           </Form.ElementHorizontal>
 
-          { values.user && values.user.trim().length>0 &&
-            <Form.ElementHorizontal label='Password'  name="password">
-              <Form.Input elementType='input'/>
+          {values.user && values.user.trim().length > 0 && (
+            <Form.ElementHorizontal label="Password" name="password">
+              <Form.Input elementType="input" />
             </Form.ElementHorizontal>
-          }
+          )}
 
-          <Form.ElementHorizontal label='Domain' name="domain">
-            <Form.Input elementType='input'/>
+          <Form.ElementHorizontal label="Domain" name="domain">
+            <Form.Input elementType="input" />
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='Server' name="server">
-            <Form.Input elementType='input'/>
+          <Form.ElementHorizontal label="Server" name="server">
+            <Form.Input elementType="input" />
             <p className="help-block">
-              <i className="fa fa-info-circle"/>You can provide an IP (ipv4) of your AD's preferred DC. It is possible to specify multiple addresses separated by commas.
+              <i className="fa fa-info-circle" />
+              You can provide an IP (ipv4) of your AD's preferred DC. It is
+              possible to specify multiple addresses separated by commas.
             </p>
           </Form.ElementHorizontal>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.close}>Cancel</Button>
-          <Form.SubmitButton label='Save'/>
+          <Form.SubmitButton label="Save" />
         </Modal.Footer>
       </React.Fragment>
     )
   }
 
-  render(){
+  render() {
+    const FormContent = this.renderForm
     return (
-      <Modal backdrop='static' show={this.state.show} onHide={this.close} bsSize="large" aria-labelledby="contained-modal-title-lg">
+      <Modal
+        backdrop="static"
+        show={this.state.show}
+        onHide={this.close}
+        bsSize="large"
+        aria-labelledby="contained-modal-title-lg"
+      >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-lg">{this.props.title}</Modal.Title>
+          <Modal.Title id="contained-modal-title-lg">
+            {this.props.title}
+          </Modal.Title>
         </Modal.Header>
 
         <Form
-          className='form form-horizontal'
+          className="form form-horizontal"
           validate={this.validate}
           onSubmit={this.onSubmit}
-          initialValues={this.props.securityService || {}}>
-
-          <this.renderForm/>
+          initialValues={this.props.securityService || {}}
+        >
+          <FormContent />
         </Form>
       </Modal>
     )
--- plugins/shared_filesystem_storage/app/javascript/widgets/app/components/share_networks/new.jsx
+++ plugins/shared_filesystem_storage/app/javascript/widgets/app/components/share_networks/new.jsx
@@ -1,11 +1,11 @@
-import { Modal, Button } from 'react-bootstrap';
-import { Form } from 'lib/elektra-form';
+import { Modal, Button } from "react-bootstrap"
+import { Form } from "lib/elektra-form"
 
 export default class NewShareNetworkForm extends React.Component {
-  constructor(props){
-  	super(props);
+  constructor(props) {
+    super(props)
 
-  	this.state = {show: true};
+    this.state = { show: true }
     this.close = this.close.bind(this)
     this.networkSubnets = this.networkSubnets.bind(this)
     this.renderForm = this.renderForm.bind(this)
@@ -14,13 +14,15 @@ export default class NewShareNetworkForm extends React.Component {
   }
 
   validate(values) {
-    return values.name && values.neutron_net_id && values.neutron_subnet_id && true
+    return (
+      values.name && values.neutron_net_id && values.neutron_subnet_id && true
+    )
   }
 
-  close(e){
-    if(e) e.stopPropagation()
-    this.setState({show: false})
-    setTimeout(() => this.props.history.replace('/share-networks'),300)
+  close(e) {
+    if (e) e.stopPropagation()
+    this.setState({ show: false })
+    setTimeout(() => this.props.history.replace("/share-networks"), 300)
   }
 
   componentDidMount() {
@@ -28,101 +30,126 @@ export default class NewShareNetworkForm extends React.Component {
   }
 
   onValueChange(name, values) {
-    if (name=='neutron_net_id') {
+    if (name == "neutron_net_id") {
       this.props.loadSubnetsOnce(values[name])
     }
   }
 
-  onSubmit(values){
-    let subnet = this.props.subnets[values.neutron_net_id].items.find(i =>
-      i.id==values.neutron_subnet_id
+  onSubmit(values) {
+    let subnet = this.props.subnets[values.neutron_net_id].items.find(
+      (i) => i.id == values.neutron_subnet_id
     )
-    let newValues = Object.assign({},values,{cidr: subnet.cidr})
+    let newValues = Object.assign({}, values, { cidr: subnet.cidr })
     return this.props.handleSubmit(newValues).then(() => this.close())
   }
 
-  networkSubnets(neutron_net_id){
-    if(this.props.subnets && neutron_net_id)
-    return this.props.subnets[neutron_net_id]
+  networkSubnets(neutron_net_id) {
+    if (this.props.subnets && neutron_net_id)
+      return this.props.subnets[neutron_net_id]
   }
 
-  renderForm({values}) {
+  renderForm({ values }) {
     let networkSubnets = this.networkSubnets(values.neutron_net_id)
 
     return (
       <React.Fragment>
         <Modal.Body>
-          <Form.Errors/>
+          <Form.Errors />
 
-          <Form.ElementHorizontal label='Name' name="name" required>
-            <Form.Input elementType='input' type='text' name='name'/>
+          <Form.ElementHorizontal label="Name" name="name" required>
+            <Form.Input elementType="input" type="text" name="name" />
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='Description' name="description">
-            <Form.Input elementType='textarea' className="text optional form-control" name="description"/>
+          <Form.ElementHorizontal label="Description" name="description">
+            <Form.Input
+              elementType="textarea"
+              className="text optional form-control"
+              name="description"
+            />
           </Form.ElementHorizontal>
 
-          <Form.ElementHorizontal label='Neutron Net' required={true} name="neutron_net_id">
-            { this.props.networks.isFetching ? (
-              <span className='spinner'/>
+          <Form.ElementHorizontal
+            label="Neutron Net"
+            required={true}
+            name="neutron_net_id"
+          >
+            {this.props.networks.isFetching ? (
+              <span className="spinner" />
             ) : (
               <Form.Input
-                elementType='select'
+                elementType="select"
                 className="select required form-control"
-                name='neutron_net_id'>
+                name="neutron_net_id"
+              >
                 <option></option>
-                {this.props.networks.items.map((network,index) =>
-                  <option value={network.id} key={network.id}>{network.name}</option>
-                )}
+                {this.props.networks.items.map((network, index) => (
+                  <option value={network.id} key={network.id}>
+                    {network.name}
+                  </option>
+                ))}
               </Form.Input>
             )}
           </Form.ElementHorizontal>
 
-          { networkSubnets &&
-            <Form.ElementHorizontal label='Neutron Subnet' required={true} name="cidr">
-              { networkSubnets.isFetching ? (
-                <span className='spinner'/>
+          {networkSubnets && (
+            <Form.ElementHorizontal
+              label="Neutron Subnet"
+              required={true}
+              name="cidr"
+            >
+              {networkSubnets.isFetching ? (
+                <span className="spinner" />
               ) : (
                 <Form.Input
-                  elementType='select'
+                  elementType="select"
                   className="select required form-control"
-                  name='neutron_subnet_id'>
+                  name="neutron_subnet_id"
+                >
                   <option></option>
-                  { networkSubnets.items.map((subnet,index) =>
-                    <option value={subnet.id} key={subnet.id}>{subnet.name} {subnet.cidr}</option>
-                  )}
+                  {networkSubnets.items.map((subnet, index) => (
+                    <option value={subnet.id} key={subnet.id}>
+                      {subnet.name} {subnet.cidr}
+                    </option>
+                  ))}
                 </Form.Input>
               )}
             </Form.ElementHorizontal>
-          }
-
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.close}>Cancel</Button>
-          <Form.SubmitButton label='Save'/>
+          <Form.SubmitButton label="Save" />
         </Modal.Footer>
-    </React.Fragment>
+      </React.Fragment>
     )
   }
 
-  render(){
+  render() {
+    const FormContent = this.renderForm
     return (
-      <Modal backdrop='static' show={this.state.show} onHide={this.close} bsSize="large" aria-labelledby="contained-modal-title-lg">
+      <Modal
+        backdrop="static"
+        show={this.state.show}
+        onHide={this.close}
+        bsSize="large"
+        aria-labelledby="contained-modal-title-lg"
+      >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-lg">New Share Network</Modal.Title>
+          <Modal.Title id="contained-modal-title-lg">
+            New Share Network
+          </Modal.Title>
         </Modal.Header>
 
         <Form
           validate={this.validate}
           onValueChange={this.onValueChange}
           initialValues={{}}
-          className='form form-horizontal'
-          onSubmit={this.onSubmit}>
-
-          <this.renderForm/>
-
+          className="form form-horizontal"
+          onSubmit={this.onSubmit}
+        >
+          <FormContent />
         </Form>
       </Modal>
-    );
+    )
   }
 }
