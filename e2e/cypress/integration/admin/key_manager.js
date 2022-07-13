05cd02d7b27679bbdbe3845053d13e5363263a0e
d064310 <andreas.pfau@sap.com>
2022-07-13 13:24:31 +0200

    fix cypress tests for key manager

--- e2e/cypress/integration/admin/key_manager.js
+++ e2e/cypress/integration/admin/key_manager.js
@@ -9,30 +9,30 @@ describe("key_manager", () => {
 
   it("open key manager and check new secret button", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/secrets`)
-    cy.contains('[data-test=page-title]','Key Manager')
-    cy.contains('a.btn','New Secret').click()
-    cy.contains('New Secret')
-    cy.contains('button','Create').click()
+    cy.contains("[data-test=page-title]", "Key Manager")
+    cy.contains("a.btn", "New Secret").click()
+    cy.contains("New Secret")
+    cy.contains("button", "Create").click()
     cy.contains("Name: can't be blank")
   })
 
   it("open key manager and check elektra-test secret", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/secrets`)
-    cy.contains('a','elektra-test').click()
-    cy.contains('code','for test do not delete')
+    cy.get(".secrets_table table tbody a").first().click()
+    cy.contains("Secret details")
   })
 
   it("open key manager and check new container button", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/containers`)
-    cy.contains('a.btn','New Container').click()
-    cy.contains('New Container')
-    cy.contains('button','Create').click()
+    cy.contains("a.btn", "New Container").click()
+    cy.contains("New Container")
+    cy.contains("button", "Create").click()
     cy.contains("Name: can't be blank")
   })
 
   it("open key manager and check elektra-test-container", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/containers`)
-    cy.contains('a','elektra-test-container').click()
+    cy.get(".secrets_table table tbody a").first().click()
+    cy.contains("Container details")
   })
-
 })
