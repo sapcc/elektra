451a8f9e5ce97d74338c6c1315c020556572abc9
d064310 <andreas.pfau@sap.com>
2022-07-15 13:51:30 +0200

    bugfix in os_api_controller: do not require headers parameter

--- app/controllers/os_api_controller.rb
+++ app/controllers/os_api_controller.rb
@@ -2,13 +2,13 @@ class OsApiController < ::AjaxController
 
   # Example how to use in browser
   # fetch("os-api/SERVICE_NAME/SERVICE_PATH?headers=HEADERS_AS_JSON")
-  
+
   # This method implements a reverse proxy to openstack API
   def reverse_proxy 
     # get http method from request
     method = request.method.downcase
     # get path from request
-    path = params.require(:path)
+    path = params[:path]
     # we remove the first part of path. It is the openstack service name
     path_tokens = path.split("/")
     service_name = path_tokens.shift
@@ -17,7 +17,13 @@ class OsApiController < ::AjaxController
 
     # byebug
     # headers are provided as a parameter as JSON string. So we have to parse it.
-    headers = JSON.parse(params.require(:headers))
+    headers = begin 
+      JSON.parse(params[:headers]) 
+    rescue StandardError => e
+      Rails.logger.error("\033[31m\033[1mOSApiController: Could not parse headers parameter\033[0m")
+      pp e
+      {}  
+    end
     # get api client for the given service name
     service = services.os_api.service(service_name)
 
@@ -38,7 +44,7 @@ class OsApiController < ::AjaxController
       # GET, HEAD, DELETE case
       service.public_send(method,path, elektronParams, headers: headers).body      
     end
-
+    
     # byebug
     # render response as json
     render json: response
