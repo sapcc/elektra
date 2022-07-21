2bc21cfd65fcf51a9ff2ee049122f9a790f7efd3
D038721 <esther.schmitz@sap.com>
2022-07-21 15:51:09 +0200

    [testikus] item fixes

--- plugins/testikus/app/javascript/widgets/app/components/Welcome.jsx
+++ plugins/testikus/app/javascript/widgets/app/components/Welcome.jsx
@@ -3,22 +3,24 @@ import React from "react"
 import { IntroBox } from "juno-ui-components"
 
 const Welcome = () => (
-  <IntroBox variant="hero" title="Welcome to testikus">
-    <p>This is an example for how to build a simple react app inside Elektra.</p>
+  <>
+    <IntroBox variant="hero" title="Welcome to testikus">
+      <p>This is an example for how to build a simple react app inside Elektra.</p>
 
-    <h5>Folder structure:</h5>
+      <h5>Folder structure:</h5>
 
-    <ul>
-      <li>
-        <strong>components:</strong> contains jsx components which can be stateful or
-        stateless.
-      </li>
-      <li>
-        <strong>reducers:</strong> contains all the methods which are responsible for
-        manipulating the state.
-      </li>
-    </ul>
-  </IntroBox>
+      <ul>
+        <li>
+          <strong>components:</strong> contains jsx components which can be stateful or
+          stateless.
+        </li>
+        <li>
+          <strong>reducers:</strong> contains all the methods which are responsible for
+          manipulating the state.
+        </li>
+      </ul>
+    </IntroBox>
+  </>
 )
 
 export default Welcome
