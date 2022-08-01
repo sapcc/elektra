f60d49570701f791237f3ff00baf68c220838119
D058266 <hans-georg.winkler@sap.com>
2022-08-01 16:07:46 +0200

    revert changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -198,22 +198,7 @@ class DashboardController < ::ScopeController
 
   protected
 
-  helper_method :release_state
 
-  # Overwrite this method in your controller if you want to set the release
-  # state of your plugin to a different value. A tag will be displayed in
-  # the main toolbar next to the page header
-  # DON'T OVERWRITE THE VALUE HERE IN THE DASHBOARD CONTROLLER
-  # Possible values:
-  # ----------------
-  # "public_release"  (plugin is properly live and works, default)
-  # "experimental"    (for plugins that barely work or don't work at all)
-  # "tech_preview"    (early preview for a new feature that probably still
-  #                    has several bugs)
-  # "beta"            (if it's almost ready for public release)
-  def release_state
-    "public_release"
-  end
 
   def show_beta?
     params[:betafeatures] == "showme"
--- lib/generators/dashboard_plugin_generator.rb
+++ lib/generators/dashboard_plugin_generator.rb
@@ -3,6 +3,7 @@
 require_relative './plugin_skeleton_generator'
 require_relative './default_plugin_generator'
 require_relative './react_plugin_generator'
+require_relative './juno_plugin_generator'
 
 class DashboardPluginGenerator < Rails::Generators::NamedBase
   PLUGINS_PATH = 'plugins'
@@ -10,12 +11,15 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   class_option :mountable, type: :boolean, default: true, description: 'Generate mountable isolated application'
   class_option :service_layer, type: :boolean, default: false, description: 'Generate service layer (app/services/service_layer/)'
-  class_option :react, type: :boolean, default: false, description: 'Generate plugin using react/redux js-lib'
+  class_option :react, type: :boolean, default: false, description: 'Generate plugin using react'
+  class_option :juno, type: :boolean, default: true, description: 'Generate plugin using react and juno ui components'
 
   def start
     PluginSkeletonGenerator.new(self, PLUGINS_PATH).run
 
-    if options.react?
+    if options.juno?
+      JunoPluginGenerator.new(self,PLUGINS_PATH).run
+    elsif options.react?
       ReactPluginGenerator.new(self, PLUGINS_PATH).run
     else
       DefaultPluginGenerator.new(self, PLUGINS_PATH).run
@@ -28,6 +32,7 @@ class DashboardPluginGenerator < Rails::Generators::NamedBase
 
       gsub_file(file, '%{PLUGIN_NAME}', name)
       gsub_file(file, '%{PLUGIN_NAME_CAMELIZE}', name.camelize)
+      gsub_file(file, '%{PLUGIN_NAME_HUMANIZE}', name.humanize)
       gsub_file(file, /\n[\n]+/, "\n\n")
     end
 
