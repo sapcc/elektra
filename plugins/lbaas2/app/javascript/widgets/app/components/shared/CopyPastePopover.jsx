7a1bbcd6caa709353c716a45db57c7b82be0a857
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-09-22 10:28:33 +0200

    [lbaas] fix accidentally removed reference to overlay

--- plugins/lbaas2/app/javascript/widgets/app/components/shared/CopyPastePopover.jsx
+++ plugins/lbaas2/app/javascript/widgets/app/components/shared/CopyPastePopover.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import uniqueId from "lodash/uniqueId"
-import { Popover, Tooltip } from "react-bootstrap"
+import { Popover, Tooltip, Overlay } from "react-bootstrap"
 import Clipboard from "react-clipboard.js"
 import { Highlighter } from "react-bootstrap-typeahead"
 
