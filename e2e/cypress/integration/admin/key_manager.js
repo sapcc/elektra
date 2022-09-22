8a617b2d4fed2d7fdf3844cbe614244bff67193c
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-09-22 15:22:45 +0200

    Reactivate cypress test since deployment is successful

--- e2e/cypress/integration/admin/key_manager.js
+++ e2e/cypress/integration/admin/key_manager.js
@@ -1,38 +1,38 @@
-// describe("key_manager", () => {
-//   beforeEach(() => {
-//     cy.elektraLogin(
-//       Cypress.env("TEST_DOMAIN"),
-//       Cypress.env("TEST_USER"),
-//       Cypress.env("TEST_PASSWORD")
-//     )
-//   })
+describe("key_manager", () => {
+  beforeEach(() => {
+    cy.elektraLogin(
+      Cypress.env("TEST_DOMAIN"),
+      Cypress.env("TEST_USER"),
+      Cypress.env("TEST_PASSWORD")
+    )
+  })
 
-//   it("open key manager and check new secret button", () => {
-//     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/secrets`)
-//     cy.contains("[data-test=page-title]", "Key Manager")
-//     cy.contains("a.btn", "New Secret").click()
-//     cy.contains("New Secret")
-//     cy.contains("button", "Create").click()
-//     cy.contains("Name: can't be blank")
-//   })
+  it("open key manager and check new secret button", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/secrets`)
+    cy.contains("[data-test=page-title]", "Key Manager")
+    cy.contains("a.btn", "New Secret").click()
+    cy.contains("New Secret")
+    cy.contains("button", "Create").click()
+    cy.contains("Name: can't be blank")
+  })
 
-//   it("open key manager and check elektra-test secret", () => {
-//     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/secrets`)
-//     cy.get(".secrets_table table tbody a").first().click()
-//     cy.contains("Secret details")
-//   })
+  it("open key manager and check elektra-test secret", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/secrets`)
+    cy.get(".secrets_table table tbody a").first().click()
+    cy.contains("Secret details")
+  })
 
-//   it("open key manager and check new container button", () => {
-//     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/containers`)
-//     cy.contains("a.btn", "New Container").click()
-//     cy.contains("New Container")
-//     cy.contains("button", "Create").click()
-//     cy.contains("Name: can't be blank")
-//   })
+  it("open key manager and check new container button", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/containers`)
+    cy.contains("a.btn", "New Container").click()
+    cy.contains("New Container")
+    cy.contains("button", "Create").click()
+    cy.contains("Name: can't be blank")
+  })
 
-//   it("open key manager and check elektra-test-container", () => {
-//     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/containers`)
-//     cy.get(".secrets_table table tbody a").first().click()
-//     cy.contains("Container details")
-//   })
-// })
+  it("open key manager and check elektra-test-container", () => {
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/key-manager/containers`)
+    cy.get(".secrets_table table tbody a").first().click()
+    cy.contains("Container details")
+  })
+})
