115546abbc4c49456e578cb1f792cd7c6d0f64ad
D058266 <hans-georg.winkler@sap.com>
2022-09-23 11:26:09 +0200

    adjust kubernetes test

--- e2e/cypress/integration/admin/kubernetes.js
+++ e2e/cypress/integration/admin/kubernetes.js
@@ -9,19 +9,18 @@ describe("kubernetes", () => {
 
   it("open kubernetes page in admin project and test create dialog", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/kubernetes/`)
-    cy.contains('[data-test=page-title]','Kubernetes as a Service')
-    cy.contains('button','Create Cluster').click()
+    cy.contains("[data-test=page-title]", "Kubernetes as a Service")
+    cy.contains("button", "Create Cluster").click()
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(3000) // waiting for ajax to complete
-    cy.contains('button[type=submit]','Create').should('be.disabled')
-    cy.get("input[placeholder='lower case letters and numbers']").type('test')
-    cy.get("input[placeholder='a-z + 0-9']").type('1234')
+    cy.contains("button[type=submit]", "Create").should("be.disabled")
+    cy.get("input[placeholder='lower case letters and numbers']").type("test")
+    cy.get("input[placeholder='a-z + 0-9']").type("test")
     cy.get("select[name=keypair").select(1)
-    cy.contains('button[type=submit]','Create').should('be.enabled')
-    cy.contains('button','Add Pool').click()
-    cy.contains('Pool 2:')
-    cy.contains('button[type=submit]','Create').should('be.disabled')
-    cy.contains('button','Close').click()
+    cy.contains("button[type=submit]", "Create").should("be.enabled")
+    cy.contains("button", "Add Pool").click()
+    cy.contains("Pool 2:")
+    cy.contains("button[type=submit]", "Create").should("be.disabled")
+    cy.contains("button", "Close").click()
   })
-
 })
