8d0cc7c6d98a69a3c5b873d42bbe5cd0a89ccb3c
Hans-Georg Winkler <hans-georg.winkler@sap.com>
2022-10-05 09:40:34 +0200

    New fields masterdata (#1132)
    * [Masterdata]: first version for new fiels in our mega form ;-)
    
    * [Masterdata]: show new available information
    
    * [Masterdata]: other contacts with no email field and fix model to render ext cert fields in error case
    
    * [Masterdata]: adjust api call and help
    
    * [Masterdata] adjust help text
    
    * [Masterdata]: adjust naming and mandatory fields

--- plugins/masterdata_cockpit/lib/masterdata_cockpit.rb
+++ plugins/masterdata_cockpit/lib/masterdata_cockpit.rb
@@ -14,6 +14,54 @@ module MasterdataCockpit
       }
     end
 
+    def self.ext_certification
+      {
+        'iso' => 'ISO',
+        'pci' => 'PCI',
+        'soc1' => 'SOC1',
+        'soc2' => 'SOC2',
+        'c5' => 'C5',
+        'sox' => 'SOX'
+      }
+    end
+
+    def self.soft_license_mode 
+      {
+        'Revenue Generating' => 'Revenue Generating',
+        'Training and Demo' => 'Training & Demo',
+        'Development' => 'Development',
+        'Test and QS' => 'Test & QS',
+        'Administration' => 'Administration',
+        'Make' => 'Make',
+        'Virtualization-Host' => 'Virtualization-Host',
+        'Productive' => 'Productive'
+      }
+    end
+
+    def self.environment
+      {
+        'Prod' => 'Production',
+        'QA' => 'QA',
+        'Admin' => 'Admininstation',
+        'DEV' => 'Development',
+        'Demo' => 'Demo',
+        'Train' => 'Training',
+        'Sandbox' => 'Sandbox',
+        'Lab' => 'Lab',
+        'Test' => 'Testing'
+      }
+    end
+
+    def self.type_of_data
+      {
+        'SAP Business Proces' => 'SAP Business Process',
+        'Customer Cloud Service' => 'Customer Cloud Service',
+        'Customer Business Process' => 'Customer Business Process',
+        'Training and Demo' => 'Training & Demo',
+        'Cloud' => 'Cloud'
+      }
+    end
+
     def self.revenue_relevances
       {
         'generating' => 'Generating',
