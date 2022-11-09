404e2fb6abbe0395d472d8ef9c0a641bda194f7a
D063222 <a.reuschenbach.puncernau@sap.com>
2022-11-09 11:38:30 +0100

    [lbaas] fix rspec tests

--- plugins/lbaas2/spec/controllers/shared.rb
+++ plugins/lbaas2/spec/controllers/shared.rb
@@ -860,6 +860,135 @@ RSpec.shared_examples 'GET action with viewer context' do
   end
 end
 
+RSpec.shared_examples 'GET action with cloud_network_admin rule' do
+  before(:each) do
+    subject
+  end
+
+  context 'loadbalancer_admin' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'loadbalancer_admin' }
+        token
+      end
+    end
+    it 'returns http success' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response).to_not be_successful
+    end
+  end
+
+  context 'network_admin' do
+    before :each do
+      stub_authentication do |token|
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'network_admin' }
+        token
+      end
+    end
+    it 'returns http success' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response).to_not be_successful
+    end
+  end
+
+  context 'cloud_network_admin' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'cloud_network_admin' }
+        token
+      end
+    end
+    it 'returns http success' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response).to be_successful
+    end
+  end
+
+  context 'member' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'member' }
+        token
+      end
+    end
+    it 'returns http success' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response).to_not be_successful
+    end
+  end
+
+  context 'network_viewer' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'network_viewer' }
+        token
+      end
+    end
+    it 'returns 403 error' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response.code).to be == ("403")
+      expect(response).to_not be_successful
+    end
+  end
+
+  context 'loadbalancer_viewer' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'loadbalancer_viewer' }
+        token
+      end
+    end
+    it 'returns 403 error' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response.code).to be == ("403")
+      expect(response).to_not be_successful
+    end
+  end
+
+  context 'loadbalancer_poolmemberadmin' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token['roles'] << { 'id' => 'lbaas2_role', 'name' => 'loadbalancer_poolmemberadmin' }
+        token
+      end
+    end
+    it 'returns 403 error' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response.code).to be == ("403")
+      expect(response).to_not be_successful
+    end
+  end
+
+  context 'empty network roles' do
+    before :each do
+      stub_authentication do |token|
+        # token['roles'].delete_if { |h| h['id'] == 'lbaas2_role' }
+        token['roles'] = []
+        token
+      end
+    end
+    it 'returns 403 error' do
+      get @path, params: @default_params.merge(@extra_params)
+      expect(response.code).to be == ("403")
+      expect(response).to_not be_successful
+    end
+  end
+
+end
+
 RSpec.shared_examples 'GET action with lbaas_admin rule.' do
   before(:each) do
     subject
