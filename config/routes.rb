ce7978dafc3f45cf7e33ecc3e651dd8c66607001
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-12 21:44:35 +0100

    test formatter esbenp.prettier-vscode

--- config/routes.rb
+++ config/routes.rb
@@ -1,55 +1,57 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount MonsoonOpenstackAuth::Engine => '/:domain_fid/auth'
+  mount MonsoonOpenstackAuth::Engine => "/:domain_fid/auth"
 
   # "jump to" routes
   # if project_id is known then the domain name or id is unnecessary
   # the jump controller will try to redirect to the requested url
-  get '/_/:project_id(/*rest)', to: 'jump#index'
+  get "/_/:project_id(/*rest)", to: "jump#index"
   # jump to a specific object like instance or network
-  get '/_jump_to/:object_id', to: 'jump#show'
+  get "/_jump_to/:object_id", to: "jump#show"
 
-  scope '/system' do
+  scope "/system" do
     # check without db connection
-    get :liveliness, to: 'health#liveliness'
+    get :liveliness, to: "health#liveliness"
     # check with db connection
-    get :readiness, to: 'health#readiness'
+    get :readiness, to: "health#readiness"
   end
 
   # mount Cloudops::Engine => '/ccadmin/cloud_admin/cloudops', as: 'cloudops'
-  get '/cloudops', to: redirect(
-    "/#{Rails.application.config.cloud_admin_domain}/" \
-    "#{Rails.configuration.cloud_admin_project}/cloudops"
-  )
+  get "/cloudops",
+      to:
+        redirect(
+          "/#{Rails.application.config.cloud_admin_domain}/" \
+            "#{Rails.configuration.cloud_admin_project}/cloudops",
+        )
 
-  scope '/:domain_id(/:project_id)(/:plugin)' do
-    get 'os-api/__auth_token', to: 'os_api#auth_token'
-    match 'os-api(/*path)', to: 'os_api#reverse_proxy', via: :all
+  scope "/:domain_id(/:project_id)(/:plugin)" do
+    get "os-api/__auth_token", to: "os_api#auth_token"
+    match "os-api(/*path)", to: "os_api#reverse_proxy", via: :all
 
     resources :cache, only: %i[index show] do
       collection do
-        get 'csv'
-        get 'types'
-        get 'users'
-        get 'groups'
-        get 'domain_projects'
-        get 'projects'
-        get 'live_search'
-        get 'related-objects' => 'cache#related_objects'
-        post 'objects' => 'cache#cache_objects'
-        get 'objects-by-ids' => 'cache#objects_by_ids'
+        get "csv"
+        get "types"
+        get "users"
+        get "groups"
+        get "domain_projects"
+        get "projects"
+        get "live_search"
+        get "related-objects" => "cache#related_objects"
+        post "objects" => "cache#cache_objects"
+        get "objects-by-ids" => "cache#objects_by_ids"
       end
     end
   end
 
-  scope '/:domain_id' do
-    match '/', to: 'pages#show', id: 'landing', via: :get, as: :landing_page
+  scope "/:domain_id" do
+    match "/", to: "pages#show", id: "landing", via: :get, as: :landing_page
 
-    scope '(/:project_id)' do
-      scope module: 'dashboard' do
-        post 'accept_terms_of_use'
-        get 'terms_of_use'
+    scope "(/:project_id)" do
+      scope module: "dashboard" do
+        post "accept_terms_of_use"
+        get "terms_of_use"
       end
 
       ###################### MOUNT PLUGINS #####################
@@ -57,46 +59,55 @@ Rails.application.routes.draw do
         next if %w[docs cloudops tools].include?(plugin.name)
 
         Logger.new(STDOUT).debug(
-          "Mount plugin #{plugin.mount_point} as #{plugin.name}_plugin"
+          "Mount plugin #{plugin.mount_point} as #{plugin.name}_plugin",
         )
         # mount point is the name of the plugin
         mount plugin.engine_class => "/#{plugin.mount_point}",
-              as: "#{plugin.name}_plugin"
+              :as => "#{plugin.name}_plugin"
       end
       ######################## END ############################
-      Logger.new(STDOUT).debug(
-        'Mount plugin tools as cc_tools_plugin'
-      )
-      mount Tools::Engine => '/cc-tools', as: :cc_tools_plugin
+      Logger.new(STDOUT).debug("Mount plugin tools as cc_tools_plugin")
+      mount Tools::Engine => "/cc-tools", :as => :cc_tools_plugin
     end
 
-    Logger.new(STDOUT).debug(
-      'Mount plugin cloudops as cloudops_plugin'
-    )
-    mount Cloudops::Engine => "/#{Rails.configuration.cloud_admin_project}" \
-                              '/cloudops', as: 'cloudops_plugin', defaults: { project_id: Rails.configuration.cloud_admin_project },
-          constraints: { domain_id: Rails.application.config.cloud_admin_domain }
+    Logger.new(STDOUT).debug("Mount plugin cloudops as cloudops_plugin")
+    mount Cloudops::Engine =>
+            "/#{Rails.configuration.cloud_admin_project}" \
+              "/cloudops",
+          :as => "cloudops_plugin",
+          :defaults => {
+            project_id: Rails.configuration.cloud_admin_project,
+          },
+          :constraints => {
+            domain_id: Rails.application.config.cloud_admin_domain,
+          }
   end
 
-  scope module: 'identity' do
-    get '/:domain_id/home' => 'domains#show', as: :domain_home
-    get '/:domain_id/:project_id/home' => 'projects#show', as: :project_home
+  scope module: "identity" do
+    get "/:domain_id/home" => "domains#show", :as => :domain_home
+    get "/:domain_id/:project_id/home" => "projects#show", :as => :project_home
   end
 
   # in case someone tries to call a project url without the trailing '/home'
-  get '/:domain_id/:project_id', to: redirect('/%{domain_id}/%{project_id}/home') 
+  get "/:domain_id/:project_id",
+      to: redirect("/%{domain_id}/%{project_id}/home")
 
   # route for overwritten High Voltage Pages controller
-  get '/pages/*id' => 'pages#show', as: :core_page, format: false
+  get "/pages/*id" => "pages#show", :as => :core_page, :format => false
 
   # root to: 'pages#show', id: 'landing'
 
-  root(to: redirect do |_params, request|
-    domain_id = request.query_parameters['domain_id']
-    "/#{domain_id || Rails.application.config.default_domain}"
-  end)
+  root(
+    to:
+      redirect do |_params, request|
+        domain_id = request.query_parameters["domain_id"]
+        "/#{domain_id || Rails.application.config.default_domain}"
+      end,
+  )
 
   # route all other urls to 404 page ignoring all formats except html
-  get '*path', to: 'errors#error_404', via: :all,
-               constraints: ->(req) { req.format == :html }
+  get "*path",
+      to: "errors#error_404",
+      via: :all,
+      constraints: ->(req) { req.format == :html }
 end
