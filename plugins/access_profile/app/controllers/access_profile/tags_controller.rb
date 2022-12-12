6b59eba1fdf93a8680dd1fab8520c01dd5298356
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-12 15:42:53 +0100

    [access-profile] beautify ruby files

--- plugins/access_profile/app/controllers/access_profile/tags_controller.rb
+++ plugins/access_profile/app/controllers/access_profile/tags_controller.rb
@@ -6,8 +6,8 @@ module AccessProfile
     authorization_required
 
     module Action
-      TAG_CREATE=1
-      TAG_DESTROY=2
+      TAG_CREATE = 1
+      TAG_DESTROY = 2
     end
 
     # Returns project tags relevant to access profiles
@@ -16,24 +16,21 @@ module AccessProfile
       tags = cloud_admin.identity.list_tags(@scoped_project_id)
       render json: { tags: filter_tags(tags) }
     rescue Elektron::Errors::ApiResponse => e
-      puts "error elektron"
+      puts 'error elektron'
       render json: { errors: e.message }, status: e.code
     rescue Exception => e
-      render json: { errors: e.message }, status: "500"
+      render json: { errors: e.message }, status: '500'
     end
 
     def create
       tag = params[:tag]
       tags = cloud_admin.identity.list_tags(@scoped_project_id)
       ok, err = validate_tag(tag, filter_tags(tags), Action::TAG_CREATE)
-      unless ok
-        return render json: {errors: err, tagSpec: GalvaniConfig["access_profiles"]}, status: 422        
-      end
+      return render json: { errors: err, tagSpec: GalvaniConfig['access_profiles'] }, status: 422 unless ok
+
       # ensure base prefix/tag is also added
       ok, err = ensure_base_prefix(tag, filter_tags(tags), Action::TAG_CREATE)
-      unless ok
-        return render json: {errors: err}, status: 422        
-      end
+      return render json: { errors: err }, status: 422 unless ok
 
       cloud_admin.identity.add_single_tag(@scoped_project_id, tag)
       audit_logger.info(current_user, 'has created tag', tag)
@@ -41,34 +38,32 @@ module AccessProfile
     rescue Elektron::Errors::ApiResponse => e
       render json: { errors: e.message }, status: e.code
     rescue Exception => e
-      render json: { errors: e.message }, status: "500"
+      render json: { errors: e.message }, status: '500'
     end
 
     def destroy
       tag = params[:id]
       tags = cloud_admin.identity.list_tags(@scoped_project_id)
       ok, err = validate_tag(tag, filter_tags(tags), Action::TAG_DESTROY)
-      unless ok
-        return render json: {errors: err, tagSpec: GalvaniConfig["access_profiles"]}, status: 422        
-      end
+      return render json: { errors: err, tagSpec: GalvaniConfig['access_profiles'] }, status: 422 unless ok
+
       # ensure base prefix/tag is also removed
       ok, err = ensure_base_prefix(tag, filter_tags(tags), Action::TAG_DESTROY)
-      unless ok
-        return render json: {errors: err}, status: 422        
-      end
-      cloud_admin.identity.remove_single_tag(@scoped_project_id, tag)      
+      return render json: { errors: err }, status: 422 unless ok
+
+      cloud_admin.identity.remove_single_tag(@scoped_project_id, tag)
       audit_logger.info(current_user, 'has deleted tag', tag)
       render json: { tag: tag }
     rescue Elektron::Errors::ApiResponse => e
       render json: { errors: e.message }, status: e.code
     rescue Exception => e
-      render json: { errors: e.message }, status: "500"
+      render json: { errors: e.message }, status: '500'
     end
 
     def profiles_config
-      render json: { config: GalvaniConfig["access_profiles"] }
+      render json: { config: GalvaniConfig['access_profiles'] }
     rescue Exception => e
-      render json: { errors: e.message }, status: "500"
+      render json: { errors: e.message }, status: '500'
     end
 
     private
@@ -76,10 +71,9 @@ module AccessProfile
     # return all access profile tags --> prefixes from config file xs:internet, ...
     # the base prefixes should be removed
     def filter_tags(tags)
-      if tags.blank?
-        return []
-      end
-      profile_prefixes = GalvaniConfig["access_profiles"].keys || []
+      return [] if tags.blank?
+
+      profile_prefixes = GalvaniConfig['access_profiles'].keys || []
       # array with splat operator(*), select returns a new array
       tags.select { |n| n.start_with?(*profile_prefixes) }
     end
@@ -88,101 +82,84 @@ module AccessProfile
     # remove base prefix/tag when last tag from this base is removed
     # base prefix/tag is being used to filter in the openstack api
     def ensure_base_prefix(tag, existing_tags, action)
-      # get base prefix 
+      # get base prefix
       base_prefix = base_prefix(tag)
-      if base_prefix.blank?
-        return false, "no base access profile found"
-      end
+      return false, 'no base access profile found' if base_prefix.blank?
 
       case action
       when Action::TAG_CREATE
         # check if the prefix is already added as tag
-        if !existing_tags.include?(base_prefix)
+        unless existing_tags.include?(base_prefix)
           begin
             cloud_admin.identity.add_single_tag(@scoped_project_id, base_prefix)
           rescue Exception => e
             return false, "Error create base access profile. #{e.message}"
-          end          
+          end
         end
       when Action::TAG_DESTROY
         # check if there is more tags with the same base_prefix
         # search for tags with "base_prefix" with ":" so not base prefixes can't be matched
-        if existing_tags.grep(/#{base_prefix}:/).count() == 1 && existing_tags.include?(tag)
+        if existing_tags.grep(/#{base_prefix}:/).count == 1 && existing_tags.include?(tag)
           begin
-            cloud_admin.identity.remove_single_tag(@scoped_project_id, base_prefix)      
+            cloud_admin.identity.remove_single_tag(@scoped_project_id, base_prefix)
           rescue Exception => e
             return false, "Error removing base access profile. #{e.message}"
-          end                    
+          end
         end
       end
-      return true, ""
+      [true, '']
     end
 
-    # validate that tag 
+    # validate that tag
     def validate_tag(tag, existing_tags, action)
       # check for empty tags
-      if tag.blank?
-        return false, "access profile is empty!"
-      end
+      return false, 'access profile is empty!' if tag.blank?
 
-      # get base prefix 
+      # get base prefix
       base_prefix = base_prefix(tag)
-      if base_prefix.blank?
-        return false, "no base access profile found"
-      end
+      return false, 'no base access profile found' if base_prefix.blank?
 
       # get the sercice
-      services = GalvaniConfig["access_profiles"][base_prefix].keys.select do|k| 
+      services = GalvaniConfig['access_profiles'][base_prefix].keys.select do |k|
         # service is everyting until the arguments $1, $2...
-        serviceKey = k.split(":$").first
-        tag.start_with?(base_prefix + ":" + serviceKey)
+        serviceKey = k.split(':$').first
+        tag.start_with?(base_prefix + ':' + serviceKey)
       end
 
-      if services.blank?
-        return false, "no service found"
-      end
+      return false, 'no service found' if services.blank?
 
       service = services[0]
       # get the service args
-      service_name_args = service.split(":$")
+      service_name_args = service.split(':$')
       service_name = service_name_args.shift
       service_args = service_name_args
       # get the tag arguments
-      tag_args_str = tag.split(base_prefix + ":" + service_name + ":")[1] || ""
-      tag_args = tag_args_str.split(":")
+      tag_args_str = tag.split(base_prefix + ':' + service_name + ':')[1] || ''
+      tag_args = tag_args_str.split(':')
 
       # check for required args from the config
-      if service_args.length != tag_args.length
-        return false, "provided arguments mismatch"
-      end    
+      return false, 'provided arguments mismatch' if service_args.length != tag_args.length
 
-      if action == Action::TAG_CREATE
+      if action == Action::TAG_CREATE && existing_tags.include?(tag)
         # check for duplicates when creating new tags
-        if existing_tags.include?(tag)
-          return false, "access profile already exists" 
-        end
+        return false, 'access profile already exists'
       end
 
-      return true, ""
+      [true, '']
     end
 
     # returns base prefix if found
     def base_prefix(tag)
-      if tag.blank?
-        return ""
-      end
+      return '' if tag.blank?
 
       # get base prefix
-      base_prefixes = GalvaniConfig["access_profiles"].keys.select do|n| 
+      base_prefixes = GalvaniConfig['access_profiles'].keys.select do |n|
         tag.start_with?(n)
       end
 
-      if base_prefixes.blank?
-        return ""
-      end
+      return '' if base_prefixes.blank?
 
       base_prefixes[0]
     end
-
   end
 end
--- plugins/access_profile/spec/controllers/tag_controller_spec.rb
+++ plugins/access_profile/spec/controllers/tag_controller_spec.rb
@@ -1,11 +1,10 @@
-
 require 'spec_helper'
 
 describe AccessProfile::TagsController, type: :controller do
   routes { AccessProfile::Engine.routes }
 
   default_params = {  domain_id: AuthenticationStub.domain_id,
-                      project_id: AuthenticationStub.project_id}
+                      project_id: AuthenticationStub.project_id }
 
   before(:all) do
     FriendlyIdEntry.find_or_create_entry(
@@ -19,7 +18,7 @@ describe AccessProfile::TagsController, type: :controller do
 
   describe "GET 'index'" do
     before :each do
-      @existing_tags = ["xs:internet:keppel_account_pull:test1","xs:internet:keppel_account_pull:test2"]
+      @existing_tags = ['xs:internet:keppel_account_pull:test1', 'xs:internet:keppel_account_pull:test2']
       allow(controller.cloud_admin).to receive(:list_tags).and_return(@existing_tags)
     end
 
@@ -50,8 +49,6 @@ describe AccessProfile::TagsController, type: :controller do
         expect(response).to_not be_successful
       end
     end
-
-
   end
 
   describe "PUT 'create a tag'" do
@@ -65,39 +62,38 @@ describe AccessProfile::TagsController, type: :controller do
         end
       end
       it 'returns http success' do
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull:cc-demo"})
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull:cc-demo' })
         expect(response).to be_successful
       end
 
       it 'adds base prefix/tag' do
-        identity = double("identity", list_tags: [])
+        identity = double('identity', list_tags: [])
         cloud_admin = double('cloud_admin', identity: identity).as_null_object
         new_tags = []
-        allow(identity).to receive(:add_single_tag) do |scoped_project_id, tag|
+        allow(identity).to receive(:add_single_tag) do |_scoped_project_id, tag|
           new_tags.push(tag)
         end
         allow_any_instance_of(::ApplicationController).to receive(:cloud_admin).and_return(cloud_admin)
 
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull:cc-demo"})
-        expect(new_tags.include?("xs:internet:keppel_account_pull:cc-demo")).to be true
-        expect(new_tags.include?("xs:internet")).to be true
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull:cc-demo' })
+        expect(new_tags.include?('xs:internet:keppel_account_pull:cc-demo')).to be true
+        expect(new_tags.include?('xs:internet')).to be true
       end
 
       it 'no adds base prefix/tag if already exists' do
-        identity = double("identity", list_tags: ["xs:internet"])
+        identity = double('identity', list_tags: ['xs:internet'])
         cloud_admin = double('cloud_admin', identity: identity).as_null_object
         new_tags = []
-        allow(identity).to receive(:add_single_tag) do |scoped_project_id, tag|
+        allow(identity).to receive(:add_single_tag) do |_scoped_project_id, tag|
           new_tags.push(tag)
         end
         allow_any_instance_of(::ApplicationController).to receive(:cloud_admin).and_return(cloud_admin)
 
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull:cc-demo"})
-        expect(new_tags.include?("xs:internet:keppel_account_pull:cc-demo")).to be true
-        expect(new_tags.include?("xs:internet")).to_not be true
-        expect(new_tags.length()).to eq(1)
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull:cc-demo' })
+        expect(new_tags.include?('xs:internet:keppel_account_pull:cc-demo')).to be true
+        expect(new_tags.include?('xs:internet')).to_not be true
+        expect(new_tags.length).to eq(1)
       end
-
     end
 
     context 'empty roles' do
@@ -108,7 +104,7 @@ describe AccessProfile::TagsController, type: :controller do
         end
       end
       it 'returns http success' do
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull:cc-demo"})
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull:cc-demo' })
         expect(response).to_not be_successful
       end
     end
@@ -121,51 +117,50 @@ describe AccessProfile::TagsController, type: :controller do
           token['roles'] << { 'id' => 'cloud_support_tools_viewer', 'name' => 'cloud_support_tools_viewer' }
           token
         end
-        @existing_tag = "xs:internet:keppel_account_pull:d063222"
+        @existing_tag = 'xs:internet:keppel_account_pull:d063222'
         allow(controller.cloud_admin).to receive(:list_tags).and_return([@existing_tag])
       end
 
       it 'returns http error if no tag given' do
         post :create, params: default_params.merge({})
         expect(response).to_not be_successful
-      end      
+      end
       it 'returns http success using profile and service with required variable' do
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull:cc-demo"})             
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull:cc-demo' })
         expect(response).to be_successful
       end
       it 'returns http error using profile and service without required variable' do
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull"})
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull' })
         expect(response).to_not be_successful
       end
       it 'returns http error using profile and service with number of variable mistmacht' do
-        post :create, params: default_params.merge({tag: "xs:internet:keppel_account_pull:cc-demo:blabla"})
+        post :create, params: default_params.merge({ tag: 'xs:internet:keppel_account_pull:cc-demo:blabla' })
         expect(response).to_not be_successful
-      end      
+      end
       it 'returns http success using profile and service' do
-        post :create, params: default_params.merge({tag: "xs:internet:dns_reader"})
+        post :create, params: default_params.merge({ tag: 'xs:internet:dns_reader' })
         expect(response).to be_successful
       end
       it 'returns http error using profile and service and not required variable' do
-        post :create, params: default_params.merge({tag: "xs:internet:dns:reader:test"})
+        post :create, params: default_params.merge({ tag: 'xs:internet:dns:reader:test' })
         expect(response).to_not be_successful
-      end     
+      end
       it 'returns http error using just the profile name' do
-        post :create, params: default_params.merge({tag: "xs:internet"})
+        post :create, params: default_params.merge({ tag: 'xs:internet' })
         expect(response).to_not be_successful
       end
       it 'returns http error if tag already exists' do
-        post :create, params: default_params.merge({tag: @existing_tag})
+        post :create, params: default_params.merge({ tag: @existing_tag })
         expect(response).to_not be_successful
       end
     end
-    
   end
 
   describe "DELETE 'tag'" do
     before :each do
-      @existing_tag = "xs:internet:keppel_account_pull:d063222"
+      @existing_tag = 'xs:internet:keppel_account_pull:d063222'
       allow(controller.cloud_admin).to receive(:list_tags).and_return([@existing_tag])
-      allow(controller.cloud_admin).to receive(:remove_single_tag).and_return("succeed")
+      allow(controller.cloud_admin).to receive(:remove_single_tag).and_return('succeed')
     end
 
     context 'project_admin' do
@@ -178,39 +173,38 @@ describe AccessProfile::TagsController, type: :controller do
         end
       end
       it 'returns http success' do
-        delete :destroy, params: default_params.merge({id: "xs:internet:keppel_account_pull:d063222"})
+        delete :destroy, params: default_params.merge({ id: 'xs:internet:keppel_account_pull:d063222' })
         expect(response).to be_successful
       end
       it 'removes base prefix/tag' do
-        all_tags = ["xs:internet","xs:internet:keppel_account_pull:cc-demo"]
-        identity = double("identity", list_tags: all_tags)
+        all_tags = ['xs:internet', 'xs:internet:keppel_account_pull:cc-demo']
+        identity = double('identity', list_tags: all_tags)
         cloud_admin = double('cloud_admin', identity: identity).as_null_object
-        allow(identity).to receive(:remove_single_tag) do |scoped_project_id, tag|
-          all_tags.delete_if {|x| x == tag } 
+        allow(identity).to receive(:remove_single_tag) do |_scoped_project_id, tag|
+          all_tags.delete_if { |x| x == tag }
         end
         allow_any_instance_of(::ApplicationController).to receive(:cloud_admin).and_return(cloud_admin)
 
-        delete :destroy, params: default_params.merge({id: "xs:internet:keppel_account_pull:cc-demo"})
-        expect(all_tags.include?("xs:internet:keppel_account_pull:cc-demo")).to be false
-        expect(all_tags.include?("xs:internet")).to be false
-        expect(all_tags.length()).to eq(0)
+        delete :destroy, params: default_params.merge({ id: 'xs:internet:keppel_account_pull:cc-demo' })
+        expect(all_tags.include?('xs:internet:keppel_account_pull:cc-demo')).to be false
+        expect(all_tags.include?('xs:internet')).to be false
+        expect(all_tags.length).to eq(0)
       end
 
       it 'keeps base prefix/tag' do
-        all_tags =  ["xs:internet","xs:internet:keppel_account_pull:cc-demo", "xs:internet:keppel_account_pull:miau"]
-        identity = double("identity", list_tags: all_tags)
+        all_tags = ['xs:internet', 'xs:internet:keppel_account_pull:cc-demo', 'xs:internet:keppel_account_pull:miau']
+        identity = double('identity', list_tags: all_tags)
         cloud_admin = double('cloud_admin', identity: identity).as_null_object
-        allow(identity).to receive(:remove_single_tag) do |scoped_project_id, tag|
-          all_tags.delete_if {|x| x == tag } 
+        allow(identity).to receive(:remove_single_tag) do |_scoped_project_id, tag|
+          all_tags.delete_if { |x| x == tag }
         end
         allow_any_instance_of(::ApplicationController).to receive(:cloud_admin).and_return(cloud_admin)
 
-        delete :destroy, params: default_params.merge({id: "xs:internet:keppel_account_pull:cc-demo"})
-        expect(all_tags.include?("xs:internet:keppel_account_pull:cc-demo")).to be false
-        expect(all_tags.include?("xs:internet")).to be true
-        expect(all_tags.length()).to eq(2)
+        delete :destroy, params: default_params.merge({ id: 'xs:internet:keppel_account_pull:cc-demo' })
+        expect(all_tags.include?('xs:internet:keppel_account_pull:cc-demo')).to be false
+        expect(all_tags.include?('xs:internet')).to be true
+        expect(all_tags.length).to eq(2)
       end
-      
     end
 
     context 'empty roles' do
@@ -221,7 +215,7 @@ describe AccessProfile::TagsController, type: :controller do
         end
       end
       it 'returns http success' do
-        delete :destroy, params: default_params.merge({id: "xs:internet:keppel_account_pull:d063222"})
+        delete :destroy, params: default_params.merge({ id: 'xs:internet:keppel_account_pull:d063222' })
         expect(response).to_not be_successful
       end
     end
@@ -237,26 +231,23 @@ describe AccessProfile::TagsController, type: :controller do
       end
 
       it 'returns http error if no tag given' do
-        delete :destroy, params: default_params.merge({id: ""})
+        delete :destroy, params: default_params.merge({ id: '' })
         expect(response).to_not be_successful
       end
 
       it 'returns http error if no tag no valid' do
-        delete :destroy, params: default_params.merge({id: "xs:internet:non_existing_service:d063222"})
+        delete :destroy, params: default_params.merge({ id: 'xs:internet:non_existing_service:d063222' })
         expect(response).to_not be_successful
       end
 
       it 'returns succeed with an existing tag which matches the config' do
-        delete :destroy, params: default_params.merge({id: "xs:internet:keppel_account_pull:d063222"})
+        delete :destroy, params: default_params.merge({ id: 'xs:internet:keppel_account_pull:d063222' })
         expect(response).to be_successful
       end
-
     end
-
   end
 
   describe "get 'profiles_config'" do
-
     context 'project_admin' do
       before :each do
         stub_authentication do |token|
@@ -284,7 +275,5 @@ describe AccessProfile::TagsController, type: :controller do
         expect(response).to_not be_successful
       end
     end
-
   end
-
 end
