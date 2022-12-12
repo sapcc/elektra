a012019193153ad57e72679ecd4d76ca65970880
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-12 11:34:22 +0100

    [startprobe] configure layout, view and controller action for the start probe

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,8 +2,17 @@ class HealthController < ActionController::Base
   def liveliness
     render plain: 'Alive!'
   end
+
   def readiness
     FriendlyIdEntry.count
     render plain: 'OK'
+  end
+
+  def startprobe
+    # check connection with the db
+    FriendlyIdEntry.count
+    # render similar layout as our application with all general JS (not specific from the a plugin)
+    @startProbeText = 'This is the start probe'
+    render layout: 'health'
   end
 end
