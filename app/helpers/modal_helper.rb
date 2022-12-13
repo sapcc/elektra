cbb9b125161a92722c7043f30ce2ea5ddc3698d4
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-13 12:34:04 +0100

    [format] formatting elektra helpers

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,5 +1,4 @@
 module ModalHelper
-
   # Like simple_form_for(), but includes all the boilerplate for a modal dialog as well.
   # Button "Cancel" is added automatically. The following options can be provided for this:
   #
@@ -8,12 +7,12 @@ module ModalHelper
   #
   def simple_modal_form_for(model_object, options = {}, &block)
     # check mandatory options
-    raise ArgumentError, 'cancel_url missing' unless options.key?(:cancel_url)
+    raise ArgumentError, "cancel_url missing" unless options.key?(:cancel_url)
     cancel_url = options.delete(:cancel_url)
 
     submit_action = options.delete(:submit_action)
     # default submit caption
-    submit_action = 'Submit' if submit_action && !submit_action.is_a?(String)
+    submit_action = "Submit" if submit_action && !submit_action.is_a?(String)
 
     # set some options that are required for the simple_form_for() to work inside a modal dialog
     options[:remote] = request.xhr? unless options.key?(:remote)
@@ -23,13 +22,16 @@ module ModalHelper
     data_hash[:modal] = true unless data_hash.key?(:modal)
 
     # the actual work is done in a layout; the helper is just there to resemble simple_form_for()
-    render(layout: 'simple_modal_form', locals: {
-      model_object:  model_object,
-      submit_action: submit_action,
-      options:       options,
-      cancel_url:    cancel_url,
-      cancel_text:   options.fetch(:cancel_text, 'Cancel')
-    }, &block)
+    render(
+      layout: "simple_modal_form",
+      locals: {
+        model_object: model_object,
+        submit_action: submit_action,
+        options: options,
+        cancel_url: cancel_url,
+        cancel_text: options.fetch(:cancel_text, "Cancel"),
+      },
+      &block
+    )
   end
-
 end
--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,12 +2,16 @@ module UrlHelper
   # prefixed to not interfere with ActionDispatch::Routing::UrlFor
   def sap_url_for(servicename)
     # special handling for global services
-    region = servicename == 'documentation' ? 'global' : "#{current_region}"
+    region = servicename == "documentation" ? "global" : "#{current_region}"
 
     return "https://#{servicename}.#{region}.#{request.domain}/"
   end
 
   def url_for_avatar
-    return eval('"' + ENV['MONSOON_DASHBOARD_AVATAR_URL'] + '"') rescue "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.email ? current_user.email : '')}?d=mm&size=24x24"
+    begin
+      return eval('"' + ENV["MONSOON_DASHBOARD_AVATAR_URL"] + '"')
+    rescue StandardError
+      "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_user.email ? current_user.email : "")}?d=mm&size=24x24"
+    end
   end
 end
