ff52a0f85438e8b83a83d2acace36baa0360107e
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-14 10:56:29 +0100

    [format]: formatted plugin block_storage

--- plugins/block_storage/app/controllers/block_storage/snapshots_controller.rb
+++ plugins/block_storage/app/controllers/block_storage/snapshots_controller.rb
@@ -2,14 +2,14 @@
 
 module BlockStorage
   class SnapshotsController < BlockStorage::ApplicationController
-    authorization_context 'block_storage'
+    authorization_context "block_storage"
     authorization_required
 
     # GET /snapshots
     def index
       per_page = (params[:per_page] || 20).to_i
 
-      options = { sort: 'id:asc', limit: per_page + 1 }
+      options = { sort: "id:asc", limit: per_page + 1 }
       options[:marker] = params[:marker] if params[:marker]
       snapshots = services.block_storage.snapshots_detail(options)
 
@@ -17,13 +17,11 @@ module BlockStorage
 
       # byebug
       render json: {
-        snapshots: snapshots,
-        has_next: snapshots.length > per_page
-      }
+               snapshots: snapshots,
+               has_next: snapshots.length > per_page,
+             }
     rescue Elektron::Errors::ApiResponse => e
-      render json: {
-        errors: e.message
-      }
+      render json: { errors: e.message }
     end
 
     def show
@@ -36,12 +34,13 @@ module BlockStorage
     end
 
     def create
-      snapshot = services.block_storage.new_snapshot(
-        params[:snapshot].merge(force: false)
-      )
+      snapshot =
+        services.block_storage.new_snapshot(
+          params[:snapshot].merge(force: false),
+        )
 
       if snapshot.save
-        audit_logger.info(current_user, 'has created', snapshot)
+        audit_logger.info(current_user, "has created", snapshot)
         render json: snapshot
       else
         render json: { errors: snapshot.errors }, status: 422
@@ -54,7 +53,7 @@ module BlockStorage
       snapshot = services.block_storage.find_snapshot!(params[:id])
 
       if snapshot.update(params[:snapshot])
-        audit_logger.info(current_user, 'has updated', snapshot)
+        audit_logger.info(current_user, "has updated", snapshot)
         extend_snapshot_data(snapshot)
         render json: snapshot
       else
@@ -68,7 +67,7 @@ module BlockStorage
       snapshot = services.block_storage.new_snapshot
       snapshot.id = params[:id]
       if snapshot.destroy
-        audit_logger.info(current_user, 'has deleted', snapshot)
+        audit_logger.info(current_user, "has deleted", snapshot)
         head 202
       else
         render json: { errors: snapshot.errors }, status: 422
@@ -81,7 +80,7 @@ module BlockStorage
       snapshot = services.block_storage.find_snapshot(params[:id])
 
       if snapshot.reset_status(params[:status])
-        audit_logger.info(current_user, 'has reset snapshot', snapshot.id)
+        audit_logger.info(current_user, "has reset snapshot", snapshot.id)
         render json: snapshot
       else
         render json: { errors: snapshot.errors }, status: 422
@@ -91,15 +90,18 @@ module BlockStorage
     end
 
     protected
+
     # this method extends volumes with data from cache
     def extend_snapshot_data(snapshots)
       snapshots = [snapshots] unless snapshots.is_a?(Array)
 
       volume_ids = snapshots.collect(&:volume_id)
 
-      cached_volumes = ObjectCache.where(id: volume_ids).pluck(:id,:name).each_with_object({}) do |v,map|
-        map[v[0]] = v[1]
-      end
+      cached_volumes =
+        ObjectCache
+          .where(id: volume_ids)
+          .pluck(:id, :name)
+          .each_with_object({}) { |v, map| map[v[0]] = v[1] }
 
       snapshots.each do |snapshot|
         if cached_volumes[snapshot.volume_id]
