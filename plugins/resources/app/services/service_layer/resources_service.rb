3a9cd3e2eb70f9a653a5bd40101f552aa66af7e3
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-14 13:06:50 +0100

    [format] formatted plugin resources

--- plugins/resources/app/services/service_layer/resources_service.rb
+++ plugins/resources/app/services/service_layer/resources_service.rb
@@ -2,28 +2,29 @@
 
 module ServiceLayer
   class ResourcesService < Core::ServiceLayer::Service
-
     def available?(_action_name_sym = nil)
-      elektron.service?('resources')
+      elektron.service?("resources")
     end
 
     def elektron_resources
-      @elektron_resources ||= elektron.service(
-        'resources', path_prefix: '/v1', interface: 'public'
-      )
+      @elektron_resources ||=
+        elektron.service("resources", path_prefix: "/v1", interface: "public")
     end
 
     def elektron_placement
-      @elektron_placement ||= elektron.service(
-        'placement', 
-        interface: 'public',
-        headers: { 'OpenStack-API-Version' => 'placement 1.18' }
-      )
+      @elektron_placement ||=
+        elektron.service(
+          "placement",
+          interface: "public",
+          headers: {
+            "OpenStack-API-Version" => "placement 1.18",
+          },
+        )
     end
 
-    def list_resource_providers(filter={})
-      providers = elektron_placement.get('resource_providers',filter)
-      return providers.body['resource_providers'] || []
+    def list_resource_providers(filter = {})
+      providers = elektron_placement.get("resource_providers", filter)
+      return providers.body["resource_providers"] || []
     end
 
     def get_resource_provider(uuid)
@@ -33,26 +34,26 @@ module ServiceLayer
 
     def list_resource_aggregates(path)
       aggregates = elektron_placement.get(path)
-      list = aggregates.body['aggregates'] || []
+      list = aggregates.body["aggregates"] || []
       return list
     end
 
     def get_resource_provider_inventory(uuid)
       data = elektron_placement.get("resource_providers/#{uuid}/inventories")
-      inventories = data.body['inventories'] || {}
+      inventories = data.body["inventories"] || {}
       return inventories
     end
 
     def get_resource_provider_usage(uuid)
       data = elektron_placement.get("resource_providers/#{uuid}/usages")
-      inventories = data.body['usages'] || {}
+      inventories = data.body["usages"] || {}
       return inventories
     end
 
-    def traits(uuid=nil)
+    def traits(uuid = nil)
       url = uuid.nil? ? "traits" : "resource_providers/#{uuid}/traits"
       data = elektron_placement.get(url)
-      traits = data.body['traits'] || []
+      traits = data.body["traits"] || []
       return traits
     end
 
@@ -60,18 +61,19 @@ module ServiceLayer
       # puts "get_resource_provider_aggregates"
       # puts "resource_providers/#{uuid}/aggregates"
       data = elektron_placement.get("resource_providers/#{uuid}/aggregates")
-      aggregates = data.body['aggregates'] || {}
+      aggregates = data.body["aggregates"] || {}
       return aggregates
     end
 
     def big_vm_available(uuid)
       big_vms = {}
-      provider = elektron_placement.get("resource_providers/#{uuid}/inventories")
-      unless provider.body['inventories'].empty?
-        reserved = provider.body['inventories']['CUSTOM_BIGVM']['reserved']
+      provider =
+        elektron_placement.get("resource_providers/#{uuid}/inventories")
+      unless provider.body["inventories"].empty?
+        reserved = provider.body["inventories"]["CUSTOM_BIGVM"]["reserved"]
         if reserved != 0
-          return false 
-        else 
+          return false
+        else
           return true
         end
       else
@@ -88,26 +90,26 @@ module ServiceLayer
 
     def get_domain(domain_id)
       resp = elektron_resources.get("domains/#{domain_id}")
-      return resp.body['domain'] || { 'services' => [] }
+      return resp.body["domain"] || { "services" => [] }
     end
 
     def get_project(domain_id, project_id)
-      resp = elektron_resources.get("domains/#{domain_id}/projects/#{project_id}")
-      return resp.body['project'] || { 'services' => [] }
+      resp =
+        elektron_resources.get("domains/#{domain_id}/projects/#{project_id}")
+      return resp.body["project"] || { "services" => [] }
     end
 
     def has_project_quotas?(domain_id, project_id)
       # look for any non-zero quota in a resource other than
       # "network/security_groups" or "network/security_group_rules" (those
       # start out non-zero because of the default security group)
-      get_project(domain_id, project_id)['services'].any? do |srv|
-        srv['resources'].any? do |res|
-          has_quota = (res['quota'] || 0) > 0
-          is_relevant = !/^security_group/.match?(res['name'] || '')
+      get_project(domain_id, project_id)["services"].any? do |srv|
+        srv["resources"].any? do |res|
+          has_quota = (res["quota"] || 0) > 0
+          is_relevant = !/^security_group/.match?(res["name"] || "")
           has_quota && is_relevant
         end
       end
     end
-
   end
 end
--- plugins/resources/config/routes.rb
+++ plugins/resources/config/routes.rb
@@ -6,24 +6,26 @@ Resources::Engine.routes.draw do
   # been removed from the UI, the value "current" is hardcoded to preserve
   # backwards compatibility.
 
-  get '/project/current/:override_domain_id/:override_project_id' \
-                 => 'application#project', as: 'foreign_project'
-  get '/project' => 'application#project', as: 'project'
-  get '/project/bigvm_resources' => 'application#bigvm_resources', as: 'bigvm_resources'
+  get "/project/current/:override_domain_id/:override_project_id" =>
+        "application#project",
+      :as => "foreign_project"
+  get "/project" => "application#project", :as => "project"
+  get "/project/bigvm_resources" => "application#bigvm_resources",
+      :as => "bigvm_resources"
 
-  get '/domain/current/:override_domain_id' \
-                => 'application#domain', as: 'foreign_domain'
-  get '/domain' => 'application#domain', as: 'domain'
+  get "/domain/current/:override_domain_id" => "application#domain",
+      :as => "foreign_domain"
+  get "/domain" => "application#domain", :as => "domain"
 
-  get '/cluster/current' => 'application#cluster', as: 'cluster'
+  get "/cluster/current" => "application#cluster", :as => "cluster"
 
   ##############################################################################
   # quota request workflows
 
-  post '/request/project' => 'request#project'
-  post '/request/domain'  => 'request#domain'
+  post "/request/project" => "request#project"
+  post "/request/domain" => "request#domain"
 
-  get '/project/init' => 'application#init_project', as: 'init_project'
+  get "/project/init" => "application#init_project", :as => "init_project"
 
-  get  'quota-usage' => 'quota_usage#index'
+  get "quota-usage" => "quota_usage#index"
 end
