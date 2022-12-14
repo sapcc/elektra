256ca1f710b0dd8738af825601b43743c875c16f
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2022-12-14 13:23:12 +0100

    [format] formatted plugin tools

--- plugins/tools/app/services/service_layer/tools_service.rb
+++ plugins/tools/app/services/service_layer/tools_service.rb
@@ -2,18 +2,16 @@
 
 module ServiceLayer
   class ToolsService < Core::ServiceLayer::Service
-
     def available?(_action_name_sym = nil)
       true
     end
 
     def has_castellum?
-      elektron.service?('castellum')
+      elektron.service?("castellum")
     end
 
     def has_limes?
-      elektron.service?('resources')
+      elektron.service?("resources")
     end
-
   end
 end
