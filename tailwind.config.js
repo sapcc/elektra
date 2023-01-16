6e2efe11f2957f9d09f942035a27b10123874f4d
d064310 <andreas.pfau@sap.com>
2023-01-16 12:11:43 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,13 @@
-// opacity helper to make custom colors work with opacity
-function withOpacity(variableName) {
-  return ({ opacityVariable, opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`
-    }
-    if (opacityVariable !== undefined) {
-      return `rgba(var(${variableName}), var(${opacityVariable}, 1))`
-    }
-    return `rgb(var(${variableName}))`
-  }
-}
-
 module.exports = {
+  // use juno tailwindcss as default
   presets: [require("juno-ui-components/tailwind.config")],
   prefix: "", // important, do not change
   content: [
     "./plugins/**/*.{js,jsx,html,haml}",
     "./app/javascript/**/*.{js,jsx,haml}",
-    "./app/views/**/*.haml",
+    "./app/views/**/*.{haml,html}",
   ],
+
   theme: {
     extend: {},
   },
