cdd25747d29e59baf3d743d67047c919d040adc6
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-04-25 11:30:09 +0200

    Fixed search help link with href javascript:void(0) to avoid trigger new load

--- app/javascript/lib/components/search_field.jsx
+++ app/javascript/lib/components/search_field.jsx
@@ -1,8 +1,8 @@
-import { Popover, OverlayTrigger } from 'react-bootstrap';
+import { Popover, OverlayTrigger, Button } from "react-bootstrap"
 import { SearchInput } from "juno-ui-components"
-import React from 'react';
+import React from "react"
 
-let counter = 0;
+let counter = 0
 
 /**
  * This component implements a serach field.
@@ -10,55 +10,59 @@ let counter = 0;
  **/
 export class SearchField extends React.Component {
   state = {
-    searchTerm: ''
+    searchTerm: "",
   }
 
-  infoText =
+  infoText = (
     <Popover id={`search_field_${this.props.id || counter++}`}>
       {this.props.text}
     </Popover>
+  )
 
   onChangeTerm = (e) => {
-    const value = e.target.value || ''
-    this.setState({searchTerm: value}, () => this.props.onChange(value))
+    const value = e.target.value || ""
+    this.setState({ searchTerm: value }, () => this.props.onChange(value))
   }
 
   reset = (e) => {
     e.preventDefault()
-    this.setState({searchTerm: ''}, () => this.props.onChange(''))
+    this.setState({ searchTerm: "" }, () => this.props.onChange(""))
   }
 
   UNSAFE_componentWillReceiveProps = (nextProps) => {
     // console.log('UNSAFE_componentWillReceiveProps',nextProps)
-    if (nextProps.value!=null) this.setState({searchTerm: nextProps.value})
+    if (nextProps.value != null) this.setState({ searchTerm: nextProps.value })
   }
 
   componentDidMount = () => {
     // console.log('componentDidMount',this.props)
-    if (this.props.value) this.setState({searchTerm: this.props.value})
+    if (this.props.value) this.setState({ searchTerm: this.props.value })
   }
-
 
   render() {
     const variant = this.props.variant
-    const empty = this.state.searchTerm.trim().length==0
-    const showSearchIcon = (this.props.searchIcon != false)
-    let iconClassName = empty ? (showSearchIcon ? 'fa fa-search' : '') : 'fa fa-times-circle'
-    if (this.props.isFetching) iconClassName = 'spinner'
+    const empty = this.state.searchTerm.trim().length == 0
+    const showSearchIcon = this.props.searchIcon != false
+    let iconClassName = empty
+      ? showSearchIcon
+        ? "fa fa-search"
+        : ""
+      : "fa fa-times-circle"
+    if (this.props.isFetching) iconClassName = "spinner"
 
     return (
       <React.Fragment>
-        { variant === "juno" ?
-          <SearchInput 
+        {variant === "juno" ? (
+          <SearchInput
             value={this.state.searchTerm}
             placeholder={this.props.placeholder}
             disabled={this.props.disabled === true}
             onChange={this.onChangeTerm}
             onClear={(e) => this.reset(e)}
           />
-        :
+        ) : (
           <React.Fragment>
-            <div className='has-feedback has-feedback-searchable'>
+            <div className="has-feedback has-feedback-searchable">
               <input
                 data-test="search"
                 type="text"
@@ -66,27 +70,38 @@ export class SearchField extends React.Component {
                 value={this.state.searchTerm}
                 placeholder={this.props.placeholder}
                 onChange={this.onChangeTerm}
-                disabled={this.props.disabled==true}
+                disabled={this.props.disabled == true}
               />
               <span
-                className={`form-control-feedback ${!empty && 'not-empty'}`}
-                onClick={(e) => iconClassName!='spinner' && !empty && this.reset(e)}>
-                <i className={iconClassName}/>
+                className={`form-control-feedback ${!empty && "not-empty"}`}
+                onClick={(e) =>
+                  iconClassName != "spinner" && !empty && this.reset(e)
+                }
+              >
+                <i className={iconClassName} />
               </span>
             </div>
-            {this.props.text &&
+            {this.props.text && (
               <div className="has-feedback-help">
-                <OverlayTrigger trigger="click" placement="top" rootClose overlay={this.infoText}>
-                  <a className='help-link' href='#' onClick={() => null}>
+                <OverlayTrigger
+                  trigger="click"
+                  placement="top"
+                  rootClose
+                  overlay={this.infoText}
+                >
+                  <a
+                    className="help-link"
+                    href="javascript:void(0)"
+                    onClick={() => null}
+                  >
                     <i className="fa fa-question-circle"></i>
                   </a>
                 </OverlayTrigger>
               </div>
-            }
+            )}
           </React.Fragment>
-        }
+        )}
       </React.Fragment>
-
     )
   }
 }
