d5ab48dff3d9a5182109bc05f27ed88dfe2829c5
d064310 <andreas.pfau@sap.com>
2023-04-26 09:23:53 +0200

    fix jest tests

--- plugins/lookup/app/javascript/widgets/reverse_app/test/components/groups.test.jsx
+++ plugins/lookup/app/javascript/widgets/reverse_app/test/components/groups.test.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import { render } from "@testing-library/react"
+import { screen } from "@testing-library/dom"
 import Groups from "../../components/groups"
-import { shallow } from "enzyme"
 
 describe("Groups component", () => {
-  it("should render one groups", () => {
+  it("should render one groups", async () => {
     const mockGroups = [
       {
         id: "9d4cd354b558aa59a842bc2e74cfd18a0f0ade1ba5277bfdfcacaa50916a8072",
@@ -15,14 +16,10 @@ describe("Groups component", () => {
         data: { name: "C123", id: "123456789", fullname: "Max Musterman" },
       },
     }
-    const wrapper = shallow(
-      <Groups groups={mockGroups} groupMembers={mockGroupMembers} />
-    )
-    // console.log(wrapper.debug())
-    expect(wrapper.find("ul.tree").exists()).toBe(true)
-    expect(wrapper.find("li").length).toBe(1)
-    expect(wrapper.find("li").text().includes("CCADMIN_CLOUD_ADMINS")).toBe(
-      true
-    )
+
+    await render(<Groups groups={mockGroups} groupMembers={mockGroupMembers} />)
+
+    const text = await screen.getByText("CCADMIN_CLOUD_ADMINS")
+    expect(text).toBeDefined()
   })
 })
