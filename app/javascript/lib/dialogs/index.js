f1b74c3265650b2b4f69314a4cd777c88742e0d5
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-05-15 11:40:00 +0200

    prettify

--- app/javascript/lib/dialogs/index.js
+++ app/javascript/lib/dialogs/index.js
@@ -1,33 +1,33 @@
-import { ModalDialog } from './dialog';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import { ModalDialog } from "./dialog"
+import ReactDOM from "react-dom"
+import React from "react"
 
-const showDialog = function(type, message, options) {
-  let cleanup;
-  let component;
-  let props;
-  let wrapper;
+const showDialog = function (type, message, options) {
+  let cleanup
+  let component
+  let props
+  let wrapper
 
   if (options == null) {
-    options = {size: 'large'};
+    options = { size: "large" }
   }
-  switch(type) {
-    case 'confirm':
-      options['confirmLabel'] = options['confirmLabel'] || 'Yes';
-      options['abortLabel'] = options['abortLabel'] || 'No';
-      options['showAbortButton'] = true;
-      break;
-    case 'info':
-      options['confirmLabel'] = options['confirmLabel'] || 'OK';
-      options['showAbortButton'] = (options['showAbortButton'] === true)
-      break;
-    case 'error':
-      options['confirmLabel'] = options['confirmLabel'] || 'OK';
-      options['showAbortButton'] = (options['showAbortButton'] === true)
-      break;
+  switch (type) {
+    case "confirm":
+      options["confirmLabel"] = options["confirmLabel"] || "Yes"
+      options["abortLabel"] = options["abortLabel"] || "No"
+      options["showAbortButton"] = true
+      break
+    case "info":
+      options["confirmLabel"] = options["confirmLabel"] || "OK"
+      options["showAbortButton"] = options["showAbortButton"] === true
+      break
+    case "error":
+      options["confirmLabel"] = options["confirmLabel"] || "OK"
+      options["showAbortButton"] = options["showAbortButton"] === true
+      break
   }
 
-  wrapper = document.body.appendChild(document.createElement('div'));
+  wrapper = document.body.appendChild(document.createElement("div"))
   // cleanup = function() {
   //   ReactDOM.unmountComponentAtNode(wrapper)
   //   return setTimeout(function() {
@@ -36,19 +36,19 @@ const showDialog = function(type, message, options) {
   // };
 
   // props = Object.assign({ message, type, onHide: cleanup}, options);
-  props = Object.assign({ message, type}, options);
-  component = ReactDOM.render(React.createElement(ModalDialog,props), wrapper);
+  props = Object.assign({ message, type }, options)
+  component = ReactDOM.render(React.createElement(ModalDialog, props), wrapper)
   return component.promise
-};
+}
 
-export const confirm = function(message, options) {
-  return showDialog('confirm', message, options);
-};
+export const confirm = function (message, options) {
+  return showDialog("confirm", message, options)
+}
 
-export const showInfoModal = function(message, options) {
-  return showDialog('info', message, options);
-};
+export const showInfoModal = function (message, options) {
+  return showDialog("info", message, options)
+}
 
-export const showErrorModal = function(message, options) {
-  return showDialog('error', message, options);
-};
+export const showErrorModal = function (message, options) {
+  return showDialog("error", message, options)
+}
