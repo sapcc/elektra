4b77846cad1586210f99fa33ea14d03f0e69ad68
Stanislav Zaprudskiy <s.zaprudskiy@sap.com>
2023-09-04 12:48:11 +0200

    Do not accept names starting or ending with whitespace
    Neither project nor domain could start or end with whitespace. It has
    already resulted into inaccurate results (e.g. project not found), with
    cause not always easily identifiable.

--- plugins/identity/app/models/identity/prodel.rb
+++ plugins/identity/app/models/identity/prodel.rb
@@ -3,8 +3,11 @@ module Identity
   # https://stackoverflow.com/questions/4997762/ruby-on-rails-fully-functional-tableless-model?rq=1
   class Prodel
     include ActiveModel::Model
-    
+
     validates_presence_of :project_name
+    validates_format_of :project_name, :project_domain_name,
+      :with => /\A[^\s]+.*[^\s]+\Z/,
+      :message => "could not start or end with whitepsace"
 
     attr_accessor :project_domain_name, :project_name
   end
