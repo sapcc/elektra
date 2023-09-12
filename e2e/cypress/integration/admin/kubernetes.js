fd9a2d478d2e9b5998e6d7bf30f77312f8f0a365
d064310 <andreas.pfau@sap.com>
2023-09-12 10:41:57 +0200

    kubernikus: fix e2e tests

--- e2e/cypress/integration/admin/kubernetes.js
+++ e2e/cypress/integration/admin/kubernetes.js
@@ -13,14 +13,14 @@ describe("kubernetes", () => {
     cy.contains("button", "Create Cluster").click()
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(3000) // waiting for ajax to complete
-    cy.contains("button[type=submit]", "Create").should("be.disabled")
+    cy.contains("button[type=submit]", "Create").should("be.enabled")
     cy.get("input[placeholder='lower case letters and numbers']").type("test")
     cy.get("input[placeholder='a-z + 0-9']").type("test")
     cy.get("select[name=keypair").select(1)
     cy.contains("button[type=submit]", "Create").should("be.enabled")
     cy.contains("button", "Add Pool").click()
     cy.contains("Pool 2:")
-    cy.contains("button[type=submit]", "Create").should("be.disabled")
+    cy.contains("button[type=submit]", "Create").should("be.enabled")
     cy.contains("button", "Close").click()
   })
 })
