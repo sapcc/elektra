059c072762d3dfe5ad871a1722871d31139e3b37
d064310 <andreas.pfau@sap.com>
2023-09-20 14:26:12 +0200

    [compute]: fixes #1260, check permissions for hard reset

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -393,36 +393,6 @@ module ApplicationHelper
   end
 
   # ---------------------------------------------------------------------------------------------------
-  # Favicon Helpers
-  # ---------------------------------------------------------------------------------------------------
-
-  def favicon_png
-    capture do
-      content_tag :link,
-               rel: "icon",
-               type: "image",
-               href: image_path("favicon.png")
-    end
-  end
-
-  def favicon_ico
-    capture do
-      content_tag :link,
-               rel: "shortcut icon",
-               type: "image/x-icon",
-               href: image_path("favicon.ico")
-    end
-  end
-
-  def apple_touch_icon
-    capture do
-      content_tag :link,
-               rel: "apple-touch-icon",
-               href: image_path("apple-touch-icon.png")
-    end
-  end
-
-  # ---------------------------------------------------------------------------------------------------
   # Text Helpers
   # ---------------------------------------------------------------------------------------------------
 
