97d2928af6a31dc86726663b0c61908bc2815a21
Hans-Georg Winkler <hans-georg.winkler@sap.com>
2023-10-10 10:30:53 +0200

    New big vm calculation (#1258)
    * get big vm data from cerebro on elektra side
    
    * first working version for bigVM
    
    * better error handling for to get bigVMResources
    
    * fix typo
    
    * add comment for bigvm cerebro docu
    
    Co-Authored-By: andreas.pfau@sap.com

--- plugins/resources/app/javascript/widgets/app/components/availability_zones/big_vm_resources.jsx
+++ plugins/resources/app/javascript/widgets/app/components/availability_zones/big_vm_resources.jsx
@@ -1,10 +1,7 @@
 import React from "react"
-import { Unit } from "../../unit"
 import { getBigvmResources } from "../../actions/elektra"
 
-const mbUnit = new Unit("MiB")
 const columnsPerRow = 3
-
 /**
  * This component shows available HANA Vms
  * @param {Array} bigVmResources a list of vms capable to host HANA instances
@@ -13,67 +10,32 @@ const columnsPerRow = 3
 const AvailableBigVmResources = () => {
   const [loading, setLoading] = React.useState(false)
   const [error, setError] = React.useState()
-  const [bigVmResources, setBigVmResources] = React.useState()
+  const [placeableVMs, setPlaceableVMs] = React.useState([])
 
   React.useEffect(() => {
     setError(null)
     setLoading(true)
     getBigvmResources()
-      .then((data) => setBigVmResources(data))
+      .then((data) => setPlaceableVMs(data))
       .catch((error) => setError(error))
       .finally(() => setLoading(false))
-  }, [])
-  /**
-   * This functions converts the list of big vms into a list of flavors
-   * grouped by availability zone.
-   */
-  const flavorsByAz = React.useMemo(() => {
-    if (!bigVmResources) return []
-    const map = {}
-
-    bigVmResources.forEach((item) => {
-      // az => availability_zone
-      const az = item.az
-      // create entry for az unless already exists
-      map[az] = map[az] || []
-      // consider flavor for free hosts only
-      if (item.status === "free") {
-        let flavors = map[az].concat(item.flavors || [])
-        // and remove duplicates
-        map[az] = flavors
-          .filter((elem, pos, arr) => {
-            const index = arr.findIndex((e) => e.name === elem.name)
-            return index === pos
-          })
-          .sort((a, b) => (a.ram > b.ram ? 1 : a.ram < b.ram ? -1 : 0))
-      }
-    })
-
-    // result of map is e.g. {"eu-de-1a" => [flavor1,flavor2]}
-
-    // convert map to array of arrays and split it into chunks (columns per row)
-    // and sort values by az
-    const result = []
-    const availabilityZones = Object.keys(map).sort()
-    for (let i = 0; i < availabilityZones.length; i += columnsPerRow) {
-      const values = availabilityZones
-        .slice(i, i + columnsPerRow)
-        .map((az) => ({ az, items: map[az] }))
-      result.push(values)
-    }
-
-    // for tests only
-    // result[0].push({ az: "eu-de-2c", items: [] })
-
-    // now the result looks like e.g. [[{az: "eu-de-1a", items: [flavor1,flavor2]}]]
-    return result
-  }, [bigVmResources])
+  }, [setPlaceableVMs, setError, setLoading])
 
   const columnWidth = React.useMemo(() => {
-    if (flavorsByAz.length === 0) return 1
-    const dataLength = flavorsByAz[0].length
-    return Math.floor(12 / dataLength)
-  }, [flavorsByAz])
+    if (!placeableVMs || placeableVMs.length === 0) return 1
+    if (placeableVMs.length >= columnsPerRow) return 12 / columnsPerRow //more than on line, max 3 columns per row
+    return Math.floor(12 / placeableVMs.length)
+  }, [placeableVMs, columnsPerRow])
+
+  const itemChunks = React.useMemo(() => {
+    if (!placeableVMs) return []
+    const result = []
+    for (let i = 0; i < placeableVMs.length; i += columnsPerRow) {
+      const availabilityZonesChunk = placeableVMs.slice(i, i + columnsPerRow)
+      result.push(availabilityZonesChunk)
+    }
+    return result
+  }, [placeableVMs])
 
   // structure of the UI:
   // eu-de-1a | eu-de-1b | eu-de-1c
@@ -90,42 +52,42 @@ const AvailableBigVmResources = () => {
           <span className="spinner"></span> Loading...
         </span>
       ) : error ? (
-        <div className="alert alert-danger">{error}</div>
-      ) : flavorsByAz.length === 0 ? (
+        <div className="alert alert-danger">{error.message}</div>
+      ) : placeableVMs.length === 0 ? (
         <span>No resources available.</span>
       ) : (
-        flavorsByAz.map((chunk, i1) => (
+        itemChunks.map((chunk, i1) => (
           <React.Fragment key={i1}>
             <div className="row">
-              {chunk.map((region, i2) => (
+              {chunk.map((azData, i2) => (
                 <div className={`col-md-${columnWidth}`} key={i2}>
                   <div>
-                    <h4>{region.az}</h4>
+                    <h4>{azData.availabilityZone}</h4>
                   </div>
                 </div>
               ))}
             </div>
 
             <div className="row">
-              {chunk.map((region, i2) => (
+              {chunk.map((azData, i2) => (
                 <div className={`col-md-${columnWidth}`} key={i2}>
                   <div
                     className={`bs-callout bs-callout-${
-                      region.items.length > 0 ? "primary" : "warning"
+                      azData.flavors.length > 0 ? "primary" : "warning"
                     }`}
                   >
                     <h5>
-                      {region.items.length > 0
+                      {azData.flavors.length > 0
                         ? "VM deployment possible from these flavors:"
                         : "No resources available"}
                     </h5>
 
                     <div>
-                      {region.items.map((f, i3) => (
+                      {azData.flavors.map((flavor, i3) => (
                         <div key={i3}>
-                          {f.name}&nbsp;
+                          {Object.keys(flavor)[0]}&nbsp;
                           <span className="small text-muted">
-                            ({mbUnit.format(f.ram)} RAM each)
+                            ({Object.values(flavor)[0]} VMs available)
                           </span>
                         </div>
                       ))}
