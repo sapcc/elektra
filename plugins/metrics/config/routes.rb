2d165d66bba67b619fb87a678202cebba4269057
hodanoori <107242553+hodanoori@users.noreply.github.com>
2023-12-05 15:02:41 +0100

    [Metrics] Remove redirect_to for maia dashboard and delete Grafana dashboard code (#1297)
    * [keymanagerng] Download payload file as a row file not a json file and show the content directly in the UI if the type is text/plain
    
    * [keymanagerng] resolve review comments
    
    * [Metrics] Remove redirect-to in metrics plugin
    
    * [Metrics] Remove Grafana dashboard call as it is already deactivated but not completely deleted

--- plugins/metrics/config/routes.rb
+++ plugins/metrics/config/routes.rb
@@ -1,5 +1,3 @@
 Metrics::Engine.routes.draw do
   get "/" => "application#index", :as => :index
-  get "/maia" => "application#maia", :as => :maia
-  get "/grafana" => "application#gaas", :as => :gaas
 end
