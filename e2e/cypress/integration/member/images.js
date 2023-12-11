80018e5a9bac3a29e91bc77553c33c9f3bcb1c6b
D058266 <hans-georg.winkler@sap.com>
2023-12-11 12:12:34 +0100

    [e2e]: fix images member test

--- e2e/cypress/integration/member/images.js
+++ e2e/cypress/integration/member/images.js
@@ -8,18 +8,21 @@ describe("images", () => {
   })
 
   it("open images page and see a list of available images", () => {
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/test/image/ng?r=/os-images/available`)
-    cy.contains('[data-test=page-title]','Server Images & Snapshots')
-    cy.contains('td','vmdk')
-    cy.contains('.btn','Load Next').click()
+    cy.visit(
+      `/${Cypress.env("TEST_DOMAIN")}/test/image/ng?r=/os-images/available`
+    )
+    cy.contains("[data-test=page-title]", "Server Images & Snapshots")
+    cy.contains("td", "vmdk")
+    cy.contains(".btn", "Load Next").click()
   })
 
   it("open images page and see a list of available images, search for windows and show details", () => {
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/test/image/ng?r=/os-images/available`)
-    cy.contains('[data-test=page-title]','Server Images & Snapshots')
-    cy.get('[data-test=search]').should('be.visible').type('windows')
-    cy.get('[data-test=images]').eq(0).click()
-    cy.contains('Hypervisor Type')
+    cy.visit(
+      `/${Cypress.env("TEST_DOMAIN")}/test/image/ng?r=/os-images/available`
+    )
+    cy.contains("[data-test=page-title]", "Server Images & Snapshots")
+    cy.get("[data-test=search]").should("be.visible").type("vsphere")
+    cy.get("[data-test=images]").eq(0).click()
+    cy.contains("Hypervisor Type")
   })
-
 })
