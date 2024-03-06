01103d6440f64b516af84f5b48fddcd72a5e0e33
d064310 <andreas.pfau@sap.com>
2024-03-06 13:28:12 +0100

    finalize the file watcher

--- app/controllers/global_notifications_controller.rb
+++ app/controllers/global_notifications_controller.rb
@@ -1,44 +1,33 @@
 require 'filewatcher'
 
 class GlobalNotificationsController < ActionController::Base
-
+  # Path to the file: elektra/config/global_notifications/messages.yaml
   GLOBAL_NOTIFICATIONS_FILE_PATH = Rails.root.join('config', 'global_notifications', 'messages.yaml') 
-  @@notifications = nil
-
-
-  # file_reloader = ActiveSupport::FileUpdateChecker.new([GLOBAL_NOTIFICATIONS_FILE_PATH]) do
-  #   pp "=================================="
-  #   @@notifications = global_notifications
-  # end
-
-  # # byebug
-  # pp ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-  # pp file_reloader.updated?
-  # file_reloader.execute_if_updated
-  def index
-    # @@notifications ||= global_notifications
-    @@notifications = global_notifications
-    file_reloader = ActiveSupport::FileUpdateChecker.new([GLOBAL_NOTIFICATIONS_FILE_PATH]) do
-      pp "=================================="
-      @@notifications = global_notifications
+   
+  # class variable to hold the notifications
+  @@notifications = []
+  
+  # FileUpdateChecker to watch for changes in the file
+  @@file_reloader = ActiveSupport::FileUpdateChecker.new([GLOBAL_NOTIFICATIONS_FILE_PATH]) do
+    begin 
+      @@notifications = YAML.load(File.read(GLOBAL_NOTIFICATIONS_FILE_PATH)).to_json
+    rescue => e
+      pp e
+      # In case of any error, set the notifications to empty array
+      []
     end
-
-    # # byebug
-    pp ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
-    pp file_reloader.updated?
-    # file_reloader.execute_if_updated
-
-    render json: @@notifications.to_json
   end
+  # execute the file reloader initially
+  @@file_reloader.execute
 
-  protected 
-
-  def global_notifications 
-    YAML.load(File.read(GLOBAL_NOTIFICATIONS_FILE_PATH))
-    # JSON.parse(File.read(GLOBAL_NOTIFICATIONS_FILE_PATH))
-  rescue => e
-    pp e
-    []
+  def self.global_notifications 
+    # execute the file reloader if the file is updated
+    @@file_reloader.execute_if_updated
+    # return the notifications
+    @@notifications
   end
-
+  
+  def index    
+    render json: self.class.global_notifications
+  end
 end
