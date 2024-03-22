1497afd0309673eaba610ad69b135ae12a86c58b
D058266 <hans-georg.winkler@sap.com>
2024-03-22 08:49:59 +0100

    fix resources test, dns edit is not possible anymore

--- e2e/cypress/integration/admin/resources.js
+++ e2e/cypress/integration/admin/resources.js
@@ -9,22 +9,9 @@ describe("resource management - project level", () => {
 
   it("start page is reachable, and you can see the project settings", () => {
     cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/resources/project`)
-    cy.contains('[data-test=page-title]','Manage Project Resources')
-    cy.contains('div','Members per Server Group')
+    cy.contains("[data-test=page-title]", "Manage Project Resources")
+    cy.contains("div", "Members per Server Group")
     cy.get('a[href*="#/compute/settings"]').click()
-    cy.contains('h4','Project Settings')
+    cy.contains("h4", "Project Settings")
   })
-  
-  it("network page is reachable, and you can edit and check DNS quota",{ defaultCommandTimeout: 30000 }, () => {
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/resources/project#/dns`)
-    cy.contains('div','Recordsets per Zone')
-    cy.get('a[href*="#/dns/edit/dns"]').click()
-    cy.get('input.form-control:first').type('{selectall}30{enter}')
-    // disabled because of problems with limes
-    //cy.get('button').contains('Check').click()
-    //cy.get('div.text-success').contains('Quota can be raised without approval')
-    //cy.get('button').contains('Submit')
-    cy.get('button').contains('Cancel').click()
-  })
-
 })
