8611ad3dfb4c087e734ee572a3afdc3b5494a8a9
D058266 <hans-georg.winkler@sap.com>
2024-04-25 14:01:39 +0200

    polishing to get bigVM Data

--- plugins/resources/app/controllers/resources/application_controller.rb
+++ plugins/resources/app/controllers/resources/application_controller.rb
@@ -150,10 +150,10 @@ module Resources
         end
 
         # QA: https://migration-recommender-service.cca-qap.cerebro.c.eu-de-1.cloud.sap/public/docs
-        # this is meaningless becaue there is no bigVM data in QA
-        # if current_region == "qa-de-1" 
-        #   cerebro_endpoint = "https://migration-recommender-service.cca-qap.cerebro.c.eu-de-1.cloud.sap/public/api/v1/placeable-vm/#{openstack_level}"
-        # end
+        # this is meaningless because there is no bigVM data in QA
+        if current_region == "qa-de-1" 
+          cerebro_endpoint = "https://migration-recommender-service.cca-qap.cerebro.c.eu-de-1.cloud.sap/public/api/v1/placeable-vm/#{openstack_level}"
+        end
 
         # for debug and development in QA use a prod region and project or domain
         # cerebro_endpoint = "https://migration-recommender-service.cca-pro.cerebro.c.eu-de-1.cloud.sap/public/api/v1/placeable-vm/domain/XXX"
@@ -171,11 +171,11 @@ module Resources
         if response.code == "200"
           big_vm_data = response.body
         else
-          render json: { error: "Couldn't retrieve placement information. API: #{response.message}" }, status: 422
+          render json: { error: "Couldn't retrieve bigVM resources. No Data available. API response: #{response.message}" }, status: 422
           return
         end
       rescue StandardError => e
-        render json: { error: "Could not load bigVMData" }, status: 422
+        render json: { error: "Couldn't load bigVMData, ist the API available in this region?" }, status: 422
         return
       end
       render json: big_vm_data
