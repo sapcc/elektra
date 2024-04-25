eaa463002c4a48f68c6eadc2307380ba24c50dfb
D058266 <hans-georg.winkler@sap.com>
2024-04-25 09:37:33 +0200

    decide for domain and project level the endpoint

--- plugins/resources/app/controllers/resources/application_controller.rb
+++ plugins/resources/app/controllers/resources/application_controller.rb
@@ -129,18 +129,36 @@ module Resources
     end
 
     def bigvm_resources
+
+      if @scoped_project_id.nil?
+        # domain level
+        openstack_level = "domain/#{@scoped_domain_id}"
+      else
+        # project level
+        openstack_level = "project/#{@scoped_project_id}"
+      end
+      
       # API Docu
       # https://migration-recommender-service.cca-pro.cerebro.c.eu-de-1.cloud.sap/public/docs#/default/get_placeable_vm_for_project_api_v1_placeable_vm_project__openstack_project_id__get
       require "net/http"
       begin
-        
-        cerebro_endpoint = "https://migration-recommender-service.cca-pro.cerebro.c.#{current_region}.cloud.sap/public/api/v1/placeable-vm/project/#{@scoped_project_id}"
+        cerebro_endpoint = "https://migration-recommender-service.cca-pro.cerebro.c.#{current_region}.cloud.sap/public/api/v1/placeable-vm/#{openstack_level}"
         if ENV.key?("CEREBRO_CUSTOM_ENDPOINT") 
           unless ENV["CEREBRO_CUSTOM_ENDPOINT"].empty? || ENV["CEREBRO_CUSTOM_ENDPOINT"].blank?
-            cerebro_endpoint = "#{ENV['CEREBRO_CUSTOM_ENDPOINT']}/public/api/v1/placeable-vm/project/#{@scoped_project_id}"
+            cerebro_endpoint = "#{ENV['CEREBRO_CUSTOM_ENDPOINT']}/public/api/v1/placeable-vm/#{openstack_level}"
           end
         end
-        
+
+        # QA: https://migration-recommender-service.cca-qap.cerebro.c.eu-de-1.cloud.sap/public/docs
+        # this is meaningless becaue there is no bigVM data in QA
+        # if current_region == "qa-de-1" 
+        #   cerebro_endpoint = "https://migration-recommender-service.cca-qap.cerebro.c.eu-de-1.cloud.sap/public/api/v1/placeable-vm/#{openstack_level}"
+        # end
+
+        # for debug and development in QA use a prod region and project or domain
+        # cerebro_endpoint = "https://migration-recommender-service.cca-pro.cerebro.c.eu-de-1.cloud.sap/public/api/v1/placeable-vm/domain/XXX"
+        # cerebro_endpoint = "https://migration-recommender-service.cca-pro.cerebro.c.na-ca-1.cloud.sap/public/api/v1/placeable-vm/project/XXX
+
         uri = URI(cerebro_endpoint)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
