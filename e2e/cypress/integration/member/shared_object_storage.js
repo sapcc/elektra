739c8ecada9c9fdd414022e3f5e1b5de4c8c2577
d064310 <andreas.pfau@sap.com>
2024-05-07 17:14:52 +0200

    e2e: fixt tests, settings in resources is not visible anymore, object-storage has a new url path

--- e2e/cypress/integration/member/shared_object_storage.js
+++ e2e/cypress/integration/member/shared_object_storage.js
@@ -8,9 +8,10 @@ describe("shared object storage", () => {
   })
 
   it("open object storage and see that I need one of the respective roles", () => {
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/test/object-storage/`)
-    cy.contains('[data-test=page-title]','Object Storage')
-    cy.contains('Object Storage can only be used when your user account has the admin or objectstore_admin or objectstore_viewer role for this project.')
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/test/object-storage/swift`)
+    cy.contains("[data-test=page-title]", "Object Storage")
+    cy.contains(
+      "Object Storage can only be used when your user account has the admin or objectstore_admin or objectstore_viewer role for this project."
+    )
   })
-
 })
