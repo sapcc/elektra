89c2788121b6250aebc446320bdeac4e2b0bdf99
VoigtS <s.voigt@sap.com>
2024-07-10 13:56:14 +0200

    Add comment to tools plugins limes and castellum now describe where they are referenced in and what they are used for

--- plugins/tools/app/javascript/widgets/limes/init.js
+++ plugins/tools/app/javascript/widgets/limes/init.js
@@ -2,6 +2,8 @@ import { createWidget } from "lib/widget"
 import * as reducers from "./reducers"
 import App from "./components/application"
 
+// The limes plugin will be reached via deeplinks in prometheus alerts.
+// Referenced in: https://github.com/sapcc/helm-charts/blob/45d0049d7a60ddcb200892e85b7756c80be47e29/openstack/limes/alerts/openstack/api.alerts
 createWidget(null).then((widget) => {
   widget.configureAjaxHelper({
     baseURL: widget.config.scriptParams.limesApi,
