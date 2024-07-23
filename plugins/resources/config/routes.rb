aff19789b34f79c05deba4a0dbc5b3e72fa37261
VoigtS <s.voigt@sap.com>
2024-07-23 11:16:15 +0200

    retrieve bigvm resources endpoint to routes

--- plugins/resources/config/routes.rb
+++ plugins/resources/config/routes.rb
@@ -6,6 +6,8 @@ Resources::Engine.routes.draw do
   # been removed from the UI, the value "current" is hardcoded to preserve
   # backwards compatibility.
   
+  get "/project/bigvm_resources" => "application#bigvm_resources",
+  :as => "bigvm_resources"
   get "/v2/project" => "v2#project", :as => "v2_project"
   get "/v2/domain" => "v2#domain", :as => "v2_domain"
   get "/v2/cluster" => "v2#cluster", :as => "v2_cluster"
