391891679aead0b7feb8f40b6207279e44a876ba
VoigtS <s.voigt@sap.com>
2024-07-23 15:05:42 +0200

    Fix cypress test for resources plugin

--- e2e/cypress/integration/admin/resources.js
+++ e2e/cypress/integration/admin/resources.js
@@ -4,12 +4,11 @@ describe("resource management - project level", () => {
       Cypress.env("TEST_DOMAIN"),
       Cypress.env("TEST_USER"),
       Cypress.env("TEST_PASSWORD")
-    )
-  })
+    );
+  });
 
   it("start page is reachable, and you can see the project settings", () => {
-    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/resources/project`)
-    cy.contains("[data-test=page-title]", "Manage Project Resources")
-    cy.contains("div", "Members per Server Group")
-  })
-})
+    cy.visit(`/${Cypress.env("TEST_DOMAIN")}/admin/resources/v2/project`);
+    cy.contains("[data-test=page-title]", "Resource Management");
+  });
+});
