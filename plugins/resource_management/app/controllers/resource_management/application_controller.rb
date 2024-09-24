cfb49ebe8f8697af33a44945013795e66a594f51
Stefan Majewsky <stefan.majewsky@sap.com>
2024-09-24 15:02:51 +0200

    remove plugins/resource_management (#1414)

--- plugins/resource_management/app/controllers/resource_management/application_controller.rb
+++
@@ -1,6 +0,0 @@
-module ResourceManagement
-  class ApplicationController < ::DashboardController
-    # This is the base class of all controllers in this plugin. Only put code in here that is shared across controllers.
-    authorization_context "resource_management"
-  end
-end
--- plugins/resource_management/lib/resource_management.rb
+++
@@ -1,6 +0,0 @@
-require "resource_management/engine"
-# load resource management related config
-require_relative "resource_management/package"
-
-module ResourceManagement
-end
