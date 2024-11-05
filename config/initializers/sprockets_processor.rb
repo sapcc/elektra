a873b1b24ea298b9f9cb747fc9600ef8f1ea4e9c
Hans-Georg Winkler <hans-georg.winkler@sap.com>
2024-11-05 15:26:53 +0100

    feat(assets) create widget.js files (#1453)

---
+++ config/initializers/sprockets_processor.rb
@@ -0,0 +1,17 @@
+# The main purpose of this code is to create a copy of '_widget.js' files without the digest (hash) in the file name.
+# This allows to access these assets with a consistent name without having the hash in the file name.
+module NonDigestAssets
+  def self.call(input)
+    filename = input[:filename]
+    # check if the file is a _widget.js file
+    # copy the file to the public/assets folder
+    if filename.end_with?('_widget.js')
+      # without the digest in the file name
+      path = File.join(Rails.public_path, 'assets', File.basename(filename))
+      FileUtils.cp(filename, path)
+    end
+    nil
+  end
+end
+
+Sprockets.register_postprocessor 'application/javascript', NonDigestAssets
