e5d430744fa3b734c66b6fed160d46b94a4c3d71
D058266 <hans-georg.winkler@sap.com>
2024-11-06 16:37:06 +0100

    all users can create projects

--- plugins/identity/app/controllers/identity/domains/create_wizard_controller.rb
+++ plugins/identity/app/controllers/identity/domains/create_wizard_controller.rb
@@ -8,6 +8,7 @@ module Identity
         @project = services.identity.new_project
         @project.cost_control = {}
         return unless @inquiry
+
         @project.attributes = @inquiry.payload
       end
 
@@ -21,12 +22,12 @@ module Identity
         @project = services.identity.new_project
         @project.attributes = project_params
 
-        @project.enabled = @project.enabled == "true"
+        @project.enabled = @project.enabled == 'true'
 
         @project.escape_attributes!
 
         if @project.save
-          audit_logger.info(current_user, "has created", @project)
+          audit_logger.info(current_user, 'has created', @project)
 
           flash.now[:notice] = "Project #{@project.name} successfully created."
           if @inquiry
@@ -41,14 +42,14 @@ module Identity
                 :approved,
                 "Project #{@project.name} approved and \
               created by #{current_user.full_name}",
-                current_user,
+                current_user
               )
             services.identity.grant_project_user_role_by_role_name(
               @project.id,
               inquiry.requester.uid,
-              "admin",
+              'admin'
             )
-            render "identity/domains/create_wizard/create", formats: :js
+            render 'identity/domains/create_wizard/create', formats: :js
           else
             # there is no requiry -> current user is the creator of this
             # project. give current user all needed roles
@@ -68,17 +69,18 @@ module Identity
 
       def load_and_authorize_inquiry
         return if params[:inquiry_id].blank?
+
         @inquiry = services.inquiry.get_inquiry(params[:inquiry_id])
 
         if @inquiry
           enforce_permissions(
-            "identity:project_create",
+            'identity:project_create',
             project: {
-              domain_id: @scoped_domain_id,
-            },
+              domain_id: @scoped_domain_id
+            }
           )
         else
-          render template: "/identity/domains/create_wizard/not_found"
+          render template: '/identity/domains/create_wizard/not_found'
         end
       end
 
@@ -86,10 +88,10 @@ module Identity
 
       def assign_needed_roles(project_id, user_id)
         %w[admin member network_admin resource_admin].each do |role_name|
-          services.identity.grant_project_user_role_by_role_name(
+          service_user.identity.grant_project_user_role_by_role_name(
             project_id,
             user_id,
-            role_name,
+            role_name
           )
         end
       end
