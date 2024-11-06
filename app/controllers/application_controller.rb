58c2274defe86e0d8e3242ad7d5a5c5d8f9c99fa
D058266 <hans-georg.winkler@sap.com>
2024-11-06 11:51:51 +0100

    fix rspec test, we do not need the before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,6 @@ class ApplicationController < ActionController::Base
 
   extend ErrorRenderer
 
-  before_action :load_domain_config
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
