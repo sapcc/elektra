e8ad1e771520b60b5b0c1eaec632991afeaed2f1
Andreas Pfau <andreas.pfau@sap.com>
2024-11-25 16:32:02 +0100

    chore(core): load limes-ui via npm (#1462)
    Co-authored-by: D058266 <hans-georg.winkler@sap.com>

---
+++ plugins/resources/app/javascript/widgets/usage/init.js
@@ -0,0 +1,20 @@
+// The widget comes from the limes-ui package, which is a separate package from the elektra package.
+// https://github.com/sapcc/LimesUI
+// limes-ui implements the juno app interface which has the mount method.
+// The mount method is used to render the widget in the DOM.
+import { mount } from "@sapcc/limes-ui/build/quotaPlugin"
+
+const currentScript = document.currentScript
+const wrapper = document.createElement("div")
+const dataset = currentScript.dataset
+wrapper.id = "limes-ui"
+currentScript.replaceWith(wrapper)
+
+let props
+try {
+  props = JSON.parse(dataset?.props)
+} catch (e) {
+  props = {}
+}
+
+mount(wrapper, { props })
