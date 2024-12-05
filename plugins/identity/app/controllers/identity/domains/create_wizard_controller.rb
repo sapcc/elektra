a39a3e56785231a93385e49e768e7f00816ff445
Hans-Georg Winkler <hans-georg.winkler@sap.com>
2024-12-05 10:23:51 +0100

    feat(identity): all users can create projects (#1457)
    Co-authored-by: Andreas Pfau <andreas.pfau@sap.com>

--- plugins/identity/app/controllers/identity/domains/create_wizard_controller.rb
+++ plugins/identity/app/controllers/identity/domains/create_wizard_controller.rb
@@ -7,7 +7,13 @@ module Identity
       def new
         @project = services.identity.new_project
         @project.cost_control = {}
+        # this is only set if the user is in the project context and creates a new sub project
+        if params[:parent_id] && params[:parent_name]
+          @project.parent_id = params[:parent_id]
+          @project.parent_name = params[:parent_name]
+        end
         return unless @inquiry
+
         @project.attributes = @inquiry.payload
       end
 
@@ -16,17 +22,16 @@ module Identity
           params.fetch(:project, {}).merge(domain_id: @scoped_domain_id)
         cost_params = project_params.delete(:cost_control)
 
-        # user is not allowed to create a project (maybe)
-        # so use admin identity for that!
         @project = services.identity.new_project
         @project.attributes = project_params
+        @project.parent_id = @scoped_project_id if @scoped_project_id
 
-        @project.enabled = @project.enabled == "true"
+        @project.enabled = @project.enabled == 'true'
 
         @project.escape_attributes!
 
         if @project.save
-          audit_logger.info(current_user, "has created", @project)
+          audit_logger.info(current_user, 'has created', @project)
 
           flash.now[:notice] = "Project #{@project.name} successfully created."
           if @inquiry
@@ -41,16 +46,16 @@ module Identity
                 :approved,
                 "Project #{@project.name} approved and \
               created by #{current_user.full_name}",
-                current_user,
+                current_user
               )
             services.identity.grant_project_user_role_by_role_name(
               @project.id,
               inquiry.requester.uid,
-              "admin",
+              'admin'
             )
-            render "identity/domains/create_wizard/create", formats: :js
+            render 'identity/domains/create_wizard/create', formats: :js
           else
-            # there is no requiry -> current user is the creator of this
+            # there is no inquiry -> current user is the creator of this
             # project. give current user all needed roles
             assign_needed_roles(@project.id, current_user.id)
 
@@ -68,17 +73,18 @@ module Identity
 
       def load_and_authorize_inquiry
         return if params[:inquiry_id].blank?
+
         @inquiry = services.inquiry.get_inquiry(params[:inquiry_id])
 
         if @inquiry
           enforce_permissions(
-            "identity:project_create",
+            'identity:project_create',
             project: {
-              domain_id: @scoped_domain_id,
-            },
+              domain_id: @scoped_domain_id
+            }
           )
         else
-          render template: "/identity/domains/create_wizard/not_found"
+          render template: '/identity/domains/create_wizard/not_found'
         end
       end
 
@@ -86,10 +92,12 @@ module Identity
 
       def assign_needed_roles(project_id, user_id)
         %w[admin member network_admin resource_admin].each do |role_name|
-          services.identity.grant_project_user_role_by_role_name(
+          # everyone can create projects but not everyone can add the admin roles to the project
+          # so we need to use the service user to assign the basic admin roles
+          service_user.identity.grant_project_user_role_by_role_name(
             project_id,
             user_id,
-            role_name,
+            role_name
           )
         end
       end
