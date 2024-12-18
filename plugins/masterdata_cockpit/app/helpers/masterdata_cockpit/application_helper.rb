fe2cd9cdba83aa8bbd493fe8b075b937fb5e6449
Hans-Georg Winkler <hans-georg.winkler@sap.com>
2024-12-18 14:46:14 +0100

    feat(masterdata-cockpit): add propagation information (#1483)
    Co-authored-by: Andreas Pfau <andreas.pfau@sap.com>

--- plugins/masterdata_cockpit/app/helpers/masterdata_cockpit/application_helper.rb
+++ plugins/masterdata_cockpit/app/helpers/masterdata_cockpit/application_helper.rb
@@ -1,4 +1,20 @@
 module MasterdataCockpit
   module ApplicationHelper
+    def render_propagation_status(propagation_type)
+      if propagation_type == 1
+        content = "Propagation-Type: New"
+        icon_name = "rocket"
+      else
+        content = "Propagation-Type: Always"
+        icon_name = "exchange"
+      end
+  
+      link_to(icon(icon_name), "#", class: "help-link", 
+      data: {
+        "content": content,
+        "popover-type": "help-hint",
+        "toggle": "popover"
+      })
+    end
   end
 end
