448c8c3b9acd18a0e5872a02db61de30c82d5f37
D058266 <hans-georg.winkler@sap.com>
2025-01-23 16:07:00 +0100

    fix(dns-service): add backward compatibility for newer designate version

--- plugins/dns_service/app/services/service_layer/dns_service_services/zone.rb
+++ plugins/dns_service/app/services/service_layer/dns_service_services/zone.rb
@@ -4,16 +4,6 @@ module ServiceLayer
   module DnsServiceServices
     # This module implements Openstack Designate Pool API
     module Zone
-
-      # this is only for qa-de-1 and qa-de-2 until the api changes are rolled out to all regions
-      def share_endpoint
-        endpoint = "zones/share"
-        if ENV["MONSOON_DASHBOARD_REGION"] == "qa-de-1" || ENV["MONSOON_DASHBOARD_REGION"] == "qa-de-2"
-          endpoint = "zones/shares"
-        end
-        endpoint
-      end
-
       def zone_map
         @zone_map ||= class_map_proc(DnsService::Zone)
       end
@@ -47,11 +37,17 @@ module ServiceLayer
           project_id = filter.delete(:project_id)
           header_options = { "x-auth-sudo-project-id": project_id }
         end
-        response =
-          elektron_dns.get(share_endpoint).map_to(
-            "body.shared_zones",
-            &shared_zone_map
-          )
+        # backward compatibility
+        begin
+          response = elektron_dns.get("zones/share").map_to("body.shared_zones", &shared_zone_map)
+        rescue StandardError => e
+          if e.code == 400
+            response = elektron_dns.get("zones/shares").map_to("body.shared_zones", &shared_zone_map)
+          else
+            raise e
+          end
+        end
+        response
       end
 
       def new_zone(attributes = {})
@@ -99,11 +95,29 @@ module ServiceLayer
       end
 
       def create_shared_zone(attributes = {})
-        elektron_dns.post(share_endpoint) { attributes }.body
+        # backward compatibility
+        begin
+          elektron_dns.post("zones/shares") { attributes }.body
+        rescue StandardError => e
+          if e.code == 400
+            elektron_dns.post("zones/shares") { attributes }.body
+          else
+            raise e
+          end
+        end
       end
 
       def delete_shared_zone(id)
-        elektron_dns.delete("#{share_endpoint}/#{id}")
+        # backward compatibility
+        begin
+          elektron_dns.delete("zones/share/#{id}")
+        rescue StandardError => e
+          if e.code == 400
+            elektron_dns.delete("zones/shares/#{id}")
+          else
+            raise e
+          end
+        end
       end
     end
   end
