96946eaedcf6f5f57e5a1d8866b20a5e094483df
Hans-Georg Winkler <hans-georg.winkler@sap.com>
2025-05-14 09:18:15 +0200

    chore(plugins): remove access-profile (#1587)

--- plugins/access_profile/config/routes.rb
+++
@@ -1,6 +0,0 @@
-AccessProfile::Engine.routes.draw do
-  root to: "application#access_profile_widget"
-  resources :tags, only: %i[index create destroy] do
-    collection { get "config" => "tags#profiles_config" }
-  end
-end
