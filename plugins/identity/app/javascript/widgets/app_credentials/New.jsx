2c02f9f7c3036c38513757c0ea6f230657a18606
hgw77 <hans-georg.winkler@sap.com>
2025-05-27 11:42:27 +0200

    fix(identity): better error handling for app credentials

--- plugins/identity/app/javascript/widgets/app_credentials/New.jsx
+++ plugins/identity/app/javascript/widgets/app_credentials/New.jsx
@@ -34,7 +34,8 @@ const NewAppCredentials = ({ userId, refreshList, setOverlay }) => {
         setOverlay(true)
       })
       .catch((error) => {
-        setError(error.data.error.error.message)
+        setError(error.data.error.error.message || "An error occurred while creating the application credential.")
+        console.error("Error creating application credential:", error)
       })
       .finally(() => {
         setIsLoading(false)
